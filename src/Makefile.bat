9185668cd1e41cfbea84f760f15bc764d9622776
Stefano Mazzocchi <stefano@apache.org>
1998-09-03 17:04:09 +0000

    This commit was generated by cvs2svn to compensate for changes in r6, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@322539 13f79535-47bb-0310-9956-ffa450edef68

--- src/Makefile.bat
+++ src/Makefile.bat
@@ -1,19 +1,26 @@
 @echo off
 
-set repository=c:\java\classes
-set compiler=jikes -O -d %repository%
+rem ########### Set your favorite java compiler and javadoc engine here ###############
+set javac=javac -O
+set javadoc=javadoc -author -version
+
+rem ########### Do not touch below here #################
 
 echo Compiling Apache JMeter...
-%compiler% .\org\apache\jmeter\timers\*.java .\org\apache\jmeter\visualisers\*.java .\org\apache\jmeter\*.java
+%javac% -d .\temp .\org\apache\jmeter\timers\*.java .\org\apache\jmeter\visualisers\*.java .\org\apache\jmeter\*.java
 
-xcopy /S %repository%\org\apache\jmeter\*.class ..\temp\org\apache\jmeter\*.class
-xcopy .\org\apache\jmeter\jmeter.properties ..\temp\org\apache\jmeter\.
-xcopy .\org\apache\jmeter\images\*.* ..\temp\org\apache\jmeter\images\*.*
+xcopy /Q .\org\apache\jmeter\jmeter.properties .\temp\org\apache\jmeter\.
+xcopy /Q .\org\apache\jmeter\images\*.* .\temp\org\apache\jmeter\images\*.*
 
 echo building jar archive...
-cd ..\temp
-jar cvf0 Apache-JMeter.jar .
+cd .\temp
+jar cf0 Apache-JMeter.jar .
 
-xcopy .\Apache-JMeter.jar ..\bin\*.jar
-deltree /Y ..\temp
-echo ...all done
+xcopy /Q .\Apache-JMeter.jar ..\..\bin\*.jar
+cd ..
+deltree /Y .\temp
+
+echo building javadoc...
+%javadoc% -d ..\docs\api org.apache.jmeter org.apache.jmeter.timers org.apache.jmeter.visualisers
+
+echo done.
