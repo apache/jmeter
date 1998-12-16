2da8384b3398c366ea9c3193152ec2c166c708a4
Stefano Mazzocchi <stefano@apache.org>
1998-12-16 23:44:27 +0000

    same updates
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@322549 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 1f507a97126fb7594ece40aa480bdbbe237ece2a

--- src/make.bat
+++ src/make.bat
@@ -1,3 +1,5 @@
+@echo off
+
 ::
 :: Win32 build script for ApacheJMeter.jar
 ::
@@ -5,8 +7,7 @@
 :: @version $Revision$ $Date$
 ::
 
-@echo off
-
+echo Creating ApacheJMeter.jar...
 echo  * Compiling Java source...
 md .\temp
 javac -O -d .\temp .\org\apache\jmeter\*.java .\org\apache\jmeter\timers\*.java .\org\apache\jmeter\visualisers\*.java
@@ -18,7 +19,7 @@ copy .\org\apache\jmeter\images\*.* .\temp\org\apache\jmeter\images\*.* >nul
 echo  * Copying property files...
 copy .\org\apache\jmeter\*.properties .\temp\org\apache\jmeter\*.properties >nul
 
-echo  * Creating ApacheJMeter.jar file...
+echo  * Creating ApacheJMeter.jar file in ..\bin ...
 cd .\temp
 jar cmf0 ..\MANIFEST ..\..\bin\ApacheJMeter.jar *.* >nul
 cd ..
