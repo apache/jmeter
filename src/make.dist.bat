82a0bcf3c7806438d7d6a5871b021526eb028ea2
Stefano Mazzocchi <stefano@apache.org>
1999-03-17 17:22:41 +0000

    JMeter 1.2
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@322556 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 1034154d1f2006e60228fdb90c41200158136184

--- src/make.dist.bat
+++ src/make.dist.bat
@@ -11,7 +11,7 @@
 if "%name%"=="" set name=Apache_JMeter
 
 :: Set package version
-if "%version%"=="" set version=1.1
+if "%version%"=="" set version=1.2
 
 :: Set temp directory name
 if "%dir%"=="" set dir="temp"
