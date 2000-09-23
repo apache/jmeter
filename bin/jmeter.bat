23e58bc37e4282a8892d7dc2ba5121155899f6e2
mstover <dev-null@apache.org>
2000-09-23 00:52:35 +0000

    Adding support for extra options in Windows startup
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@322597 13f79535-47bb-0310-9956-ffa450edef68

--- bin/jmeter.bat
+++ bin/jmeter.bat
@@ -1,4 +1,14 @@
-@echo off
+@echo on
 set PROP=jmeter.properties
-IF "%1" NEQ "" set PROP=%1
-java -cp %CLASSPATH%;ApacheJMeter.jar org.apache.jmeter.Driver %PROP%
+set HOST=
+set PORT=
+IF "%1" EQU "-f" set PROP=%2
+IF "%1" EQU "-h" set HOST=-Dhttp.proxyHost=%2
+IF "%1" EQU "-p" set PORT=-Dhttp.proxyPort=%2
+IF "%3" EQU "-f" set PROP=%4
+IF "%3" EQU "-h" set HOST=-Dhttp.proxyHost=%4
+IF "%3" EQU "-p" set PORT=-Dhttp.proxyPort=%4
+IF "%5" EQU "-f" set PROP=%6
+IF "%5" EQU "-h" set HOST=-Dhttp.proxyHost=%6
+IF "%5" EQU "-p" set PORT=-Dhttp.proxyPort=%6
+java -cp %CLASSPATH%;ApacheJMeter.jar %HOST% %PORT% org.apache.jmeter.Driver %PROP%
