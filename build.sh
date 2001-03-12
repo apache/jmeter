4284f9b6bb0a98d55512a09f94b991bae514dc50
Jon Scott Stevens <jon@apache.org>
2001-03-12 23:57:42 +0000

    removed old files added a build.sh script added project.xml rebuilt website documentation
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@322621 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 0ea66f2d5766f4b522fd8c35a4a3ce735671170f

---
+++ build.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+#--------------------------------------------
+# No need to edit anything past here
+#--------------------------------------------
+if test -z "${JAVA_HOME}" ; then
+    echo "ERROR: JAVA_HOME not found in your environment."
+    echo "Please, set the JAVA_HOME variable in your environment to match the"
+    echo "location of the Java Virtual Machine you want to use."
+    exit
+fi
+
+if test -f ${JAVA_HOME}/lib/tools.jar ; then
+    CLASSPATH=${CLASSPATH}:${JAVA_HOME}/lib/tools.jar
+fi
+
+# convert the existing path to unix
+if [ "$OSTYPE" = "cygwin32" ] || [ "$OSTYPE" = "cygwin" ] ; then
+   CLASSPATH=`cygpath --path --unix "$CLASSPATH"`
+fi
+
+for i in ./lib/*.jar
+do
+    CLASSPATH=${CLASSPATH}:$i
+done
+
+for i in ../jakarta-site2/lib/*.jar
+do
+    CLASSPATH=${CLASSPATH}:$i
+done
+
+# convert the unix path to windows
+if [ "$OSTYPE" = "cygwin32" ] || [ "$OSTYPE" = "cygwin" ] ; then
+   CLASSPATH=`cygpath --path --windows "$CLASSPATH"`
+fi
+
+BUILDFILE=build.xml
+
+${JAVA_HOME}/bin/java -classpath ${CLASSPATH} org.apache.tools.ant.Main \
+                      -buildfile ${BUILDFILE} "$@"
