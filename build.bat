30e1281fae4812f874b066698727d54bd2314051
Michael Stover <mstover1@apache.org>
2001-06-10 02:22:55 +0000

    Can save timers and listeners
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@322638 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 9106046eda163dd73c8361de38382a6e5384f8eb

---
+++ build.bat
@@ -0,0 +1,12 @@
+echo off
+
+set LOCALCLASSPATH=%JAVA_HOME%\lib\tools.jar
+
+for %%i in (".\lib\*.jar") do CALL lcp %%i
+echo %LOCALCLASSPATH%
+
+for %%i in (..\jakarta-site2\lib\*.jar) do set LOCALCLASSPATH=%LOCALCLASSPATH%;%%i
+
+SET BUILDFILE=build.xml
+echo %LOCALCLASSPATH%
+java -classpath %LOCALCLASSPATH% org.apache.tools.ant.Main -buildfile %BUILDFILE% %1
