fb19993688b64f5602fede8d8d4450d77c466a3a
Michael Stover <mstover1@apache.org>
2001-07-23 21:00:06 +0000

    Updating changes.html.  Changing keyboard shortcut key for "Start". PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@322647 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: e359a3511c7efc0168b1d5af550ff53bb279c17e

--- bin/jmeter.bat
+++ bin/jmeter.bat
@@ -3,13 +3,13 @@ set PROP=jmeter.properties
 set HOST=
 set PORT=
 IF "%1" EQU "-f" set PROP=%2
-IF "%1" EQU "-h" set HOST=-Dhttp.proxyHost=%2
-IF "%1" EQU "-p" set PORT=-Dhttp.proxyPort=%2
+IF "%1" EQU "-h" set HOST=-Dhttp.proxyHost=%2 -Dhttps.proxyHost=%2
+IF "%1" EQU "-p" set PORT=-Dhttp.proxyPort=%2 -Dhttps.proxyPort=%2
 IF "%3" EQU "-f" set PROP=%4
-IF "%3" EQU "-h" set HOST=-Dhttp.proxyHost=%4
-IF "%3" EQU "-p" set PORT=-Dhttp.proxyPort=%4
+IF "%3" EQU "-h" set HOST=-Dhttp.proxyHost=%4 -Dhttps.proxyHost=%4
+IF "%3" EQU "-p" set PORT=-Dhttp.proxyPort=%4 -Dhttps.proxyPort=%4
 IF "%5" EQU "-f" set PROP=%6
-IF "%5" EQU "-h" set HOST=-Dhttp.proxyHost=%6
-IF "%5" EQU "-p" set PORT=-Dhttp.proxyPort=%6
+IF "%5" EQU "-h" set HOST=-Dhttp.proxyHost=%6 -Dhttps.proxyHost=%6
+IF "%5" EQU "-p" set PORT=-Dhttp.proxyPort=%6 -Dhttps.proxyPort=%6
 java -cp %CLASSPATH%;../lib/xerces.jar;ApacheJMeter.jar;../lib/Tidy.jar;../lib/log4j-core.jar %HOST% %PORT% 
 org.apache.jmeter.NewDriver %PROP%
