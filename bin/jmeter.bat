af4c82716788bbe5364572fd6a54296583732f14
Michael Stover <mstover1@apache.org>
2001-07-23 22:26:15 +0000

    Refactoring Proxy server. Fixing jmeter.bat New HTML parsing tools
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@322648 13f79535-47bb-0310-9956-ffa450edef68

--- bin/jmeter.bat
+++ bin/jmeter.bat
@@ -11,5 +11,4 @@ IF "%3" EQU "-p" set PORT=-Dhttp.proxyPort=%4 -Dhttps.proxyPort=%4
 IF "%5" EQU "-f" set PROP=%6
 IF "%5" EQU "-h" set HOST=-Dhttp.proxyHost=%6 -Dhttps.proxyHost=%6
 IF "%5" EQU "-p" set PORT=-Dhttp.proxyPort=%6 -Dhttps.proxyPort=%6
-java -cp %CLASSPATH%;../lib/xerces.jar;ApacheJMeter.jar;../lib/Tidy.jar;../lib/log4j-core.jar %HOST% %PORT% 
-org.apache.jmeter.NewDriver %PROP%
+java -cp %CLASSPATH%;../lib/xerces.jar;ApacheJMeter.jar;../lib/Tidy.jar;../lib/log4j-core.jar %HOST% %PORT% org.apache.jmeter.NewDriver %PROP%
