7e0dab309be26580d70324fdabb85ecd40cdf5b5
Michael Stover <mstover1@apache.org>
2001-07-23 19:17:38 +0000

    Adding logj4 and JTidy to libs.  Adding new Web testing elements that download all images with web pages. PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@322646 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 93797fa165e16b237f46df6a0341c5c534ecf35e

--- bin/jmeter.bat
+++ bin/jmeter.bat
@@ -11,4 +11,5 @@ IF "%3" EQU "-p" set PORT=-Dhttp.proxyPort=%4
 IF "%5" EQU "-f" set PROP=%6
 IF "%5" EQU "-h" set HOST=-Dhttp.proxyHost=%6
 IF "%5" EQU "-p" set PORT=-Dhttp.proxyPort=%6
-java -cp %CLASSPATH%;../lib/xerces.jar;ApacheJMeter.jar %HOST% %PORT% org.apache.jmeter.NewDriver %PROP%
+java -cp %CLASSPATH%;../lib/xerces.jar;ApacheJMeter.jar;../lib/Tidy.jar;../lib/log4j-core.jar %HOST% %PORT% 
+org.apache.jmeter.NewDriver %PROP%
