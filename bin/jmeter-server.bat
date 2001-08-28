d4ea1b58165b0cecdbf7ec94aa712d1a2278f1b4
Michael Stover <mstover1@apache.org>
2001-08-28 21:28:34 +0000

    New docs, fixing bugs PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@322660 13f79535-47bb-0310-9956-ffa450edef68

--- bin/jmeter-server.bat
+++ bin/jmeter-server.bat
@@ -1,13 +1,20 @@
+@echo off
 set PROP=jmeter.properties
+set LOG4JCONFIG=log4j.configuration=log4j.conf
 set HOST=
 set PORT=
 IF "%1" EQU "-f" set PROP=%2
-IF "%1" EQU "-h" set HOST=-Dhttp.proxyHost=%2
-IF "%1" EQU "-p" set PORT=-Dhttp.proxyPort=%2
+IF "%1" EQU "-h" set HOST=-Dhttp.proxyHost=%2 -Dhttps.proxyHost=%2
+IF "%1" EQU "-p" set PORT=-Dhttp.proxyPort=%2 -Dhttps.proxyPort=%2
 IF "%3" EQU "-f" set PROP=%4
-IF "%3" EQU "-h" set HOST=-Dhttp.proxyHost=%4
-IF "%3" EQU "-p" set PORT=-Dhttp.proxyPort=%4
+IF "%3" EQU "-h" set HOST=-Dhttp.proxyHost=%4 -Dhttps.proxyHost=%4
+IF "%3" EQU "-p" set PORT=-Dhttp.proxyPort=%4 -Dhttps.proxyPort=%4
 IF "%5" EQU "-f" set PROP=%6
-IF "%5" EQU "-h" set HOST=-Dhttp.proxyHost=%6
-IF "%5" EQU "-p" set PORT=-Dhttp.proxyPort=%6
-c:\jbuilder4\jdk1.3\jre\bin\java -cp ../lib/xerces.jar;c:\development\compiled\jmeter %HOST% %PORT% org.apache.jmeter.engine.RemoteJMeterEngineImpl
+IF "%5" EQU "-h" set HOST=-Dhttp.proxyHost=%6 -Dhttps.proxyHost=%6
+IF "%5" EQU "-p" set PORT=-Dhttp.proxyPort=%6 -Dhttps.proxyPort=%6
+
+set LOCALCLASSPATH=%CLASSPATH%
+
+for %%i in ("..\lib\*.jar") do CALL ..\lcp %%i
+for %%i in ("..\ext\*.jar") do CALL ..\lcp %%i
+java -cp %LOCALCLASSPATH%;ApacheJMeter.jar -D%LOG4JCONFIG% %HOST% %PORT% org.apache.jmeter.engine.RemoteJMeterEngineImpl %PROP%
