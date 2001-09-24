041644299ba271873cfa2c8acf4bd28b839d22a9
Michael Stover <mstover1@apache.org>
2001-09-24 12:02:28 +0000

    Fixing bugs in windows, new assertion docs
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@322668 13f79535-47bb-0310-9956-ffa450edef68

--- bin/jmeter.bat
+++ bin/jmeter.bat
@@ -1,17 +1,17 @@
-@echo off
+@echo on
 set PROP=jmeter.properties
 set LOG4JCONFIG=log4j.configuration=log4j.conf
 set HOST=
 set PORT=
-IF "%1" EQU "-f" set PROP=%2
-IF "%1" EQU "-h" set HOST=-Dhttp.proxyHost=%2 -Dhttps.proxyHost=%2
-IF "%1" EQU "-p" set PORT=-Dhttp.proxyPort=%2 -Dhttps.proxyPort=%2
-IF "%3" EQU "-f" set PROP=%4
-IF "%3" EQU "-h" set HOST=-Dhttp.proxyHost=%4 -Dhttps.proxyHost=%4
-IF "%3" EQU "-p" set PORT=-Dhttp.proxyPort=%4 -Dhttps.proxyPort=%4
-IF "%5" EQU "-f" set PROP=%6
-IF "%5" EQU "-h" set HOST=-Dhttp.proxyHost=%6 -Dhttps.proxyHost=%6
-IF "%5" EQU "-p" set PORT=-Dhttp.proxyPort=%6 -Dhttps.proxyPort=%6
+IF "%1" == "-f" set PROP=%2
+IF "%1" == "-h" set HOST=-Dhttp.proxyHost=%2 -Dhttps.proxyHost=%2
+IF "%1" == "-p" set PORT=-Dhttp.proxyPort=%2 -Dhttps.proxyPort=%2
+IF "%3" == "-f" set PROP=%4
+IF "%3" == "-h" set HOST=-Dhttp.proxyHost=%4 -Dhttps.proxyHost=%4
+IF "%3" == "-p" set PORT=-Dhttp.proxyPort=%4 -Dhttps.proxyPort=%4
+IF "%5" == "-f" set PROP=%6
+IF "%5" == "-h" set HOST=-Dhttp.proxyHost=%6 -Dhttps.proxyHost=%6
+IF "%5" == "-p" set PORT=-Dhttp.proxyPort=%6 -Dhttps.proxyPort=%6
 
 set LOCALCLASSPATH=%CLASSPATH%
 
