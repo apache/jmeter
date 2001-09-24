1d257674cfbbbe447b73089c25bd8893e7a7c8ed
Michael Stover <mstover1@apache.org>
2001-09-24 13:17:01 +0000

    fixing mouse bug PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@322669 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 6bfb348ad70ed28f9b4ea89e910c9775db3a8a5a

--- build.sh
+++ build.sh
@@ -3,15 +3,15 @@
 #--------------------------------------------
 # No need to edit anything past here
 #--------------------------------------------
-if test -z "${JAVA_HOME}" ; then
+if test -z "$JAVA_HOME" ; then
     echo "ERROR: JAVA_HOME not found in your environment."
     echo "Please, set the JAVA_HOME variable in your environment to match the"
     echo "location of the Java Virtual Machine you want to use."
     exit
 fi
 
-if test -f ${JAVA_HOME}/lib/tools.jar ; then
-    CLASSPATH=${CLASSPATH}:${JAVA_HOME}/lib/tools.jar
+if test -f $JAVA_HOME/lib/tools.jar ; then
+    CLASSPATH=$CLASSPATH:${JAVA_HOME}/lib/tools.jar
 fi
 
 # convert the existing path to unix
@@ -21,12 +21,12 @@ fi
 
 for i in ./lib/*.jar
 do
-    CLASSPATH=${CLASSPATH}:$i
+    CLASSPATH=$CLASSPATH:$i
 done
 
 for i in ../jakarta-site2/lib/*.jar
 do
-    CLASSPATH=${CLASSPATH}:$i
+    CLASSPATH=$CLASSPATH:$i
 done
 
 # convert the unix path to windows
@@ -35,6 +35,7 @@ if [ "$OSTYPE" = "cygwin32" ] || [ "$OSTYPE" = "cygwin" ] ; then
 fi
 
 BUILDFILE=build.xml
+echo $JAVA_HOME/bin/java -classpath $CLASSPATH org.apache.tools.ant.Main -buildfile $BUILDFILE "$@"
 
-${JAVA_HOME}/bin/java -classpath ${CLASSPATH} org.apache.tools.ant.Main \
-                      -buildfile ${BUILDFILE} "$@"
+$JAVA_HOME/bin/java -classpath $CLASSPATH org.apache.tools.ant.Main \
+                      -buildfile $BUILDFILE "$@"
