7f170688950b99f8b10deb022343eda6901ae1c6
Michael Stover <mstover1@apache.org>
2002-06-05 03:29:06 +0000

    Reworked startup scripts improving compatability with JMeter 1.7 test scripts
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@322800 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: be42614f8d728637458df2b777a2c80794be8d2d

--- bin/jmeter-cl.bat
+++
@@ -1,1 +0,0 @@
-java -classpath %CLASSPATH%;ApacheJMeter.jar org.apache.jmeter.TextDriver -p "jmeter.properties" "%1"
--- bin/jmeter-server.bat
+++ bin/jmeter-server.bat
@@ -1,23 +1,2 @@
 @echo off
-
-set PROP=jmeter.properties
-set LOG4JCONFIG=log4j.configuration=log4j.conf
-set HOST=
-set PORT=
-
-IF "%1" == "-f" set PROP=%2
-IF "%1" == "-h" set HOST=-Dhttp.proxyHost=%2 -Dhttps.proxyHost=%2
-IF "%1" == "-p" set PORT=-Dhttp.proxyPort=%2 -Dhttps.proxyPort=%2
-IF "%3" == "-f" set PROP=%4
-IF "%3" == "-h" set HOST=-Dhttp.proxyHost=%4 -Dhttps.proxyHost=%4
-IF "%3" == "-p" set PORT=-Dhttp.proxyPort=%4 -Dhttps.proxyPort=%4
-IF "%5" == "-f" set PROP=%6
-IF "%5" == "-h" set HOST=-Dhttp.proxyHost=%6 -Dhttps.proxyHost=%6
-IF "%5" == "-p" set PORT=-Dhttp.proxyPort=%6 -Dhttps.proxyPort=%6
-
-set LOCALCLASSPATH=%CLASSPATH%
-
-for %%i in ("..\lib\*.jar") do CALL ..\lcp %%i
-for %%i in ("..\ext\*.jar") do CALL ..\lcp %%i
-
-java -cp %LOCALCLASSPATH%;ApacheJMeter.jar -D%LOG4JCONFIG% %HOST% %PORT% org.apache.jmeter.engine.RemoteJMeterEngineImpl %PROP%
+jmeter -s
