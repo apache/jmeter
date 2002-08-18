a2409154330b7e52a10f853ad4afd2b6c9eab819
Michael Stover <mstover1@apache.org>
2002-08-18 03:12:49 +0000

    New User Parameter Config Element (replacement and enhancement to the current HTTP User Parameter Modifier)
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@322855 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: a07d4917dabc9935ba6f5cdd9b333978efc65a05

---
+++ src/core/org/apache/jmeter/testelement/VariablesCollection.java
@@ -0,0 +1,27 @@
+package org.apache.jmeter.testelement;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.jmeter.threads.JMeterVariables;
+
+/**
+ * @author Administrator
+ *
+ * To change this generated comment edit the template variable "typecomment":
+ * Window>Preferences>Java>Templates.
+ */
+public class VariablesCollection {
+	private Map varMap = new HashMap();
+	
+	public void addJMeterVariables(JMeterVariables jmVars)
+	{
+		varMap.put(Thread.currentThread().getName(),jmVars);
+	}
+	
+	public JMeterVariables getVariables()
+	{
+		return (JMeterVariables)varMap.get(Thread.currentThread().getName());
+	}
+
+}
