9998c1e37896960f4caa5e75b72a3c93db498a5f
Michael Stover <mstover1@apache.org>
2002-08-24 00:45:25 +0000

    Fixing recording and replay of multipart requests where the user chose no file.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@322869 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 176e9cb65d9a28e6019501dfe599b6eda4ba37c3

--- src/protocol/http/org/apache/jmeter/protocol/http/sampler/PostWriter.java
+++ src/protocol/http/org/apache/jmeter/protocol/http/sampler/PostWriter.java
@@ -94,7 +94,7 @@ public class PostWriter
 		((HttpURLConnection)connection).setRequestMethod("POST");
 
 		// If filename was specified then send the post using multipart syntax
-		String filename = sampler.getFilename();
+		String filename = sampler.getFileField();
 		if ((filename != null) && (filename.trim().length() > 0))
 		{
 			connection.setRequestProperty("Content-type", "multipart/form-data; boundary=" + BOUNDARY);
