3cfb3b8ff3983864df7493e34f66fb7c72fd98e0
Michael Stover <mstover1@apache.org>
2002-08-28 13:47:15 +0000

    no message
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@322881 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 73df636953ef1b2bb4a3d9492aa3b8740605ce69

---
+++ src/components/org/apache/jmeter/control/gui/RecordController.java
@@ -0,0 +1,18 @@
+package org.apache.jmeter.control.gui;
+
+import org.apache.jmeter.util.JMeterUtils;
+
+/**
+ * @author Administrator
+ *
+ * To change this generated comment edit the template variable "typecomment":
+ * Window>Preferences>Java>Templates.
+ */
+public class RecordController extends LogicControllerGui
+{
+	
+	public String getStaticLabel()
+	{
+		return JMeterUtils.getResString("record_controller_title");
+	}
+}
