a2bfaea017aacb780c20220523323317cad87987
Michael Stover <mstover1@apache.org>
2002-09-07 16:54:34 +0000

    Making the help system compatible with JDK1.3
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@322898 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/core/org/apache/jmeter/swing/HtmlPane.java
@@ -0,0 +1,16 @@
+package org.apache.jmeter.swing;
+import javax.swing.JTextPane;
+/**
+ * @author Administrator
+ *
+ * To change this generated comment edit the template variable "typecomment":
+ * Window>Preferences>Java>Templates.
+ */
+public class HtmlPane extends JTextPane
+{
+	
+	public void scrollToReference(String reference)
+	{
+		super.scrollToReference(reference);
+	}
+}
