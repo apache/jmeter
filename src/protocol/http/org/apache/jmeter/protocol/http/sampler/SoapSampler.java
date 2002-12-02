62375bacb7abc31ea228df2a7080cf3869f542d3
jboutcher <dev-null@apache.org>
2002-12-02 03:07:43 +0000

    fix for bug 14145 - thank you jordi
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@322914 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 9e6a4d4bb7028c7d39caaac90338ec1bac7fd6d7

--- src/protocol/http/org/apache/jmeter/protocol/http/sampler/SoapSampler.java
+++ src/protocol/http/org/apache/jmeter/protocol/http/sampler/SoapSampler.java
@@ -7,26 +7,43 @@ import java.net.URLConnection;
 
 
 /**
- * @author Administrator
+ * Sampler to handle SOAP Requests
  *
- * To change this generated comment edit the template variable "typecomment":
- * Window>Preferences>Java>Templates.
+ *
+ * @author Jordi Salvat i Alabart
+ * @version $Id$
  */
-public class SoapSampler extends HTTPSampler 
+public class SoapSampler extends HTTPSampler
 {
 	public static final String XML_DATA = "HTTPSamper.xml_data";
-	
+
 	public void setXmlData(String data)
 	{
 		setProperty(XML_DATA,data);
 	}
-	
+
 	public String getXmlData()
 	{
 		return getPropertyAsString(XML_DATA);
 	}
 
-		/****************************************
+	/****************************************
+	 * Set the HTTP request headers in preparation to open the connection
+	 * and sending the POST data:
+	 *
+	 *@param connection       <code>URLConnection</code> to set headers on
+	 *@exception IOException  if an I/O exception occurs
+	 ***************************************/
+	public void setHeaders(URLConnection connection)
+	  		throws IOException
+	{
+		((HttpURLConnection)connection).setRequestMethod("POST");
+		connection.setRequestProperty("Content-length", "" + getXmlData().length());
+		connection.setRequestProperty("Content-type", "text/xml");
+		connection.setDoOutput(true);
+	}
+
+	/****************************************
 	 * Send POST data from <code>Entry</code> to the open connection.
 	 *
 	 *@param connection       <code>URLConnection</code> of where POST data should
@@ -37,12 +54,9 @@ public class SoapSampler extends HTTPSampler
 	public void sendPostData(URLConnection connection)
 			 throws IOException
 	{
-		((HttpURLConnection)connection).setRequestMethod("POST");
-		connection.setRequestProperty("Content-length", "" + getXmlData().length());
-		connection.setRequestProperty("Content-type", "text/xml");
-		connection.setDoOutput(true);
 		PrintWriter out = new PrintWriter(connection.getOutputStream());
 		out.print(getXmlData());
 		out.close();
 	}
 }
+
