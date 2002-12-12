d95947f1e0a2685abebc21efbebdb80f379a4a4b
Michael Stover <mstover1@apache.org>
2002-12-12 22:00:16 +0000

    Fixing bug #15087
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@322916 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 5bda06ab9f31b32790ef7d0d2d12bdba2a4a64c9

--- src/protocol/http/org/apache/jmeter/protocol/http/sampler/PostWriter.java
+++ src/protocol/http/org/apache/jmeter/protocol/http/sampler/PostWriter.java
@@ -92,7 +92,7 @@ public class PostWriter
 			 throws IOException
 	{
 		// If filename was specified then send the post using multipart syntax
-		String filename = sampler.getFileField();
+		String filename = sampler.getFilename();
 		if ((filename != null) && (filename.trim().length() > 0))
 		{
 			OutputStream out = connection.getOutputStream();//new FileOutputStream("c:\\data\\experiment.txt");//new ByteArrayOutputStream();//
