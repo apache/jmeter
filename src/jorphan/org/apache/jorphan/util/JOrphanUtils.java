58f2b39df40d4f35c05aaac1d0dfe7b070bcd2c0
Jordi Salvat i Alabart <jsalvata@apache.org>
2002-12-27 15:51:18 +0000

    Now compiles and tests correctly under JDK 1.3, too.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@322930 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 068367c7d37f22bc603314fe13b6a8e0279a2887

---
+++ src/jorphan/org/apache/jorphan/util/JOrphanUtils.java
@@ -0,0 +1,116 @@
+/*
+ * ====================================================================
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2002 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in
+ * the documentation and/or other materials provided with the
+ * distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ * if any, must include the following acknowledgment:
+ * "This product includes software developed by the
+ * Apache Software Foundation (http://www.apache.org/)."
+ * Alternately, this acknowledgment may appear in the software itself,
+ * if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "Apache" and "Apache Software Foundation" and
+ * "Apache JMeter" must not be used to endorse or promote products
+ * derived from this software without prior written permission. For
+ * written permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ * "Apache JMeter", nor may "Apache" appear in their name, without
+ * prior written permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
+package org.apache.jorphan.util;
+
+import java.util.StringTokenizer;
+import java.util.Vector;
+
+/**
+ * This class contains frequently-used static utility methods.
+ *
+ * @author     <a href="mailto://jsalvata@atg.com">Jordi Salvat i Alabart</a>
+ * @created    27th December 2002
+ * @version    $Revision$
+ */
+public class JOrphanUtils
+{
+    /**
+     * This is equivalent to the String.split method in JDK 1.4. It is
+     * here to enable us to support earlier JDKs.
+     *
+     * <P>This piece of code used to be part of JMeterUtils, but was moved
+     * here because some JOrphan classes use it too.
+     *
+     * @param  splittee   String to be split
+     * @param  splitChar  Character to split the string on
+     * @return            Array of all the tokens.
+     */
+    public static String[] split(String splittee, String splitChar)
+    {
+	if (splittee == null || splitChar == null)
+	{
+	    return new String[0];
+	}
+	StringTokenizer tokens;
+	String temp;
+	int spot;
+	while ((spot = splittee.indexOf(splitChar + splitChar)) != -1)
+	{
+	    splittee = splittee.substring(0, spot + splitChar.length())
+		       + splittee.substring(spot + 2 * splitChar.length(),
+			       splittee.length());
+	}
+	Vector returns = new Vector();
+	int start = 0;
+	int length = splittee.length();
+	spot = 0;
+	while (start<length && (spot = splittee.indexOf(splitChar, start)) > -1)
+	{
+	    if (spot > 0)
+	    {
+		returns.addElement(splittee.substring(start, spot));
+	    }
+	    start = spot + splitChar.length();
+	}
+	if (start < length)
+	{
+	    returns.add(splittee.substring(start));
+	}
+	String[] values = new String[returns.size()];
+	returns.copyInto(values);
+	return values;
+    }
+    // End Method split(String, String)
+}
