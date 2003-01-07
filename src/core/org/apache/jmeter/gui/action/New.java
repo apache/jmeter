48d04fab42006267ae4faea80ffad25c2ea550b1
Jordi Salvat i Alabart <jsalvata@apache.org>
2003-01-07 01:32:00 +0000

    Clear test plan file name on "File->New". Fixes bug#15514.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@322960 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 2b0322a65b18ef6b4c3a6070ced50b523be23b5f

--- src/core/org/apache/jmeter/gui/action/New.java
+++ src/core/org/apache/jmeter/gui/action/New.java
@@ -118,6 +118,11 @@ public class New implements Command
 		}
 		guiPackage.getTreeModel().clearTestPlan();
 		guiPackage.getTreeListener().getJTree().setSelectionRow(1);
+
+		// Clear the name of the test plan file
+		Save save = (Save) ActionRouter.getInstance().getAction("save", "org.apache.jmeter.gui.action.Save");
+		save.setTestPlanFile(null);
+		
 		ActionRouter.getInstance().actionPerformed(new ActionEvent(
 				e.getSource(),e.getID(),CheckDirty.ADD_ALL));
 	}
