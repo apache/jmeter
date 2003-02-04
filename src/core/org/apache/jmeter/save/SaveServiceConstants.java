1681be677e90cf3d0ef61c9ab286aca9068cdcd3
kcassell <dev-null@apache.org>
2003-02-04 21:02:13 +0000

    Reformatted to Turbine standards
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@323070 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 9b20d45e4602bc76f63b5d0c924c6f6ca11e1215

--- src/core/org/apache/jmeter/save/SaveServiceConstants.java
+++ src/core/org/apache/jmeter/save/SaveServiceConstants.java
@@ -54,6 +54,7 @@
  */
 package org.apache.jmeter.save;
 
+
 /************************************************************
  *  This interface defines a number of constants used in the properties file
  *  that is used to indicate which portions of the results will be
@@ -68,111 +69,111 @@ package org.apache.jmeter.save;
 
 public interface SaveServiceConstants
 {
-        //---------------------------------------------------------------------
-        // PROPERTY FILE CONSTANTS
-        //---------------------------------------------------------------------
+    // ---------------------------------------------------------------------
+    // PROPERTY FILE CONSTANTS
+    // ---------------------------------------------------------------------
 
-        /** The file containing the properties governing the information to
-            be saved.  **/
-        public static final String PROPS_FILE =
-        "jmeter.properties";
+    /** The file containing the properties governing the information to
+     be saved.  **/
+    public static final String PROPS_FILE =
+            "jmeter.properties";
 
-        /** Indicates that the results file should be in XML format.  **/
-        public static final String XML = "xml";
+    /** Indicates that the results file should be in XML format.  **/
+    public static final String XML = "xml";
 
-        /** Indicates that the results file should be in CSV format.  **/
-        public static final String CSV = "csv";
+    /** Indicates that the results file should be in CSV format.  **/
+    public static final String CSV = "csv";
 
-        /** Indicates that the results should be stored in a database.  **/
-        public static final String DATABASE = "db";
+    /** Indicates that the results should be stored in a database.  **/
+    public static final String DATABASE = "db";
 
-        /** A properties file indicator for true.  **/
-        public static final String TRUE = "true";
+    /** A properties file indicator for true.  **/
+    public static final String TRUE = "true";
 
-        /** A properties file indicator for false.  **/
-        public static final String FALSE = "false";
+    /** A properties file indicator for false.  **/
+    public static final String FALSE = "false";
 
-        /** A properties file indicator for milliseconds.  **/
-        public static final String MILLISECONDS = "ms";
+    /** A properties file indicator for milliseconds.  **/
+    public static final String MILLISECONDS = "ms";
 
-        /** A properties file indicator for none.  **/
-        public static final String NONE = "none";
+    /** A properties file indicator for none.  **/
+    public static final String NONE = "none";
 
-        /** A properties file indicator for the first of a series.  **/
-        public static final String FIRST = "first";
+    /** A properties file indicator for the first of a series.  **/
+    public static final String FIRST = "first";
 
-        /** A properties file indicator for all of a series.  **/
-        public static final String ALL = "all";
+    /** A properties file indicator for all of a series.  **/
+    public static final String ALL = "all";
 
-        /** The name of the property indicating which assertion results
-            should be saved.  **/
-        public static final String ASSERTION_RESULTS_PROP
-        = "jmeter.save.saveservice.assertion_results";
+    /** The name of the property indicating which assertion results
+     should be saved.  **/
+    public static final String ASSERTION_RESULTS_PROP
+            = "jmeter.save.saveservice.assertion_results";
 
-        /** The name of the property indicating whether the data type
-            should be saved.  **/
-        public static final String SAVE_DATA_TYPE_PROP
-        = "jmeter.save.saveservice.data_type";
+    /** The name of the property indicating whether the data type
+     should be saved.  **/
+    public static final String SAVE_DATA_TYPE_PROP
+            = "jmeter.save.saveservice.data_type";
 
-        /** The name of the property indicating whether the label
-            should be saved.  **/
-        public static final String SAVE_LABEL_PROP
-        = "jmeter.save.saveservice.label";
+    /** The name of the property indicating whether the label
+     should be saved.  **/
+    public static final String SAVE_LABEL_PROP
+            = "jmeter.save.saveservice.label";
 
-        /** The name of the property indicating whether the response code
-            should be saved.  **/
-        public static final String SAVE_RESPONSE_CODE_PROP
-        = "jmeter.save.saveservice.response_code";
+    /** The name of the property indicating whether the response code
+     should be saved.  **/
+    public static final String SAVE_RESPONSE_CODE_PROP
+            = "jmeter.save.saveservice.response_code";
 
-        /** The name of the property indicating whether the response data
-            should be saved.  **/
-        public static final String SAVE_RESPONSE_DATA_PROP
-        = "jmeter.save.saveservice.response_data";
+    /** The name of the property indicating whether the response data
+     should be saved.  **/
+    public static final String SAVE_RESPONSE_DATA_PROP
+            = "jmeter.save.saveservice.response_data";
 
-        /** The name of the property indicating whether the response message
-            should be saved.  **/
-        public static final String SAVE_RESPONSE_MESSAGE_PROP
-        = "jmeter.save.saveservice.response_message";
+    /** The name of the property indicating whether the response message
+     should be saved.  **/
+    public static final String SAVE_RESPONSE_MESSAGE_PROP
+            = "jmeter.save.saveservice.response_message";
 
-        /** The name of the property indicating whether the success indicator
-            should be saved.  **/
-        public static final String SAVE_SUCCESSFUL_PROP
-        = "jmeter.save.saveservice.successful";
+    /** The name of the property indicating whether the success indicator
+     should be saved.  **/
+    public static final String SAVE_SUCCESSFUL_PROP
+            = "jmeter.save.saveservice.successful";
 
-        /** The name of the property indicating whether the thread name
-            should be saved.  **/
-        public static final String SAVE_THREAD_NAME_PROP
-        = "jmeter.save.saveservice.thread_name";
+    /** The name of the property indicating whether the thread name
+     should be saved.  **/
+    public static final String SAVE_THREAD_NAME_PROP
+            = "jmeter.save.saveservice.thread_name";
 
-        /** The name of the property indicating whether the time
-            should be saved.  **/
-        public static final String SAVE_TIME_PROP
-        = "jmeter.save.saveservice.time";
+    /** The name of the property indicating whether the time
+     should be saved.  **/
+    public static final String SAVE_TIME_PROP
+            = "jmeter.save.saveservice.time";
 
-        /** The name of the property indicating whether the time stamp
-            should be saved.  **/
-        public static final String TIME_STAMP_FORMAT_PROP
-        = "jmeter.save.saveservice.timestamp_format";
+    /** The name of the property indicating whether the time stamp
+     should be saved.  **/
+    public static final String TIME_STAMP_FORMAT_PROP
+            = "jmeter.save.saveservice.timestamp_format";
 
-        //---------------------------------------------------------------------
-        // XML RESULT FILE CONSTANTS
-        //---------------------------------------------------------------------
+    // ---------------------------------------------------------------------
+    // XML RESULT FILE CONSTANTS
+    // ---------------------------------------------------------------------
 
-        public final static String PRESERVE = "preserve";
-        public final static String XML_SPACE = "xml:space";
-        public static final String ASSERTION_RESULT_TAG_NAME = "assertionResult";
-        public static final String BINARY = "binary";
-        public static final String DATA_TYPE = "dataType";
-        public static final String ERROR = "error";
-        public static final String FAILURE = "failure";
-        public static final String FAILURE_MESSAGE = "failureMessage";
-        public static final String LABEL = "label";
-        public static final String RESPONSE_CODE = "responseCode";
-        public static final String RESPONSE_MESSAGE = "responseMessage";
-        public static final String SAMPLE_RESULT_TAG_NAME = "sampleResult";
-        public static final String SUCCESSFUL = "success";
-        public static final String THREAD_NAME = "threadName";
-        public static final String TIME = "time";
-        public static final String TIME_STAMP = "timeStamp";
+    public final static String PRESERVE = "preserve";
+    public final static String XML_SPACE = "xml:space";
+    public static final String ASSERTION_RESULT_TAG_NAME = "assertionResult";
+    public static final String BINARY = "binary";
+    public static final String DATA_TYPE = "dataType";
+    public static final String ERROR = "error";
+    public static final String FAILURE = "failure";
+    public static final String FAILURE_MESSAGE = "failureMessage";
+    public static final String LABEL = "label";
+    public static final String RESPONSE_CODE = "responseCode";
+    public static final String RESPONSE_MESSAGE = "responseMessage";
+    public static final String SAMPLE_RESULT_TAG_NAME = "sampleResult";
+    public static final String SUCCESSFUL = "success";
+    public static final String THREAD_NAME = "threadName";
+    public static final String TIME = "time";
+    public static final String TIME_STAMP = "timeStamp";
 
 }
