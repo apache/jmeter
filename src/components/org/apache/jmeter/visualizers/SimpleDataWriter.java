cdbad9b6690fa222c73a34699636c924ebfe4c50
kcassell <dev-null@apache.org>
2003-02-13 21:35:29 +0000

    Initial revision of listener that can record results to a file but not to the UI.  It is meant to provide an efficient means of recording data by eliminating GUI overhead.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@323084 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/components/org/apache/jmeter/visualizers/SimpleDataWriter.java
@@ -0,0 +1,163 @@
+/*
+ * ====================================================================
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2003 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in
+ * the documentation and/or other materials provided with the
+ * distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ * if any, must include the following acknowledgment:
+ * "This product includes software developed by the
+ * Apache Software Foundation (http://www.apache.org/)."
+ * Alternately, this acknowledgment may appear in the software itself,
+ * if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "Apache" and "Apache Software Foundation" and
+ * "Apache JMeter" must not be used to endorse or promote products
+ * derived from this software without prior written permission. For
+ * written permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ * "Apache JMeter", nor may "Apache" appear in their name, without
+ * prior written permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
+package org.apache.jmeter.visualizers;
+
+
+import java.awt.BorderLayout;
+import java.awt.Font;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
+
+import org.apache.jmeter.samplers.Clearable;
+import org.apache.jmeter.samplers.SampleResult;
+import org.apache.jmeter.util.JMeterUtils;
+import org.apache.jmeter.visualizers.gui.AbstractVisualizer;
+import org.apache.jorphan.gui.layout.VerticalLayout;
+
+
+/************************************************************
+ *  This listener can record results to a file
+ *  but not to the UI.  It is meant to provide an efficient means of
+ *  recording data by eliminating GUI overhead.
+ *
+ *@author     <a href="mailto:kcassell&#X0040;apache.org">Keith Cassell</a>
+ *@created    $Date$
+ *@version    $Revision$ $Date$
+ ***********************************************************/
+
+public class SimpleDataWriter
+    extends AbstractVisualizer
+    implements Clearable
+{
+    /****************************************
+     * Create the SimpleDataWriter.
+     ***************************************/
+
+    public SimpleDataWriter()
+    {
+        init();
+        setName(getStaticLabel());
+    }
+
+
+    /****************************************
+     * Reurns the name of this component to be used by the GUI.
+     *
+     *@return the name of this component
+     ***************************************/
+    public String getStaticLabel()
+    {
+        //return "Simple Data Writer";
+        return JMeterUtils.getResString("simple_data_writer_title");
+    }
+
+
+    /**
+        Initialize the component in the UI
+    **/
+
+    private void init()
+    {
+        this.setLayout(new BorderLayout());
+
+        // MAIN PANEL
+        JPanel mainPanel = new JPanel();
+        Border margin = new EmptyBorder(10, 10, 5, 10);
+
+        mainPanel.setBorder(margin);
+        mainPanel.setLayout(new VerticalLayout(5, VerticalLayout.LEFT));
+
+        // TITLE
+        //JLabel panelTitleLabel = new JLabel("Simple Data Writer");
+        JLabel panelTitleLabel =
+            new JLabel(JMeterUtils.getResString("simple_data_writer_title"));
+        Font curFont = panelTitleLabel.getFont();
+        int curFontSize = curFont.getSize();
+
+        curFontSize += 4;
+        panelTitleLabel.setFont(new Font(curFont.getFontName(),
+                                         curFont.getStyle(),
+                                         curFontSize));
+        mainPanel.add(panelTitleLabel);
+
+        // NAME
+        mainPanel.add(getNamePanel());
+        mainPanel.add(getFilePanel());
+
+        this.add(mainPanel, BorderLayout.NORTH);
+    }
+
+
+    /**
+        Does nothing, but required by interface.
+    **/
+
+    public void clear()
+    {
+    }
+
+
+    /**
+        Does nothing, but required by interface.
+        @param sample ignored
+    **/
+
+    public void add(SampleResult sample)
+    {
+    }
+}
