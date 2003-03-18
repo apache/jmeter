e7001ce8fc0eda2a030bfa93150f9bb524d93594
Michael Stover <mstover1@apache.org>
2003-03-18 21:36:03 +0000

    PreProcessor interface is now fully wired in
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@323118 13f79535-47bb-0310-9956-ffa450edef68

--- src/core/org/apache/jmeter/processor/PostProcessor.java
+++ src/core/org/apache/jmeter/processor/PostProcessor.java
@@ -1,17 +1,15 @@
 package org.apache.jmeter.processor;
 
-import org.apache.jmeter.samplers.SampleResult;
-import org.apache.jmeter.testelement.ThreadListener;
 
 /**
  * The PostProcessor is activated after a sample result has been generated.
  */
-public interface PostProcessor extends ThreadListener
+public interface PostProcessor
 {
    /**
     * Provides the PostProcessor with a SampleResult object from which to extract
     * values for use in future Queries.
     */
-   public void process(SampleResult result);
+   public void process();
 
 }
