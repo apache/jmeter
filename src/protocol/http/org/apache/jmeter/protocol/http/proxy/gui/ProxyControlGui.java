2b5079d5b1cfdad1462356ae8d5b21c679d1a5aa
Jeremy Arnold <jeremy_a@apache.org>
2003-04-21 14:51:06 +0000

    Fix for bug 19191 -- Add to Proxy Server exclude list actually adds to include list.  Bug introduced by fix to 19129. PR: 19191 CVS: ---------------------------------------------------------------------- CVS: PR: CVS:   If this change addresses a PR in the problem report tracking CVS:   database, then enter the PR number(s) here. CVS: Obtained from: CVS:   If this change has been taken from another system, such as NCSA, CVS:   then name the system in this line, otherwise delete it. CVS: Submitted by: CVS:   If this code has been contributed to Apache by someone else; i.e., CVS:   they sent us a patch or a new module, then include their name/email CVS:   address here. If this is your work then delete this line. CVS: Reviewed by: CVS:   If we are doing pre-commit code reviews and someone else has CVS:   reviewed your changes, include their name(s) here. CVS:   If you have not had it reviewed then delete this line.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@323179 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 90dfce668c01921de48c63e41f8367c2f2cdfa90

--- src/protocol/http/org/apache/jmeter/protocol/http/proxy/gui/ProxyControlGui.java
+++ src/protocol/http/org/apache/jmeter/protocol/http/proxy/gui/ProxyControlGui.java
@@ -467,7 +467,7 @@ public class ProxyControlGui extends AbstractJMeterGuiComponent implements JMete
         JPanel buttonPanel = new JPanel();
 
         JButton addButton = new JButton(JMeterUtils.getResString("add"));
-        addButton.setActionCommand(ADD_INCLUDE);
+        addButton.setActionCommand(addCommand);
         addButton.addActionListener(this);
         buttonPanel.add(addButton);
 
