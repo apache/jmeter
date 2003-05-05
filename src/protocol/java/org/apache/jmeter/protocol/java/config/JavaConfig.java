17118a68c0540fc737fe751426fa2c52995e58dc
Jeremy Arnold <jeremy_a@apache.org>
2003-05-05 22:07:26 +0000

    Javadoc and formatting updates. Removed unused private field.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@323255 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: f8798151c6368c7f1d59646492e1f107a4979643

--- src/protocol/java/org/apache/jmeter/protocol/java/config/JavaConfig.java
+++ src/protocol/java/org/apache/jmeter/protocol/java/config/JavaConfig.java
@@ -2,7 +2,7 @@
  * ====================================================================
  * The Apache Software License, Version 1.1
  *
- * Copyright (c) 2002 The Apache Software Foundation.  All rights
+ * Copyright (c) 2002,2003 The Apache Software Foundation.  All rights
  * reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -59,85 +59,102 @@ import java.io.Serializable;
 import org.apache.jmeter.config.Arguments;
 import org.apache.jmeter.config.ConfigTestElement;
 import org.apache.jmeter.protocol.java.sampler.JavaSampler;
-import org.apache.jmeter.protocol.java.sampler.JavaSamplerClient;
 import org.apache.jmeter.testelement.property.TestElementProperty;
 import org.apache.log.Hierarchy;
 import org.apache.log.Logger;
 
-
 /**
- *
  * The <code>JavaConfig</code> class contains the configuration data
- * necessary for the Java protocol.
+ * necessary for the Java protocol.  This data is used to configure a
+ * {@link org.apache.jmeter.protocol.java.sampler.JavaSamplerClient}
+ * instance to perform performance test samples.
  *
- *@author     Brad Kiewel
- *@created    $Date$
- *@version    $Revision$
+ * @author     Brad Kiewel
+ * @created    $Date$
+ * @version    $Revision$
  */
+public class JavaConfig extends ConfigTestElement implements Serializable {
+    /**
+     * Logging
+     */
+    transient private static Logger log =
+        Hierarchy.getDefaultHierarchy().getLoggerFor("jmeter.protocol.java");
 
-public class JavaConfig extends ConfigTestElement implements Serializable
-{
+    /**
+     *  Constructor for the JavaConfig object
+     */
+    public JavaConfig() {
+        setArguments(new Arguments());
+    }
 
-	/** Logging  */
-	transient private static Logger log = Hierarchy.getDefaultHierarchy().getLoggerFor("jmeter.protocol.java");
-	
-	private transient JavaSamplerClient javaClient = null;
+    /**
+     * Sets the class name attribute of the JavaConfig object.  This is the
+     * class name of the
+     * {@link org.apache.jmeter.protocol.java.sampler.JavaSamplerClient}
+     * implementation which will be used to execute the test.
+     *
+     * @param classname  the new classname value
+     */
+    public void setClassname(String classname) {
+        setProperty(JavaSampler.CLASSNAME, classname);
+    }
 
-	/**
-	 *  Constructor for the JavaConfig object
-	 */
-	public JavaConfig()
-	{
-		setArguments(new Arguments());
-	}
+    /**
+     * Gets the class name attribute of the JavaConfig object.  This is the
+     * class name of the
+     * {@link org.apache.jmeter.protocol.java.sampler.JavaSamplerClient}
+     * implementation which will be used to execute the test.
+     *
+     * @return           the classname value
+     */
+    public String getClassname() {
+        return getPropertyAsString(JavaSampler.CLASSNAME);
+    }
 
-	/**
-	 *  Sets the Classname attribute of the JavaConfig object
-	 *
-	 *@param  classname  The new Classname value
-	 */
-	public void setClassname(String classname)
-	{
-		setProperty(JavaSampler.CLASSNAME, classname);
-	}
+    /**
+     * Adds an argument to the list of arguments for this JavaConfig object.
+     * The {@link org.apache.jmeter.protocol.java.sampler.JavaSamplerClient}
+     * implementation can access these arguments through the
+     * {@link org.apache.jmeter.protocol.java.sampler.JavaSamplerContext}.
+     * 
+     * @param name       the name of the argument to be added
+     * @param value      the value of the argument to be added
+     */
+    public void addArgument(String name, String value) {
+        Arguments args = this.getArguments();
+        args.addArgument(name, value);
+    }
 
+    /**
+     * Removes all of the arguments associated with this JavaConfig object.
+     */
+    public void removeArguments() {
+        setProperty(
+            new TestElementProperty(JavaSampler.ARGUMENTS, new Arguments()));
+    }
 
+    /**
+     * Set all of the arguments for this JavaConfig object.  This will replace
+     * any previously added arguments.  The
+     * {@link org.apache.jmeter.protocol.java.sampler.JavaSamplerClient}
+     * implementation can access these arguments through the
+     * {@link org.apache.jmeter.protocol.java.sampler.JavaSamplerContext}.
+     * 
+     * @param args       the new arguments
+     */
+    public void setArguments(Arguments args) {
+        setProperty(new TestElementProperty(JavaSampler.ARGUMENTS, args));
+    }
 
-	/**
-	 *  Gets the Classname attribute of the JavaConfig object
-	 *
-	 *@return    The Classname value
-	 */
-	public String getClassname()
-	{
-		return getPropertyAsString(JavaSampler.CLASSNAME);
-	}
-
-	public void addArgument(String name,String value)
-	{
-		Arguments args = this.getArguments();
-		args.addArgument(name,value);
-	}
-
-	public void removeArguments()
-	{
-		this.setProperty(new TestElementProperty(JavaSampler.ARGUMENTS,new Arguments()));
-	}
-	
-	public void setArguments(Arguments args)
-	{
-		setProperty(new TestElementProperty(JavaSampler.ARGUMENTS,args));
-	}
-
-	/**
-	 *  Gets the Arguments attribute of the JavaConfig object
-	 *
-	 *@return    The Arguments value
-	 */
-	public Arguments getArguments()
-	{
-		return (Arguments) getProperty(JavaSampler.ARGUMENTS).getObjectValue();
-	}
+    /**
+     * Gets the arguments for this JavaConfig object.  The
+     * {@link org.apache.jmeter.protocol.java.sampler.JavaSamplerClient}
+     * implementation can access these arguments through the
+     * {@link org.apache.jmeter.protocol.java.sampler.JavaSamplerContext}.
+     *
+     * @return           the arguments
+     */
+    public Arguments getArguments() {
+        return (Arguments) getProperty(JavaSampler.ARGUMENTS).getObjectValue();
+    }
 }
-
-
