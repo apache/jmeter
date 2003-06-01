bb0f39405d88051efa74e55b3c73020562238a8c
Jeremy Arnold <jeremy_a@apache.org>
2003-06-01 16:55:17 +0000

    Reformatted to Turbine (JMeter) conventions.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@323298 13f79535-47bb-0310-9956-ffa450edef68

--- src/protocol/http/org/apache/jmeter/protocol/http/proxy/Proxy.java
+++ src/protocol/http/org/apache/jmeter/protocol/http/proxy/Proxy.java
@@ -2,7 +2,7 @@
  * ====================================================================
  * The Apache Software License, Version 1.1
  *
- * Copyright (c) 2001 The Apache Software Foundation.  All rights
+ * Copyright (c) 2001,2003 The Apache Software Foundation.  All rights
  * reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -53,6 +53,7 @@
  * <http://www.apache.org/>.
  */
 package org.apache.jmeter.protocol.http.proxy;
+
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
@@ -86,191 +87,237 @@ import org.apache.log.Logger;
  *@author     mike
  *@created    June 8, 2001
  */
-public class Proxy extends Thread {
-	transient private static Logger log = Hierarchy.getDefaultHierarchy().getLoggerFor(
-			"jmeter.protocol.http");
-	//
-	// Member variables
-	//
-	Socket ClientSocket = null;
-	// Socket to client
-	Socket SrvrSocket = null;
-	// Socket to web server
-	Cache cache = null;
-	// Static cache manager object
-	String localHostName = null;
-	// Local machine name
-	String localHostIP = null;
-	// Local machine IP address
-	String adminPath = null;
-	// Path of admin applet
-	Config config = null;
-	// UrlConfig object for saving test cases
-	ProxyControl target;
-	CookieManager cookieManager;
-	
-	
-	/**
-	 * Constructor.  Configures Proxy at same time.
-	 * @param clientSocket
-	 * @param CacheManager
-	 * @param configObject
-	 * @param target
-	 * @param cookieManager
-	 */
-	Proxy(Socket clientSocket,Cache CacheManager,Config configObject,
-		ProxyControl target,CookieManager cookieManager) 
-	{
-		configure(clientSocket,CacheManager,configObject,target,cookieManager);
-	}
-	
-	public Proxy()
-	{
-		
-	}
-	
-	public void configure(
-		Socket clientSocket,
-		Cache CacheManager,
-		Config configObject,
-		ProxyControl target,
-		CookieManager cookieManager)
-	{
-		this.cookieManager = cookieManager;
-		this.target = target;
-		config = configObject;
-		ClientSocket = clientSocket;
-		cache = CacheManager;
-		localHostName = config.getLocalHost();
-		localHostIP = config.getLocalIP();
-		adminPath = config.getAdminPath();
-	}
-	//
-	// run - Main work is done here:
-	//
-	/**
-	 *  Main processing method for the Proxy object
-	 */
-	public void run() {
-		String serverName = "";
-		HttpURLConnection url;
-		byte line[];
-		HttpRequestHdr request = new HttpRequestHdr();
-		HttpReplyHdr reply = new HttpReplyHdr();
-		FileInputStream fileInputStream = null;
-		FileOutputStream fileOutputStream = null;
-		boolean TakenFromCache = false;
-		boolean isCachable = false;
-		try {
-			byte[] clientRequest = request.parse(new BufferedInputStream(
-				ClientSocket.getInputStream()));
-			HTTPSampler sampler = request.getSampler();
-			HeaderManager headers = request.getHeaderManager();
-			sampler.setHeaderManager(headers);
-			byte[] serverResponse = sampler.sample().getResponseData();
-			writeToClient(serverResponse,
-				new BufferedOutputStream(ClientSocket.getOutputStream()));
-			headers.removeHeaderNamed("cookie");
-			target.deliverSampler(sampler,new TestElement[]{headers},serverResponse);
-		} catch (UnknownHostException uhe) {
-			log.warn("Server Not Found.",uhe);
-			try {
-				DataOutputStream out = new DataOutputStream(ClientSocket.getOutputStream());
-				out.writeBytes(reply.formServerNotFound());
-				out.flush();
-			} catch (Exception uhe2) {}
-		} catch (Exception e) {
-			log.error("",e);
-			try {
-				if (TakenFromCache) {
-					fileInputStream.close();
-				} else
-					if (isCachable) {
-						fileOutputStream.close();
-					}
-				DataOutputStream out = new DataOutputStream(ClientSocket.getOutputStream());
-				out.writeBytes(reply.formTimeout());
-				out.flush();
-			} catch (Exception uhe2) {}
-		} finally {
-			try {
-				ClientSocket.close();
-			} catch (Exception e) {
-				log.error("",e);
-			}
-		}
-	}
-	public byte[] sampleServer(HTTPSampler sampler)
-		throws IllegalAccessException, InstantiationException {
-		SampleResult result = sampler.sample();
-		return result.getResponseData();
-	}
-	//
-	// Private methods
-	//
-	//
-	// Send to administrator web page containing reference to applet
-	//
-	private void sendAppletWebPage() {
-		log.info("Sending the applet...");
-		String page = "";
-		try {
-			File appletHtmlPage =
-				new File(config.getAdminPath() + File.separator + "Admin.html");
-			BufferedReader in =
-				new BufferedReader(new InputStreamReader(new FileInputStream(appletHtmlPage)));
-			String s = null;
-			while ((s = in.readLine()) != null) {
-				page += s;
-			}
-			page =
-				page.substring(0, page.indexOf("PORT"))
-					+ config.getAdminPort()
-					+ page.substring(page.indexOf("PORT") + 4);
-			in.close();
-			DataOutputStream out = new DataOutputStream(ClientSocket.getOutputStream());
-			out.writeBytes(page);
-			out.flush();
-			out.close();
-		} catch (Exception e) {
-			log.error("can't open applet html page",e);
-		}
-	}
-	//
-	// Send the applet to administrator
-	//
-	private void sendAppletClass(String className) {
-		try {
-			byte data[] = new byte[2000];
-			int count;
-			HttpReplyHdr reply = new HttpReplyHdr();
-			File appletFile = new File(adminPath + File.separatorChar + className);
-			long length = appletFile.length();
-			FileInputStream in = new FileInputStream(appletFile);
-			DataOutputStream out = new DataOutputStream(ClientSocket.getOutputStream());
-			out.writeBytes(reply.formOk("application/octet-stream", length));
-			while (-1 < (count = in.read(data))) {
-				out.write(data, 0, count);
-			}
-			out.flush();
-			in.close();
-			out.close();
-		} catch (Exception e) {}
-	}
-	private void writeToClient(
-		byte[] inBytes,
-		OutputStream out)
-		throws IOException {
-		try {
-			out.write(inBytes);
-			out.flush();
-			log.info("Done writing to client");
-		} catch (IOException e) {
-			log.error("",e);
-		} finally {
-			try {
-				out.close();
-			} catch (Exception ex) {}
-		}
-	}
+public class Proxy extends Thread
+{
+    transient private static Logger log = Hierarchy.getDefaultHierarchy().getLoggerFor(
+            "jmeter.protocol.http");
+    //
+    // Member variables
+    //
+    Socket ClientSocket = null;
+    // Socket to client
+    Socket SrvrSocket = null;
+    // Socket to web server
+    Cache cache = null;
+    // Static cache manager object
+    String localHostName = null;
+    // Local machine name
+    String localHostIP = null;
+    // Local machine IP address
+    String adminPath = null;
+    // Path of admin applet
+    Config config = null;
+    // UrlConfig object for saving test cases
+    ProxyControl target;
+    CookieManager cookieManager;
+
+    /**
+     * Constructor.  Configures Proxy at same time.
+     * @param clientSocket
+     * @param CacheManager
+     * @param configObject
+     * @param target
+     * @param cookieManager
+     */
+    Proxy(Socket clientSocket,Cache CacheManager,Config configObject,
+        ProxyControl target,CookieManager cookieManager) 
+    {
+        configure(clientSocket,CacheManager,configObject,target,cookieManager);
+    }
+
+    public Proxy()
+    {
+    }
+
+    public void configure(
+        Socket clientSocket,
+        Cache CacheManager,
+        Config configObject,
+        ProxyControl target,
+        CookieManager cookieManager)
+    {
+        this.cookieManager = cookieManager;
+        this.target = target;
+        config = configObject;
+        ClientSocket = clientSocket;
+        cache = CacheManager;
+        localHostName = config.getLocalHost();
+        localHostIP = config.getLocalIP();
+        adminPath = config.getAdminPath();
+    }
+
+    //
+    // run - Main work is done here:
+    //
+    /**
+     *  Main processing method for the Proxy object
+     */
+    public void run()
+    {
+        String serverName = "";
+        HttpURLConnection url;
+        byte line[];
+        HttpRequestHdr request = new HttpRequestHdr();
+        HttpReplyHdr reply = new HttpReplyHdr();
+        FileInputStream fileInputStream = null;
+        FileOutputStream fileOutputStream = null;
+        boolean TakenFromCache = false;
+        boolean isCachable = false;
+        try
+        {
+            byte[] clientRequest = request.parse(new BufferedInputStream(
+                ClientSocket.getInputStream()));
+            HTTPSampler sampler = request.getSampler();
+            HeaderManager headers = request.getHeaderManager();
+            sampler.setHeaderManager(headers);
+            byte[] serverResponse = sampler.sample().getResponseData();
+            writeToClient(serverResponse,
+                new BufferedOutputStream(ClientSocket.getOutputStream()));
+            headers.removeHeaderNamed("cookie");
+            target.deliverSampler(sampler,new TestElement[]{headers},serverResponse);
+        }
+        catch (UnknownHostException uhe)
+        {
+            log.warn("Server Not Found.",uhe);
+            try
+            {
+                DataOutputStream out = new DataOutputStream(ClientSocket.getOutputStream());
+                out.writeBytes(reply.formServerNotFound());
+                out.flush();
+            }
+            catch (Exception uhe2)
+            {
+            }
+        }
+        catch (Exception e)
+        {
+            log.error("",e);
+            try
+            {
+                if (TakenFromCache)
+                {
+                    fileInputStream.close();
+                }
+                else if (isCachable)
+                {
+                    fileOutputStream.close();
+                }
+                DataOutputStream out = new DataOutputStream(ClientSocket.getOutputStream());
+                out.writeBytes(reply.formTimeout());
+                out.flush();
+            }
+            catch (Exception uhe2)
+            {
+            }
+        }
+        finally
+        {
+            try
+            {
+                ClientSocket.close();
+            }
+            catch (Exception e)
+            {
+                log.error("",e);
+            }
+        }
+    }
+    
+    public byte[] sampleServer(HTTPSampler sampler)
+        throws IllegalAccessException, InstantiationException
+    {
+        SampleResult result = sampler.sample();
+        return result.getResponseData();
+    }
+    //
+    // Private methods
+    //
+    //
+    // Send to administrator web page containing reference to applet
+    //
+    private void sendAppletWebPage()
+    {
+        log.info("Sending the applet...");
+        String page = "";
+        try
+        {
+            File appletHtmlPage =
+                new File(config.getAdminPath() + File.separator + "Admin.html");
+            BufferedReader in =
+                new BufferedReader(new InputStreamReader(new FileInputStream(appletHtmlPage)));
+            String s = null;
+            while ((s = in.readLine()) != null)
+            {
+                page += s;
+            }
+            page =
+                page.substring(0, page.indexOf("PORT"))
+                    + config.getAdminPort()
+                    + page.substring(page.indexOf("PORT") + 4);
+            in.close();
+            DataOutputStream out = new DataOutputStream(ClientSocket.getOutputStream());
+            out.writeBytes(page);
+            out.flush();
+            out.close();
+        }
+        catch (Exception e)
+        {
+            log.error("can't open applet html page",e);
+        }
+    }
+    
+    //
+    // Send the applet to administrator
+    //
+    private void sendAppletClass(String className)
+    {
+        try
+        {
+            byte data[] = new byte[2000];
+            int count;
+            HttpReplyHdr reply = new HttpReplyHdr();
+            File appletFile = new File(adminPath + File.separatorChar + className);
+            long length = appletFile.length();
+            FileInputStream in = new FileInputStream(appletFile);
+            DataOutputStream out = new DataOutputStream(ClientSocket.getOutputStream());
+            out.writeBytes(reply.formOk("application/octet-stream", length));
+            while (-1 < (count = in.read(data)))
+            {
+                out.write(data, 0, count);
+            }
+            out.flush();
+            in.close();
+            out.close();
+        } 
+        catch (Exception e)
+        {
+        }
+    }
+    
+    private void writeToClient(
+        byte[] inBytes,
+        OutputStream out)
+        throws IOException
+    {
+        try
+        {
+            out.write(inBytes);
+            out.flush();
+            log.info("Done writing to client");
+        }
+        catch (IOException e)
+        {
+            log.error("",e);
+        }
+        finally
+        {
+            try
+            {
+                out.close();
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+    }
 }
