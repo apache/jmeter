293befb08e2fc4e75dce414171298b1764dc9491
Jeremy Arnold <jeremy_a@apache.org>
2003-06-01 19:08:22 +0000

    Reformatted to Turbine (JMeter) conventions.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@323302 13f79535-47bb-0310-9956-ffa450edef68

--- src/protocol/http/org/apache/jmeter/protocol/http/proxy/HttpReplyHdr.java
+++ src/protocol/http/org/apache/jmeter/protocol/http/proxy/HttpReplyHdr.java
@@ -1,317 +1,318 @@
 package org.apache.jmeter.protocol.http.proxy;
-/******************************************************************
-*** File HttpReplyHdr.java
-***
-***/
 
-
-//
-// Class:     HttpReplyHdr
-// Abstract:  The headers of the server HTTP reply.
-//
-
+/**
+ * The headers of the server HTTP reply.
+ */
 public class HttpReplyHdr
 {
+    private static final String CR = "\r\n";
+    private static final String HTTP_PROTOCOL = "HTTP/1.0";
+    private static final String HTTP_SERVER = "Java Proxy Server";
 
-	static String CR="\r\n";
-	static String HTTP_PROTOCOL="HTTP/1.0";
-	static String HTTP_SERVER="Java Proxy Server";
+    private String lastModified = "";
+    private String extraErrorString = "";
 
-	String lastModified ="";
-	long   contentLength=0;
-	String extraErrorString ="";
+    /**
+     * Sets the last modified date for a header;
+     *
+     * @param date  A string holding an interner date
+     * @return      true
+     */
+    public boolean setModifiedDate(String date)
+    {
+        lastModified = date;
+        return true;
+    }
 
-/**
- * Sets the last modified date for a header;
- *
- * @param date  A string holding an interner date
- * @return      true
- */
-public boolean setModifiedDate(String date)
-	{
-		lastModified = date;
-		return true;
-	}
+    /**
+     * Adds an extra explanation. This extra information is
+     * added to the http headers failure explanation.
+     *
+     * @param str  Description to add.
+     * @return     true.
+     */
+    public boolean addErrorDescription(String str)
+    {
+        extraErrorString = str;
+        return true;
+    }
 
-/**
- * Adds an extra explanation. This extra information is
- * Added to the http headers failure explanation.
- *
- * @param str  Description to add.
- * @return     true.
- */
-public boolean addErrorDescription(String str)
-	{
-		extraErrorString = str;
-		return true;
-	}
+    /**
+     * Forms a http ok reply header
+     *
+     * @param contentType   the mime-type of the content
+     * @param contentLength the length of the content
+     * @return              a string with the header in it
+     */
+    public String formOk(String contentType, long contentLength)
+    {
+        String out = new String();
 
-/**
- * Forms a http ok reply header
- *
- * @param ContentType The mime-type of the content
- * @return            A string with the header in it
- */
-public String formOk(String ContentType,long  ContentLength)
-	{
+        out += HTTP_PROTOCOL + " 200 Ok" + CR;
+        out += "Server: " + HTTP_SERVER + CR;
+        out += "MIME-version: 1.0" + CR;
 
-		 contentLength = ContentLength;
+        if (0 < contentType.length())
+        {
+            out += "Content-type: " + contentType + CR;
+        }
+        else
+        {
+            out += "Content-Type: text/html" + CR;
+        }
 
-		 String out =new String();
+        if (0 != contentLength)
+        {
+            out += "Content-Length: " + Long.toString(contentLength) + CR;
+        }
 
-		 out += HTTP_PROTOCOL + " 200 Ok" + CR;
-		 out += "Server: " + HTTP_SERVER  + CR;
-		 out += "MIME-version: 1.0"       + CR;
+        if (0 < lastModified.length())
+        {
+            out += "Last-Modified: " + lastModified + CR;
+        }
 
-		 if (0 < ContentType.length())
-			  out += "Content-type: " + ContentType + CR;
-		 else
-			  out += "Content-Type: text/html" + CR;
+        out += CR;
 
-		 if (0 != contentLength)
-			  out += "Content-Length: " + Long.toString(contentLength) + CR;
+        return out;
+    }
 
-		 if (0 < lastModified.length())
-			  out +="Last-Modified: " + lastModified + CR;
+    /**
+     * private! builds an http document describing a headers reason.
+     *
+     * @param error        Error name.
+     * @param description  Errors description.
+     * @return             A string with the HTML description body
+     */
+    private String formErrorBody(String error, String description)
+    {
+        String out;
+        //Generate Error Body
+        out = "<HTML><HEAD><TITLE>";
+        out += error;
+        out += "</TITLE></HEAD>";
+        out += "<BODY><H2>" + error + "</H2>\n";
+        out += "</P></H3>";
+        out += description;
+        out += "</BODY></HTML>";
+        return out;
+    }
 
-		 out +=CR;
+    /**
+     * builds an http document describing an error.
+     *
+     * @param error        Error name.
+     * @param description  Errors description.
+     * @return             A string with the HTML description body
+     */
+    private String formError(String error, String description)
+    {
+        /* A HTTP RESPONCE HEADER LOOKS ALOT LIKE:
+         *
+         * HTTP/1.0 200 OK
+         * Date: Wednesday, 02-Feb-94 23:04:12 GMT
+         * Server: NCSA/1.1
+         * MIME-version: 1.0
+         * Last-modified: Monday, 15-Nov-93 23:33:16 GMT
+         * Content-type: text/html
+         * Content-length: 2345
+         * \r\n
+         */
 
-		 return out;
-	}
+        String body = formErrorBody(error, description);
+        String header = new String();
 
+        header += HTTP_PROTOCOL + " " + error + CR;
+        header += "Server: " + HTTP_SERVER + CR;
+        header += "MIME-version: 1.0" + CR;
+        header += "Content-type: text/html" + CR;
 
-/**
- * private! builds an http document describing a headers reason.
- *
- * @param Error        Error name.
- * @param Description  Errors description.
- * @return             A string with the HTML description body
- */
-private String formErrorBody(String Error,String Description)
-	{
-	String out;
-	//Generate Error Body
-	out  ="<HTML><HEAD><TITLE>";
-	out += Error ;
-	out +="</TITLE></HEAD>";
-	out +="<BODY><H2>" + Error +"</H2>\n";
-	out +="</P></H3>";
-	out += Description;
-	out +="</BODY></HTML>";
-	return out;
-	}
+        if (0 < lastModified.length())
+        {
+            header += "Last-Modified: " + lastModified + CR;
+        }
 
+        header += "Content-Length: " + String.valueOf(body.length()) + CR;
 
+        header += CR;
+        header += body;
 
-/**
- * builds an http document describing an error.
- *
- * @param Error        Error name.
- * @param Description  Errors description.
- * @return             A string with the HTML description body
- */
-private String formError(String Error, String Description)
-	{
-	/* A HTTP RESPONCE HEADER LOOKS ALOT LIKE:
-	 *
-	 * HTTP/1.0 200 OK
-	 * Date: Wednesday, 02-Feb-94 23:04:12 GMT
-	 * Server: NCSA/1.1
-	 * MIME-version: 1.0
-	 * Last-modified: Monday, 15-Nov-93 23:33:16 GMT
-	 * Content-type: text/html
-	 * Content-length: 2345
-	 * \r\n
-	 */
+        return header;
+    }
 
-	String body=formErrorBody(Error,Description);
-	String header =new String();
+    /**
+     * Indicates a new file was created.
+     *
+     * @return    The header in a string;
+     */
+    public String formCreated()
+    {
+        return formError("201 Created", "Object was created");
+    }
 
-	header +=HTTP_PROTOCOL +" " + Error + CR;
-	header +="Server: " + HTTP_SERVER   + CR;
-	header +="MIME-version: 1.0"        + CR;
-	header +="Content-type: text/html"  + CR;
+    /**
+     * Indicates the document was accepted.
+     *
+     * @return    The header in a string;
+     */
+    public String formAccepted()
+    {
+        return formError("202 Accepted", "Object checked in");
+    }
 
-	if (0 < lastModified.length())
-		 header +="Last-Modified: " + lastModified +CR;
+    /**
+     * Indicates only a partial responce was sent.
+     *
+     * @return    The header in a string;
+     */
+    public String formPartial()
+    {
+        return formError("203 Partial", "Only partail document available");
+    }
 
-	header +="Content-Length: " + String.valueOf(body.length())+ CR;
+    /**
+     * Indicates a requested URL has moved to a new address or name.
+     *
+     * @return    The header in a string;
+     */
+    public String formMoved()
+    {
+        //300 codes tell client to do actions
+        return formError("301 Moved", "File has moved");
+    }
 
-	header += CR;
-	header += body;
+    /**
+     * Never seen this used.
+     *
+     * @return    The header in a string;
+     */
+    public String formFound()
+    {
+        return formError("302 Found", "Object was found");
+    }
 
-	return header;
-	}
+    /**
+     * The requested method is not implemented by the server.
+     *
+     * @return    The header in a string;
+     */
+    public String formMethod()
+    {
+        return formError("303 Method unseported", "Method unseported");
+    }
 
+    /**
+     * Indicates remote copy of the requested object is current.
+     *
+     * @return    The header in a string;
+     */
+    public String formNotModified()
+    {
+        return formError("304 Not modified", "Use local copy");
+    }
 
-/**
- * Indicates a new file was created.
- *
- * @return    The header in a string;
- */
-public String formCreated()
-	{
-	return formError("201 Created","Object was created");
-	}
+    /**
+     * Client not otherized for the request.
+     *
+     * @return    The header in a string;
+     */
+    public String formUnautorized()
+    {
+        return formError("401 Unathorized", "Unathorized use of this service");
+    }
 
-/**
- * Indicates the document was accepted.
- *
- * @return    The header in a string;
- */
-public String formAccepted()
-	{
-	return formError("202 Accepted","Object checked in");
-	}
+    /**
+     * Payment is required for service.
+     *
+     * @return    The header in a string;
+     */
+    public String formPaymentNeeded()
+    {
+        return formError("402 Payment required", "Payment is required");
+    }
 
-/**
- * Indicates only a partial responce was sent.
- *
- * @return    The header in a string;
- */
-public String  formPartial()
-	{
-	return formError("203 Partial","Only partail document available");
-	}
+    /**
+     * Client if forbidden to get the request service.
+     *
+     * @return    The header in a string;
+     */
+    public String formForbidden()
+    {
+        return formError(
+            "403 Forbidden",
+            "You need permission for this service");
+    }
 
-/**
- * Indicates a requested URL has moved to a new address or name.
- *
- * @return    The header in a string;
- */
-public String formMoved()
-	{
-	//300 codes tell client to do actions
-	return formError("301 Moved","File has moved");
-	}
+    /**
+     * The requested object was not found.
+     *
+     * @return    The header in a string;
+     */
+    public String formNotFound()
+    {
+        return formError("404 Not_found", "Requested object was not found");
+    }
 
-/**
- * Never seen this used.
- *
- * @return    The header in a string;
- */
-public String formFound()
-	{
-	return formError("302 Found","Object was found");
-	}
+    /**
+     * The server had a problem and could not fulfill the request.
+     *
+     * @return    The header in a string;
+     */
+    public String formInternalError()
+    {
+        return formError("500 Internal server error", "Server broke");
+    }
 
-/**
- * The requested method is not implemented by the server.
- *
- * @return    The header in a string;
- */
-public String formMethod()
-	{
-	return formError("303 Method unseported","Method unseported");
-	}
+    /**
+     * Server does not do the requested feature.
+     *
+     * @return    The header in a string;
+     */
+    public String formNotImplemented()
+    {
+        return formError(
+            "501 Method not implemented",
+            "Service not implemented, programer was lazy");
+    }
 
-/**
- * Indicates remote copy of the requested object is current.
- *
- * @return    The header in a string;
- */
-public String formNotModified()
-	{
-	return formError("304 Not modified","Use local copy");
-	}
+    /**
+     * Server is overloaded, client should try again latter.
+     *
+     * @return    The header in a string;
+     */
+    public String formOverloaded()
+    {
+        return formError("502 Server overloaded", "Try again latter");
+    }
 
-/**
- * Client not otherized for the request.
- *
- * @return    The header in a string;
- */
-public String formUnautorized()
-	{
-	return formError("401 Unathorized","Unathorized use of this service");
-	}
+    /**
+     * Indicates the request took to long.
+     *
+     * @return    The header in a string;
+     */
+    public String formTimeout()
+    {
+        return formError("503 Gateway timeout", "The connection timed out");
+    }
 
-/**
- * Payment is required for service.
- *
- * @return    The header in a string;
- */
-public String formPaymentNeeded()
-	{
-	return formError("402 Payment required","Payment is required");
-	}
+    /**
+     * Indicates the client's proxies could not locate a server.
+     *
+     * @return    The header in a string;
+     */
+    public String formServerNotFound()
+    {
+        return formError(
+            "503 Gateway timeout",
+            "The requested server was not found");
+    }
 
-/**
- * Client if forbidden to get the request service.
- *
- * @return    The header in a string;
- */
-public String formForbidden()
-	{
-	return formError("403 Forbidden","You need permission for this service");
-	}
-
-/**
- * The requested object was not found.
- *
- * @return    The header in a string;
- */
-public String formNotFound()
-	{
-	return formError("404 Not_found","Requested object was not found");
-	}
-
-/**
- * The server had a problem and could not fulfill the request.
- *
- * @return    The header in a string;
- */
-public String formInternalError()
-	{
-	return formError("500 Internal server error","Server broke");
-	}
-
-/**
- * Server does not do the requested feature.
- *
- * @return    The header in a string;
- */
-public String formNotImplemented()
-	{
-	return formError("501 Method not implemented","Service not implemented, programer was lazy");
-	}
-
-/**
- * Server is overloaded, client should try again latter.
- *
- * @return    The header in a string;
- */
-public String formOverloaded()
-	{
-	return formError("502 Server overloaded","Try again latter");
-	}
-
-/**
- * Indicates the request took to long.
- *
- * @return    The header in a string;
- */
-public String formTimeout()
-	{
-	return formError("503 Gateway timeout","The connection timed out");
-	}
-
-/**
- * Indicates the client's proxies could not locate a server.
- *
- * @return    The header in a string;
- */
-public String formServerNotFound()
-	{
-	return formError("503 Gateway timeout","The requested server was not found");
-	}
-
-/**
- * Indicates the client is not allowed to access the object.
- *
- * @return    The header in a string;
- */
-public String formNotAllowed()
-	{
-	return formError("403 Access Denied","Access is not allowed");
-	}
+    /**
+     * Indicates the client is not allowed to access the object.
+     *
+     * @return    The header in a string;
+     */
+    public String formNotAllowed()
+    {
+        return formError("403 Access Denied", "Access is not allowed");
+    }
 }
