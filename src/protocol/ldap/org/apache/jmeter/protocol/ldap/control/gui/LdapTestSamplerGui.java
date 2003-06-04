77281ac1f985d8df4ac167e74a1769cbd243bdb7
Michael Stover <mstover1@apache.org>
2003-06-04 14:31:45 +0000

    New LDAP Sampler
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@323309 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: d47df2b47979618b4da8993086fb69a1b55fde30

---
+++ src/protocol/ldap/org/apache/jmeter/protocol/ldap/control/gui/LdapTestSamplerGui.java
@@ -0,0 +1,138 @@
+/*
+ * ====================================================================
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2001 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in
+ * the documentation and/or other materials provided with the
+ * distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ * if any, must include the following acknowledgment:
+ * "This product includes software developed by the
+ * Apache Software Foundation (http://www.apache.org/)."
+ * Alternately, this acknowledgment may appear in the software itself,
+ * if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "Apache" and "Apache Software Foundation" and
+ * "Apache JMeter" must not be used to endorse or promote products
+ * derived from this software without prior written permission. For
+ * written permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ * "Apache JMeter", nor may "Apache" appear in their name, without
+ * prior written permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
+package org.apache.jmeter.protocol.ldap.control.gui;
+import javax.swing.BorderFactory;
+import javax.swing.JPanel;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+
+import org.apache.jmeter.gui.util.VerticalPanel;
+import org.apache.jmeter.config.gui.LoginConfigGui;
+import org.apache.jmeter.protocol.ldap.config.gui.LdapConfigGui;
+import org.apache.jmeter.protocol.ldap.sampler.LDAPSampler;
+import org.apache.jmeter.samplers.gui.AbstractSamplerGui;
+import org.apache.jmeter.testelement.TestElement;
+import org.apache.jmeter.util.JMeterUtils;
+
+/****************************************
+ * Title: JMeter Description: Copyright: Copyright (c) 2000 Company: Apache
+ *
+ *@author    T.Elanjchezhiyan(chezhiyan@siptech.co.in)
+ *@created   Apr 29 2003 11:52 AM
+ *@company   Sip Technologies and Exports Ltd.
+ *@version   1.0
+ ***************************************/
+public class LdapTestSamplerGui extends AbstractSamplerGui {
+    private LoginConfigGui loginPanel;
+    private LdapConfigGui ldapDefaultPanel;
+
+    /****************************************
+     * !ToDo (Constructor description)
+     ***************************************/
+    public LdapTestSamplerGui() {
+        init();
+    }
+
+    /**
+     * A newly created component can be initialized with the contents of
+     * a Test Element object by calling this method.  The component is
+     * responsible for querying the Test Element object for the
+     * relevant information to display in its GUI.
+     *
+     * @param element the TestElement to configure 
+     */
+    public void configure(TestElement element) {
+        super.configure(element);
+        loginPanel.configure(element);
+        ldapDefaultPanel.configure(element);
+    }
+
+    public TestElement createTestElement() {
+        LDAPSampler sampler = new LDAPSampler();
+        modifyTestElement(sampler);
+        return sampler;
+    }
+
+    /**
+     * Modifies a given TestElement to mirror the data in the gui components.
+     * @see org.apache.jmeter.gui.JMeterGUIComponent#modifyTestElement(TestElement)
+     */
+    public void modifyTestElement(TestElement sampler) {
+        sampler.clear();
+        ((LDAPSampler)sampler).addTestElement(ldapDefaultPanel.createTestElement());
+        ((LDAPSampler)sampler).addTestElement(loginPanel.createTestElement());
+        this.configureTestElement(sampler);
+    }
+
+    public String getStaticLabel() {
+        return JMeterUtils.getResString("ldap_testing_title");
+    }
+    
+    private void init() {
+        setLayout(new BorderLayout(0, 5));
+        setBorder(makeBorder());
+        // MAIN PANEL
+        VerticalPanel mainPanel = new VerticalPanel();
+        loginPanel = new LoginConfigGui(false);
+        ldapDefaultPanel = new LdapConfigGui(false);
+        loginPanel.setBorder(BorderFactory.createTitledBorder(JMeterUtils.getResString("login_config")));
+        add(makeTitlePanel(),BorderLayout.NORTH);
+        mainPanel.add(loginPanel);
+        mainPanel.add(ldapDefaultPanel);
+        add(mainPanel,BorderLayout.CENTER);
+    }
+}
