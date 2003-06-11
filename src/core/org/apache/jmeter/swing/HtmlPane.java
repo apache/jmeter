8cdc82f3d7d9645dde4c7e5ce2e9028a9ce63413
Michael Stover <mstover1@apache.org>
2003-06-11 15:38:33 +0000

    Doc updates enabling hypertext in JMeter Help window
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@323334 13f79535-47bb-0310-9956-ffa450edef68

--- src/core/org/apache/jmeter/swing/HtmlPane.java
+++ src/core/org/apache/jmeter/swing/HtmlPane.java
@@ -1,5 +1,10 @@
 package org.apache.jmeter.swing;
 import javax.swing.JTextPane;
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
+
+import org.apache.jorphan.logging.LoggingManager;
+import org.apache.log.Logger;
 /**
  * @author Administrator
  *
@@ -8,6 +13,24 @@ import javax.swing.JTextPane;
  */
 public class HtmlPane extends JTextPane
 {
+    Logger log = LoggingManager.getLoggerForClass();
+    
+	public HtmlPane(){	
+		this.addHyperlinkListener(
+			new HyperlinkListener(){
+				public void hyperlinkUpdate(HyperlinkEvent e) {
+					if (e.getEventType()==HyperlinkEvent.EventType.ACTIVATED){
+						String ref=e.getURL().getRef();
+						if (ref != null && ref.length() >0) {
+                            log.warn("reference to scroll to = " + ref);
+							scrollToReference(ref);
+						}
+					}
+				}
+			}
+		);
+	}
+
 	
 	public void scrollToReference(String reference)
 	{
