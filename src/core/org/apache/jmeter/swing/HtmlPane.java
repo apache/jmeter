dbb5591047b81fbe2fe84fb9ce457b5d51dab9f1
Jeremy Arnold <jeremy_a@apache.org>
2003-06-27 21:31:50 +0000

    Reformatted to JMeter conventions. Minor JavaDoc updates.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@323375 13f79535-47bb-0310-9956-ffa450edef68

--- src/core/org/apache/jmeter/swing/HtmlPane.java
+++ src/core/org/apache/jmeter/swing/HtmlPane.java
@@ -5,35 +5,35 @@ import javax.swing.event.HyperlinkListener;
 
 import org.apache.jorphan.logging.LoggingManager;
 import org.apache.log.Logger;
+
 /**
- * @author Administrator
- *
- * To change this generated comment edit the template variable "typecomment":
- * Window>Preferences>Java>Templates.
+ * @version $Revision$
  */
 public class HtmlPane extends JTextPane
 {
     private static Logger log = LoggingManager.getLoggerForClass();
     
-	public HtmlPane(){	
-		this.addHyperlinkListener(
-			new HyperlinkListener(){
-				public void hyperlinkUpdate(HyperlinkEvent e) {
-					if (e.getEventType()==HyperlinkEvent.EventType.ACTIVATED){
-						String ref=e.getURL().getRef();
-						if (ref != null && ref.length() >0) {
-                            log.warn("reference to scroll to = " + ref);
-							scrollToReference(ref);
-						}
-					}
-				}
-			}
-		);
-	}
+    public HtmlPane()
+    {
+        this.addHyperlinkListener(new HyperlinkListener()
+        {
+            public void hyperlinkUpdate(HyperlinkEvent e)
+            {
+                if (e.getEventType() == HyperlinkEvent.EventType.ACTIVATED)
+                {
+                    String ref = e.getURL().getRef();
+                    if (ref != null && ref.length() > 0)
+                    {
+                        log.warn("reference to scroll to = " + ref);
+                        scrollToReference(ref);
+                    }
+                }
+            }
+        });
+    }
 
-	
-	public void scrollToReference(String reference)
-	{
-		super.scrollToReference(reference);
-	}
+    public void scrollToReference(String reference)
+    {
+        super.scrollToReference(reference);
+    }
 }
