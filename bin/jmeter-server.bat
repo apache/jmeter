87b933f1153e4292f96b77ebee465a786d8f3629
Michael Stover <mstover1@apache.org>
2003-07-15 13:56:20 +0000

    Various minor fixes
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@323384 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 180b457bfee5b88e2bd5616d10fdcecea5ce0650

--- bin/jmeter-server.bat
+++ bin/jmeter-server.bat
@@ -1,10 +1,27 @@
-echo off
+@echo off
+
+REM Protect environment against changes if possible:
+if "%OS%"=="Windows_NT" setlocal
+
+if exist %JMETER_HOME%\lib\ext\ApacheJMeter_core.jar goto setCP
+echo Could not find ApacheJmeter_core.jar ...
+REM Try to work out JMETER_HOME
+echo ... Trying JMETER_HOME=..
+set JMETER_HOME=..
+if exist %JMETER_HOME%\lib\ext\ApacheJMeter_core.jar goto setCP
+echo ... trying JMETER_HOME=.
+set JMETER_HOME=.
+if exist %JMETER_HOME%\lib\ext\ApacheJMeter_core.jar goto setCP
+echo Cannot determine JMETER_HOME !
+goto exit
+
+:setCP
+echo Found ApacheJMeter_core.jar
 set CLASSPATH=%JMETER_HOME%\lib\ext\ApacheJMeter_core.jar;%JMETER_HOME%\lib\jorphan.jar;%JMETER_HOME%\lib\logkit-1.2.jar
 START rmiregistry
 
 if not "%OS%"=="Windows_NT" goto win9xStart
 :winNTStart
-@setlocal
 
 rem Need to check if we are using the 4NT shell...
 if "%eval[2+2]" == "4" goto setup4NT
@@ -34,3 +51,5 @@ rem This label provides a place for the argument list loop to break out
 rem and for NT handling to skip to.
 
 jmeter -s %JMETER_CMD_LINE_ARGS%
+
+:exit
