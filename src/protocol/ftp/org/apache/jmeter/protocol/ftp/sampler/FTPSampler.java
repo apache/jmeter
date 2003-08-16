22cb2e42854090c65c6a34755d0ae7cf2abd0a6a
Jeremy Arnold <jeremy_a@apache.org>
2003-08-16 15:11:17 +0000

    Reformatted to JMeter conventions. Minor JavaDoc updates (mostly formatting). No code changes.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@323419 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 67b4f21f21ebe2ee1e440247a5fa76b227394125

--- src/protocol/ftp/org/apache/jmeter/protocol/ftp/sampler/FTPSampler.java
+++ src/protocol/ftp/org/apache/jmeter/protocol/ftp/sampler/FTPSampler.java
@@ -63,111 +63,103 @@ import org.apache.jmeter.samplers.AbstractSampler;
 import org.apache.jmeter.samplers.Entry;
 import org.apache.jmeter.samplers.SampleResult;
 
-/************************************************************
- *  A sampler which understands FTP file requests
+/**
+ * A sampler which understands FTP file requests.
  *
- *@author     $Author$
- *@created    $Date$
- *@version    $Revision$
- ***********************************************************/
+ * @version    $Revision$
+ */
+public class FTPSampler extends AbstractSampler
+{
+    public final static String SERVER = "FTPSampler.server";
+    public final static String FILENAME = "FTPSampler.filename";
 
-public class FTPSampler extends AbstractSampler {
-	public final static String SERVER = "FTPSampler.server";
-	public final static String FILENAME = "FTPSampler.filename";
+    public FTPSampler()
+    {
+    }
 
-	/************************************************************
-	 *  !ToDo (Constructor description)
-	 ***********************************************************/
-	public FTPSampler()
-	{
-	}
+    public String getUsername()
+    {
+        return getPropertyAsString(ConfigTestElement.USERNAME);
+    }
 
-	public String getUsername()
-	{
-		return getPropertyAsString(ConfigTestElement.USERNAME);
-	}
-	
-	public String getPassword()
-	{
-		return getPropertyAsString(ConfigTestElement.PASSWORD);
-	}
-	
-	public void setServer(String newServer)
-	{
-		this.setProperty(SERVER,newServer);
-	}
-	public String getServer()
-	{
-		return getPropertyAsString(SERVER);
-	}
-	public void setFilename(String newFilename)
-	{
-		this.setProperty(FILENAME,newFilename);
-	}
-	public String getFilename()
-	{
-		return getPropertyAsString(FILENAME);
-	}
-	
-	/**
-	  * Returns a formatted string label describing this sampler
-	  * Example output:
-	  *      ftp://ftp.nowhere.com/pub/README.txt
-	  *
-	  * @return a formatted string label describing this sampler
-	  */
-	 public String getLabel() {
-		  return ("ftp://" + this.getServer() + "/" + this.getFilename());
-	 }
+    public String getPassword()
+    {
+        return getPropertyAsString(ConfigTestElement.PASSWORD);
+    }
 
-	/************************************************************
-	 *  !ToDo (Method description)
-	 *
-	 *@param  e  !ToDo (Parameter description)
-	 *@return    !ToDo (Return description)
-	 ***********************************************************/
-	public SampleResult sample(Entry e)
-	{
-		SampleResult res = new SampleResult();
-		Connection con = null;
-		ResultSet rs = null;
-		Statement stmt = null;
-		  boolean isSuccessful = false;
-		//FtpConfig ftpConfig = (FtpConfig)e.getConfigElement(FtpConfig.class);
-		  res.setSampleLabel(getLabel());
-		  //LoginConfig loginConfig = (LoginConfig)e.getConfigElement(LoginConfig.class);
-		long start = System.currentTimeMillis();
-		try
-		{
-			FtpClient ftp = new FtpClient();
-			ftp.connect(getServer(), getUsername(),getPassword());
-			ftp.setPassive(true); // this should probably come from the setup dialog
-			String s = ftp.get(getFilename());
-			res.setResponseData(s.getBytes());
-				// set the response code here somewhere
-			ftp.disconnect();
-				isSuccessful = true;
-		}
-		catch (java.net.ConnectException cex)
-		{
-				// java.net.ConnectException -- 502 error code?
-				// in the future, possibly define and place error codes into the
-				// result so we know exactly what happened.
-				res.setResponseData(cex.toString().getBytes());
-		}
-		  catch (Exception ex) {
-				// general exception
-				res.setResponseData(ex.toString().getBytes());
-		  }
+    public void setServer(String newServer)
+    {
+        this.setProperty(SERVER, newServer);
+    }
+    public String getServer()
+    {
+        return getPropertyAsString(SERVER);
+    }
+    public void setFilename(String newFilename)
+    {
+        this.setProperty(FILENAME, newFilename);
+    }
+    public String getFilename()
+    {
+        return getPropertyAsString(FILENAME);
+    }
 
-		  // Calculate response time
-		long end = System.currentTimeMillis();
-		  res.setTime(end - start);
+    /**
+     * Returns a formatted string label describing this sampler
+     * Example output:
+     *      ftp://ftp.nowhere.com/pub/README.txt
+     *
+     * @return a formatted string label describing this sampler
+     */
+    public String getLabel()
+    {
+        return ("ftp://" + this.getServer() + "/" + this.getFilename());
+    }
 
-		  // Set if we were successful or not
-		  res.setSuccessful(isSuccessful);
+    public SampleResult sample(Entry e)
+    {
+        SampleResult res = new SampleResult();
+        Connection con = null;
+        ResultSet rs = null;
+        Statement stmt = null;
+        boolean isSuccessful = false;
+        //FtpConfig ftpConfig = (FtpConfig)e.getConfigElement(FtpConfig.class);
+        res.setSampleLabel(getLabel());
+        //LoginConfig loginConfig =
+        //  (LoginConfig)e.getConfigElement(LoginConfig.class);
+        long start = System.currentTimeMillis();
+        try
+        {
+            FtpClient ftp = new FtpClient();
+            ftp.connect(getServer(), getUsername(), getPassword());
+            ftp.setPassive(true);
+            // this should probably come from the setup dialog
+            String s = ftp.get(getFilename());
+            res.setResponseData(s.getBytes());
+            // set the response code here somewhere
+            ftp.disconnect();
+            isSuccessful = true;
+        }
+        catch (java.net.ConnectException cex)
+        {
+            // java.net.ConnectException -- 502 error code?
+            // in the future, possibly define and place error codes into the
+            // result so we know exactly what happened.
+            res.setResponseData(cex.toString().getBytes());
+        }
+        catch (Exception ex)
+        {
+            // general exception
+            res.setResponseData(ex.toString().getBytes());
+        }
 
-		return res;
-	}
+        // Calculate response time
+        long end = System.currentTimeMillis();
+        res.setTime(end - start);
+
+        // Set if we were successful or not
+        res.setSuccessful(isSuccessful);
+
+        return res;
+    }
 }
-
