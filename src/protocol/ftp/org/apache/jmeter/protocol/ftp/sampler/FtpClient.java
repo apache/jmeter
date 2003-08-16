67b4f21f21ebe2ee1e440247a5fa76b227394125
Jeremy Arnold <jeremy_a@apache.org>
2003-08-16 15:11:17 +0000

    Reformatted to JMeter conventions. Minor JavaDoc updates (mostly formatting). No code changes.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@323419 13f79535-47bb-0310-9956-ffa450edef68

--- src/protocol/ftp/org/apache/jmeter/protocol/ftp/sampler/FtpClient.java
+++ src/protocol/ftp/org/apache/jmeter/protocol/ftp/sampler/FtpClient.java
@@ -69,342 +69,326 @@ import java.net.Socket;
 import org.apache.log.Hierarchy;
 import org.apache.log.Logger;
 
-/*
- * Simple FTP client (non-passive transfers don't work yet)
- * kind of a hack, lots of room for optimizations
- */
-
 /**
- *  Description of the Class
+ * Simple FTP client (non-passive transfers don't work yet).
+ * Kind of a hack, lots of room for optimizations.
  *
- *@author     mike
- *@created    August 31, 2001
+ * @author     mike
+ * @created    August 31, 2001
+ * @version $Revision$
  */
 public class FtpClient
 {
-	transient private static Logger log = Hierarchy.getDefaultHierarchy().getLoggerFor(
-			"jmeter.protocol.ftp");
-	File f = new File("e:\\");
-	BufferedWriter out;
-	BufferedReader in;
-	Socket s;
-	boolean passive = false;
-	static int port = 21;
-	static int dataPort = 4096;
+    transient private static Logger log =
+        Hierarchy.getDefaultHierarchy().getLoggerFor("jmeter.protocol.ftp");
+    File f = new File("e:\\");
+    BufferedWriter out;
+    BufferedReader in;
+    Socket s;
+    boolean passive = false;
+    static int port = 21;
+    static int dataPort = 4096;
 
-	/**
-	 *  Constructor for the FtpClient object
-	 */
-	public FtpClient()
-	{
-	}
+    /**
+     *  Constructor for the FtpClient object.
+     */
+    public FtpClient()
+    {
+    }
 
-	/**
-	 *  set passive mode
-	 *
-	 *@param  flag  The new Passive value
-	 */
-	public void setPassive(boolean flag)
-	{
-		passive = flag;
-	}
+    /**
+     * Set passive mode.
+     *
+     *@param  flag  the new Passive value
+     */
+    public void setPassive(boolean flag)
+    {
+        passive = flag;
+    }
 
-	/**
-	 *  get a file from the server
-	 *
-	 *@return                  The Response value
-	 *@exception  IOException  Description of Exception
-	 */
-	public String getResponse() throws IOException
-	{
-		StringBuffer response = new StringBuffer();
-		String line = in.readLine();
-		response.append(line);
-		log.info("FtpClient.getResponse(): #" + line + "#");
-		while (line.charAt(3) == '-')
-		{
-			line = in.readLine();
-			response.append("\n");
-			response.append(line);
-			log.info("FtpClient.getResponse(): #" + line + "#");
-		}
-		log.info("return response");
-		return response.toString();
-	}
+    /**
+     * Get a file from the server.
+     *
+     * @return                  the Response value
+     */
+    public String getResponse() throws IOException
+    {
+        StringBuffer response = new StringBuffer();
+        String line = in.readLine();
+        response.append(line);
+        log.info("FtpClient.getResponse(): #" + line + "#");
+        while (line.charAt(3) == '-')
+        {
+            line = in.readLine();
+            response.append("\n");
+            response.append(line);
+            log.info("FtpClient.getResponse(): #" + line + "#");
+        }
+        log.info("return response");
+        return response.toString();
+    }
 
-	/**
-	 *  get a file from the server
-	 *
-	 *@param  file           Description of Parameter
-	 *@return                Description of the Returned Value
-	 *@exception  Exception  Description of Exception
-	 */
-	public String get(String file) throws Exception
-	{
-		send("SYST");
-		getResponse();
-		send("PWD");
-		getResponse();
-		send("TYPE I");
-		getResponse();
-		String data = "";
-		if (!passive)
-		{
-			dataPort++;
-			int upper = getUpper(dataPort);
-			int lower = getLower(dataPort);
-			String ip = InetAddress.getLocalHost().getHostAddress().replace('.', ',');
-			String port = ip + "," + upper + "," + lower;
-			log.info("port:" + port);
-			send("PORT " + port);
-			getResponse();
-			dataGrabber grab = new dataGrabber(ip, dataPort);
-			while (!grab.isPortCreated())
-			{
-			}
-			send("RETR " + file);
-			String response = in.readLine();
-			log.info(response);
-			log.info(""+dataPort);
-			data = "FTP client - File Not Found";
-			if (!response.startsWith("5"))
-			{
-				while (!grab.isDone())
-				{
-				}
-				data = grab.getData();
-			}
-		}
-		else
-		{
-			send("PASV");
-			String port = getResponse();
-			while (!port.startsWith("227"))
-			{
-				port = getResponse();
-			}
-			int start = port.indexOf('(');
-			int end = port.indexOf(')');
-			port = port.substring(start + 1, end);
-			int a = port.indexOf(',');
-			int b = port.indexOf(',', a + 1);
-			int c = port.indexOf(',', b + 1);
-			int d = port.indexOf(',', c + 1);
-			int e = port.indexOf(',', d + 1);
-			String ip = port.substring(0, a) + "." + port.substring(a + 1, b) + "." + port.substring(b + 1, c) + "." + port.substring(c + 1, d);
-			int upper = Integer.parseInt(port.substring(d + 1, e));
-			int lower = Integer.parseInt(port.substring(e + 1));
-			int dataPort = getPort(upper, lower);
-			send("RETR " + file);
-			dataGrabber grab = new dataGrabber(ip, dataPort);
-			getResponse();
-			while (!grab.isDone())
-			{
-			}
-			data = grab.getData();
-		}
-		return data;
-	}
+    /**
+     * Get a file from the server.
+     */
+    public String get(String file) throws Exception
+    {
+        send("SYST");
+        getResponse();
+        send("PWD");
+        getResponse();
+        send("TYPE I");
+        getResponse();
+        String data = "";
+        if (!passive)
+        {
+            dataPort++;
+            int upper = getUpper(dataPort);
+            int lower = getLower(dataPort);
+            String ip =
+                InetAddress.getLocalHost().getHostAddress().replace('.', ',');
+            String port = ip + "," + upper + "," + lower;
+            log.info("port:" + port);
+            send("PORT " + port);
+            getResponse();
+            dataGrabber grab = new dataGrabber(ip, dataPort);
+            while (!grab.isPortCreated())
+            {
+            }
+            send("RETR " + file);
+            String response = in.readLine();
+            log.info(response);
+            log.info("" + dataPort);
+            data = "FTP client - File Not Found";
+            if (!response.startsWith("5"))
+            {
+                while (!grab.isDone())
+                {
+                }
+                data = grab.getData();
+            }
+        }
+        else
+        {
+            send("PASV");
+            String port = getResponse();
+            while (!port.startsWith("227"))
+            {
+                port = getResponse();
+            }
+            int start = port.indexOf('(');
+            int end = port.indexOf(')');
+            port = port.substring(start + 1, end);
+            int a = port.indexOf(',');
+            int b = port.indexOf(',', a + 1);
+            int c = port.indexOf(',', b + 1);
+            int d = port.indexOf(',', c + 1);
+            int e = port.indexOf(',', d + 1);
+            String ip =
+                port.substring(0, a)
+                    + "."
+                    + port.substring(a + 1, b)
+                    + "."
+                    + port.substring(b + 1, c)
+                    + "."
+                    + port.substring(c + 1, d);
+            int upper = Integer.parseInt(port.substring(d + 1, e));
+            int lower = Integer.parseInt(port.substring(e + 1));
+            int dataPort = getPort(upper, lower);
+            send("RETR " + file);
+            dataGrabber grab = new dataGrabber(ip, dataPort);
+            getResponse();
+            while (!grab.isDone())
+            {
+            }
+            data = grab.getData();
+        }
+        return data;
+    }
 
-	/**
-	 *  connect to server
-	 *
-	 *@param  host           Description of Parameter
-	 *@param  username       Description of Parameter
-	 *@param  password       Description of Parameter
-	 *@exception  Exception  Description of Exception
-	 */
-	public void connect(String host, String username, String password) throws Exception
-	{
-		InetAddress addr = InetAddress.getByName(host);
-		s = new Socket(addr, port);
-		out = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
-		
-		InputStreamReader isr = new InputStreamReader(s.getInputStream());
-		in = new BufferedReader(isr);
-		send("USER " + username);
-		send("PASS " + password);
-	}
+    /**
+     * Connect to server.
+     */
+    public void connect(String host, String username, String password)
+        throws Exception
+    {
+        InetAddress addr = InetAddress.getByName(host);
+        s = new Socket(addr, port);
+        out = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
 
-	/**
-	 *  disconnect from the server
-	 */
-	public void disconnect()
-	{
-		try
-		{
-			send("QUIT");
-			getResponse();
-		}
-		catch (Exception e)
-		{
-			log.error("FTP client - ",e);
-		}
-		try
-		{
-			in.close();
-			out.close();
-			s.close();
-		}
-		catch (Exception e)
-		{
-			log.error("FTP client - ",e);
-		}
-	}
+        InputStreamReader isr = new InputStreamReader(s.getInputStream());
+        in = new BufferedReader(isr);
+        send("USER " + username);
+        send("PASS " + password);
+    }
 
-	/**
-	 *  send a command to the server
-	 *
-	 *@param  command          Description of Parameter
-	 *@exception  IOException  Description of Exception
-	 */
-	public void send(String command) throws IOException
-	{
-		for (int i = 0; i < command.length(); i++)
-		{
-			out.write(command.charAt(i));
-		}
-		out.write('\r');
-		out.write('\n');
-		out.flush();
-	}
+    /**
+     * Disconnect from the server
+     */
+    public void disconnect()
+    {
+        try
+        {
+            send("QUIT");
+            getResponse();
+        }
+        catch (Exception e)
+        {
+            log.error("FTP client - ", e);
+        }
+        try
+        {
+            in.close();
+            out.close();
+            s.close();
+        }
+        catch (Exception e)
+        {
+            log.error("FTP client - ", e);
+        }
+    }
 
-	/**
-	 *  Gets the Port attribute of the FtpClient class
-	 *
-	 *@param  upper  Description of Parameter
-	 *@param  lower  Description of Parameter
-	 *@return        The Port value
-	 */
-	public static int getPort(int upper, int lower)
-	{
-		return upper * 256 + lower;
-	}
+    /**
+     * Send a command to the server.
+     */
+    public void send(String command) throws IOException
+    {
+        for (int i = 0; i < command.length(); i++)
+        {
+            out.write(command.charAt(i));
+        }
+        out.write('\r');
+        out.write('\n');
+        out.flush();
+    }
 
-	/**
-	 *  Gets the Upper attribute of the FtpClient class
-	 *
-	 *@param  port  Description of Parameter
-	 *@return       The Upper value
-	 */
-	public static int getUpper(int port)
-	{
-		return port / 256;
-	}
+    /**
+     * Gets the Port attribute of the FtpClient class.
+     * @return        the Port value
+     */
+    public static int getPort(int upper, int lower)
+    {
+        return upper * 256 + lower;
+    }
 
-	/**
-	 *  Gets the Lower attribute of the FtpClient class
-	 *
-	 *@param  port  Description of Parameter
-	 *@return       The Lower value
-	 */
-	public static int getLower(int port)
-	{
-		return port % 256;
-	}
+    /**
+     * Gets the Upper attribute of the FtpClient class.
+     * @return       the Upper value
+     */
+    public static int getUpper(int port)
+    {
+        return port / 256;
+    }
 
-	/**
-	 *  grabs the data from the dataport
-	 *
-	 *@author     mike
-	 *@created    August 31, 2001
-	 */
-	public class dataGrabber implements Runnable
-	{
-		StringBuffer buffer = new StringBuffer();
-		Socket s;
-		boolean done = false;
-		boolean portCreated = false;
-		String host = "";
-		int port = 22;
+    /**
+     * Gets the Lower attribute of the FtpClient class.
+     *
+     * @return       the Lower value
+     */
+    public static int getLower(int port)
+    {
+        return port % 256;
+    }
 
-		/**
-		 *  Constructor for the dataGrabber object
-		 *
-		 *@param  host           Description of Parameter
-		 *@param  port           Description of Parameter
-		 *@exception  Exception  Description of Exception
-		 */
-		public dataGrabber(String host, int port) throws Exception
-		{
-			this.host = host;
-			this.port = port;
-			new Thread((Runnable) this).start();
-		}
+    /**
+     * Grabs the data from the dataport.
+     *
+     * @author     mike
+     * @created    August 31, 2001
+     * @version $Revision$
+     */
+    public class dataGrabber implements Runnable
+    {
+        StringBuffer buffer = new StringBuffer();
+        Socket s;
+        boolean done = false;
+        boolean portCreated = false;
+        String host = "";
+        int port = 22;
 
-		/**
-		 *  Gets the Done attribute of the dataGrabber object
-		 *
-		 *@return    The Done value
-		 */
-		public boolean isDone()
-		{
-			return done;
-		}
+        /**
+         * Constructor for the dataGrabber object.
+         */
+        public dataGrabber(String host, int port) throws Exception
+        {
+            this.host = host;
+            this.port = port;
+            new Thread((Runnable) this).start();
+        }
 
-		/**
-		 *  Gets the Data attribute of the dataGrabber object
-		 *
-		 *@return    The Data value
-		 */
-		public String getData()
-		{
-			return buffer.toString();
-		}
+        /**
+         * Gets the Done attribute of the dataGrabber object.
+         *
+         * @return    the Done value
+         */
+        public boolean isDone()
+        {
+            return done;
+        }
 
-		/**
-		 *  Gets the PortCreated attribute of the dataGrabber object
-		 *
-		 *@return    The PortCreated value
-		 */
-		public boolean isPortCreated()
-		{
-			return portCreated;
-		}
+        /**
+         * Gets the Data attribute of the dataGrabber object.
+         *
+         * @return    the Data value
+         */
+        public String getData()
+        {
+            return buffer.toString();
+        }
 
-		/**
-		 *  Main processing method for the dataGrabber object
-		 */
-		public void run()
-		{
-			try
-			{
-				if (passive)
-				{
-					s = new Socket(host, port);
-				}
-				else
-				{
-					log.info("creating socket on " + port);
-					ServerSocket server = new ServerSocket(port);
-					log.info("accepting...");
-					portCreated = true;
-					s = server.accept();
-					log.info("accepted");
-				}
-			}
-			catch (Exception e)
-			{
-			}
-			try
-			{
-				InputStream in = s.getInputStream();
-				BufferedInputStream dataIn = new BufferedInputStream(in);
-				int bufferSize = 4096;
-				byte[] inputBuffer = new byte[bufferSize];
-				int i = 0;
-				while ((i = dataIn.read(inputBuffer, 0, bufferSize)) != -1)
-				{
-					buffer.append((char) i);
-				}
-				dataIn.close();
-				s.close();
-			}
-			catch (Exception e)
-			{
-				log.error("FTP client: dataGrabber",e);
-			}
-			done = true;
-		}
-	}
+        /**
+         * Gets the PortCreated attribute of the dataGrabber object.
+         *
+         * @return    the PortCreated value
+         */
+        public boolean isPortCreated()
+        {
+            return portCreated;
+        }
+
+        /**
+         * Main processing method for the dataGrabber object.
+         */
+        public void run()
+        {
+            try
+            {
+                if (passive)
+                {
+                    s = new Socket(host, port);
+                }
+                else
+                {
+                    log.info("creating socket on " + port);
+                    ServerSocket server = new ServerSocket(port);
+                    log.info("accepting...");
+                    portCreated = true;
+                    s = server.accept();
+                    log.info("accepted");
+                }
+            }
+            catch (Exception e)
+            {
+            }
+            try
+            {
+                InputStream in = s.getInputStream();
+                BufferedInputStream dataIn = new BufferedInputStream(in);
+                int bufferSize = 4096;
+                byte[] inputBuffer = new byte[bufferSize];
+                int i = 0;
+                while ((i = dataIn.read(inputBuffer, 0, bufferSize)) != -1)
+                {
+                    buffer.append((char) i);
+                }
+                dataIn.close();
+                s.close();
+            }
+            catch (Exception e)
+            {
+                log.error("FTP client: dataGrabber", e);
+            }
+            done = true;
+        }
+    }
 }
