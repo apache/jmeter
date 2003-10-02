254eef9fdb7cd86c8013df5798b2f335188e6023
Sebastian Bazley <sebb@apache.org>
2003-10-02 22:23:26 +0000

    Moved VERSION to separate file
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@323466 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/core/org/apache/jmeter/util/JMeterVersion.java
@@ -0,0 +1,34 @@
+/*
+ * Created on 02-Oct-2003
+ *
+ * This class defines the JMeter version only (moved from JMeterUtils)
+ * 
+ * Version changes no longer change the JMeterUtils source file
+ * - easier to spot when JMeterUtils really changes 
+ * - much smaller to download when the version changes
+ * 
+ */
+package org.apache.jmeter.util;
+
+/**
+ * Utility class to define the JMeter Version string
+ * 
+ * @author sebb AT apache.org
+ * @version $revision$ $date$
+ */
+public class JMeterVersion
+{
+
+	/*
+	 * The VERSION string is updated by the Ant build file, which looks for the
+	 * pattern: VERSION = <quote>.*<quote>
+	 * 
+	 */
+	static final String VERSION = "1.9.20031002";
+
+    private JMeterVersion() // Not instantiable
+    {
+        super();
+    }
+
+}
