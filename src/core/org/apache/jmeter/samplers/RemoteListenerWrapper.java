85afb8b80e909191a964189deaf784332ab161b5
Sebastian Bazley <sebb@apache.org>
2003-10-09 18:51:08 +0000

    Added sample caching code
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@323501 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 418210a49a726cefefee0970a18c4c2e80f0ce57

--- src/core/org/apache/jmeter/samplers/RemoteListenerWrapper.java
+++ src/core/org/apache/jmeter/samplers/RemoteListenerWrapper.java
@@ -9,10 +9,18 @@ import org.apache.jmeter.testelement.AbstractTestElement;
 import org.apache.jmeter.testelement.TestListener;
 import org.apache.jorphan.logging.LoggingManager;
 import org.apache.log.Logger;
+import org.apache.jmeter.util.JMeterUtils;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Iterator;
 
 /**
  * @author unascribed
- * @version $Revision$
+ * 
+ * Lars-Erik Helander provided the idea (and original implementation)
+ * for the caching functionality (sampleStore).
+ * 
+ * @version $Revision$ Updated on: $Date$
  */
 public class RemoteListenerWrapper
     extends AbstractTestElement
@@ -20,21 +28,35 @@ public class RemoteListenerWrapper
 {
     transient private static Logger log = LoggingManager.getLoggerForClass();
     RemoteSampleListener listener;
+    
+    private boolean holdSamples; //Hold samples to end of test?
+    private List sampleStore; // Samples stored here
+
+    private void setUpStore(){
+		holdSamples = JMeterUtils.getPropDefault("hold_samples",false);
+		if (holdSamples){
+			sampleStore = new ArrayList();
+			log.info("Using Sample store for this test run");
+		}
+    }
 
     public RemoteListenerWrapper(RemoteSampleListener l)
     {
         listener = l;
     }
 
-    public RemoteListenerWrapper()
+
+    public RemoteListenerWrapper() //TODO: not used - make private?
     {
     }
 
     public void testStarted()
     {
+    	log.info("Test Started()"); // should this be debug?
+    	setUpStore();
         try
         {
-            listener.testStarted();
+           listener.testStarted();
         }
         catch (Exception ex)
         {
@@ -44,9 +66,18 @@ public class RemoteListenerWrapper
     }
     public void testEnded()
     {
+    	log.debug("Test ended");
         try
         {
+        	if (holdSamples){
+				Iterator i = sampleStore.iterator();
+				while (i.hasNext()) {
+				  SampleEvent se = (SampleEvent) i.next();
+				  listener.sampleOccurred(se);
+				}
+        	}
             listener.testEnded();
+            sampleStore = null;
         }
         catch (Exception ex)
         {
@@ -55,6 +86,8 @@ public class RemoteListenerWrapper
     }
     public void testStarted(String host)
     {
+		log.info("Test Started on "+host); // should this be debug?
+		setUpStore();
         try
         {
             listener.testStarted(host);
@@ -66,9 +99,18 @@ public class RemoteListenerWrapper
     }
     public void testEnded(String host)
     {
+    	log.info("Test Ended"); // should this be debug?
         try
         {
+        	if (holdSamples){
+				Iterator i = sampleStore.iterator();
+				while (i.hasNext()) {
+				  SampleEvent se = (SampleEvent) i.next();
+				  listener.sampleOccurred(se);
+				}
+        	}
             listener.testEnded(host);
+            sampleStore = null;
         }
         catch (Exception ex)
         {
@@ -78,17 +120,32 @@ public class RemoteListenerWrapper
 
     public void sampleOccurred(SampleEvent e)
     {
+    	log.debug("Sample occurred");
         try
         {
+          if (holdSamples) {
+            sampleStore.add(e);
+          } else { 
             listener.sampleOccurred(e);
+          }
         }
         catch (RemoteException err)
         {
             log.error("", err);
         }
     }
+
+//	Note that sampleStarted() and sampleStopped() is not made to appear
+//	in synch with sampleOccured() when replaying held samples.
+//	For now this is not critical since sampleStarted() and sampleStopped()
+//	is not used, but it may become an issue in the future. Then these
+//	events must also be stored so that replay of all events may occur and
+//	in the right order. Each stored event must then be tagged with something
+//	that lets you distinguish between occured, started and ended.
+
     public void sampleStarted(SampleEvent e)
     {
+		log.debug("Sample started");
         try
         {
             listener.sampleStarted(e);
@@ -100,6 +157,7 @@ public class RemoteListenerWrapper
     }
     public void sampleStopped(SampleEvent e)
     {
+		log.debug("Sample stopped");
         try
         {
             listener.sampleStopped(e);
