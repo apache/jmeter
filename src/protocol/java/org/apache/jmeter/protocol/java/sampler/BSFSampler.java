28c8718a7fc6bb85f10945c61aacef207e151fd6
Sebastian Bazley <sebb@apache.org>
2003-11-17 00:52:14 +0000

    Alpha version of BSF sampler
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@323696 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/protocol/java/org/apache/jmeter/protocol/java/sampler/BSFSampler.java
@@ -0,0 +1,134 @@
+package org.apache.jmeter.protocol.java.sampler;
+
+import org.apache.bsf.*;
+import org.apache.jmeter.samplers.AbstractSampler;
+import org.apache.jmeter.samplers.Entry;
+import org.apache.jmeter.samplers.SampleResult;
+import org.apache.jorphan.logging.LoggingManager;
+import org.apache.log.Logger;
+
+/**
+ * A sampler which understands BSF
+ *
+ * @author sebb AT apache DOT org
+ * @version    $Revision$ Updated on: $Date$
+ */
+public class BSFSampler extends AbstractSampler
+{
+
+    protected static Logger log = LoggingManager.getLoggerForClass();
+
+    public static final String FILENAME   = "BSFSampler.filename"; //$NON-NLS-1$
+	public static final String SCRIPT     = "BSFSampler.query"; //$NON-NLS-1$
+	public static final String LANGUAGE   = "BSFSampler.language"; //$NON-NLS-1$
+	public static final String PARAMETERS = "BSFSampler.parameters"; //$NON-NLS-1$
+
+	private BSFManager mgr;
+	private BSFEngine bsfEngine;
+	
+	public BSFSampler()
+	{
+		try {
+			// register beanshell with the BSF framework
+			BSFManager.registerScriptingEngine("beanshell",
+			 "bsh.util.BeanShellBSFEngine", new String [] { "bsh"} );
+		} catch (NoClassDefFoundError e){
+		}
+	     
+	   //TODO: register other scripting languages ...
+
+	}
+    
+	public String getFilename()
+	{
+		return getPropertyAsString(FILENAME);
+	}
+	public void setFilename(String newFilename)
+	{
+		this.setProperty(FILENAME, newFilename);
+	}
+	public String getScript()
+	{
+		return this.getPropertyAsString(SCRIPT);
+	}
+	public void setScript(String newScript)
+	{
+		this.setProperty(SCRIPT, newScript);
+	}
+	public String getParameters()
+	{
+		return this.getPropertyAsString(PARAMETERS);
+	}
+	public void setParameters(String newScript)
+	{
+		this.setProperty(PARAMETERS, newScript);
+	}
+	public String getScriptLanguage()
+	{
+		return this.getPropertyAsString(LANGUAGE);
+	}
+	public void setScriptLanguage(String lang)
+	{
+		this.setProperty(LANGUAGE,lang);
+	}
+
+    /**
+     * Returns a formatted string label describing this sampler
+     *
+     * @return a formatted string label describing this sampler
+     */
+
+    public String getLabel()
+    {
+        return getName();
+    }
+
+
+    public SampleResult sample(Entry e)// Entry tends to be ignored ...
+    {
+    	log.info(getLabel()+" "+getFilename());
+        SampleResult res = new SampleResult();
+        boolean isSuccessful = false;
+        res.setSampleLabel(getLabel());
+        long start = System.currentTimeMillis();
+        try
+        {
+        	String request=getScript();
+			res.setSamplerData(request);
+
+			mgr.registerBean("Label",getLabel());
+			mgr.registerBean("Name",getFilename());
+
+			bsfEngine = mgr.loadScriptingEngine(getScriptLanguage());
+
+			Object bsfOut = bsfEngine.eval("Sampler",0,0,request);
+
+	        res.setResponseData(bsfOut.toString().getBytes());
+	        res.setDataType(SampleResult.TEXT);
+	        res.setResponseCode("200");//TODO set from script
+	        res.setResponseMessage("OK");//TODO set from script
+	        isSuccessful = true;//TODO set from script
+        }
+		catch (NoClassDefFoundError ex)
+		{
+			log.warn("",ex);
+			res.setResponseCode("500");
+			res.setResponseMessage(ex.toString());
+		}
+        catch (Exception ex)
+        {
+        	log.warn("",ex);
+			res.setResponseCode("500");
+            res.setResponseMessage(ex.toString());
+        }
+
+        // Calculate response time
+        long end = System.currentTimeMillis();
+        res.setTime(end - start);
+
+        // Set if we were successful or not
+        res.setSuccessful(isSuccessful);
+
+        return res;
+    }
+}
