1110169c01e1f155cdb0c62b6fe38e6ead6e019e
Sebastian Bazley <sebb@apache.org>
2003-11-25 22:17:17 +0000

    Remove unused imports
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@323732 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 026c0e7f3c105f44e695a85df436aee23f2957a7

--- src/protocol/tcp/org/apache/jmeter/protocol/tcp/control/gui/TCPSamplerGui.java
+++ src/protocol/tcp/org/apache/jmeter/protocol/tcp/control/gui/TCPSamplerGui.java
@@ -1,8 +1,6 @@
 package org.apache.jmeter.protocol.tcp.control.gui;
 
 import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
 import org.apache.jmeter.config.gui.LoginConfigGui;
