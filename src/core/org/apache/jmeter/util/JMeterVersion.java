9a175233d054ef8355d99a68444c56b1e2bf3c46
Sebastian Bazley <sebb@apache.org>
2003-11-30 03:29:06 +0000

    New version
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@323776 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 554431d98c5e82dddb204d239e4ff94d70ac3e0b

--- src/core/org/apache/jmeter/util/JMeterVersion.java
+++ src/core/org/apache/jmeter/util/JMeterVersion.java
@@ -24,7 +24,7 @@ public class JMeterVersion
 	 * pattern: VERSION = <quote>.*<quote>
 	 * 
 	 */
-	static final String VERSION = "1.9.20031125";
+	static final String VERSION = "1.9.20031130";
 
     private JMeterVersion() // Not instantiable
     {
