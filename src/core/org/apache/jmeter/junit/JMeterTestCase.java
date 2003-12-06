1655edfce570b3da64bf5c763c07bddeab78be69
Sebastian Bazley <sebb@apache.org>
2003-12-06 20:53:47 +0000

    Initialise JMeter properties if not done already (for testing individual modules)
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@323833 13f79535-47bb-0310-9956-ffa450edef68

--- src/core/org/apache/jmeter/junit/JMeterTestCase.java
+++ src/core/org/apache/jmeter/junit/JMeterTestCase.java
@@ -7,7 +7,7 @@
 package org.apache.jmeter.junit;
 
 import junit.framework.TestCase;
-
+import org.apache.jmeter.util.JMeterUtils;
 import org.apache.jorphan.logging.LoggingManager;
 import org.apache.log.Logger;
 
@@ -23,6 +23,22 @@ public abstract class JMeterTestCase extends TestCase
     public JMeterTestCase(String name)
     {
         super(name);
+    }
+    
+    /*
+     * If not running under AllTests.java, make sure that the properties
+     * (and log file) are set up correctly.
+     * 
+     * N.B. In order for this to work correctly, the JUnit test must be started
+     * in the bin directory, and all the JMeter jars (plus any others needed at
+     * run-time) need to be on the classpath.
+     * 
+     */
+    static {
+    	if (JMeterUtils.getJMeterProperties() == null){
+    		JMeterUtils jmu = new JMeterUtils();
+    		jmu.initializeProperties("./jmetertest.properties");
+    	}
     }
     
     protected static final Logger testLog = LoggingManager.getLoggerForClass();
