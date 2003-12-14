34d431394209b03b66cb963467ed995be3cca3be
Jordi Salvat i Alabart <jsalvata@apache.org>
2003-12-14 11:11:16 +0000

    Add support for input tag attribute values surrounded in single quotes.
    PR: 22629
    
    
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@323883 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: d389af4869d26ac79326d862810c2c8a7655ee65

--- src/protocol/http/org/apache/jmeter/protocol/http/modifier/URLRewritingModifier.java
+++ src/protocol/http/org/apache/jmeter/protocol/http/modifier/URLRewritingModifier.java
@@ -12,6 +12,7 @@ import org.apache.jmeter.samplers.SampleResult;
 import org.apache.jmeter.samplers.Sampler;
 import org.apache.jmeter.testelement.AbstractTestElement;
 import org.apache.jmeter.testelement.property.BooleanProperty;
+import org.apache.jmeter.testelement.property.PropertyIterator;
 import org.apache.jmeter.threads.JMeterContext;
 import org.apache.jmeter.threads.JMeterContextService;
 import org.apache.jmeter.util.JMeterUtils;
@@ -106,18 +107,23 @@ public class URLRewritingModifier
                 Perl5Compiler.MULTILINE_MASK);
         case2 =
             JMeterUtils.getPatternCache().getPattern(
-                "[Nn][Aa][Mm][Ee]=\""
+                "[Nn][Aa][Mm][Ee]=[\"']"
                     + argName
-                    + "\"[^>]+[vV][Aa][Ll][Uu][Ee]=\"([^\"]*)\"",
+                    + "[\"'][^>]+[vV][Aa][Ll][Uu][Ee]=[\"']([^\"']*)[\"']",
                 Perl5Compiler.MULTILINE_MASK);
         case3 =
             JMeterUtils.getPatternCache().getPattern(
-                "[vV][Aa][Ll][Uu][Ee]=\"([^\"]*)\"[^>]+[Nn][Aa][Mm][Ee]=\""
+                "[vV][Aa][Ll][Uu][Ee]=[\"']([^\"']*)[\"'][^>]+[Nn][Aa][Mm][Ee]=[\"']"
                     + argName
-                    + "\"",
+                    + "[\"']",
                 Perl5Compiler.MULTILINE_MASK);
+            // NOTE: the handling of simple- vs. double-quotes could be formally
+            // more accurate, but I can't imagine a session id containing
+            // either, so we should be OK. The whole set of expressions is a
+            // quick hack anyway, so who cares.
+            
         // case1 could be re-written "=?([^..."  instead of creating a new
-        // pattern?
+        // pattern? Maybe not: note the semicolons
         case4 =
             JMeterUtils.getPatternCache().getPattern(
                 argName + "([^\"'>& \n\r]*)[& \\n\\r\"'>]?$?",
@@ -245,6 +251,35 @@ public class URLRewritingModifier
             assertEquals(
                 "index.html;%24sid%24KQNq3AAADQZoEQAxlkX8uQV5bjqVBPbT",
                 sampler.getPath());
+        }
+
+        public void testGrabSessionIdFromForm() throws Exception
+        {
+            String[] html = new String[] {
+                "<input name=\"sid\" value=\"myId\">",
+                "<input name='sid' value='myId'>",
+                "<input value=\"myId\" NAME='sid'>",
+                "<input VALUE='myId' name=\"sid\">",
+                "<input blah blah value=\"myId\" yoda yoda NAME='sid'>",
+            };
+            for (int i=0; i<html.length; i++)
+            {
+                response = new SampleResult();
+                response.setResponseData(html[i].getBytes());
+                URLRewritingModifier mod = new URLRewritingModifier();
+                mod.setArgumentName("sid");
+                mod.setPathExtension(false);
+                HTTPSampler sampler = createSampler();
+                context.setCurrentSampler(sampler);
+                context.setPreviousResult(response);
+                mod.process();
+                Arguments args = sampler.getArguments();
+                assertEquals(
+                    "For case i="+i,
+                    "myId",
+                    ((Argument) args.getArguments().get(0).getObjectValue())
+                        .getValue());
+            }
         }
     }
 }
