463478db75e051de8903a0babeadcbce2eebd1cc
Jordi Salvat i Alabart <jsalvata@apache.org>
2003-12-24 10:40:40 +0000

    Added capability to handle text files with explicit encodings (different from the platform's default). Also removed useless constructor.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@323943 13f79535-47bb-0310-9956-ffa450edef68

--- src/jorphan/org/apache/jorphan/io/TextFile.java
+++ src/jorphan/org/apache/jorphan/io/TextFile.java
@@ -51,48 +51,111 @@
  * individuals on behalf of the Apache Software Foundation.  For more
  * information on the Apache Software Foundation, please see
  * <http://www.apache.org/>.
+ * 
+ * @author Giles Cope (gilescope at users.sourceforge.net)
+ * @author Michael Stover (mstover1 at apache.org)
+ * @author <a href="mailto:jsalvata@apache.org">Jordi Salvat i Alabart</a>
+ * @version $Id$
  */
 package org.apache.jorphan.io;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
 import java.io.Writer;
 
 import org.apache.jorphan.logging.LoggingManager;
 import org.apache.log.Logger;
 
 /**
- * @author Giles Cope (gilescope at users.sourceforge.net)
- * @author Michael Stover (mstover1 at apache.org)
- * @version $Revision$
+ * Utility class to handle text files as a single lump of text.
+ * <p>
+ * Note this is just as memory-inefficient as handling a text file can be. Use
+ * with restraint.
  */
 public class TextFile extends File
 {
     transient private static Logger log = LoggingManager.getLoggerForClass();
 
+    /**
+     * File encoding. null means use the platform's default.
+     */
+    private String encoding= null;
+    
+    /**
+     * Create a TextFile object to handle the named file with the given encoding.
+     * 
+     * @param filename File to be read & written through this object.
+     * @param encoding Encoding to be used when reading & writing this file.
+     */
+    public TextFile(File filename, String encoding)
+    {
+        super(filename.toString());
+        setEncoding(encoding);
+    }
+    
+    /**
+     * Create a TextFile object to handle the named file with the platform
+     * default encoding.
+     * 
+     * @param filename File to be read & written through this object.
+     */
     public TextFile(File filename)
     {
         super(filename.toString());
     }
 
-    public TextFile()
-    {
-        super("");
-    }
-
+    /**
+     * Create a TextFile object to handle the named file with the platform
+     * default encoding.
+     * 
+     * @param filename Name of the file to be read & written through this object.
+     */
     public TextFile(String filename)
     {
         super(filename);
     }
 
+    /**
+     * Create a TextFile object to handle the named file with the given
+     * encoding.
+     * 
+     * @param filename Name of the file to be read & written through this object.
+     * @param encoding Encoding to be used when reading & writing this file.
+     */
+    public TextFile(String filename, String encoding)
+    {
+        super(filename);
+    }
+
+    /**
+     * Create the file with the given string as content -- or replace it's
+     * content with the given string if the file already existed.
+     * 
+     * @param body New content for the file.
+     */
     public void setText(String body)
     {
         try
         {
-            Writer writer = new FileWriter(this);
+            Writer writer;
+            if (encoding == null)
+            {
+                writer = new FileWriter(this);
+            }
+            else
+            {
+                writer = new OutputStreamWriter(
+                    new FileOutputStream(this),
+                    encoding);
+            }
             writer.write(body);
             writer.flush();
             writer.close();
@@ -103,13 +166,29 @@ public class TextFile extends File
         }
     }
 
+    /**
+     * Read the whole file content and return it as a string.
+     *  
+     * @return the content of the file
+     */
     public String getText()
     {
         String lineEnd = System.getProperty("line.separator");
         StringBuffer sb = new StringBuffer();
         try
         {
-            BufferedReader br = new BufferedReader(new FileReader(this));
+            Reader reader;
+            if (encoding == null)
+            {
+                reader= new FileReader(this);
+            }
+            else
+            {
+                reader= new InputStreamReader(
+                    new FileInputStream(this),
+                    encoding);
+            }
+            BufferedReader br = new BufferedReader(reader);
             String line = "NOTNULL";
             while (line != null)
             {
@@ -125,5 +204,21 @@ public class TextFile extends File
             log.error("", ioe);
         }
         return sb.toString();
+    }
+
+    /**
+     * @return Encoding being used to read & write this file.
+     */
+    public String getEncoding()
+    {
+        return encoding;
+    }
+
+    /**
+     * @param string Encoding to be used to read & write this file.
+     */
+    public void setEncoding(String string)
+    {
+        encoding= string;
     }
 }
