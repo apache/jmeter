8ea8be72ef928942136c7ddef5d5afe5d06b24f7
Sebastian Bazley <sebb@apache.org>
2004-01-07 17:56:53 +0000

    For JDK1.3 support
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@323996 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/protocol/http/org/apache/jmeter/protocol/http/parser/HTMLParseError.java
@@ -0,0 +1,114 @@
+/*
+ * ====================================================================
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2004 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in
+ * the documentation and/or other materials provided with the
+ * distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ * if any, must include the following acknowledgment:
+ * "This product includes software developed by the
+ * Apache Software Foundation (http://www.apache.org/)."
+ * Alternately, this acknowledgment may appear in the software itself,
+ * if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "Apache" and "Apache Software Foundation" and
+ * "Apache JMeter" must not be used to endorse or promote products
+ * derived from this software without prior written permission. For
+ * written permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ * "Apache JMeter", nor may "Apache" appear in their name, without
+ * prior written permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ * 
+ * @author <a href="mailto:jsalvata@apache.org">Jordi Salvat i Alabart</a>
+ * @version $Id$
+ */
+package org.apache.jmeter.protocol.http.parser;
+
+/**
+ * Error class for use with HTMLParser classes.
+ * The main rationale for the class is to support chained
+ * Errors in JDK 1.3
+ * 
+ */
+public class HTMLParseError extends Error
+{
+	private Throwable savedCause; //Support JDK1.4 getCause() on JDK1.3
+	
+    /**
+     * 
+     */
+    public HTMLParseError()
+    {
+        super();
+    }
+
+    /**
+     * @param message
+     */
+    public HTMLParseError(String message)
+    {
+        super(message);
+    }
+
+    /**
+     * @param cause
+     */
+    public HTMLParseError(Throwable cause)
+    {
+        //JDK1.4: super(cause);
+        savedCause = cause;
+    }
+
+    /**
+     * @param message
+     * @param cause
+     */
+    public HTMLParseError(String message, Throwable cause)
+    {
+        //JDK1.4: super(message, cause);
+        super(message);
+		savedCause = cause;
+    }
+    
+    /**
+     * Local verstion of getCause() for JDK1.3 support
+     * 
+     */
+    public Throwable getCause()
+    {
+    	return savedCause;
+    }
+}
