450bfcdd023759d9f9436fcc6cd041fc011e52cd
Sebastian Bazley <sebb@apache.org>
2004-02-12 09:30:29 +0000

    Treat a blank line as EOF when reading the CSV file
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@324270 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: a8adb1e8e0ff098cb52452e6f26e6a8b7ba68247

--- src/functions/org/apache/jmeter/functions/FileRowColContainer.java
+++ src/functions/org/apache/jmeter/functions/FileRowColContainer.java
@@ -90,7 +90,10 @@ public class FileRowColContainer
 			FileReader fis = new FileReader(fileName);
 			myBread = new BufferedReader(fis);
 			String line = myBread.readLine();
-			while (line != null)
+			/* N.B. Stop reading the file if we get a blank line:
+			 * This allows for trailing comments in the file
+			 */
+			while (line != null && line.length() > 0)
 			{
 				fileData.add(splitLine(line,delimiter));
 				line = myBread.readLine();
