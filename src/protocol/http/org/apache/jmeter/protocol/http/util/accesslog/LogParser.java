da54d94e739c952bd9a02468f8353f43fff47434
Sebastian Bazley <sebb@apache.org>
2004-02-12 00:33:56 +0000

    ASF L V2
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@324266 13f79535-47bb-0310-9956-ffa450edef68

--- src/protocol/http/org/apache/jmeter/protocol/http/util/accesslog/LogParser.java
+++ src/protocol/http/org/apache/jmeter/protocol/http/util/accesslog/LogParser.java
@@ -1,68 +1,24 @@
+// $Header$
 /*
- * ====================================================================
- * The Apache Software License, Version 1.1
+ * Copyright 2003-2004 The Apache Software Foundation.
  *
- * Copyright (c) 2001 The Apache Software Foundation.  All rights
- * reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * 1. Redistributions of source code must retain the above copyright
- * notice, this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright
- * notice, this list of conditions and the following disclaimer in
- * the documentation and/or other materials provided with the
- * distribution.
- *
- * 3. The end-user documentation included with the redistribution,
- * if any, must include the following acknowledgment:
- * "This product includes software developed by the
- * Apache Software Foundation (http://www.apache.org/)."
- * Alternately, this acknowledgment may appear in the software itself,
- * if and wherever such third-party acknowledgments normally appear.
- *
- * 4. The names "Apache" and "Apache Software Foundation" and
- * "Apache JMeter" must not be used to endorse or promote products
- * derived from this software without prior written permission. For
- * written permission, please contact apache@apache.org.
- *
- * 5. Products derived from this software may not be called "Apache",
- * "Apache JMeter", nor may "Apache" appear in their name, without
- * prior written permission of the Apache Software Foundation.
- *
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
- * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
- * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
- * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- * ====================================================================
- *
- * This software consists of voluntary contributions made by many
- * individuals on behalf of the Apache Software Foundation.  For more
- * information on the Apache Software Foundation, please see
- * <http://www.apache.org/>.
- */
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+*/
 
 package org.apache.jmeter.protocol.http.util.accesslog;
 
 /**
- * Title:		JMeter Access Log utilities<br>
- * Copyright:	Apache.org<br>
- * Company:		nobody<br>
- * License:<br>
- * <br>
- * Look at the apache license at the top.<br>
- * <br>
  * Description:<br>
  * <br>
  * LogParser is the base interface for classes
@@ -76,10 +32,9 @@ package org.apache.jmeter.protocol.http.util.accesslog;
  * make it more generic and extensible. The
  * first version was hard coded and written
  * over the weekend.<p>
- * Author:	Peter Lin<br>
- * Version: 	0.1<br>
+ * @author:	Peter Lin<br>
+ * @version: 	$Revision$ last updated $Date$
  * Created on:	Jun 23, 2003<br>
- * Last Modified:	5:59:05 PM<br>
  */
 
 public interface LogParser {
