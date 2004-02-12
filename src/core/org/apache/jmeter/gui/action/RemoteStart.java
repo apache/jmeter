a1ecba4abb31e583e371ae50c4c3df084bebb76a
Sebastian Bazley <sebb@apache.org>
2004-02-12 23:59:02 +0000

    ASF L V2; added Remote Exit functionality
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@324275 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: eae70826a51860762bf8ff04b261fb2afa2d4e43

--- src/core/org/apache/jmeter/gui/action/RemoteStart.java
+++ src/core/org/apache/jmeter/gui/action/RemoteStart.java
@@ -1,57 +1,21 @@
+// $Header$
 /*
- * ====================================================================
- * The Apache Software License, Version 1.1
+ * Copyright 2001-2004 The Apache Software Foundation.
  *
- * Copyright (c) 2001,2003 The Apache Software Foundation.  All rights
- * reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * 1. Redistributions of source code must retain the above copyright
- * notice, this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright
- * notice, this list of conditions and the following disclaimer in
- * the documentation and/or other materials provided with the
- * distribution.
- *
- * 3. The end-user documentation included with the redistribution,
- * if any, must include the following acknowledgment:
- * "This product includes software developed by the
- * Apache Software Foundation (http://www.apache.org/)."
- * Alternately, this acknowledgment may appear in the software itself,
- * if and wherever such third-party acknowledgments normally appear.
- *
- * 4. The names "Apache" and "Apache Software Foundation" and
- * "Apache JMeter" must not be used to endorse or promote products
- * derived from this software without prior written permission. For
- * written permission, please contact apache@apache.org.
- *
- * 5. Products derived from this software may not be called "Apache",
- * "Apache JMeter", nor may "Apache" appear in their name, without
- * prior written permission of the Apache Software Foundation.
- *
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
- * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
- * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
- * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- * ====================================================================
- *
- * This software consists of voluntary contributions made by many
- * individuals on behalf of the Apache Software Foundation.  For more
- * information on the Apache Software Foundation, please see
- * <http://www.apache.org/>.
- */
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+*/
+
 package org.apache.jmeter.gui.action;
 import java.awt.Component;
 import java.awt.event.ActionEvent;
@@ -74,7 +38,7 @@ import org.apache.log.Logger;
 /**
  * @author  Michael Stover
  * @author  Drew Gulino
- * @version $Revision$
+ * @version $Revision$ Last updated $Date$
  */
 public class RemoteStart extends AbstractAction
 {
@@ -87,6 +51,8 @@ public class RemoteStart extends AbstractAction
         commands.add("remote_stop");
         commands.add("remote_start_all");
         commands.add("remote_stop_all");
+        commands.add("remote_exit");
+		commands.add("remote_exit_all");
     }
 
     private Map remoteEngines = new HashMap();
@@ -142,6 +108,22 @@ public class RemoteStart extends AbstractAction
                 doRemoteStop(el.trim());
             }
         }
+		else if (action.equals("remote_exit"))
+		{
+			doRemoteExit(name);
+		}
+		else if (action.equals("remote_exit_all"))
+		{
+			String remote_hosts_string =
+				JMeterUtils.getPropDefault("remote_hosts", "127.0.0.1");
+			java.util.StringTokenizer st =
+				new java.util.StringTokenizer(remote_hosts_string, ",");
+			while (st.hasMoreElements())
+			{
+				String el = (String) st.nextElement();
+				doRemoteExit(el.trim());
+			}
+		}
     }
 
     /**
@@ -156,6 +138,20 @@ public class RemoteStart extends AbstractAction
         JMeterEngine engine = (JMeterEngine) remoteEngines.get(name);
         engine.stopTest();
     }
+
+	/**
+	 * Exits a remote testing engine
+	 * 
+	 * @param name the DNS name or IP address of the remote testing engine
+	 * 
+	 */
+	private void doRemoteExit(String name)
+	{
+		JMeterEngine engine = (JMeterEngine) remoteEngines.get(name);
+		if (engine == null) return;
+		//GuiPackage.getInstance().getMainFrame().showStoppingMessage(name);
+		engine.exit();
+	}
 
     /**
      * Starts a remote testing engine
