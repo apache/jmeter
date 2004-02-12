eae70826a51860762bf8ff04b261fb2afa2d4e43
Sebastian Bazley <sebb@apache.org>
2004-02-12 23:59:02 +0000

    ASF L V2; added Remote Exit functionality
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@324275 13f79535-47bb-0310-9956-ffa450edef68

--- src/core/org/apache/jmeter/gui/util/JMeterMenuBar.java
+++ src/core/org/apache/jmeter/gui/util/JMeterMenuBar.java
@@ -1,57 +1,21 @@
+// $Header$
 /*
- * ====================================================================
- * The Apache Software License, Version 1.1
+ * Copyright 2001-2004 The Apache Software Foundation.
  *
- * Copyright (c) 2001,2003 The Apache Software Foundation.  All rights
- * reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * 1. Redistributions of source code must retain the above copyright
- * notice, this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright
- * notice, this list of conditions and the following disclaimer in
- * the documentation and/or other materials provided with the
- * distribution.
- *
- * 3. The end-user documentation included with the redistribution,
- * if any, must include the following acknowledgment:
- * "This product includes software developed by the
- * Apache Software Foundation (http://www.apache.org/)."
- * Alternately, this acknowledgment may appear in the software itself,
- * if and wherever such third-party acknowledgments normally appear.
- *
- * 4. The names "Apache" and "Apache Software Foundation" and
- * "Apache JMeter" must not be used to endorse or promote products
- * derived from this software without prior written permission. For
- * written permission, please contact apache@apache.org.
- *
- * 5. Products derived from this software may not be called "Apache",
- * "Apache JMeter", nor may "Apache" appear in their name, without
- * prior written permission of the Apache Software Foundation.
- *
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
- * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
- * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
- * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- * ====================================================================
- *
- * This software consists of voluntary contributions made by many
- * individuals on behalf of the Apache Software Foundation.  For more
- * information on the Apache Software Foundation, please see
- * <http://www.apache.org/>.
- */
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+*/
+
 package org.apache.jmeter.gui.util;
 import java.awt.Component;
 import java.awt.event.KeyEvent;
@@ -116,10 +80,15 @@ public class JMeterMenuBar extends JMenuBar implements LocaleChangeListener
     JMenuItem help_about;
     String[] remoteHosts;
 
+	private JMenu remote_exit;
+	private JMenuItem remote_exit_all;
+	private Collection remote_engine_exit;
+
     public JMeterMenuBar()
     {
         remote_engine_start = new LinkedList();
         remote_engine_stop = new LinkedList();
+		remote_engine_exit = new LinkedList();
         remoteHosts =
             JOrphanUtils.split(
                 JMeterUtils.getPropDefault("remote_hosts", ""),
@@ -414,6 +383,18 @@ public class JMeterMenuBar extends JMenuBar implements LocaleChangeListener
         remote_stop_all.addActionListener(ActionRouter.getInstance());
         remote_stop_all.setActionCommand("remote_stop_all");
         runMenu.add(remote_stop_all);
+        
+		if (remote_exit != null)
+		{
+			runMenu.add(remote_exit);
+		}
+		remote_exit_all =
+			new JMenuItem(JMeterUtils.getResString("remote_exit_all"));
+		remote_exit_all.addActionListener(ActionRouter.getInstance());
+		remote_exit_all.setActionCommand("remote_exit_all");
+		runMenu.add(remote_exit_all);
+
+
         runMenu.addSeparator();
         runMenu.add(run_clear);
         runMenu.add(run_clearAll);
@@ -494,10 +475,12 @@ public class JMeterMenuBar extends JMenuBar implements LocaleChangeListener
 
         Iterator iter = remote_engine_start.iterator();
         Iterator iter2 = remote_engine_stop.iterator();
-        while (iter.hasNext() && iter2.hasNext())
+		Iterator iter3 = remote_engine_exit.iterator();
+        while (iter.hasNext() && iter2.hasNext() && iter3.hasNext())
         {
             JMenuItem start = (JMenuItem) iter.next();
             JMenuItem stop = (JMenuItem) iter2.next();
+			JMenuItem exit = (JMenuItem) iter3.next();
             if (start.getText().equals(host))
             {
 				log.info("Found start host: " + start.getText());
@@ -508,6 +491,11 @@ public class JMeterMenuBar extends JMenuBar implements LocaleChangeListener
 				log.info("Found stop  host: " + stop.getText());
                 stop.setEnabled(running);
             }
+			if (exit.getText().equals(host))
+			{
+				log.info("Found exit  host: " + exit.getText());
+				exit.setEnabled(true);
+			}
         }
     }
 
@@ -524,6 +512,7 @@ public class JMeterMenuBar extends JMenuBar implements LocaleChangeListener
         {
             remote_start = new JMenu(JMeterUtils.getResString("remote_start"));
             remote_stop = new JMenu(JMeterUtils.getResString("remote_stop"));
+			remote_exit = new JMenu(JMeterUtils.getResString("remote_exit"));
 
             for (int i = 0; i < remoteHosts.length; i++)
             {
@@ -541,6 +530,13 @@ public class JMeterMenuBar extends JMenuBar implements LocaleChangeListener
                 item.setEnabled(false);
                 remote_engine_stop.add(item);
                 remote_stop.add(item);
+				item = new JMenuItem(remoteHosts[i]);
+				item.setActionCommand("remote_exit");
+				item.setName(remoteHosts[i]);
+				item.addActionListener(ActionRouter.getInstance());
+				item.setEnabled(false);
+				remote_engine_exit.add(item);
+				remote_exit.add(item);
             }
         }
     }
