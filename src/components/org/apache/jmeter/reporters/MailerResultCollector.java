050205b859c76bb356594eab40549220bdf8e569
Sebastian Bazley <sebb@apache.org>
2004-02-13 01:39:07 +0000

    ASF L V2
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@324279 13f79535-47bb-0310-9956-ffa450edef68

--- src/components/org/apache/jmeter/reporters/MailerResultCollector.java
+++ src/components/org/apache/jmeter/reporters/MailerResultCollector.java
@@ -1,17 +1,31 @@
+// $Header$
+/*
+ * Copyright 2001-2004 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+*/
+
 package org.apache.jmeter.reporters;
 
 import java.io.Serializable;
 
 import org.apache.jmeter.samplers.SampleEvent;
 import org.apache.jmeter.testelement.property.TestElementProperty;
-import org.apache.jorphan.logging.LoggingManager;
-import org.apache.log.Logger;
+//import org.apache.jorphan.logging.LoggingManager;
+//import org.apache.log.Logger;
 
 /**
- * Title:        Apache JMeter
- * Description:
- * Copyright:    Copyright (c) 2000
- * Company:      Apache Foundation
  * @author Michael Stover
  * @version $Revision$
  */
@@ -19,7 +33,7 @@ public class MailerResultCollector
     extends ResultCollector
     implements Serializable
 {
-    transient private static Logger log = LoggingManager.getLoggerForClass();
+    //transient private static Logger log = LoggingManager.getLoggerForClass();
     public static final String MAILER_MODEL =
         "MailerResultCollector.mailer_model";
 
