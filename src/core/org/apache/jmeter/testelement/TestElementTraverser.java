9af480bc37bf0eb7ba46f4c19018f4b8f57a6d37
Sebastian Bazley <sebb@apache.org>
2004-02-14 03:34:31 +0000

    ASF L V2
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@324292 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 6454ddae32d996ae552dcb2c817bba4e72ac350b

--- src/core/org/apache/jmeter/testelement/TestElementTraverser.java
+++ src/core/org/apache/jmeter/testelement/TestElementTraverser.java
@@ -1,3 +1,21 @@
+// $Header$
+/*
+ * Copyright 2001-2004 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+*/
+
 package org.apache.jmeter.testelement;
 
 import org.apache.jmeter.testelement.property.JMeterProperty;
@@ -6,8 +24,7 @@ import org.apache.jmeter.testelement.property.JMeterProperty;
  * For traversing Test Elements, which contain property that can be other test
  * elements, strings, collections, maps, objects
  *
- * To change this generated comment edit the template variable "typecomment":
- * Window>Preferences>Java>Templates.
+ * @version $Revision$ on $Date$
  */
 public interface TestElementTraverser
 {
