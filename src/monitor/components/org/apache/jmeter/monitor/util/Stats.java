3c4a3a36ea5fb794d4986e40d5ec24592053e834
Peter Lin <woolfel@apache.org>
2004-03-13 05:58:31 +0000

    Adding the source to CVS, but I'm going to wait to add the build task to compile and package the monitor. Once I get the custom parser ready I will add the ANT task.
    peter lin
    
    
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@324421 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 8beb58b75fa18aee0ab7f5e8def8b3b16c1eb0a2

---
+++ src/monitor/components/org/apache/jmeter/monitor/util/Stats.java
@@ -0,0 +1,177 @@
+// $Header:
+/*
+ * Copyright 2001-2004 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jmeter.monitor.util;
+
+import javax.xml.bind.*;
+import org.apache.jorphan.tomcat.manager.*;
+
+/**
+ * 
+ * Description:<p>
+ * Stats is responsible for calculating the load and
+ * health of a given server. It uses tomcat's status
+ * servlet results. A schema was generated for the
+ * XML output and JAXB was used to generate classes.
+ * <p>
+ * The equations are:<p>
+ * memory weight = (int)(33 * (used/max))<br>
+ * thread weight = (int)(67 * (current/max))<p>
+ * The load factors are stored in the properties
+ * files. Simply change the values in the properties
+ * to change how load is calculated. The defaults
+ * values are memory (33) and threads (67). The sum
+ * of the factors must equal 100.
+ */
+public class Stats
+{
+
+	public static final int DEAD = 0;
+	public static final int ACTIVE = 2;
+	public static final int WARNING = 1;
+	public static final int HEALTHY = 3;
+
+	public static int DEFAULT_MEMORY_FACTOR = 50;
+	public static int DEFAULT_THREAD_FACTOR = 50;
+	public static double HEALTHY_PER = 0.00;
+	public static double ACTIVE_PER = 0.25;
+	public static double WARNING_PER = 0.67;
+
+	/**
+	 * The method is responsible for taking a status
+	 * object and calculating an int value from 1 to
+	 * 100. We use a combination of free memory and
+	 * free threads. Since memory is a bigger risk,
+	 * it counts for 2/3.<p>
+	 * @param stat
+	 * @return
+	 */
+	public static int calculateLoad(Status stat){
+		if (stat != null){
+			// equation for calculating the weight
+			// w = (int)(33 * (used/max))
+			long totMem = stat.getJvm().getMemory().getTotal();
+			long usedMem = stat.getJvm().getMemory().getFree();
+			double memdiv = (double)usedMem/(double)totMem;
+			double memWeight = DEFAULT_MEMORY_FACTOR * memdiv;
+
+			Connector cntr = (Connector)stat.getConnector().get(0);
+			int maxThread = cntr.getThreadInfo().getMaxThreads();
+			int curThread = cntr.getThreadInfo().getCurrentThreadsBusy();
+			double thdiv = (double)curThread/(double)maxThread;
+			double threadWeight = DEFAULT_THREAD_FACTOR * thdiv;
+			return (int)(memWeight + threadWeight);
+		} else {
+			return 0;
+		}
+	}
+	
+	/**
+	 * Method should calculate if the server is:
+	 * dead, active, warning or healthy. We do
+	 * this by looking at the current busy threads.
+	 * <ol>
+	 * <li> free &gt; spare is healthy
+	 * <li> free &lt; spare is active
+	 * <li> busy threads &gt; 75% is warning
+	 * <li> none of the above is dead
+	 * </ol>
+	 * @param stat
+	 * @return
+	 */
+	public static int calculateStatus(Status stat){
+		if (stat != null){
+			Connector cntr = (Connector)stat.getConnector().get(0);
+			int max = cntr.getThreadInfo().getMaxThreads();
+			int current = cntr.getThreadInfo().getCurrentThreadsBusy();
+			int spare = cntr.getThreadInfo().getMaxSpareThreads();
+			double per = (double)current/(double)max;
+			if (per > WARNING_PER){
+				return WARNING;
+			} else if (per >= ACTIVE_PER && per <= WARNING_PER){
+				return ACTIVE;
+			} else if (per < ACTIVE_PER && per > HEALTHY_PER){
+				return HEALTHY;
+			} else {
+				return DEAD;
+			}
+		} else {
+			return DEAD;
+		}
+	}
+
+	/**
+	 * Method will calculate the memory load:
+	 * used / max = load. The load value is an
+	 * integer between 1 and 100. It is the
+	 * percent memory used.
+	 * @param stat
+	 * @return
+	 */	
+	public static int calculateMemoryLoad(Status stat){
+		double load = 0;
+		if (stat != null){
+			double total = (double)stat.getJvm().getMemory().getTotal();
+			double used = (double)stat.getJvm().getMemory().getFree();
+			load = (used/total);
+		}
+		return (int)(load * 100);
+	}
+
+	/**
+	 * Method will calculate the thread load:
+	 * busy / max = load. The value is an
+	 * integer between 1 and 100. It is the
+	 * percent busy.
+	 * @param stat
+	 * @return
+	 */	
+	public static int calculateThreadLoad(Status stat){
+		int load = 0;
+		if (stat != null){
+			Connector cntr = (Connector)stat.getConnector().get(0);
+			double max = (double)cntr.getThreadInfo().getMaxThreads();
+			double current =
+				(double)cntr.getThreadInfo().getCurrentThreadsBusy();
+			load = (int)((current/max) * 100);
+		}
+		return load;
+	}
+	
+	/**
+	 * Simple unit test to make sure it works correctly.
+	 * Will write a JUnit test later for this.
+	 * @param args
+	 */
+    public static void main(String[] args)
+    {
+    	try {
+			JAXBContext jxbc = new ObjectFactory();
+				// ObjectFactory.newInstance("org.apache.jorphan.tomcat.manager");
+			Unmarshaller mar = jxbc.createUnmarshaller();
+			Object ld = mar.unmarshal(new java.io.File("status3.xml"));
+			System.out.println("successfully unmarshalled sample status");
+			if (ld instanceof Status){
+				System.out.println("status is " + calculateStatus((Status)ld));
+				System.out.println("load is " + calculateLoad((Status)ld));
+				System.out.println("mem load is " + calculateMemoryLoad((Status)ld));
+				System.out.println("th load is " + calculateThreadLoad((Status)ld));
+			}
+    	} catch (JAXBException e){
+    		e.printStackTrace();
+    	}
+    }
+}
---
+++ src/monitor/components/org/apache/jmeter/visualizers/MonitorGraph.java
@@ -0,0 +1,242 @@
+/*
+ * Created on Mar 9, 2004
+ *
+ * To change the template for this generated file go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+package org.apache.jmeter.visualizers;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.swing.JComponent;
+
+import org.apache.jmeter.gui.util.JMeterColor;
+import org.apache.jmeter.samplers.Clearable;
+
+public class MonitorGraph
+    extends JComponent
+    implements MouseListener, MonitorGuiListener, Clearable
+{
+	private static int width = 500;
+	private MonitorAccumModel MODEL;
+	private MonitorModel CURRENT;
+	
+	private boolean CPU = false;
+	private boolean HEALTH = true;
+	private boolean LOAD = true;
+	private boolean MEM = true;
+	private boolean THREAD = true;
+	private boolean YGRID = true;
+	private boolean XGRID = true;
+
+	private int COUNT = 0;
+	private int GRAPHMAX = 0;
+    /**
+     * 
+     */
+    public MonitorGraph(MonitorAccumModel model)
+    {
+        this.MODEL = model;
+        GRAPHMAX = model.getBufferSize();
+        init();
+    }
+
+	private void init(){
+		repaint();
+	}
+	
+	public void setCpu(boolean cpu){
+		this.CPU = cpu;
+	}
+	
+	public void setHealth(boolean health){
+		this.HEALTH = health;
+	}
+	
+	public void setLoad(boolean load){
+		this.LOAD = load;
+	}
+	
+	public void setMem(boolean mem){
+		this.MEM = mem;
+	}
+	
+	public void setThread(boolean thread){
+		this.THREAD = thread;
+	}
+	
+    /* (non-Javadoc)
+     * @see java.awt.event.MouseListener#mouseClicked(java.awt.event.MouseEvent)
+     */
+    public void mouseClicked(MouseEvent e)
+    {
+    }
+
+    /* (non-Javadoc)
+     * @see java.awt.event.MouseListener#mouseEntered(java.awt.event.MouseEvent)
+     */
+    public void mouseEntered(MouseEvent e)
+    {
+    }
+
+    /* (non-Javadoc)
+     * @see java.awt.event.MouseListener#mouseExited(java.awt.event.MouseEvent)
+     */
+    public void mouseExited(MouseEvent e)
+    {
+    }
+
+    /* (non-Javadoc)
+     * @see java.awt.event.MouseListener#mousePressed(java.awt.event.MouseEvent)
+     */
+    public void mousePressed(MouseEvent e)
+    {
+    }
+
+    /* (non-Javadoc)
+     * @see java.awt.event.MouseListener#mouseReleased(java.awt.event.MouseEvent)
+     */
+    public void mouseReleased(MouseEvent e)
+    {
+
+    }
+
+    /* (non-Javadoc)
+     * @see org.apache.jmeter.visualizers.MonitorGuiListener#updateGui(org.apache.jmeter.visualizers.MonitorModel)
+     */
+    public void updateGui(final MonitorModel model)
+    {
+    	if (this.isShowing()){
+			this.CURRENT = model;
+			repaint();
+    	}
+    }
+
+	public void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);
+		if (this.CURRENT != null){
+			synchronized(MODEL){
+				List samples = MODEL.getAllSamples(this.CURRENT.getURL());
+				int size = samples.size();
+				synchronized (samples)
+				{
+					Iterator e;
+					if (size > getWidth()){
+						e = samples.listIterator(size - getWidth());
+					} else {
+						e = samples.iterator();
+					}
+					MonitorModel last = null;
+					for (int i = 0; e.hasNext(); i++)
+					{
+						MonitorModel s = (MonitorModel) e.next();
+						if (last == null){
+							last = s;
+						}
+						drawSample(i, s, g, last);
+						last = s;
+					}
+				}
+			}
+		}
+	}
+
+    /* (non-Javadoc)
+     * @see org.apache.jmeter.visualizers.MonitorGuiListener#updateGui()
+     */
+    public void updateGui()
+    {
+    	repaint();
+    }
+
+    /* (non-Javadoc)
+     * @see org.apache.jmeter.samplers.Clearable#clear()
+     */
+    public void clear()
+    {
+    	paintComponent(getGraphics());
+    	this.repaint();
+    }
+    
+	private void drawSample(int x, MonitorModel model,
+		Graphics g, MonitorModel last)
+	{
+		double width = (double)getWidth();
+		double height = (double)getHeight() - 10.0;
+		// int xaxis = (int)(x % width);
+		// int lastx = (int)((x - 1) % width);
+		int xaxis = (int)(width * (x /width));
+		int lastx = (int)(width * ((x - 1)/width));
+		
+		// draw grid
+		if (YGRID && x == 1){
+			int q1 = (int)(height * 0.25);
+			int q2 = (int)(height * 0.50);
+			int q3 = (int)(height * 0.75);
+			g.setColor(JMeterColor.lightGray);
+			g.drawLine(0,q1,getWidth(),q1);
+			g.drawLine(0,q2,getWidth(),q2);
+			g.drawLine(0,q3,getWidth(),q3);
+		}
+		if (XGRID && x == 1){
+			int x1 = (int)(width * 0.25);
+			int x2 = (int)(width * 0.50);
+			int x3 = (int)(width * 0.75);
+			g.drawLine(x1,0,x1,getHeight());
+			g.drawLine(x2,0,x2,getHeight());
+			g.drawLine(x3,0,x3,getHeight());
+			g.drawLine(getWidth(),0,getWidth(),getHeight());
+		}
+		
+		if (HEALTH)
+		{
+			int hly =
+				(int)(height - (height * ((double)model.getHealth()/3.0)));
+			int lasty =
+				(int)(height - (height * ((double)last.getHealth()/3.0)));
+
+			g.setColor(JMeterColor.GREEN);
+			g.drawLine(lastx,lasty,xaxis,hly);
+		}
+
+		if (LOAD)
+		{
+			int ldy =
+				(int)(height - (height * ((double)model.getLoad()/100.0)));
+			int lastldy =
+				(int)(height - (height * ((double)last.getLoad()/100.0)));
+
+			g.setColor(Color.BLUE);
+			g.drawLine(lastx,lastldy,xaxis,ldy);
+		}
+
+		if (MEM)
+		{
+			int mmy =
+				(int)(height - (height * ((double)model.getMemload()/100.0)));
+			int lastmmy =
+				(int)(height - (height * ((double)last.getMemload()/100.0)));
+
+			g.setColor(JMeterColor.ORANGE);
+			g.drawLine(lastx,lastmmy,xaxis,mmy);
+		}
+
+		if (THREAD)
+		{
+			int thy =
+				(int)(height - (height * ((double)model.getThreadload()/100.0)));
+			int lastthy =
+				(int)(height - (height * ((double)last.getThreadload()/100.0)));
+
+			g.setColor(Color.RED);
+			g.drawLine(lastx,lastthy,xaxis,thy);
+		}
+	}
+
+}
