913cd4d70ba06142499ea17ac945ecc3a120a39c
Peter Lin <woolfel@apache.org>
2004-03-14 17:37:35 +0000

    Adding constants for the custom parser peter lin
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@324442 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 5a31017e2609f7201b47cbaf0bc640b0febd9e5c

---
+++ src/monitor/model/org/apache/jmeter/monitor/parser/Constants.java
@@ -0,0 +1,53 @@
+/*
+ * Created on Mar 13, 2004
+ *
+ * To change the template for this generated file go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+package org.apache.jmeter.monitor.parser;
+
+/**
+ * @author pete<p>
+ * @version 0.1<p>
+ * 
+ * Description:<p>
+ * To change the template for this generated type comment go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public class Constants
+{
+	public static final String STATUS = "status";
+	public static final String JVM = "jvm";
+	public static final String CONNECTOR = "connector";
+	public static final String MEMORY = "memory";
+	public static final String THREADINFO = "threadInfo";
+	public static final String REQUESTINFO = "requestInfo";
+	public static final String WORKER = "worker";
+	public static final String WORKERS = "workers";
+	public static final String MEMORY_FREE = "free";
+	public static final String MEMORY_TOTAL = "total";
+	public static final String MEMORY_MAX = "max";
+	public static final String ATTRIBUTE_NAME = "name";
+	
+	public static final String MAXTHREADS =  "maxThreads";
+	public static final String MINSPARETHREADS = "minSpareThreads";
+	public static final String MAXSPARETHREADS = "maxSpareThreads";
+	public static final String CURRENTTHREADCOUNT = "currentThreadCount";
+	public static final String CURRENTBUSYTHREADS = "currentThreadsBusy=";
+	public static final String MAXTIME = "maxTime=";
+	public static final String PROCESSINGTIME = "processingTime=";
+	public static final String REQUESTCOUNT = "requestCount";
+	public static final String ERRORCOUNT = "errorCount=";
+	public static final String BYTESRECEIVED = "bytesReceived";
+	public static final String BYTESSENT = "bytesSent";
+	public static final String STAGE = "stage";
+	public static final String REQUESTPROCESSINGTIME = "requestProcessingTime=";
+	public static final String REQUESTBYTESSENT = "requestBytesSent";
+	public static final String REQUESTBYTESRECEIVED = "requestBytesReceived";
+	public static final String REMOTEADDR = "remoteAddr";
+	public static final String VIRTUALHOST = "virtualHost";
+	public static final String METHOD = "method";
+	public static final String CURRENTURI = "currentUri";
+	public static final String CURRENTQUERYSTRING = "currentQueryString";
+	public static final String PROTOCOL = "protocol";
+}
