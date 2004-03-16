85995d88dfc13a6919864bc6b6b390a8e7c42da3
Peter Lin <woolfel@apache.org>
2004-03-16 03:35:58 +0000

    ObjectFactory is not fully implemented. Still need to write a unit test for the class. peter lin
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@324450 13f79535-47bb-0310-9956-ffa450edef68

--- src/monitor/model/org/apache/jmeter/monitor/model/Jvm.java
+++ src/monitor/model/org/apache/jmeter/monitor/model/Jvm.java
@@ -17,14 +17,6 @@
 
 package org.apache.jmeter.monitor.model;
 
-/**
- * @author pete<p>
- * @version 0.1<p>
- * 
- * Description:<p>
- * To change the template for this generated type comment go to
- * Window>Preferences>Java>Code Generation>Code and Comments
- */
 public interface Jvm
 {
 	Memory getMemory();
