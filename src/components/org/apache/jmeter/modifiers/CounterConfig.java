7ad0de88a623565db64e766b9cab7402ee9011ac
Sebastian Bazley <sebb@apache.org>
2004-03-23 14:23:27 +0000

    Cannot use getThreadContext() here
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@324503 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 8c7d0527e55f42afda1e09d65b3268cc6e1bf0e9

--- src/components/org/apache/jmeter/modifiers/CounterConfig.java
+++ src/components/org/apache/jmeter/modifiers/CounterConfig.java
@@ -26,6 +26,7 @@ import org.apache.jmeter.engine.util.NoThreadClone;
 import org.apache.jmeter.testelement.AbstractTestElement;
 import org.apache.jmeter.testelement.property.BooleanProperty;
 import org.apache.jmeter.testelement.property.IntegerProperty;
+import org.apache.jmeter.threads.JMeterContextService;
 import org.apache.jmeter.threads.JMeterVariables;
 import org.apache.jorphan.logging.LoggingManager;
 import org.apache.log.Logger;
@@ -51,7 +52,9 @@ public class CounterConfig
      */
     public synchronized void iterationStart(LoopIterationEvent event)
     {
-        JMeterVariables variables = getThreadContext().getVariables();
+    	// Cannot use getThreadContext() as not cloned per thread
+        JMeterVariables variables = 
+        	JMeterContextService.getContext().getVariables();
         int start = getStart(), end = getEnd(), increment = getIncrement();
         if (!isPerUser())
         {
