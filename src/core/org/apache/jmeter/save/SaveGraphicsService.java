a39eca7dcfcd9e40ad564490675b7b6de55febdb
Peter Lin <woolfel@apache.org>
2004-06-08 02:36:54 +0000

    I updated the class to use JAI instead of the stock com.sun.image. codec.jpeg.  the output generated by the stock one is terrible and looks aweful. JAI produces much better quality. The new method saves PNG files. I checked the license of JAI and it allows redistribution in binary form. I will add the jar's to CVS.
    peter
    
    
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@324634 13f79535-47bb-0310-9956-ffa450edef68

--- src/core/org/apache/jmeter/save/SaveGraphicsService.java
+++ src/core/org/apache/jmeter/save/SaveGraphicsService.java
@@ -29,6 +29,10 @@ import javax.swing.JComponent;
 
 import com.sun.image.codec.jpeg.JPEGCodec;
 import com.sun.image.codec.jpeg.JPEGImageEncoder;
+import com.sun.image.codec.jpeg.JPEGEncodeParam;
+import com.sun.media.jai.codec.ImageCodec;
+import com.sun.media.jai.codec.ImageEncoder;
+import com.sun.media.jai.codec.PNGEncodeParam;
 
 /**
  * Class is responsible for taking a component and saving it
@@ -49,7 +53,8 @@ public class SaveGraphicsService implements SaveServiceConstants {
 
 	/**
 	 * Method saves a given component to a file as a
-	 * JPeg.
+	 * JPeg. I should remove this method later, once
+	 * everything is worked out.
 	 * @param filename
 	 * @param component
 	 */
@@ -63,9 +68,12 @@ public class SaveGraphicsService implements SaveServiceConstants {
 		component.paint(grp);
 		
 		File outfile = new File(filename);
-		System.out.println("file: " + outfile.getAbsolutePath());
 		FileOutputStream fos = createFile(outfile);
 		JPEGImageEncoder encoder = JPEGCodec.createJPEGEncoder(fos);
+		JPEGEncodeParam param = JPEGCodec.getDefaultJPEGEncodeParam(image);
+
+		param.setQuality(1,false);
+		encoder.setJPEGEncodeParam(param);
 		try {
 			encoder.encode(image);
 			fos.close();
@@ -75,6 +83,33 @@ public class SaveGraphicsService implements SaveServiceConstants {
 		}
 	}
 
+	/**
+	 * Method uses JAI to save the graph instead of the
+	 * stock com.sun.image.codec.jpeg API. The stock
+	 * codec is a bit lame and generates poor quality.
+	 * @param filename
+	 * @param component
+	 */
+	public void saveJComponentWithJAI(String filename, JComponent component){
+		Dimension size = component.getSize();
+		BufferedImage image = new BufferedImage(size.width, size.height,
+			BufferedImage.TYPE_INT_RGB);
+		Graphics2D grp = image.createGraphics();
+		component.setBackground(Color.white);
+		component.paint(grp);
+		
+		File outfile = new File(filename);
+		FileOutputStream fos = createFile(outfile);
+		ImageEncoder encoder = ImageCodec.createImageEncoder("PNG",fos,null);
+		PNGEncodeParam param = new PNGEncodeParam.RGB();
+		encoder.setParam(param);
+		try {
+			encoder.encode(image);
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+	
 	/**
 	 * Create a new file for the graphics. Since the method
 	 * creates a new file, we shouldn't get a FNFE.
