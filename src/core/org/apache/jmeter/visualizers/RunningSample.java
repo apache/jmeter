6ef8583b531dec9bc14e1fbf2f351df5edfdff1f
Sebastian Bazley <sebb@apache.org>
2004-06-11 00:44:19 +0000

    Remove extraneous "/"
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/branches/rel-2_0@324666 13f79535-47bb-0310-9956-ffa450edef68

--- src/core/org/apache/jmeter/visualizers/RunningSample.java
+++ src/core/org/apache/jmeter/visualizers/RunningSample.java
@@ -175,7 +175,7 @@ public class RunningSample
         if (rate < 1.0)
         {
             rate *= 60.0;
-            unit = "/hour";
+            unit = "hour";
         }
 
         String rval = rateFormatter.format(rate) + "/" + unit;
