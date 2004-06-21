666fca4a2c980e7ff8059ff87cd5a689e0f80969
Sebastian Bazley <sebb@apache.org>
2004-06-21 23:37:40 +0000

    Added getVersion() and ASF licences
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@324724 13f79535-47bb-0310-9956-ffa450edef68

--- src/core/org/apache/jmeter/save/converters/HashTreeConverter.java
+++ src/core/org/apache/jmeter/save/converters/HashTreeConverter.java
@@ -1,16 +1,25 @@
 /*
- * Created on Jun 18, 2004
+ * Copyright 2004 The Apache Software Foundation.
  *
- * To change the template for this generated file go to
- * Window - Preferences - Java - Code Generation - Code and Comments
- */
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+*/
+
 package org.apache.jmeter.save.converters;
 
 import java.util.Iterator;
 
 import org.apache.jorphan.collections.HashTree;
-import org.apache.jorphan.logging.LoggingManager;
-import org.apache.log.Logger;
 
 import com.thoughtworks.xstream.alias.ClassMapper;
 import com.thoughtworks.xstream.converters.MarshallingContext;
@@ -27,7 +36,9 @@ import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
  */
 public class HashTreeConverter extends AbstractCollectionConverter
 {
-   Logger log = LoggingManager.getLoggerForClass();
+	
+    /** Returns the converter version; used to check for possible incompatibilities */
+	public static String getVersion(){	return "$Revision$";}
 
    /* (non-Javadoc)
     * @see com.thoughtworks.xstream.converters.Converter#canConvert(java.lang.Class)
