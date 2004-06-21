0295d85072600f464b9a0201ee76a07acc38f816
Sebastian Bazley <sebb@apache.org>
2004-06-21 23:37:40 +0000

    Added getVersion() and ASF licences
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@324724 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 666fca4a2c980e7ff8059ff87cd5a689e0f80969

--- src/core/org/apache/jmeter/save/converters/SampleResultConverter.java
+++ src/core/org/apache/jmeter/save/converters/SampleResultConverter.java
@@ -1,9 +1,20 @@
 /*
- * Created on Jun 19, 2004
+ * Copyright 2004 The Apache Software Foundation.
  *
- * To change the template for this generated file go to
- * Window - Preferences - Java - Code Generation - Code and Comments
- */
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+*/
+
 package org.apache.jmeter.save.converters;
 
 import org.apache.jmeter.samplers.SampleResult;
@@ -23,6 +34,9 @@ import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
  */
 public class SampleResultConverter extends AbstractCollectionConverter
 {
+
+    /** Returns the converter version; used to check for possible incompatibilities */
+	public static String getVersion(){	return "$Revision$";}
 
    /* (non-Javadoc)
     * @see com.thoughtworks.xstream.converters.Converter#canConvert(java.lang.Class)
