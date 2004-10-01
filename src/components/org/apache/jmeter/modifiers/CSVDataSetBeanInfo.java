6492eb4a5648d726f0034ecbb9167daa5bf5c87a
Michael Stover <mstover1@apache.org>
2004-10-01 16:25:08 +0000

    Beginnings of new CSVDataSet component
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@324893 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/components/org/apache/jmeter/modifiers/CSVDataSetBeanInfo.java
@@ -0,0 +1,27 @@
+/*
+ * Created on Sep 29, 2004
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package org.apache.jmeter.modifiers;
+
+import org.apache.jmeter.testbeans.BeanInfoSupport;
+
+/**
+ * @author mstover
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class CSVDataSetBeanInfo extends BeanInfoSupport
+{
+
+    /**
+     * @param beanClass
+     */
+    public CSVDataSetBeanInfo()
+    {
+        super(CSVDataSet.class);
+    }
+}
