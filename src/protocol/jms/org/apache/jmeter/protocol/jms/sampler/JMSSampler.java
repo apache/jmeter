16efb5e932ac22b780398895fbcf43e9bc4cd2b0
Sebastian Bazley <sebb@apache.org>
2005-01-05 03:01:14 +0000

    Remove unused import(s)
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@325084 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: d4cc6f479e20b1fdfc65ff02dcb095efa9f77727

--- src/protocol/jms/org/apache/jmeter/protocol/jms/sampler/JMSSampler.java
+++ src/protocol/jms/org/apache/jmeter/protocol/jms/sampler/JMSSampler.java
@@ -27,7 +27,7 @@ import org.apache.jmeter.samplers.AbstractSampler;
 import org.apache.jmeter.samplers.Entry;
 import org.apache.jmeter.samplers.SampleResult;
 import org.apache.jmeter.testelement.property.BooleanProperty;
-import org.apache.jmeter.testelement.property.JMeterProperty;
+//import org.apache.jmeter.testelement.property.JMeterProperty;
 import org.apache.jmeter.testelement.property.TestElementProperty;
 
 /**
