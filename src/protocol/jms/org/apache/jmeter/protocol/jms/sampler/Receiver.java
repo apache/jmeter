c1b6a6137daf69ef4449c2eb914ac85b35db4b4c
Martijn Blankestijn <mblankestijn@apache.org>
2005-06-01 19:18:41 +0000

    Fixed ASF Bugzilla Bug 35133, receiver thread is stopped at the end
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@325420 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 66752579e3d3f6f9216b783fffad157da5583509

--- src/protocol/jms/org/apache/jmeter/protocol/jms/sampler/Receiver.java
+++ src/protocol/jms/org/apache/jmeter/protocol/jms/sampler/Receiver.java
@@ -12,98 +12,99 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  */
 
 package org.apache.jmeter.protocol.jms.sampler;
 
-import javax.jms.JMSException;
-import javax.jms.Message;
-import javax.jms.Queue;
-import javax.jms.QueueConnection;
-import javax.jms.QueueConnectionFactory;
-import javax.jms.QueueReceiver;
-import javax.jms.QueueSession;
-import javax.jms.Session;
-
 import org.apache.jorphan.logging.LoggingManager;
 import org.apache.log.Logger;
 
+import javax.jms.*;
+
 /**
- * Receiver of pseudo-synchronous reply messages.  
- * @author Martijn Blankestijn 
+ * Receiver of pseudo-synchronous reply messages.
+ *
+ * @author Martijn Blankestijn
  * @version $Id$.
  */
 public class Receiver implements Runnable {
-	private boolean active;
-	private QueueSession session;
-	private QueueReceiver consumer;
-	private QueueConnection conn;
-	private static Receiver receiver;
-	static Logger log = LoggingManager.getLoggerForClass();
+    private boolean active;
+    private QueueSession session;
+    private QueueReceiver consumer;
+    private QueueConnection conn;
+    private static Receiver receiver;
+    static Logger log = LoggingManager.getLoggerForClass();
 
-	private Receiver(QueueConnectionFactory factory, Queue receiveQueue) throws JMSException {
-		conn = factory.createQueueConnection();
-		session = conn.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
-		consumer = session.createReceiver(receiveQueue);
+    private Receiver(QueueConnectionFactory factory, Queue receiveQueue) throws JMSException {
+        conn = factory.createQueueConnection();
+        session = conn.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
+        consumer = session.createReceiver(receiveQueue);
         if (log.isDebugEnabled()) {
             log.debug("Receiver - ctor. Starting connection now");
         }
-		conn.start();
+        conn.start();
         if (log.isInfoEnabled()) {
             log.info("Receiver - ctor. Connection to messaging system established");
         }
-	}
-	
-	public static synchronized Receiver createReceiver(QueueConnectionFactory factory, Queue receiveQueue) throws JMSException {
-		if (receiver==null) {
-			receiver = new Receiver(factory, receiveQueue);
-			Thread thread = new Thread(receiver);
-			thread.start();
-		}
-		return receiver;
-	}
-	
-	public void run() {
-		activate();
-		Message reply;
-		
-		while(isActive()) {
-			reply = null;
-			try {
-				reply = consumer.receive();
-                if (log.isDebugEnabled()) {
-				    log.debug("Received message, correlation id:" + reply.getJMSCorrelationID());
+    }
+
+    public static synchronized Receiver createReceiver(QueueConnectionFactory factory, Queue receiveQueue) throws JMSException {
+        if (receiver == null) {
+            receiver = new Receiver(factory, receiveQueue);
+            Thread thread = new Thread(receiver);
+            thread.start();
+        }
+        return receiver;
+    }
+
+    public void run() {
+        activate();
+        Message reply;
+
+        while (isActive()) {
+            reply = null;
+            try {
+                reply = consumer.receive(5000);
+                if (reply != null) {
+
+                    if (log.isDebugEnabled()) {
+                        log.debug("Received message, correlation id:" + reply.getJMSCorrelationID());
+                    }
+
+                    if (reply.getJMSCorrelationID() == null) {
+                        log.warn("Received message with correlation id null. Discarding message ...");
+                    } else {
+                        MessageAdmin.getAdmin().putReply(reply.getJMSCorrelationID(), reply);
+                    }
                 }
 
-                if (reply.getJMSCorrelationID()==null) {
-					log.warn("Received message with correlation id null. Discarding message ...");
-				}
-				else {
-					MessageAdmin.getAdmin().putReply(reply.getJMSCorrelationID(), reply);
-				}
-				
-			} catch (JMSException e1) {
-				e1.printStackTrace();
-			}
-		}
-		// not active anymore
-		if (session!=null) try {
-			session.close();
-			if (conn!=null) conn.close();
-		} catch (JMSException e) {
-			e.printStackTrace();
-		}
-	}
-	public synchronized void activate() {
-		active = true;
-	}
-	public synchronized void deactivate() {
-		active = false;
-	}
-	private synchronized boolean isActive() {
-		return active;
-	}
-	
-	
+            } catch (JMSException e1) {
+                e1.printStackTrace();
+            }
+        }
+        // not active anymore
+        if (session != null) {
+            try {
+                session.close();
+                if (conn != null) conn.close();
+            } catch (JMSException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public synchronized void activate() {
+        active = true;
+    }
+
+    public synchronized void deactivate() {
+        active = false;
+    }
+
+    private synchronized boolean isActive() {
+        return active;
+    }
+
+
 }
