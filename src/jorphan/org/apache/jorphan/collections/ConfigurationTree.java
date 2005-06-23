7c91355374328ac3df2f1fe4f7c3728628e70e0b
Sebastian Bazley <sebb@apache.org>
2005-06-23 00:10:12 +0000

    Add ASF licence
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@325490 13f79535-47bb-0310-9956-ffa450edef68

--- src/jorphan/org/apache/jorphan/collections/ConfigurationTree.java
+++ src/jorphan/org/apache/jorphan/collections/ConfigurationTree.java
@@ -1,8 +1,23 @@
 /*
+ * Copyright 2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+*/
+
+/*
  * Created on Feb 8, 2005
  *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
  */
 package org.apache.jorphan.collections;
 
@@ -19,8 +34,6 @@ import java.util.Properties;
 /**
  * @author mike
  * 
- * TODO To change the template for this generated type comment go to Window -
- * Preferences - Java - Code Style - Code Templates
  */
 public class ConfigurationTree implements Serializable, Cloneable {
     private static final long serialVersionUID = 1;
