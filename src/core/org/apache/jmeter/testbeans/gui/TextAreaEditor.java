ecce0c8abefba0c469586bece17765fabca7b3ba
Sebastian Bazley <sebb@apache.org>
2005-06-23 00:39:39 +0000

    Add ASF licence
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@325491 13f79535-47bb-0310-9956-ffa450edef68

--- src/core/org/apache/jmeter/testbeans/gui/TextAreaEditor.java
+++ src/core/org/apache/jmeter/testbeans/gui/TextAreaEditor.java
@@ -1,8 +1,22 @@
 /*
+ * Copyright 2004-2005 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *  
+ */
+
+/*
  * Created on May 21, 2004
- *
- * To change the template for this generated file go to
- * Window - Preferences - Java - Code Generation - Code and Comments
  */
 package org.apache.jmeter.testbeans.gui;
 
@@ -17,8 +31,6 @@ import javax.swing.JTextArea;
 /**
  * @author mstover
  *
- * To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Generation - Code and Comments
  */
 public class TextAreaEditor extends PropertyEditorSupport implements
       FocusListener
