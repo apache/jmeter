d28fcaeb6aba863a88e9b71eeefa9a049591473b
Sebastian Bazley <sebb@apache.org>
2005-11-03 00:54:25 +0000

    Allow serialisation to be checked
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/branches/rel-2-1@330398 13f79535-47bb-0310-9956-ffa450edef68

--- src/components/org/apache/jmeter/visualizers/StatVisualizer.java
+++ src/components/org/apache/jmeter/visualizers/StatVisualizer.java
@@ -154,86 +154,86 @@ public class StatVisualizer extends AbstractVisualizer implements Clearable {
  * @version $Revision$
  */
 
-class SortFilterModel extends AbstractTableModel {
-	private TableModel model;
-
-	private int sortColumn;
-
-	private Row[] rows;
-
-	public SortFilterModel(TableModel m) {
-		model = m;
-		rows = new Row[model.getRowCount()];
-		for (int i = 0; i < rows.length; i++) {
-			rows[i] = new Row();
-			rows[i].index = i;
-		}
-	}
-
-	public SortFilterModel() {
-	}
-
-	public void setValueAt(Object aValue, int r, int c) {
-		model.setValueAt(aValue, rows[r].index, c);
-	}
-
-	public Object getValueAt(int r, int c) {
-		return model.getValueAt(rows[r].index, c);
-	}
-
-	public boolean isCellEditable(int r, int c) {
-		return model.isCellEditable(rows[r].index, c);
-	}
-
-	public int getRowCount() {
-		return model.getRowCount();
-	}
-
-	public int getColumnCount() {
-		return model.getColumnCount();
-	}
-
-	public String getColumnName(int c) {
-		return model.getColumnName(c);
-	}
-
-	public Class getColumnClass(int c) {
-		return model.getColumnClass(c);
-	}
-
-	public void sort(int c) {
-		sortColumn = c;
-		Arrays.sort(rows);
-		fireTableDataChanged();
-	}
-
-	public void addMouseListener(final JTable table) {
-		table.getTableHeader().addMouseListener(new MouseAdapter() {
-			public void mouseClicked(MouseEvent event) {
-				if (event.getClickCount() < 2) {
-					return;
-				}
-				int tableColumn = table.columnAtPoint(event.getPoint());
-				int modelColumn = table.convertColumnIndexToModel(tableColumn);
-
-				sort(modelColumn);
-			}
-		});
-	}
-
-	private class Row implements Comparable {
-		public int index;
-
-		public int compareTo(Object other) {
-			Row otherRow = (Row) other;
-			Object a = model.getValueAt(index, sortColumn);
-			Object b = model.getValueAt(otherRow.index, sortColumn);
-
-			if (a instanceof Comparable) {
-				return ((Comparable) a).compareTo(b);
-			} else {
-				return index - otherRow.index;
-			}
-		}
-	}
-} // class SortFilterModel
+//class SortFilterModel extends AbstractTableModel {
+//	private TableModel model;
+//
+//	private int sortColumn;
+//
+//	private Row[] rows;
+//
+//	public SortFilterModel(TableModel m) {
+//		model = m;
+//		rows = new Row[model.getRowCount()];
+//		for (int i = 0; i < rows.length; i++) {
+//			rows[i] = new Row();
+//			rows[i].index = i;
+//		}
+//	}
+//
+//	public SortFilterModel() {
+//	}
+//
+//	public void setValueAt(Object aValue, int r, int c) {
+//		model.setValueAt(aValue, rows[r].index, c);
+//	}
+//
+//	public Object getValueAt(int r, int c) {
+//		return model.getValueAt(rows[r].index, c);
+//	}
+//
+//	public boolean isCellEditable(int r, int c) {
+//		return model.isCellEditable(rows[r].index, c);
+//	}
+//
+//	public int getRowCount() {
+//		return model.getRowCount();
+//	}
+//
+//	public int getColumnCount() {
+//		return model.getColumnCount();
+//	}
+//
+//	public String getColumnName(int c) {
+//		return model.getColumnName(c);
+//	}
+//
+//	public Class getColumnClass(int c) {
+//		return model.getColumnClass(c);
+//	}
+//
+//	public void sort(int c) {
+//		sortColumn = c;
+//		Arrays.sort(rows);
+//		fireTableDataChanged();
+//	}
+//
+//	public void addMouseListener(final JTable table) {
+//		table.getTableHeader().addMouseListener(new MouseAdapter() {
+//			public void mouseClicked(MouseEvent event) {
+//				if (event.getClickCount() < 2) {
+//					return;
+//				}
+//				int tableColumn = table.columnAtPoint(event.getPoint());
+//				int modelColumn = table.convertColumnIndexToModel(tableColumn);
+//
+//				sort(modelColumn);
+//			}
+//		});
+//	}
+//
+//	private class Row implements Comparable {
+//		public int index;
+//
+//		public int compareTo(Object other) {
+//			Row otherRow = (Row) other;
+//			Object a = model.getValueAt(index, sortColumn);
+//			Object b = model.getValueAt(otherRow.index, sortColumn);
+//
+//			if (a instanceof Comparable) {
+//				return ((Comparable) a).compareTo(b);
+//			} else {
+//				return index - otherRow.index;
+//			}
+//		}
+//	}
+//} // class SortFilterModel
