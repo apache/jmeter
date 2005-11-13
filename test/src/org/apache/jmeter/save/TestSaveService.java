c7a15b339149f1b727a7aa786f29c1564901f21f
Sebastian Bazley <sebb@apache.org>
2005-11-13 20:31:47 +0000

    Add ASF L V 2; remove empty setUp() method
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/branches/rel-2-1@333116 13f79535-47bb-0310-9956-ffa450edef68

--- test/src/org/apache/jmeter/save/TestSaveService.java
+++ test/src/org/apache/jmeter/save/TestSaveService.java
@@ -1,9 +1,20 @@
 /*
- * Created on Jun 19, 2004
+ * Copyright 2005 The Apache Software Foundation.
  *
- * To change the template for this generated file go to
- * Window - Preferences - Java - Code Generation - Code and Comments
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
  */
+
 package org.apache.jmeter.save;
 
 import java.io.ByteArrayInputStream;
@@ -21,8 +32,6 @@ import org.apache.jorphan.collections.HashTree;
 /**
  * @author mstover
  * 
- * To change the template for this generated type comment go to Window -
- * Preferences - Java - Code Generation - Code and Comments
  */
 public class TestSaveService extends JMeterTestCase {
 	private static final String[] FILES = new String[] { "AssertionTestPlan.jmx", "AuthManagerTestPlan.jmx",
@@ -34,9 +43,6 @@ public class TestSaveService extends JMeterTestCase {
 
 	public TestSaveService(String name) {
 		super(name);
-	}
-
-	public void setUp() {
 	}
 
 	public void testLoadAndSave() throws Exception {
