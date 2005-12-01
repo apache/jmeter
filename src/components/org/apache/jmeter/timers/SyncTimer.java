5a3225d76499daa2d95acc84fded98304a82e1fb
Sebastian Bazley <sebb@apache.org>
2005-12-01 01:50:59 +0000

    Bug 37705 - reset counter at end
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/branches/rel-2-1@350097 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: aa5d9a93564c75b6ac4cde1b497d92705ad05719

--- src/components/org/apache/jmeter/timers/SyncTimer.java
+++ src/components/org/apache/jmeter/timers/SyncTimer.java
@@ -34,15 +34,16 @@ import org.apache.log.Logger;
  * 
  */
 public class SyncTimer extends AbstractTestElement implements Timer, Serializable, TestBean {
-	private static final long serialVersionUID = 1;
+	private static final long serialVersionUID = 2;
 
-	static Logger log = LoggingManager.getLoggerForClass();
+	private static final Logger log = LoggingManager.getLoggerForClass();
 
-	int[] timerCounter = new int[] { 0 };
+    // Must be an Object so it will be shared between threads
+	private int[] timerCounter = new int[] { 0 };
 
-	transient Object sync = new Object();
+	private transient Object sync = new Object();
 
-	int groupSize;
+	private int groupSize;
 
 	/**
 	 * @return Returns the numThreads.
@@ -67,23 +68,28 @@ public class SyncTimer extends AbstractTestElement implements Timer, Serializabl
 	public long delay() {
 		synchronized (sync) {
 			timerCounter[0]++;
-			if ((getGroupSize() == 0 && timerCounter[0] >= JMeterContextService.getNumberOfThreads())
-					|| (getGroupSize() > 0 && timerCounter[0] >= getGroupSize())) {
-				timerCounter[0] = 0;
+			final int groupSz = getGroupSize();
+            final int count = timerCounter[0];
+            if (
+                (groupSz == 0 && count >= JMeterContextService.getNumberOfThreads())
+				||
+                (groupSz > 0 && count >= groupSz)
+                ) {
 				sync.notifyAll();
 			} else {
 				try {
 					sync.wait();
 				} catch (InterruptedException e) {
-					e.printStackTrace();
+					log.warn(e.getLocalizedMessage());
 				}
 			}
+            timerCounter[0]=0; // Reset for next time
 		}
 		return 0;
 	}
 
 	/**
-	 * We have to controll the cloning process because we need some cross-thread
+	 * We have to control the cloning process because we need some cross-thread
 	 * communication if our synctimers are to be able to determine when to block
 	 * and when to release.
 	 */
