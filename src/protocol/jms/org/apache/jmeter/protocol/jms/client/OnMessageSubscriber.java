c3a49ed8ec79ce872b606bdd51704acd7d80ab0a
Sebastian Bazley <sebb@apache.org>
2006-03-12 19:00:31 +0000

    Remove unused call
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/branches/rel-2-1@385345 13f79535-47bb-0310-9956-ffa450edef68

--- src/protocol/jms/org/apache/jmeter/protocol/jms/client/OnMessageSubscriber.java
+++ src/protocol/jms/org/apache/jmeter/protocol/jms/client/OnMessageSubscriber.java
@@ -24,7 +24,6 @@ import javax.jms.JMSException;
 import javax.jms.MessageListener;
 import javax.jms.Topic;
 import javax.jms.TopicConnection;
-import javax.jms.TopicConnectionFactory;
 import javax.jms.TopicSession;
 import javax.jms.TopicSubscriber;
 
@@ -117,7 +116,6 @@ public class OnMessageSubscriber {
 	 */
 	public void initConnection(Context ctx, String connfactory, String topic) {
 		try {
-			TopicConnectionFactory connfac = ConnectionFactory.getTopicConnectionFactory(ctx, connfactory);
 			this.CONN = ConnectionFactory.getTopicConnection();
 			this.TOPIC = InitialContextFactory.lookupTopic(ctx, topic);
 			this.SESSION = this.CONN.createTopicSession(false, TopicSession.AUTO_ACKNOWLEDGE);
@@ -151,7 +149,6 @@ public class OnMessageSubscriber {
 			this.SUBSCRIBER = null;
 			this.SESSION = null;
 			this.CONN = null;
-			this.finalize();
 		} catch (JMSException e) {
 			log.error(e.getMessage());
 		} catch (Throwable e) {
