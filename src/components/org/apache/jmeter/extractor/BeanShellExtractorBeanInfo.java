0f394a6892b6fc25d219e327e57f24cf64b97e85
Sebastian Bazley <sebb@apache.org>
2006-03-13 18:59:40 +0000

    Initial stubs for additional BeanShell test elements Note: they do nothing except fail at present
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/branches/rel-2-1@385618 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: f3753d91fd81df7442ec50c447adfc010690f39e

---
+++ src/components/org/apache/jmeter/extractor/BeanShellExtractorBeanInfo.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2006 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *  
+ */
+
+package org.apache.jmeter.extractor;
+
+import java.beans.PropertyDescriptor;
+
+import org.apache.jmeter.testbeans.BeanInfoSupport;
+
+public class BeanShellExtractorBeanInfo extends BeanInfoSupport {
+
+	/**
+	 * @param beanClass
+	 */
+	public BeanShellExtractorBeanInfo() {
+		super(BeanShellExtractor.class);
+
+        createPropertyGroup("scripting", new String[] { "script" });
+
+		PropertyDescriptor p = property("script");
+		p.setValue(NOT_UNDEFINED, Boolean.TRUE);
+		p.setValue(DEFAULT, "");
+	}
+
+}
