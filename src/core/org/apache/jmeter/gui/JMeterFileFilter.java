bf2cf49937c36cfafdf87afe4623f95b35003252
Sebastian Bazley <sebb@apache.org>
2006-04-08 16:26:24 +0000

    Add option to exclude directories from the filter
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/branches/rel-2-1@392552 13f79535-47bb-0310-9956-ffa450edef68

--- src/core/org/apache/jmeter/gui/JMeterFileFilter.java
+++ src/core/org/apache/jmeter/gui/JMeterFileFilter.java
@@ -1,4 +1,3 @@
-// $Header$
 /*
  * Copyright 2001-2004 The Apache Software Foundation.
  *
@@ -25,23 +24,39 @@ import java.util.Arrays;
  * A file filter which allows files to be filtered based on a list of allowed
  * extensions.
  * 
- * @author unknown
- * @version $Revision$
+ * Optionally returns directories.
+ * 
  */
 public class JMeterFileFilter extends javax.swing.filechooser.FileFilter implements java.io.FileFilter {
 	/** The list of extensions allowed by this filter. */
-	private String[] exts;
+	private final String[] exts;
+    
+    private final boolean allowDirs; // Should we allow directories?
 
 	/**
 	 * Create a new JMeter file filter which allows the specified extensions. If
 	 * the array of extensions contains no elements, any file will be allowed.
-	 * 
+     * 
+	 * This constructor will also return all directories
+     *  
 	 * @param extensions
 	 *            non-null array of allowed file extensions
 	 */
 	public JMeterFileFilter(String[] extensions) {
-		exts = extensions;
+        this(extensions,true);
 	}
+
+    /**
+     * Create a new JMeter file filter which allows the specified extensions. If
+     * the array of extensions contains no elements, any file will be allowed.
+     * 
+     * @param extensions non-null array of allowed file extensions
+     * @param allow should directories be returned ?
+     */
+    public JMeterFileFilter(String[] extensions, boolean allow) {
+        exts = extensions;
+        allowDirs = allow;
+    }
 
 	/**
 	 * Determine if the specified file is allowed by this filter. The file will
@@ -55,7 +70,7 @@ public class JMeterFileFilter extends javax.swing.filechooser.FileFilter impleme
 	 * @return true if the file should be allowed, false otherwise
 	 */
 	public boolean accept(File f) {
-		return f.isDirectory() || accept(f.getName().toLowerCase());
+		return (allowDirs && f.isDirectory()) || accept(f.getName().toLowerCase());
 	}
 
 	/**
