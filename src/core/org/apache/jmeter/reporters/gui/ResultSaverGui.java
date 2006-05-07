e8a9568d6f77287fa8f1551196868a2b87a97b14
Sebastian Bazley <sebb@apache.org>
2006-05-07 17:05:03 +0000

    Gui must implement Clearable if the Test Element does
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/branches/rel-2-1@404798 13f79535-47bb-0310-9956-ffa450edef68

--- src/core/org/apache/jmeter/reporters/gui/ResultSaverGui.java
+++ src/core/org/apache/jmeter/reporters/gui/ResultSaverGui.java
@@ -1,6 +1,5 @@
-// $Header$
 /*
- * Copyright 2003-2004 The Apache Software Foundation.
+ * Copyright 2003-2004,2006 The Apache Software Foundation.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,6 +26,7 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 import org.apache.jmeter.reporters.ResultSaver;
+import org.apache.jmeter.samplers.Clearable;
 import org.apache.jmeter.processor.gui.AbstractPostProcessorGui;
 import org.apache.jmeter.testelement.TestElement;
 import org.apache.jmeter.util.JMeterUtils;
@@ -38,7 +38,7 @@ import org.apache.jorphan.util.JOrphanUtils;
  * 
  * @version $Revision$ Last updated: $Date$
  */
-public class ResultSaverGui extends AbstractPostProcessorGui {
+public class ResultSaverGui extends AbstractPostProcessorGui implements Clearable {
 
 	private JTextField filename;
 
