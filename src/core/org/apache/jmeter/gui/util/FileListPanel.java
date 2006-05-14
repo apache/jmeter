fd6e84b694b6a3ecd437bc488e985cd8a9f044ba
Sebastian Bazley <sebb@apache.org>
2006-05-14 22:07:53 +0000

    Document special use of ObjectTableModel
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/branches/rel-2-1@406446 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: e11f126c4cebcc8948fa9ccceee1f5ca6a60e040

--- src/core/org/apache/jmeter/gui/util/FileListPanel.java
+++ src/core/org/apache/jmeter/gui/util/FileListPanel.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2004 The Apache Software Foundation.
+ * Copyright 2002-2004,2006 The Apache Software Foundation.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -186,8 +186,8 @@ public class FileListPanel extends JPanel implements ActionListener {
 	}
 
     protected void initializeTableModel() {
-        tableModel = new ObjectTableModel(new String[] { "Library" }, new Functor[0], // $NON-NLS-1$
-                new Functor[0],
+        tableModel = new ObjectTableModel(new String[] { "Library" },
+                new Functor[0] , new Functor[0] , // i.e. bypass the Functors
                 new Class[] { String.class });
     }
     
