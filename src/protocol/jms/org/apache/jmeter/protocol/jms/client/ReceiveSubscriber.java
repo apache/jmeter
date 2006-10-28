07429e041c940c3dd5e5a80cd9e18975b9ef97c0
Peter Lin <woolfel@apache.org>
2006-10-28 23:56:51 +0000

    fixing a bug with the initConnection. Some JMS providers have issues if getTopicConnectionFactory isn't called first.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/branches/rel-2-2@468789 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 41ea67b50832dec202c1de4cc5ea55e93449ff35

--- src/protocol/jms/org/apache/jmeter/protocol/jms/client/ReceiveSubscriber.java
+++ src/protocol/jms/org/apache/jmeter/protocol/jms/client/ReceiveSubscriber.java
@@ -112,6 +112,7 @@ public class ReceiveSubscriber implements Runnable {
 	 */
 	public void initConnection(Context ctx, String connfactory, String topic) {
 		try {
+			ConnectionFactory.getTopicConnectionFactory(ctx,connfactory);
 			this.CONN = ConnectionFactory.getTopicConnection();
 			this.TOPIC = InitialContextFactory.lookupTopic(ctx, topic);
 			this.SESSION = this.CONN.createTopicSession(false, TopicSession.AUTO_ACKNOWLEDGE);
