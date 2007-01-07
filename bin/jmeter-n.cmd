051877bbf937afa11f10fc291252aaccce949db1
Sebastian Bazley <sebb@apache.org>
2007-01-07 19:19:26 +0000

    Update license (remove copyright)
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/branches/rel-2-2@493813 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 9e7352a7cfbe7b29054a8d65909dca05e61dbd22

--- bin/jmeter-n.cmd
+++ bin/jmeter-n.cmd
@@ -1,11 +1,12 @@
 @echo off
 
 rem   $Id$
-rem   Copyright 2004,2006 The Apache Software Foundation
-rem 
-rem   Licensed under the Apache License, Version 2.0 (the "License");
-rem   you may not use this file except in compliance with the License.
-rem   You may obtain a copy of the License at
+rem   Licensed to the Apache Software Foundation (ASF) under one or more
+rem   contributor license agreements.  See the NOTICE file distributed with
+rem   this work for additional information regarding copyright ownership.
+rem   The ASF licenses this file to You under the Apache License, Version 2.0
+rem   (the "License"); you may not use this file except in compliance with
+rem   the License.  You may obtain a copy of the License at
 rem 
 rem       http://www.apache.org/licenses/LICENSE-2.0
 rem 
