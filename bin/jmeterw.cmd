a5b98e3deaba0f8a73367c5402337ea0b5d1454f
Sebastian Bazley <sebb@apache.org>
2007-02-02 21:12:01 +0000

    Update ASF licence headers
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/branches/rel-2-2@502750 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: feac373224c1ded262beb7b1d26603c6ea956928

--- bin/jmeterw.cmd
+++ bin/jmeterw.cmd
@@ -1,7 +1,6 @@
 @echo off
 rem   Run JMeter using javaw
 
-rem   $Id$
 rem   Licensed to the Apache Software Foundation (ASF) under one or more
 rem   contributor license agreements.  See the NOTICE file distributed with
 rem   this work for additional information regarding copyright ownership.
