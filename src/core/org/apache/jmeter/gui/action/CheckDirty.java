6f6b4d5b25b40446526f95331bf75c5cb1fa494d
Sebastian Bazley <sebb@apache.org>
2007-02-21 23:35:42 +0000

    Not sure why the Map was static
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/branches/rel-2-2@510304 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 60bc343a1c3da4d04f0cb1d890478472bbe40473

--- src/core/org/apache/jmeter/gui/action/CheckDirty.java
+++ src/core/org/apache/jmeter/gui/action/CheckDirty.java
@@ -40,7 +40,7 @@ import org.apache.log.Logger;
 public class CheckDirty extends AbstractAction implements HashTreeTraverser, ActionListener {
 	private static final Logger log = LoggingManager.getLoggerForClass();
 
-	private static Map previousGuiItems;
+	private Map previousGuiItems;
 
 	private boolean checkMode = false;
 
