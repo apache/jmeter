3efebf03265647055e32061bb21a89e37bbd613f
Sebastian Bazley <sebb@apache.org>
2007-03-02 21:16:04 +0000

    Remove all String routines; formatting is now done by the Renderers
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/branches/rel-2-2@513957 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 47807720ece92d867e2b9f36def361906be9da27

--- src/core/org/apache/jmeter/util/Calculator.java
+++ src/core/org/apache/jmeter/util/Calculator.java
@@ -18,8 +18,6 @@
 
 package org.apache.jmeter.util;
 
-import java.text.DecimalFormat;
-
 import org.apache.jmeter.samplers.SampleResult;
 
 /**
@@ -30,12 +28,6 @@ import org.apache.jmeter.samplers.SampleResult;
  * - maximum
  */
 public class Calculator {
-
-    private static DecimalFormat rateFormatter = new DecimalFormat("#.0");
-
-    private static DecimalFormat errorFormatter = new DecimalFormat("#0.00%");
-
-    private static DecimalFormat kbFormatter = new DecimalFormat("#0.00");
 
 	private double sum = 0;
 
@@ -58,7 +50,7 @@ public class Calculator {
     private final String label;
     
     public Calculator() {
-        this.label = "";
+        this("");
     }
 
 	public Calculator(String label) {
@@ -163,22 +155,6 @@ public class Calculator {
     }
 
     /**
-     * Returns a String which represents the percentage of sample errors that
-     * have occurred. ("0.00%" through "100.00%")
-     * 
-     * @return a String which represents the percentage of sample errors that
-     *         have occurred.
-     */
-    public String getErrorPercentageString() {
-        double myErrorPercentage = this.getErrorPercentage();
-        if (myErrorPercentage < 0) {
-            myErrorPercentage = 0.0;
-        }
-
-        return (errorFormatter.format(myErrorPercentage));
-    }
-
-    /**
      * Returns the throughput associated to this sampler in requests per second.
      * May be slightly skewed because it takes the timestamps of the first and
      * last samples as the total time passed, and the test may actually have
@@ -189,41 +165,6 @@ public class Calculator {
             return 0.0;
 
         return ((double) count / (double) elapsedTime ) * 1000;
-    }
-
-    /**
-     * Returns a String that represents the throughput associated for this
-     * sampler, in units appropriate to its dimension:
-     * <p>
-     * The number is represented in requests/second or requests/minute or
-     * requests/hour.
-     * <p>
-     * Examples: "34.2/sec" "0.1/sec" "43.0/hour" "15.9/min"
-     * 
-     * @return a String representation of the rate the samples are being taken
-     *         at.
-     */
-    public String getRateString() {
-        double rate = getRate();
-
-        if (rate == Double.MAX_VALUE) {
-            return "N/A";
-        }
-
-        String unit = "sec";
-
-        if (rate < 1.0) {
-            rate *= 60.0;
-            unit = "min";
-        }
-        if (rate < 1.0) {
-            rate *= 60.0;
-            unit = "hour";
-        }
-
-        String rval = rateFormatter.format(rate) + "/" + unit;
-
-        return (rval);
     }
 
     /**
@@ -240,16 +181,6 @@ public class Calculator {
     }
 
     /**
-     * formats the rate
-     * 
-     * @return
-     */
-    public String getPageSizeString() {
-        double rate = getPageSize() / 1024;
-        return kbFormatter.format(rate);
-    }
-
-    /**
      * Throughput in bytes / second
      * 
      * @return
@@ -262,12 +193,12 @@ public class Calculator {
     }
 
     /**
-     * formats the Page Size
+     * Throughput in kilobytes / second
      * 
      * @return
      */
-    public String getKBPerSecondString() {
-        double rate = getBytesPerSecond() / 1024;
-        return kbFormatter.format(rate);
+    public double getKBPerSecond() {
+        return getBytesPerSecond() / 1024;
     }
+
 }
