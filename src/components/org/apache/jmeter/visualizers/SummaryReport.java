834bab089d505d19b51cb0c6cb245047adb4dce8
Sebastian Bazley <sebb@apache.org>
2007-03-02 21:15:13 +0000

    Use new ObjectTableModel constructor; add testFunctors() - unit test hook Apply local renderers instead of relying on Calculator routines
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/branches/rel-2-2@513956 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: ea8916f47591bf211e0451d39395caf863b8a342

--- src/components/org/apache/jmeter/visualizers/SummaryReport.java
+++ src/components/org/apache/jmeter/visualizers/SummaryReport.java
@@ -30,6 +30,7 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.TableCellRenderer;
 
 import org.apache.jmeter.samplers.Clearable;
 import org.apache.jmeter.samplers.SampleResult;
@@ -37,7 +38,10 @@ import org.apache.jmeter.testelement.TestElement;
 import org.apache.jmeter.util.Calculator;
 import org.apache.jmeter.util.JMeterUtils;
 import org.apache.jmeter.visualizers.gui.AbstractVisualizer;
+import org.apache.jorphan.gui.NumberRenderer;
 import org.apache.jorphan.gui.ObjectTableModel;
+import org.apache.jorphan.gui.RateRenderer;
+import org.apache.jorphan.gui.RendererUtils;
 import org.apache.jorphan.reflect.Functor;
 
 /**
@@ -68,19 +72,34 @@ public class SummaryReport extends AbstractVisualizer implements Clearable {
 
 	Map tableRows = Collections.synchronizedMap(new HashMap());
 
-	public SummaryReport() {
+	// Column renderers
+	private static final TableCellRenderer[] RENDERERS = 
+		new TableCellRenderer[]{
+		    null, // Label
+		    null, // count
+		    null, // Mean
+		    null, // Min
+		    null, // Max
+		    new NumberRenderer("#0.00%"), // Error %age
+		    new RateRenderer("#.0"),      // Throughpur
+		    new NumberRenderer("#0.00"),  // kB/sec
+		    new NumberRenderer("#.0"),    // avg. pageSize
+		};
+
+    public SummaryReport() {
 		super();
-		model = new ObjectTableModel(COLUMNS, 
+		model = new ObjectTableModel(COLUMNS,
+				Calculator.class,// All rows have this class
                 new Functor[] { 
-                    new Functor("getLabel"),   //$NON-NLS-1$
-                    new Functor("getCount"),  //$NON-NLS-1$
-    				new Functor("getMeanAsNumber"),   //$NON-NLS-1$
-                    new Functor("getMin"),  //$NON-NLS-1$
-                    new Functor("getMax"),   //$NON-NLS-1$
-                    new Functor("getErrorPercentageString"),   //$NON-NLS-1$
-                    new Functor("getRateString"),  //$NON-NLS-1$
-    				new Functor("getKBPerSecondString"),   //$NON-NLS-1$
-                    new Functor("getPageSizeString"),   //$NON-NLS-1$
+                    new Functor("getLabel"),              //$NON-NLS-1$
+                    new Functor("getCount"),              //$NON-NLS-1$
+    				new Functor("getMeanAsNumber"),       //$NON-NLS-1$
+                    new Functor("getMin"),                //$NON-NLS-1$
+                    new Functor("getMax"),                //$NON-NLS-1$
+                    new Functor("getErrorPercentage"),    //$NON-NLS-1$
+                    new Functor("getRate"),               //$NON-NLS-1$
+    				new Functor("getKBPerSecond"),        //$NON-NLS-1$
+                    new Functor("getPageSize"),           //$NON-NLS-1$
                 },
                 new Functor[] { null, null, null, null, null, null, null, null , null }, 
                 new Class[] { String.class, Long.class, Long.class, Long.class,
@@ -88,7 +107,12 @@ public class SummaryReport extends AbstractVisualizer implements Clearable {
 		clear();
 		init();
 	}
-
+    
+	public static boolean testFunctors(){
+		SummaryReport instance = new SummaryReport();
+		return instance.model.checkFunctors(null);
+	}
+	
 	public String getLabelResource() {
 		return "summary_report";  //$NON-NLS-1$
 	}
@@ -145,6 +169,7 @@ public class SummaryReport extends AbstractVisualizer implements Clearable {
 
 		myJTable = new JTable(model);
 		myJTable.setPreferredScrollableViewportSize(new Dimension(500, 70));
+		RendererUtils.applyRenderers(myJTable, RENDERERS);
 		myScrollPane = new JScrollPane(myJTable);
 		this.add(mainPanel, BorderLayout.NORTH);
 		this.add(myScrollPane, BorderLayout.CENTER);
