5eb64669174c841ac6bac8efbc05696a5b532bc0
Sebastian Bazley <sebb@apache.org>
2007-03-04 02:17:52 +0000

    Remove deprecated JMeterUtils.split()
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/branches/rel-2-2@514330 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 4b9534dd369ee48b650bfad4bddff123b45578bd

--- test/src/org/apache/jmeter/util/TestJMeterUtils.java
+++ test/src/org/apache/jmeter/util/TestJMeterUtils.java
@@ -33,51 +33,5 @@ public class TestJMeterUtils extends TestCase {
     public TestJMeterUtils(String arg0) {
         super(arg0);
     }
-    
-
-        public void testSplit1() {
-            String in = "a,bc,,"; // Test ignore trailing split characters
-            String out[] = JMeterUtils.split(in, ",","?");// with default
-            assertEquals(3, out.length);
-            assertEquals("a", out[0]);
-            assertEquals("bc", out[1]);
-            assertEquals("?", out[2]);
-        }
-
-        public void testSplit1a() {
-            String in = "a,bc,,"; // Test ignore trailing split characters
-            String out[] = JMeterUtils.split(in, ",",""); // with no default
-            assertEquals(3, out.length);
-            assertEquals("a", out[0]);
-            assertEquals("bc", out[1]);
-            assertEquals("", out[2]);
-        }
-
-        public void testSplit2() {
-            String in = ",,a,bc"; // Test leading split characters with default
-            String out[] = JMeterUtils.split(in, ",","?");
-            assertEquals(3, out.length);
-            assertEquals("?", out[0]);
-            assertEquals("a", out[1]);
-            assertEquals("bc", out[2]);
-        }
-        
-        public void testSplit3() {
-            String in = ",,a,bc"; // Test leading split characters no default
-            String out[] = JMeterUtils.split(in, ",","");
-            assertEquals(3, out.length);
-            assertEquals("", out[0]);
-            assertEquals("a", out[1]);
-            assertEquals("bc", out[2]);
-        }        
-
-        public void testSplit4() {
-            String in = ",,,a,bc"; // Test leading split characters no default
-            String out[] = JMeterUtils.split(in, ",","");
-            assertEquals(4, out.length);
-            assertEquals("", out[0]);
-            assertEquals("", out[1]);
-            assertEquals("a", out[2]);
-            assertEquals("bc", out[3]);
-        }        
+    //TODO add some tests now that split() has been removed
 }
