205fa4fa7b4341936529fd34d6aa797b7fb62cd3
Sebastian Bazley <sebb@apache.org>
2007-03-15 17:08:20 +0000

    Bug 41259 - Comment field added to all test elements
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/branches/rel-2-2@518693 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 5d42fbc4c0c42c37f6f58c7c78e101b9ed95a0ae

---
+++ src/core/org/apache/jmeter/gui/CommentPanel.java
@@ -0,0 +1,69 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+
+package org.apache.jmeter.gui;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+import org.apache.jmeter.gui.util.VerticalPanel;
+import org.apache.jmeter.util.JMeterUtils;
+
+/**
+ * Generic comment panel for Test Elements
+ *
+ */
+public class CommentPanel extends JPanel {
+	/** A text field containing the comment. */
+	private JTextArea commentField;
+
+	/**
+	 * Create a new NamePanel with the default name.
+	 */
+	public CommentPanel() {
+		init();
+	}
+
+	/**
+	 * Initialize the GUI components and layout.
+	 */
+	private void init() {
+		setLayout(new BorderLayout(5, 0));
+
+		commentField = new JTextArea();
+		JLabel commentLabel = new JLabel(JMeterUtils.getResString("testplan_comments"));
+		commentLabel.setLabelFor(commentField);
+
+		VerticalPanel commentPanel = new VerticalPanel();
+
+		commentPanel.add(commentLabel);
+		commentPanel.add(commentField);
+		add(commentPanel);
+	}
+
+	public void setText(String comment) {
+		this.commentField.setText(comment);
+	}
+
+	public String getText() {
+		return this.commentField.getText();
+	}
+}
