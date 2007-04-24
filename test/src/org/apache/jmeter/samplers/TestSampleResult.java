87f59db972ba7d44263166b1c15444797974b40f
Sebastian Bazley <sebb@apache.org>
2007-04-24 21:35:53 +0000

    Allow some margin of error
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/branches/rel-2-2@532089 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: a933487bee3d9000f8ee0f2f672a3ac29d3ab8b8

--- test/src/org/apache/jmeter/samplers/TestSampleResult.java
+++ test/src/org/apache/jmeter/samplers/TestSampleResult.java
@@ -163,7 +163,7 @@ public class TestSampleResult extends TestCase {
             calculator.addSample(resWithSubResults);
             assertEquals(600, calculator.getTotalBytes());
             assertEquals(1, calculator.getCount());
-            assertEquals(1d / (totalTime / 1000d), calculator.getRate(),0d);
+            assertEquals(1d / (totalTime / 1000d), calculator.getRate(),0.0001d); // Allow for some margin of error
             // Check that the throughput uses the time elapsed for the sub results
             assertFalse(1d / (sampleWithSubResultsTime / 1000d) <= calculator.getRate());
         }
