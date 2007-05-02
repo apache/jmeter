a933082feb51822a9c1deb65ab52125f982ef827
Sebastian Bazley <sebb@apache.org>
2007-05-02 23:32:35 +0000

    Bug 42267 - Concurrent GUI update failure in Proxy Recording
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/branches/rel-2-2@534655 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 9717ae2de9755bf16c1762ab58dc23b53a3ab632

--- src/protocol/http/org/apache/jmeter/protocol/http/proxy/ProxyControl.java
+++ src/protocol/http/org/apache/jmeter/protocol/http/proxy/ProxyControl.java
@@ -366,7 +366,7 @@ public class ProxyControl extends GenericController {
 	 * test tree. param serverResponse to be added to allow saving of the
 	 * server's response while recording. A future consideration.
 	 */
-	public void deliverSampler(HTTPSamplerBase sampler, TestElement[] subConfigs, SampleResult result) {
+	public synchronized void deliverSampler(HTTPSamplerBase sampler, TestElement[] subConfigs, SampleResult result) {
 		if (filterContentType(result) && filterUrl(sampler)) {
 			JMeterTreeNode myTarget = findTargetControllerNode();
 			Collection defaultConfigurations = findApplicableElements(myTarget, ConfigTestElement.class, false);
