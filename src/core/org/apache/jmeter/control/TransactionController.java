cf1c0dc659d92dd463f89e198130ee22ff5c3f60
Sebastian Bazley <sebb@apache.org>
2007-05-26 10:16:18 +0000

    Bug 41913 - TransactionController now creates samples as sub-samples of the transaction
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/branches/rel-2-2@541883 13f79535-47bb-0310-9956-ffa450edef68

--- src/core/org/apache/jmeter/control/TransactionController.java
+++ src/core/org/apache/jmeter/control/TransactionController.java
@@ -20,15 +20,7 @@ package org.apache.jmeter.control;
 
 import java.io.Serializable;
 
-import org.apache.jmeter.samplers.SampleEvent;
-import org.apache.jmeter.samplers.SampleListener;
-import org.apache.jmeter.samplers.SampleResult;
 import org.apache.jmeter.samplers.Sampler;
-import org.apache.jmeter.threads.JMeterContext;
-import org.apache.jmeter.threads.JMeterThread;
-import org.apache.jmeter.threads.JMeterVariables;
-import org.apache.jmeter.threads.ListenerNotifier;
-import org.apache.jmeter.threads.SamplePackage;
 import org.apache.jorphan.logging.LoggingManager;
 import org.apache.log.Logger;
 
@@ -36,108 +28,37 @@ import org.apache.log.Logger;
  * Transaction Controller to measure transaction times
  * 
  */
-public class TransactionController extends GenericController implements SampleListener, Controller, Serializable {
-	protected static final Logger log = LoggingManager.getLoggerForClass();
+public class TransactionController extends GenericController implements Controller, Serializable {
+	private static final Logger log = LoggingManager.getLoggerForClass();
 
-	transient private String threadName;
+    transient private TransactionSampler transactionSampler;
 
-	transient private ListenerNotifier lnf;
-
-	transient private SampleResult res;
-    
-    transient private int calls;
-
-    transient private int noFailingSamples;
-
-	/**
-	 * Creates a Transaction Controller
-	 */
-	public TransactionController() {
-		threadName = Thread.currentThread().getName();
-		lnf = new ListenerNotifier();
-	}
-
-    private Object readResolve(){
-        threadName = Thread.currentThread().getName();
-        lnf = new ListenerNotifier();
-        return this;
-    }
-
-    private void log_debug(String s) {
-		String n = this.getName();
-		log.debug(threadName + " " + n + " " + s);
-	}
-    
 	/**
 	 * @see org.apache.jmeter.control.Controller#next()
 	 */
 	public Sampler next() {
+        // Check if transaction is done
+        if(transactionSampler != null && transactionSampler.isTransactionDone()) {
+            log.debug("End of transaction");
+            // This transaction is done
+            transactionSampler = null;
+            return null;
+        }
+        
+        // Check if it is the start of a new transaction
 		if (isFirst()) // must be the start of the subtree
 		{
-			log_debug("+++++++++++++++++++++++++++++");
-			calls = 0;
-            noFailingSamples = 0;
-			res = new SampleResult();
-            res.setSampleLabel(getName());
-            // Assume success
-            res.setSuccessful(true);
-			res.sampleStart();
+		    log.debug("Start of transaction");
+		    transactionSampler = new TransactionSampler(this, getName());
 		}
 
-        Sampler returnValue = super.next();
-        
-		if (returnValue == null) // Must be the end of the controller
-		{
-			log_debug("-----------------------------" + calls);
-			if (res == null) {
-				log_debug("already called");
-			} else {
-				res.sampleEnd();
-                res.setResponseMessage("Number of samples in transaction : " + calls + ", number of failing samples : " + noFailingSamples);
-                if(res.isSuccessful()) {
-                    res.setResponseCodeOK();
-                }
-
-				// TODO could these be done earlier (or just once?)
-                JMeterContext threadContext = getThreadContext();
-                JMeterVariables threadVars = threadContext.getVariables();
-
-				SamplePackage pack = (SamplePackage) threadVars.getObject(JMeterThread.PACKAGE_OBJECT);
-				if (pack == null) {
-					log.warn("Could not fetch SamplePackage");
-				} else {
-                    SampleEvent event = new SampleEvent(res, getName());
-                    // We must set res to null now, before sending the event for the transaction,
-                    // so that we can ignore that event in our sampleOccured method 
-                    res = null;
-					lnf.notifyListeners(event, pack.getSampleListeners());
-				}
-			}
-		}
-        else {
-            // We have sampled one of our children
-            calls++;            
+        // Sample the children of the transaction
+		Sampler subSampler = super.next();
+        transactionSampler.setSubSampler(subSampler);
+        // If we do not get any sub samplers, the transaction is done
+        if (subSampler == null) {
+            transactionSampler.setTransactionDone();
         }
-
-		return returnValue;
+        return transactionSampler;
 	}
-    
-    public void sampleOccurred(SampleEvent se) {
-        // Check if we have are still sampling our children
-        if(res != null) {
-            SampleResult sampleResult = se.getResult(); 
-            res.setThreadName(sampleResult.getThreadName());
-            res.setBytes(res.getBytes() + sampleResult.getBytes());
-            if(!sampleResult.isSuccessful()) {
-                res.setSuccessful(false);
-                noFailingSamples++;
-            }
-       }
-    }
-
-    public void sampleStarted(SampleEvent e) {
-    }
-
-    public void sampleStopped(SampleEvent e) {
-    }
 }
