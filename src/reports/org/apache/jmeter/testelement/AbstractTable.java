ac303cc8e1d9020aa99a2a4c8b19ad61d994933b
Sebastian Bazley <sebb@apache.org>
2007-06-05 23:00:36 +0000

    Remove unnecessary override
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/branches/rel-2-2@544670 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 5bce33d0026091acfc5627d6206263f811157321

--- src/reports/org/apache/jmeter/testelement/AbstractTable.java
+++ src/reports/org/apache/jmeter/testelement/AbstractTable.java
@@ -1,4 +1,3 @@
-//$Header$
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -59,10 +58,6 @@ public abstract class AbstractTable extends AbstractTestElement
 		super();
 	}
 
-    public void clear() {
-        super.clear();
-    }
-    
     public boolean getMean() {
     	return getPropertyAsBoolean(REPORT_TABLE_MEAN);
     }
