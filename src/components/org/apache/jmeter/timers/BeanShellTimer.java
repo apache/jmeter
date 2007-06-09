f82d9ec6fdbdc984f29bd444608679221a6a2005
Sebastian Bazley <sebb@apache.org>
2007-06-09 21:51:13 +0000

    Add TestListener and ThreadListener support to BeanShell test elements
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/branches/rel-2-2@545799 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 92cd12785db462ac7aab58e93cfe28a1e78297bc

--- src/components/org/apache/jmeter/timers/BeanShellTimer.java
+++ src/components/org/apache/jmeter/timers/BeanShellTimer.java
@@ -20,8 +20,11 @@ package org.apache.jmeter.timers;
 
 import java.io.Serializable;
 
+import org.apache.jmeter.engine.event.LoopIterationEvent;
 import org.apache.jmeter.testbeans.TestBean;
 import org.apache.jmeter.testelement.AbstractTestElement;
+import org.apache.jmeter.testelement.TestListener;
+import org.apache.jmeter.testelement.ThreadListener;
 import org.apache.jmeter.threads.JMeterContext;
 import org.apache.jmeter.threads.JMeterContextService;
 import org.apache.jmeter.threads.JMeterVariables;
@@ -31,7 +34,7 @@ import org.apache.jorphan.logging.LoggingManager;
 import org.apache.jorphan.util.JMeterException;
 import org.apache.log.Logger;
 
-public class BeanShellTimer extends AbstractTestElement implements Timer, Serializable, TestBean {
+public class BeanShellTimer extends AbstractTestElement implements Timer, Serializable, TestBean, ThreadListener, TestListener {
     private static final Logger log = LoggingManager.getLoggerForClass();
     
     private static final long serialVersionUID = 2;
@@ -104,4 +107,68 @@ public class BeanShellTimer extends AbstractTestElement implements Timer, Serial
     public void setScript(String s){
         script=s;
     }
+
+	public void threadStarted() {
+		if (bshInterpreter == null) return;
+		try {
+			bshInterpreter.eval("threadStarted()"); // $NON-NLS-1$
+		} catch (JMeterException ignored) {
+			log.debug(ignored.getLocalizedMessage());
+		}
+	}
+
+	public void threadFinished() {
+		if (bshInterpreter == null) return;
+		try {
+			bshInterpreter.eval("threadFinished()"); // $NON-NLS-1$
+		} catch (JMeterException ignored) {
+			log.debug(ignored.getLocalizedMessage());
+		}		
+	}
+
+	public void testEnded() {
+		if (bshInterpreter == null) return;
+		try {
+			bshInterpreter.eval("testEnded()"); // $NON-NLS-1$
+		} catch (JMeterException ignored) {
+			log.debug(ignored.getLocalizedMessage());
+		}		
+	}
+
+	public void testEnded(String host) {
+		if (bshInterpreter == null) return;
+		try {
+			bshInterpreter.eval((new StringBuffer("testEnded(")) // $NON-NLS-1$
+					.append(host)
+					.append(")") // $NON-NLS-1$
+					.toString()); // $NON-NLS-1$
+		} catch (JMeterException ignored) {
+			log.debug(ignored.getLocalizedMessage());
+		}		
+	}
+
+	public void testIterationStart(LoopIterationEvent event) {
+		// Not implemented
+	}
+
+	public void testStarted() {
+		if (bshInterpreter == null) return;
+		try {
+			bshInterpreter.eval("testStarted()"); // $NON-NLS-1$
+		} catch (JMeterException ignored) {
+			log.debug(ignored.getLocalizedMessage());
+		}		
+	}
+
+	public void testStarted(String host) {
+		if (bshInterpreter == null) return;
+		try {
+			bshInterpreter.eval((new StringBuffer("testStarted(")) // $NON-NLS-1$
+					.append(host)
+					.append(")") // $NON-NLS-1$
+					.toString()); // $NON-NLS-1$
+		} catch (JMeterException ignored) {
+			log.debug(ignored.getLocalizedMessage());
+		}		
+	}
 }
