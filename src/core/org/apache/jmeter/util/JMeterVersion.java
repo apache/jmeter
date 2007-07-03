bb59487b77831b1c2e27337b6a994a06823da649
Sebastian Bazley <sebb@apache.org>
2007-07-03 17:08:04 +0000

    Prepare for release
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/branches/rel-2-2@552917 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: cda1288c678f702b6d4b225599b5def573e6fce1

--- src/core/org/apache/jmeter/util/JMeterVersion.java
+++ src/core/org/apache/jmeter/util/JMeterVersion.java
@@ -42,7 +42,7 @@ public class JMeterVersion {
 	 * JMeterUtils This ensures that JMeterUtils always gets the correct
 	 * version, even if it is not re-compiled during the build.
 	 */
-	private static final String VERSION = "2.2.1";
+	private static final String VERSION = "2.3";
 
 	static final String COPYRIGHT = "Copyright (c) 1998-2007 The Apache Software Foundation";
 
