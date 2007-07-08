9efb5971c67b7bfe5777a963337d17276f99438f
Sebastian Bazley <sebb@apache.org>
2007-07-08 20:46:03 +0000

    Re-work Transaction Controller so original behaviour is the default
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/branches/rel-2-2@554438 13f79535-47bb-0310-9956-ffa450edef68

--- src/core/org/apache/jmeter/control/TransactionController.java
+++ src/core/org/apache/jmeter/control/TransactionController.java
@@ -20,23 +20,76 @@ package org.apache.jmeter.control;
 
 import java.io.Serializable;
 
+import org.apache.jmeter.samplers.SampleEvent;
+import org.apache.jmeter.samplers.SampleListener;
+import org.apache.jmeter.samplers.SampleResult;
 import org.apache.jmeter.samplers.Sampler;
+import org.apache.jmeter.testelement.property.BooleanProperty;
+import org.apache.jmeter.threads.JMeterContext;
+import org.apache.jmeter.threads.JMeterThread;
+import org.apache.jmeter.threads.JMeterVariables;
+import org.apache.jmeter.threads.ListenerNotifier;
+import org.apache.jmeter.threads.SamplePackage;
 import org.apache.jorphan.logging.LoggingManager;
 import org.apache.log.Logger;
 
 /**
  * Transaction Controller to measure transaction times
  * 
+ * There are two different modes for the controller:
+ * - generate additional total sample after nested samples (as in JMeter 2.2)
+ * - generate parent sampler containing the nested samples
+ * 
  */
-public class TransactionController extends GenericController implements Controller, Serializable {
+public class TransactionController extends GenericController implements SampleListener, Controller, Serializable {
 	private static final Logger log = LoggingManager.getLoggerForClass();
 
     transient private TransactionSampler transactionSampler;
 
+	transient private ListenerNotifier lnf;
+
+	transient private SampleResult res;
+    
+    transient private int calls;
+
+    transient private int noFailingSamples;
+    
+	private static final String PARENT = "TransactionController.parent";// $NON-NLS-1$
+
+	/**
+	 * Creates a Transaction Controller
+	 */
+	public TransactionController() {
+		lnf = new ListenerNotifier();
+	}
+
+    private Object readResolve(){
+        lnf = new ListenerNotifier();
+        return this;
+    }
+
+	public void setParent(boolean _parent){
+		setProperty(new BooleanProperty(PARENT, _parent));
+	}
+
+	public boolean isParent(){
+		return getPropertyAsBoolean(PARENT);
+	}
+
+	public Sampler next(){
+		if (isParent()){
+			return next1();
+		} else {
+			return next2();
+		}
+	}
+	
+///////////////// Transaction Controller - parent ////////////////
+
 	/**
 	 * @see org.apache.jmeter.control.Controller#next()
 	 */
-	public Sampler next() {
+	public Sampler next1() {
         // Check if transaction is done
         if(transactionSampler != null && transactionSampler.isTransactionDone()) {
         	if (log.isDebugEnabled()) {
@@ -67,6 +120,9 @@ public class TransactionController extends GenericController implements Controll
 	}
 	
 	protected Sampler nextIsAController(Controller controller) throws NextIsNullException {
+		if (!isParent()) {
+			return super.nextIsAController(controller);
+		}
 		Sampler returnValue;
 		Sampler sampler = controller.next();
 		if (sampler == null) {
@@ -80,4 +136,74 @@ public class TransactionController extends GenericController implements Controll
 		}
 		return returnValue;
 	}
+
+////////////////////// Transaction Controller - additional sample //////////////////////////////
+
+	public Sampler next2() {
+		if (isFirst()) // must be the start of the subtree
+		{
+			calls = 0;
+            noFailingSamples = 0;
+			res = new SampleResult();
+            res.setSampleLabel(getName());
+            // Assume success
+            res.setSuccessful(true);
+			res.sampleStart();
+		}
+
+        Sampler returnValue = super.next();
+        
+		if (returnValue == null) // Must be the end of the controller
+		{
+			if (res != null) {
+				res.sampleEnd();
+                res.setResponseMessage("Number of samples in transaction : " + calls + ", number of failing samples : " + noFailingSamples);
+                if(res.isSuccessful()) {
+                    res.setResponseCodeOK();
+                }
+
+				// TODO could these be done earlier (or just once?)
+                JMeterContext threadContext = getThreadContext();
+                JMeterVariables threadVars = threadContext.getVariables();
+
+				SamplePackage pack = (SamplePackage) threadVars.getObject(JMeterThread.PACKAGE_OBJECT);
+				if (pack == null) {
+					log.warn("Could not fetch SamplePackage");
+				} else {
+                    SampleEvent event = new SampleEvent(res, getName());
+                    // We must set res to null now, before sending the event for the transaction,
+                    // so that we can ignore that event in our sampleOccured method 
+                    res = null;
+					lnf.notifyListeners(event, pack.getSampleListeners());
+				}
+			}
+		}
+        else {
+            // We have sampled one of our children
+            calls++;            
+        }
+
+		return returnValue;
+	}
+    
+    public void sampleOccurred(SampleEvent se) {
+    	if (!isParent()) {
+	        // Check if we are still sampling our children
+	        if(res != null) {
+	            SampleResult sampleResult = se.getResult(); 
+	            res.setThreadName(sampleResult.getThreadName());
+	            res.setBytes(res.getBytes() + sampleResult.getBytes());
+	            if(!sampleResult.isSuccessful()) {
+	                res.setSuccessful(false);
+	                noFailingSamples++;
+	            }
+	        }
+        }
+    }
+
+    public void sampleStarted(SampleEvent e) {
+    }
+
+    public void sampleStopped(SampleEvent e) {
+    }
 }
