4880d01feef2f5e57cd7416d411f7c1cbc4d91a3
Sebastian Bazley <sebb@apache.org>
2007-08-14 10:43:12 +0000

    Allow for null URL in extracted list
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/branches/rel-2-2@565699 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 923924dc78c18042853d6aee7744eabd4c215df5

--- src/protocol/http/org/apache/jmeter/protocol/http/sampler/HTTPSamplerBase.java
+++ src/protocol/http/org/apache/jmeter/protocol/http/sampler/HTTPSamplerBase.java
@@ -1027,23 +1027,27 @@ public abstract class HTTPSamplerBase extends AbstractSampler implements TestLis
 				Object binURL = urls.next();
 				try {
 					URL url = (URL) binURL;
-                    String urlstr = url.toString();
-                    String urlStrEnc=encodeSpaces(urlstr);
-                    if (!urlstr.equals(urlStrEnc)){// There were some spaces in the URL
-                        try {
-                            url = new URL(urlStrEnc);
-                        } catch (MalformedURLException e) {
-                            res.addSubResult(errorResult(new Exception(urlStrEnc + " is not a correct URI"), res));
-                            res.setSuccessful(false);
-                            continue;
-                        }
-                    }
-                    if (pattern != null && !localMatcher.matches(urlStrEnc, pattern)) {
-                    	continue; // we have a pattern and the URL does not match, so skip it
-                    }
-                    HTTPSampleResult binRes = sample(url, GET, false, frameDepth + 1);
-					res.addSubResult(binRes);
-					res.setSuccessful(res.isSuccessful() && binRes.isSuccessful());
+					if (url == null) {
+						log.warn("Null URL detected (should not happen)");
+					} else {
+						String urlstr = url.toString();
+	                    String urlStrEnc=encodeSpaces(urlstr);
+	                    if (!urlstr.equals(urlStrEnc)){// There were some spaces in the URL
+	                        try {
+	                            url = new URL(urlStrEnc);
+	                        } catch (MalformedURLException e) {
+	                            res.addSubResult(errorResult(new Exception(urlStrEnc + " is not a correct URI"), res));
+	                            res.setSuccessful(false);
+	                            continue;
+	                        }
+	                    }
+	                    if (pattern != null && !localMatcher.matches(urlStrEnc, pattern)) {
+	                    	continue; // we have a pattern and the URL does not match, so skip it
+	                    }
+	                    HTTPSampleResult binRes = sample(url, GET, false, frameDepth + 1);
+						res.addSubResult(binRes);
+						res.setSuccessful(res.isSuccessful() && binRes.isSuccessful());
+					}
 				} catch (ClassCastException e) {
 					res.addSubResult(errorResult(new Exception(binURL + " is not a correct URI"), res));
 					res.setSuccessful(false);
