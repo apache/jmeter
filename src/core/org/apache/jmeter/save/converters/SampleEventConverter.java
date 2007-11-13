bc6b330ea5034b6d73c4c84997e051b483abd88a
Sebastian Bazley <sebb@apache.org>
2007-11-13 01:20:20 +0000

    Initial attempt at saving using SampleEvent. Does not work yet; also needs to be enabled in SaveService.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@594385 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/core/org/apache/jmeter/save/converters/SampleEventConverter.java
@@ -0,0 +1,66 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+
+package org.apache.jmeter.save.converters;
+
+import org.apache.jmeter.protocol.http.sampler.HTTPSampleResult;
+import org.apache.jmeter.samplers.SampleEvent;
+import org.apache.jmeter.samplers.SampleResult;
+
+import com.thoughtworks.xstream.converters.Converter;
+import com.thoughtworks.xstream.converters.MarshallingContext;
+import com.thoughtworks.xstream.converters.UnmarshallingContext;
+import com.thoughtworks.xstream.io.HierarchicalStreamReader;
+import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
+
+/**
+ * XStream Converter for the SampleResult class
+ */
+public class SampleEventConverter implements Converter {
+	/**
+	 * Returns the converter version; used to check for possible
+	 * incompatibilities
+	 */
+	public static String getVersion() {
+		return "$Revision$"; //$NON-NLS-1$
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.thoughtworks.xstream.converters.Converter#canConvert(java.lang.Class)
+	 */
+	public boolean canConvert(Class arg0) {
+		return SampleEvent.class.equals(arg0);
+	}
+
+    // TODO save hostname; save sample type (plain or http)
+	public void marshal(Object source, HierarchicalStreamWriter writer,
+			MarshallingContext context) {
+		SampleEvent evt = (SampleEvent) source;
+		Object res = evt.getResult();
+		context.convertAnother(res);
+	}
+
+    // TODO does not work yet; need to determine the sample type
+	public Object unmarshal(HierarchicalStreamReader reader,
+			UnmarshallingContext context) {
+		SampleEvent evt = new SampleEvent();
+		return evt;
+	}
+}
