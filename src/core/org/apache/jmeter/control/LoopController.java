3c4609edbd0389a4d3c5a0786b65c09b740df405
Sebastian Bazley <sebb@apache.org>
2007-12-13 00:09:33 +0000

    Document meaning of forever setting; constructor should not call overridable method
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@603781 13f79535-47bb-0310-9956-ffa450edef68

--- src/core/org/apache/jmeter/control/LoopController.java
+++ src/core/org/apache/jmeter/control/LoopController.java
@@ -27,26 +27,32 @@ import org.apache.jmeter.testelement.property.JMeterProperty;
 import org.apache.jmeter.testelement.property.StringProperty;
 
 /**
- * @author Michael Stover
- * @author Thad Smith
- * @version $Revision$
+ * Class that implements the Loop Controller.
  */
 public class LoopController extends GenericController implements Serializable {
 
 	private final static String LOOPS = "LoopController.loops"; // $NON-NLS-1$
+	
+	private static final long serialVersionUID = 232L;
 
 	/*
-	 * TODO: eliminate this setting?
-	 * Most places assume that forever is the same as loopcount = -1 or < 0,
-	 * so having a separate flag is potentially ambiguous.
-	 * In fact, this already happens with ThreadGroup which can set forever = false and loops = -1
+	 * In spite of the name, this is actually used to determine if the loop controller is repeatable.
+	 * 
+	 * The value is only used in nextIsNull() when the loop end condition has been detected:
+	 * If forever==true, then it calls resetLoopCount(), otherwise it calls setDone(true).
+	 * 
+	 * Loop Controllers always set forever=true, so that they will be executed next time
+	 * the parent invokes them.
+	 * 
+	 * Thread Group sets the value false, so nextIsNull() sets done, and the Thread Group will not be repeated.
+	 * However, it's not clear that a Thread Group could ever be repeated.  
 	 */
 	private final static String CONTINUE_FOREVER = "LoopController.continue_forever"; // $NON-NLS-1$
 
 	private transient int loopCount = 0;
 
 	public LoopController() {
-		setContinueForever(true);
+		setContinueForever_private(true);
 	}
 
 	public void setLoops(int loops) {
@@ -77,6 +83,10 @@ public class LoopController extends GenericController implements Serializable {
 	 *            true if the loop must be reset after ending a run
 	 */
 	public void setContinueForever(boolean forever) {
+		setContinueForever_private(forever);
+	}
+
+	private void setContinueForever_private(boolean forever) {
 		setProperty(new BooleanProperty(CONTINUE_FOREVER, forever));
 	}
 
