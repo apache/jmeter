797aaa9c554523be7fb857df34d6ff5fb44033ec
Sebastian Bazley <sebb@apache.org>
2008-03-30 13:19:15 +0000

    Bug 42204 - add thread group name to Aggregate and Summary reports
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@642732 13f79535-47bb-0310-9956-ffa450edef68

--- src/components/org/apache/jmeter/visualizers/StatVisualizer.java
+++ src/components/org/apache/jmeter/visualizers/StatVisualizer.java
@@ -22,7 +22,6 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -35,6 +34,7 @@ import java.util.Map;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFileChooser;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -93,6 +93,9 @@ public class StatVisualizer extends AbstractVisualizer implements Clearable, Act
     protected JButton saveTable = 
         new JButton(JMeterUtils.getResString("aggregate_graph_save_table"));			//$NON-NLS-1$
     
+    private JCheckBox useGroupName = 
+        new JCheckBox(JMeterUtils.getResString("aggregate_graph_use_group_name"));            //$NON-NLS-1$
+    
 	transient private ObjectTableModel model;
 
 	Map tableRows = Collections.synchronizedMap(new HashMap());
@@ -147,10 +150,11 @@ public class StatVisualizer extends AbstractVisualizer implements Clearable, Act
 
 	public void add(SampleResult res) {
 		SamplingStatCalculator row = null;
+        final String sampleLabel = res.getSampleLabel(useGroupName.isSelected());
 		synchronized (tableRows) {
-			row = (SamplingStatCalculator) tableRows.get(res.getSampleLabel());
+            row = (SamplingStatCalculator) tableRows.get(sampleLabel);
 			if (row == null) {
-				row = new SamplingStatCalculator(res.getSampleLabel());
+				row = new SamplingStatCalculator(sampleLabel);
 				tableRows.put(row.getLabel(), row);
 				model.insertRow(row, model.getRowCount() - 1);
 			}
@@ -194,16 +198,21 @@ public class StatVisualizer extends AbstractVisualizer implements Clearable, Act
 		this.add(mainPanel, BorderLayout.NORTH);
 		this.add(myScrollPane, BorderLayout.CENTER);
 		saveTable.addActionListener(this);
-		this.add(saveTable,BorderLayout.SOUTH);
+        JPanel opts = new JPanel();
+        opts.add(useGroupName, BorderLayout.WEST);
+        opts.add(saveTable, BorderLayout.CENTER);
+        this.add(opts,BorderLayout.SOUTH);
 	}
 
 	public void actionPerformed(ActionEvent ev) {
 		if (ev.getSource() == saveTable) {
 	        JFileChooser chooser = FileDialoger.promptToSaveFile("aggregate.csv");//$NON-NLS-1$
-			File output = chooser.getSelectedFile();
+	        if (chooser == null) {
+	            return;
+	        }
 			FileWriter writer = null;
 			try {
-			    writer = new FileWriter(output);
+			    writer = new FileWriter(chooser.getSelectedFile());
 			    CSVSaveService.saveCSVStats(model,writer);
 			} catch (FileNotFoundException e) {
 			    log.warn(e.getMessage());
