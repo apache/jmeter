876b413ba8cd7ff77bce59170f7fa811aa6bdb7c
Sebastian Bazley <sebb@apache.org>
2008-04-17 00:50:51 +0000

    Start collecting common HTTP/HTML conversion utilities
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@648909 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 34912e6447647b39711a3612ed8c0e74c6171104

---
+++ src/protocol/http/org/apache/jmeter/protocol/http/util/ConversionUtils.java
@@ -0,0 +1,57 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+
+package org.apache.jmeter.protocol.http.util;
+
+import org.apache.jorphan.util.JOrphanUtils;
+
+/**
+ * General purpose conversion utilities related to HTTP/HTML
+ */
+public class ConversionUtils {
+
+    private static final String CHARSET_EQ = "charset="; // $NON-NLS-1$
+    private static final int CHARSET_EQ_LEN = CHARSET_EQ.length();
+
+    /**
+     * Extract the encoding (charset) from the Content-Type,
+     * e.g. "text/html; charset=utf-8".
+     * 
+     * @param contentType
+     * @return the charset encoding
+     */
+    public static String getEncodingFromContentType(String contentType){
+        String charSet = null;
+        if (contentType != null) {
+            int charSetStartPos = contentType.toLowerCase().indexOf(CHARSET_EQ);
+            if (charSetStartPos > 0) {
+                charSet = contentType.substring(charSetStartPos + CHARSET_EQ_LEN);
+                if (charSet != null) {
+                    // Remove quotes from charset name
+                    charSet = JOrphanUtils.replaceAllChars(charSet, '"', "");
+                    charSet = charSet.trim();
+                    if (charSet.length() > 0) {
+                        return charSet;
+                    }
+                    return null;
+                }
+            }
+        }
+        return charSet;
+    }
+}
