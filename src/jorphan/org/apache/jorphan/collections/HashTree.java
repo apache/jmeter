a113b9f30207236de2a3963dce216211a9a940dc
Sebastian Bazley <sebb@apache.org>
2008-10-05 01:35:49 +0000

    Unused import
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@701744 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: a1f10ecb11b0485ead37085142b3babf44e9550e

--- src/jorphan/org/apache/jorphan/collections/HashTree.java
+++ src/jorphan/org/apache/jorphan/collections/HashTree.java
@@ -24,7 +24,6 @@ import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
