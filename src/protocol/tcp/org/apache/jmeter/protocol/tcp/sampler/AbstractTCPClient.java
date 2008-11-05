02873face6080338f2f1660f237b45d088ae9a3e
Sebastian Bazley <sebb@apache.org>
2008-11-05 17:02:41 +0000

    Abstract implementation of TCPClient
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@711621 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/protocol/tcp/org/apache/jmeter/protocol/tcp/sampler/AbstractTCPClient.java
@@ -0,0 +1,51 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+
+package org.apache.jmeter.protocol.tcp.sampler;
+
+/**
+ * Basic implementation of TCPClient interface.
+ */
+public abstract class AbstractTCPClient implements TCPClient {
+
+    /* (non-Javadoc)
+     * @see org.apache.jmeter.protocol.tcp.sampler.TCPClient#getEolByte()
+     */
+    public byte getEolByte() {
+        return 0;
+    }
+
+    /* (non-Javadoc)
+     * @see org.apache.jmeter.protocol.tcp.sampler.TCPClient#setEolByte(byte)
+     */
+    public void setEolByte(byte eolByte) {
+    }
+
+    /* (non-Javadoc)
+     * @see org.apache.jmeter.protocol.tcp.sampler.TCPClient#setupTest()
+     */
+    public void setupTest() {
+    }
+
+    /* (non-Javadoc)
+     * @see org.apache.jmeter.protocol.tcp.sampler.TCPClient#teardownTest()
+     */
+    public void teardownTest() {
+    }
+
+}
