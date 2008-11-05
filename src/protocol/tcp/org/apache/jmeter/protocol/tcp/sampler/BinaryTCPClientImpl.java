a94d0f4e7c188bf74992663e857252fde1275db4
Sebastian Bazley <sebb@apache.org>
2008-11-05 16:18:49 +0000

    Bug 46030 - Extend TCP Sampler to Support Length-Prefixed Binary Data Commit of 3rd party code (ICLA and CCLA have been received)
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@711601 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 38382a2cc9e588164de61d89967de8b1cdbf5196

---
+++ src/protocol/tcp/org/apache/jmeter/protocol/tcp/sampler/BinaryTCPClientImpl.java
@@ -0,0 +1,199 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+/*
+ * TCP Sampler Client implementation which reads and writes binary data.  
+ * 
+ * Input/Output strings are passed as hex-encoded binary strings.
+ *
+ */
+package org.apache.jmeter.protocol.tcp.sampler;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InterruptedIOException;
+import java.io.OutputStream;
+import java.net.SocketTimeoutException;
+
+import org.apache.jmeter.util.JMeterUtils;
+import org.apache.jorphan.logging.LoggingManager;
+import org.apache.jorphan.util.JOrphanUtils;
+import org.apache.log.Logger;
+
+/**
+ * Sample TCPClient implementation
+ * 
+ */
+public class BinaryTCPClientImpl implements TCPClient {
+    private static final Logger log = LoggingManager.getLoggerForClass();
+
+    private int eolInt = JMeterUtils.getPropDefault("tcp.eolByte", 1000); // default
+
+    // is
+    // not
+    // in
+    // range
+
+    private byte eolByte = (byte) eolInt; // -128 to +127
+
+    private boolean eolIgnore = eolInt < -128 || eolInt > 127;
+    
+    public BinaryTCPClientImpl() {
+        super();
+        if (!eolIgnore) {
+            log.info("Using eolByte=" + eolByte);
+        }
+    }
+
+    /**
+     * Convert hex string to binary byte array.
+     * 
+     * @param s - hex-encoded binary string
+     * @return Byte array containing binary representation of input hex-encoded string 
+     */
+    public static final byte[] hexStringToByteArray(String s) {
+        if (s.length() % 2 == 0) {
+            char[] sc = s.toCharArray();
+            byte[] ba = new byte[sc.length / 2];
+
+            for (int i = 0; i < ba.length; i++) {
+                int nibble0 = Character.digit(sc[i * 2], 16);
+                int nibble1 = Character.digit(sc[i * 2 + 1], 16);
+                ba[i] = (byte) ((nibble0 << 4) | (nibble1));
+            }
+
+            return ba;
+        } else {
+            throw new IllegalArgumentException(
+                    "Hex-encoded binary string contains an uneven no. of digits");
+        }
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.apache.jmeter.protocol.tcp.sampler.TCPClient#setupTest()
+     */
+    public void setupTest() {
+        log.info("setuptest");
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.apache.jmeter.protocol.tcp.sampler.TCPClient#teardownTest()
+     */
+    public void teardownTest() {
+        log.info("teardowntest");
+
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.apache.jmeter.protocol.tcp.sampler.TCPClient#write(java.io.OutputStream
+     * , java.lang.String)
+     */
+    public void write(OutputStream os, String s) {
+        try {
+            os.write(hexStringToByteArray(s));
+            os.flush();
+        } catch (IOException e) {
+            log.warn("Write error", e);
+        }
+        log.debug("Wrote: " + s);
+        return;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.apache.jmeter.protocol.tcp.sampler.TCPClient#write(java.io.OutputStream
+     * , java.io.InputStream)
+     */
+    public void write(OutputStream os, InputStream is) {
+        throw new UnsupportedOperationException(
+                "Method not supported for Length-Prefixed data.");
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.apache.jmeter.protocol.tcp.sampler.TCPClient#read(java.io.InputStream
+     * )
+     */
+    public String read(InputStream is) {
+        byte[] buffer = new byte[4096];
+        ByteArrayOutputStream w = new ByteArrayOutputStream();
+        int x = 0;
+        try {
+            while ((x = is.read(buffer)) > -1) {
+                w.write(buffer, 0, x);
+                if (!eolIgnore && (buffer[x - 1] == eolByte)) {
+                    break;
+                }
+            }
+            /*
+             * Timeout is reported as follows: JDK1.3: InterruptedIOException
+             * JDK1.4: SocketTimeoutException, which extends
+             * InterruptedIOException
+             *
+             * So to make the code work on both, just check for
+             * InterruptedIOException
+             *
+             * If 1.3 support is dropped, can change to using
+             * SocketTimeoutException
+             *
+             * For more accurate detection of timeouts under 1.3, one could
+             * perhaps examine the Exception message text...
+             *
+             */
+        } catch (SocketTimeoutException e) {
+            // drop out to handle buffer
+        } catch (InterruptedIOException e) {
+            // drop out to handle buffer
+        } catch (IOException e) {
+            log.warn("Read error:" + e);
+            return "";
+        }
+
+        // do we need to close byte array (or flush it?)
+        log.debug("Read: " + w.size() + "\n" + w.toString());
+        return JOrphanUtils.baToHexString(w.toByteArray());
+    }
+
+    /**
+     * @return Returns the eolByte.
+     */
+    public byte getEolByte() {
+        return eolByte;
+    }
+
+    /**
+     * @param eolByte
+     *            The eolByte to set.
+     */
+    public void setEolByte(byte eolByte) {
+        this.eolByte = eolByte;
+        eolIgnore = false;
+    }
+}
---
+++ src/protocol/tcp/org/apache/jmeter/protocol/tcp/sampler/LengthPrefixedBinaryTCPClientImpl.java
@@ -0,0 +1,177 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+/*
+ * TCP Sampler Client implementation which reads and writes length-prefixed binary data.  
+ * 
+ * Input/Output strings are passed as hex-encoded binary strings.
+ * 
+ * 2-Byte or 4-Byte length prefixes are supported.
+ * 
+ * Length prefix is binary of length specified by property "tcp.length.prefix.length".
+ *
+ */
+package org.apache.jmeter.protocol.tcp.sampler;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InterruptedIOException;
+import java.io.OutputStream;
+import java.net.SocketTimeoutException;
+
+import org.apache.jmeter.util.JMeterUtils;
+import org.apache.jorphan.logging.LoggingManager;
+import org.apache.jorphan.util.JOrphanUtils;
+import org.apache.log.Logger;
+
+/**
+ * Sample TCPClient implementation
+ * 
+ */
+public class LengthPrefixedBinaryTCPClientImpl extends TCPClientDecorator {
+    private static final Logger log = LoggingManager.getLoggerForClass();
+
+    private int eolInt = JMeterUtils.getPropDefault("tcp.eolByte", 1000); // default
+
+    // is
+    // not
+    // in
+    // range
+
+    private byte eolByte = (byte) eolInt; // -128 to +127
+
+    private boolean eolIgnore = eolInt < -128 || eolInt > 127;
+    
+    private int lengthPrefixLen = JMeterUtils.getPropDefault("tcp.length.prefix.length", 2);
+
+    public LengthPrefixedBinaryTCPClientImpl() {
+        super(new BinaryTCPClientImpl());
+        if (!eolIgnore) {
+            log.info("Using eolByte=" + eolByte);
+        }
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.apache.jmeter.protocol.tcp.sampler.TCPClient#setupTest()
+     */
+    public void setupTest() {
+        log.info("setuptest");
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.apache.jmeter.protocol.tcp.sampler.TCPClient#teardownTest()
+     */
+    public void teardownTest() {
+        log.info("teardowntest");
+
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.apache.jmeter.protocol.tcp.sampler.TCPClient#write(java.io.OutputStream
+     * , java.lang.String)
+     */
+    public void write(OutputStream os, String s) {
+        try {
+            os.write(intToByteArray(s.length()/2,lengthPrefixLen));
+            this.tcpClient.write(os, s);
+        } catch (IOException e) {
+            log.warn("Write error", e);
+        }
+        log.debug("Wrote: " + s);
+        return;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.apache.jmeter.protocol.tcp.sampler.TCPClient#write(java.io.OutputStream
+     * , java.io.InputStream)
+     */
+    public void write(OutputStream os, InputStream is) {
+        this.tcpClient.write(os, is);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.apache.jmeter.protocol.tcp.sampler.TCPClient#read(java.io.InputStream
+     * )
+     */
+    public String read(InputStream is) {
+        byte[] msg = null;
+        int msgLen = 0;
+        try {
+            byte[] lengthBuffer = new byte[lengthPrefixLen];
+            if (is.read(lengthBuffer, 0, lengthPrefixLen) == lengthPrefixLen) {
+                msgLen = byteArrayToInt(lengthBuffer);
+                msg = new byte[msgLen];
+                is.read(msg);
+            }
+            /*
+             * Timeout is reported as follows: JDK1.3: InterruptedIOException
+             * JDK1.4: SocketTimeoutException, which extends
+             * InterruptedIOException
+             * 
+             * So to make the code work on both, just check for
+             * InterruptedIOException
+             * 
+             * If 1.3 support is dropped, can change to using
+             * SocketTimeoutException
+             * 
+             * For more accurate detection of timeouts under 1.3, one could
+             * perhaps examine the Exception message text...
+             */
+        } catch (SocketTimeoutException e) {
+            // drop out to handle buffer
+        } catch (InterruptedIOException e) {
+            // drop out to handle buffer
+        } catch (IOException e) {
+            log.warn("Read error:" + e);
+            return JOrphanUtils.baToHexString(msg);
+        }
+
+        // do we need to close byte array (or flush it?)
+        log.debug("Read: " + msgLen + "\n" + msg.toString());
+        return JOrphanUtils.baToHexString(msg);
+    }
+
+    /**
+     * @return Returns the eolByte.
+     */
+    public byte getEolByte() {
+        return eolByte;
+    }
+
+    /**
+     * @param eolByte
+     *            The eolByte to set.
+     */
+    public void setEolByte(byte eolByte) {
+        this.eolByte = eolByte;
+        eolIgnore = false;
+    }
+}
