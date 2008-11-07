76f56a77f631de6af27d1483c1f1211234b96862
Sebastian Bazley <sebb@apache.org>
2008-11-07 02:39:37 +0000

    Unused imports
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@712043 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 92e9dfe56e29afe9f96fdbc08502026bf53980de

--- src/core/org/apache/jmeter/reporters/ResultCollector.java
+++ src/core/org/apache/jmeter/reporters/ResultCollector.java
@@ -35,7 +35,6 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
-import java.util.Set;
 
 import org.apache.avalon.framework.configuration.DefaultConfigurationSerializer;
 import org.apache.jmeter.engine.event.LoopIterationEvent;
