402b382345a72f375e34d1193443b4867095286b
Sebastian Bazley <sebb@apache.org>
2009-04-24 15:47:10 +0000

    Share code
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@768336 13f79535-47bb-0310-9956-ffa450edef68

--- src/reports/org/apache/jmeter/report/gui/action/ReportLookAndFeelCommand.java
+++ src/reports/org/apache/jmeter/report/gui/action/ReportLookAndFeelCommand.java
@@ -18,54 +18,8 @@
 
 package org.apache.jmeter.report.gui.action;
 
-import java.awt.event.ActionEvent;
-import java.util.HashSet;
-import java.util.Set;
+import org.apache.jmeter.gui.action.LookAndFeelCommand;
 
-import javax.swing.SwingUtilities;
-import javax.swing.UIManager;
-
-import org.apache.jmeter.gui.action.Command;
-import org.apache.jmeter.gui.ReportGuiPackage;
-import org.apache.jmeter.util.JMeterUtils;
-
-public class ReportLookAndFeelCommand implements Command {
-
-    private static final Set commands = new HashSet();
-    static {
-        UIManager.LookAndFeelInfo[] lfs = UIManager.getInstalledLookAndFeels();
-        for (int i = 0; i < lfs.length; i++) {
-            commands.add("laf:" + lfs[i].getClassName());
-        }
-
-        try {
-            String defaultUI = JMeterUtils.getPropDefault("jmeter.laf", UIManager
-                    .getCrossPlatformLookAndFeelClassName());
-            UIManager.setLookAndFeel(defaultUI);
-        } catch (Exception e) {
-        }
-    }
-
-    public ReportLookAndFeelCommand() {
-    }
-
-    public void doAction(ActionEvent ev) {
-        try {
-            String className = ev.getActionCommand().substring(4).replace('/', '.');
-            UIManager.setLookAndFeel(className);
-            SwingUtilities.updateComponentTreeUI(ReportGuiPackage.getInstance().getMainFrame());
-        } catch (javax.swing.UnsupportedLookAndFeelException e) {
-            JMeterUtils.reportErrorToUser("Look and Feel unavailable:" + e.toString());
-        } catch (InstantiationException e) {
-            JMeterUtils.reportErrorToUser("Look and Feel unavailable:" + e.toString());
-        } catch (ClassNotFoundException e) {
-            JMeterUtils.reportErrorToUser("Look and Feel unavailable:" + e.toString());
-        } catch (IllegalAccessException e) {
-            JMeterUtils.reportErrorToUser("Look and Feel unavailable:" + e.toString());
-        }
-    }
-
-    public Set getActionNames() {
-        return commands;
-    }
+public class ReportLookAndFeelCommand extends LookAndFeelCommand {
+// same code as for default JMeter class
 }
