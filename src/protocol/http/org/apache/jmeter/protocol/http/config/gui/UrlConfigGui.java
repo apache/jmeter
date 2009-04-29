c3d66593ae289b022c162a619e8a2a00aba50dfb
Sebastian Bazley <sebb@apache.org>
2009-04-29 00:18:16 +0000

    HTTP Samplers now support connection and request timeouts (requires Java 1.5 for Java Http sampler)
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@769598 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 1450ac4de87c2cb7e4d74bc5d1004ef7b1203362

--- src/protocol/http/org/apache/jmeter/protocol/http/config/gui/UrlConfigGui.java
+++ src/protocol/http/org/apache/jmeter/protocol/http/config/gui/UrlConfigGui.java
@@ -34,6 +34,7 @@ import javax.swing.event.ChangeListener;
 
 import org.apache.jmeter.config.Arguments;
 import org.apache.jmeter.config.ConfigTestElement;
+import org.apache.jmeter.gui.util.HorizontalPanel;
 import org.apache.jmeter.gui.util.VerticalPanel;
 import org.apache.jmeter.protocol.http.gui.HTTPArgumentsPanel;
 import org.apache.jmeter.protocol.http.sampler.HTTPSamplerBase;
@@ -48,34 +49,21 @@ import org.apache.jorphan.gui.JLabeledChoice;
 /**
  * Basic URL / HTTP Request configuration:
  * - host and port
+ * - connect and response timeouts
  * - path, method, encoding, parameters
  * - redirects & keepalive
  */
 public class UrlConfigGui extends JPanel implements ChangeListener {
-    protected HTTPArgumentsPanel argsPanel;
-
-    private static final String DOMAIN = "domain"; // $NON-NLS-1$
-
-    private static final String PORT = "port"; // $NON-NLS-1$
-
-    private static final String PROTOCOL = "protocol"; // $NON-NLS-1$
-
-    private static final String CONTENT_ENCODING = "content_encoding"; // $NON-NLS-1$
-
-    private static final String PATH = "path"; // $NON-NLS-1$
-
-    private static final String FOLLOW_REDIRECTS = "follow_redirects"; // $NON-NLS-1$
-
-    private static final String AUTO_REDIRECTS = "auto_redirects"; // $NON-NLS-1$
-
-    private static final String USE_KEEPALIVE = "use_keepalive"; // $NON-NLS-1$
-
-    private static final String USE_MULTIPART_FOR_POST = "use_multipart_for_post"; // $NON-NLS-1$
+    private HTTPArgumentsPanel argsPanel;
 
     private JTextField domain;
 
     private JTextField port;
 
+    private JTextField connectTimeOut;
+    
+    private JTextField responseTimeOut;
+    
     private JTextField protocol;
 
     private JTextField contentEncoding;
@@ -105,12 +93,6 @@ public class UrlConfigGui extends JPanel implements ChangeListener {
         init();
     }
 
-    protected void configureTestElement(TestElement mc) {
-        mc.setName(getName());
-        mc.setProperty(TestElement.GUI_CLASS, this.getClass().getName());
-        mc.setProperty(TestElement.TEST_CLASS, mc.getClass().getName());
-    }
-
     public void clear() {
         domain.setText(""); // $NON-NLS-1$
         if (notConfigOnly){
@@ -122,6 +104,8 @@ public class UrlConfigGui extends JPanel implements ChangeListener {
         }
         path.setText(""); // $NON-NLS-1$
         port.setText(""); // $NON-NLS-1$
+        connectTimeOut.setText(""); // $NON-NLS-1$
+        responseTimeOut.setText(""); // $NON-NLS-1$
         protocol.setText(""); // $NON-NLS-1$
         contentEncoding.setText(""); // $NON-NLS-1$
         argsPanel.clear();
@@ -130,7 +114,9 @@ public class UrlConfigGui extends JPanel implements ChangeListener {
     public TestElement createTestElement() {
         ConfigTestElement element = new ConfigTestElement();
 
-        this.configureTestElement(element);
+        element.setName(this.getName());
+        element.setProperty(TestElement.GUI_CLASS, this.getClass().getName());
+        element.setProperty(TestElement.TEST_CLASS, element.getClass().getName());
         modifyTestElement(element);
         return element;
     }
@@ -147,6 +133,8 @@ public class UrlConfigGui extends JPanel implements ChangeListener {
         element.setProperty(new TestElementProperty(HTTPSamplerBase.ARGUMENTS, args));
         element.setProperty(HTTPSamplerBase.DOMAIN, domain.getText());
         element.setProperty(HTTPSamplerBase.PORT, port.getText());
+        element.setProperty(HTTPSamplerBase.CONNECT_TIMEOUT, connectTimeOut.getText());
+        element.setProperty(HTTPSamplerBase.RESPONSE_TIMEOUT, responseTimeOut.getText());
         element.setProperty(HTTPSamplerBase.PROTOCOL, protocol.getText());
         element.setProperty(HTTPSamplerBase.CONTENT_ENCODING, contentEncoding.getText());
         element.setProperty(HTTPSamplerBase.PATH, path.getText());
@@ -178,6 +166,8 @@ public class UrlConfigGui extends JPanel implements ChangeListener {
         } else {
             port.setText(portString);
         }
+        connectTimeOut.setText(el.getPropertyAsString(HTTPSamplerBase.CONNECT_TIMEOUT));
+        responseTimeOut.setText(el.getPropertyAsString(HTTPSamplerBase.RESPONSE_TIMEOUT));
         protocol.setText(el.getPropertyAsString(HTTPSamplerBase.PROTOCOL));
         contentEncoding.setText(el.getPropertyAsString(HTTPSamplerBase.CONTENT_ENCODING));
         path.setText(el.getPropertyAsString(HTTPSamplerBase.PATH));
@@ -193,35 +183,51 @@ public class UrlConfigGui extends JPanel implements ChangeListener {
     private void init() {// called from ctor, so must not be overridable
         this.setLayout(new BorderLayout());
 
-        // WEB SERVER PANEL
-        VerticalPanel webServerPanel = new VerticalPanel();
-        webServerPanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(),
-                JMeterUtils.getResString("web_server"))); // $NON-NLS-1$
-        final JPanel domainPanel = getDomainPanel();
-        final JPanel portPanel = getPortPanel();
-        domainPanel.add(portPanel,BorderLayout.EAST);
-        webServerPanel.add(domainPanel);
-
-        JPanel northPanel = new JPanel();
-        northPanel.setLayout(new BoxLayout(northPanel, BoxLayout.Y_AXIS));
-        northPanel.add(getProtocolAndMethodPanel());
-        northPanel.add(getPathPanel());
-
         // WEB REQUEST PANEL
         JPanel webRequestPanel = new JPanel();
         webRequestPanel.setLayout(new BorderLayout());
         webRequestPanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(),
                 JMeterUtils.getResString("web_request"))); // $NON-NLS-1$
 
+        JPanel northPanel = new JPanel();
+        northPanel.setLayout(new BoxLayout(northPanel, BoxLayout.Y_AXIS));
+        northPanel.add(getProtocolAndMethodPanel());
+        northPanel.add(getPathPanel());
+
         webRequestPanel.add(northPanel, BorderLayout.NORTH);
         webRequestPanel.add(getParameterPanel(), BorderLayout.CENTER);
 
-        this.add(webServerPanel, BorderLayout.NORTH);
+        this.add(getWebServerTimeoutPanel(), BorderLayout.NORTH);
         this.add(webRequestPanel, BorderLayout.CENTER);
     }
-    protected JPanel getPortPanel() {
-        port = new JTextField(6);
-        port.setName(PORT);
+
+    protected final JPanel getWebServerTimeoutPanel() {
+        // WEB SERVER PANEL
+        JPanel webServerPanel = new HorizontalPanel();
+        webServerPanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(),
+                JMeterUtils.getResString("web_server"))); // $NON-NLS-1$
+        final JPanel domainPanel = getDomainPanel();
+        final JPanel portPanel = getPortPanel();
+        webServerPanel.add(domainPanel);
+        webServerPanel.add(portPanel);
+        
+        JPanel timeOut = new HorizontalPanel();
+        timeOut.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(),
+                JMeterUtils.getResString("web_server_timeout_title"))); // $NON-NLS-1$
+        final JPanel connPanel = getConnectTimeOutPanel();
+        final JPanel reqPanel = getResponseTimeOutPanel();
+        timeOut.add(connPanel);
+        timeOut.add(reqPanel);
+        
+        
+        JPanel webServerTimeoutPanel = new VerticalPanel();
+        webServerTimeoutPanel.add(webServerPanel, BorderLayout.CENTER);
+        webServerTimeoutPanel.add(timeOut, BorderLayout.EAST);
+        return webServerTimeoutPanel;
+    }
+
+    private JPanel getPortPanel() {
+        port = new JTextField(4);
 
         JLabel label = new JLabel(JMeterUtils.getResString("web_server_port")); // $NON-NLS-1$
         label.setLabelFor(port);
@@ -233,9 +239,34 @@ public class UrlConfigGui extends JPanel implements ChangeListener {
         return panel;
     }
 
-    protected JPanel getDomainPanel() {
+    private JPanel getConnectTimeOutPanel() {
+        connectTimeOut = new JTextField(4);
+
+        JLabel label = new JLabel(JMeterUtils.getResString("web_server_timeout_connect")); // $NON-NLS-1$
+        label.setLabelFor(connectTimeOut);
+
+        JPanel panel = new JPanel(new BorderLayout(5, 0));
+        panel.add(label, BorderLayout.WEST);
+        panel.add(connectTimeOut, BorderLayout.CENTER);
+
+        return panel;
+    }
+
+    private JPanel getResponseTimeOutPanel() {
+        responseTimeOut = new JTextField(4);
+
+        JLabel label = new JLabel(JMeterUtils.getResString("web_server_timeout_response")); // $NON-NLS-1$
+        label.setLabelFor(responseTimeOut);
+
+        JPanel panel = new JPanel(new BorderLayout(5, 0));
+        panel.add(label, BorderLayout.WEST);
+        panel.add(responseTimeOut, BorderLayout.CENTER);
+
+        return panel;
+    }
+
+    private JPanel getDomainPanel() {
         domain = new JTextField(20);
-        domain.setName(DOMAIN);
 
         JLabel label = new JLabel(JMeterUtils.getResString("web_server_domain")); // $NON-NLS-1$
         label.setLabelFor(domain);
@@ -255,27 +286,22 @@ public class UrlConfigGui extends JPanel implements ChangeListener {
      */
     protected Component getPathPanel() {
         path = new JTextField(15);
-        path.setName(PATH);
 
         JLabel label = new JLabel(JMeterUtils.getResString("path")); //$NON-NLS-1$
         label.setLabelFor(path);
 
         if (notConfigOnly){
             followRedirects = new JCheckBox(JMeterUtils.getResString("follow_redirects")); // $NON-NLS-1$
-            followRedirects.setName(FOLLOW_REDIRECTS);
             followRedirects.setSelected(false);
 
             autoRedirects = new JCheckBox(JMeterUtils.getResString("follow_redirects_auto")); //$NON-NLS-1$
-            autoRedirects.setName(AUTO_REDIRECTS);
             autoRedirects.addChangeListener(this);
             autoRedirects.setSelected(true);// Default changed in 2.3
 
             useKeepAlive = new JCheckBox(JMeterUtils.getResString("use_keepalive")); // $NON-NLS-1$
-            useKeepAlive.setName(USE_KEEPALIVE);
             useKeepAlive.setSelected(true);
 
             useMultipartForPost = new JCheckBox(JMeterUtils.getResString("use_multipart_for_http_post")); // $NON-NLS-1$
-            useMultipartForPost.setName(USE_MULTIPART_FOR_POST);
             useMultipartForPost.setSelected(false);
         }
 
@@ -303,10 +329,8 @@ public class UrlConfigGui extends JPanel implements ChangeListener {
     protected JPanel getProtocolAndMethodPanel() {
         // PROTOCOL
         protocol = new JTextField(10);
-        protocol.setName(PROTOCOL);
         // CONTENT_ENCODING
         contentEncoding = new JTextField(10);
-        contentEncoding.setName(CONTENT_ENCODING);
 
         JLabel protocolLabel = new JLabel(JMeterUtils.getResString("protocol")); // $NON-NLS-1$
         protocolLabel.setLabelFor(protocol);
