f7f49f51a36dbcc5866263e2e51e8f362873af12
Sebastian Bazley <sebb@apache.org>
2009-05-05 15:27:54 +0000

    Use separate classes for each test
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@771802 13f79535-47bb-0310-9956-ffa450edef68

---
+++ test/src/org/apache/jmeter/assertions/ResponseAssertionTest.java
@@ -0,0 +1,194 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+
+package org.apache.jmeter.assertions;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import junit.framework.TestCase;
+
+import org.apache.jmeter.samplers.SampleResult;
+import org.apache.jmeter.threads.JMeterContext;
+import org.apache.jmeter.threads.JMeterContextService;
+import org.apache.jmeter.threads.JMeterVariables;
+
+public class ResponseAssertionTest  extends TestCase {
+
+    public ResponseAssertionTest() {
+    }
+
+    private JMeterContext jmctx;
+    private ResponseAssertion assertion;
+    private SampleResult sample;
+    private JMeterVariables vars;
+    private AssertionResult result;
+    
+    public void setUp() throws MalformedURLException {
+        jmctx = JMeterContextService.getContext();
+        assertion = new ResponseAssertion();
+        assertion.setThreadContext(jmctx);
+        sample = new SampleResult();
+        vars = new JMeterVariables();
+        jmctx.setVariables(vars);
+        jmctx.setPreviousResult(sample);
+        sample.setResponseData(
+                (
+                "response Data\n" +
+                "line 2\n\nEOF"
+                ).getBytes());
+        sample.setURL(new URL("http://localhost/Sampler/Data/"));
+        sample.setResponseCode("401");
+        sample.setResponseHeaders("X-Header: abcd");
+    }
+
+    public void testResponseAssertionEquals() throws Exception{
+        assertion.unsetNotType();
+        assertion.setToEqualsType();
+        assertion.setTestFieldURL();
+        assertion.addTestString("Sampler Label");
+        assertion.addTestString("Sampler labelx");      
+        result = assertion.getResult(sample);
+        assertFailed();
+
+        assertion.setToNotType();
+        assertion.clearTestStrings();
+        assertion.addTestString("Sampler LabeL");
+        assertion.addTestString("Sampler Labelx");      
+        result = assertion.getResult(sample);
+        assertPassed();
+    }
+    
+    public void testResponseAssertionHeaders() throws Exception{
+        assertion.unsetNotType();
+        assertion.setToEqualsType();
+        assertion.setTestFieldResponseHeaders();
+        assertion.addTestString("X-Header: abcd");
+        assertion.addTestString("X-Header: abcdx");
+        result = assertion.getResult(sample);
+        assertFailed();
+
+        assertion.clearTestStrings();
+        assertion.addTestString("X-Header: abcd");
+        result = assertion.getResult(sample);
+        assertPassed();
+    }
+    
+    public void testResponseAssertionContains() throws Exception{
+        assertion.unsetNotType();
+        assertion.setToContainsType();
+        assertion.setTestFieldURL();
+        assertion.addTestString("Sampler");
+        assertion.addTestString("Label");
+        assertion.addTestString(" x");
+        
+        result = assertion.getResult(sample);
+        assertFailed();
+        
+        assertion.setToNotType();
+        
+        result = assertion.getResult(sample);
+        assertFailed();
+
+        assertion.clearTestStrings();
+        assertion.addTestString("r l");
+        result = assertion.getResult(sample);
+        assertPassed();
+
+        assertion.unsetNotType();
+        assertion.setTestFieldResponseData();
+        
+        assertion.clearTestStrings();
+        assertion.addTestString("line 2");
+        result = assertion.getResult(sample);
+        assertPassed();
+
+        assertion.clearTestStrings();
+        assertion.addTestString("(?s)line \\d+.*EOF");
+        result = assertion.getResult(sample);
+        assertPassed();
+
+        assertion.setTestFieldResponseCode();
+        
+        assertion.clearTestStrings();
+        assertion.addTestString("401");
+        result = assertion.getResult(sample);
+        assertPassed();
+
+    }
+    
+    public void testResponseAssertionSubstring() throws Exception{
+        assertion.unsetNotType();
+        assertion.setToSubstringType();
+        assertion.setTestFieldURL();
+        assertion.addTestString("Sampler");
+        assertion.addTestString("Label");
+        assertion.addTestString("+(");
+        
+        result = assertion.getResult(sample);
+        assertFailed();
+        
+        assertion.setToNotType();
+        
+        result = assertion.getResult(sample);
+        assertFailed();
+
+        assertion.clearTestStrings();
+        assertion.addTestString("r l");
+        result = assertion.getResult(sample);
+        assertPassed();
+
+        assertion.unsetNotType();
+        assertion.setTestFieldResponseData();
+        
+        assertion.clearTestStrings();
+        assertion.addTestString("line 2");
+        result = assertion.getResult(sample);
+        assertPassed();
+
+        assertion.clearTestStrings();
+        assertion.addTestString("line 2\n\nEOF");
+        result = assertion.getResult(sample);
+        assertPassed();
+
+        assertion.setTestFieldResponseCode();
+        
+        assertion.clearTestStrings();
+        assertion.addTestString("401");
+        result = assertion.getResult(sample);
+        assertPassed();
+
+    }
+
+//TODO - need a lot more tests
+    
+    private void assertPassed() throws Exception{
+        assertNull(result.getFailureMessage(),result.getFailureMessage());
+        assertFalse("Not expecting error: "+result.getFailureMessage(),result.isError());
+        assertFalse("Not expecting error",result.isError());
+        assertFalse("Not expecting failure",result.isFailure());        
+    }
+    
+    private void assertFailed() throws Exception{
+        assertNotNull(result.getFailureMessage());
+        assertFalse("Should not be: Response was null","Response was null".equals(result.getFailureMessage()));
+        assertFalse("Not expecting error: "+result.getFailureMessage(),result.isError());
+        assertTrue("Expecting failure",result.isFailure());     
+        
+    }
+}
