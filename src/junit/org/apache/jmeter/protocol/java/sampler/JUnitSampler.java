725d15409c078e22bac060da50fdff8814f35a2d
Sebastian Bazley <sebb@apache.org>
2009-08-14 21:46:02 +0000

    Fix varargs method
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@804389 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 78a5360d43a93f1b822eec91fdd28fe3b627e0c9

--- src/junit/org/apache/jmeter/protocol/java/sampler/JUnitSampler.java
+++ src/junit/org/apache/jmeter/protocol/java/sampler/JUnitSampler.java
@@ -335,7 +335,7 @@ public class JUnitSampler extends AbstractSampler {
 
                 if (!getDoNotSetUpTearDown() && SETUP_METHOD != null){
                     try {
-                        SETUP_METHOD.invoke(this.TEST_INSTANCE,new Class[0]);
+                        SETUP_METHOD.invoke(this.TEST_INSTANCE,new Object[0]);
                     } catch (InvocationTargetException e) {
                         tr.addFailure(this.TEST_INSTANCE,
                                 new AssertionFailedError(e.getMessage()));
@@ -357,7 +357,7 @@ public class JUnitSampler extends AbstractSampler {
                 // will include setup and teardown.
                 Protectable p = new Protectable() {
                     public void protect() throws Throwable {
-                        m.invoke(theClazz,new Class[0]);
+                        m.invoke(theClazz,new Object[0]);
                     }
                 };
                 tr.runProtected(theClazz, p);
@@ -365,7 +365,7 @@ public class JUnitSampler extends AbstractSampler {
                 sresult.sampleEnd();
 
                 if (!getDoNotSetUpTearDown() && TDOWN_METHOD != null){
-                    TDOWN_METHOD.invoke(TEST_INSTANCE,new Class[0]);
+                    TDOWN_METHOD.invoke(TEST_INSTANCE,new Object[0]);
                 }
             } catch (InvocationTargetException e) {
                 // log.warn(e.getMessage());
