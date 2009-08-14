020d5adcd173ea966f2c840f910eedc787c3043e
Sebastian Bazley <sebb@apache.org>
2009-08-14 21:46:02 +0000

    Fix varargs method
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@804389 13f79535-47bb-0310-9956-ffa450edef68

--- src/protocol/http/org/apache/jmeter/protocol/http/sampler/HTTPSampler.java
+++ src/protocol/http/org/apache/jmeter/protocol/http/sampler/HTTPSampler.java
@@ -102,7 +102,7 @@ public class HTTPSampler extends HTTPSamplerBase implements Interruptible {
             return;
         }
         try {
-            method.invoke(conn, new Integer[]{new Integer(timeout)});
+            method.invoke(conn, new Object[]{new Integer(timeout)});
         } catch (IllegalArgumentException e1) {
             log.warn("Failed to set timeout: "+e1);
         } catch (IllegalAccessException e1) {
