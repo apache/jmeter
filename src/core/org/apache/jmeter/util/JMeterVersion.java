dee874b8111c6cce5d2ee7d60c49ad4517dd50a6
Sebastian Bazley <sebb@apache.org>
2009-08-18 13:08:12 +0000

    Next version is likely to be 2.4, not 2.5
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@805403 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 93a95ca8aace0599551e1d9d2fa7e901a2f7f93e

--- src/core/org/apache/jmeter/util/JMeterVersion.java
+++ src/core/org/apache/jmeter/util/JMeterVersion.java
@@ -44,7 +44,7 @@ public class JMeterVersion {
      * This ensures that JMeterUtils always gets the correct
      * version, even if JMeterUtils is not re-compiled during the build.
      */
-    private static final String VERSION = "2.3.4";
+    private static final String VERSION = "2.4";
 
     static final String COPYRIGHT = "Copyright (c) 1998-2009 The Apache Software Foundation";
 
