ffb93eb8efe18fc8df0e8390aba1dc7055aa789e
Sebastian Bazley <sebb@apache.org>
2009-09-07 20:26:55 +0000

    Added DataStrippingSample sender - supports "Stripped" and "StrippedBatch" modes. [code copied from branches/java1.5_prototype-was_trunk]
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@812266 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/core/org/apache/jmeter/samplers/DataStrippingSampleSender.java
@@ -0,0 +1,62 @@
+package org.apache.jmeter.samplers;
+
+import java.io.Serializable;
+import java.rmi.RemoteException;
+
+import org.apache.jorphan.logging.LoggingManager;
+import org.apache.log.Logger;
+
+/**
+ * The standard remote sample reporting should be more friendly to the main purpose of
+ * remote testing - which is scalability.  To increase scalability, this class strips out the 
+ * response data before sending.
+ * 
+ *
+ */
+public class DataStrippingSampleSender implements SampleSender, Serializable {
+	private static final long serialVersionUID = 1;
+	private static final Logger log = LoggingManager.getLoggerForClass();
+	
+	private final RemoteSampleListener listener;
+	private final SampleSender decoratedSender;
+
+	DataStrippingSampleSender(RemoteSampleListener listener) {
+		this.listener = listener;
+		decoratedSender = null;
+	}
+	
+	DataStrippingSampleSender(SampleSender decorate)
+	{
+		this.decoratedSender = decorate;
+        this.listener = null;
+	}
+
+	public void testEnded() {
+		if(decoratedSender != null) decoratedSender.testEnded();
+	}
+
+	public void testEnded(String host) {
+		if(decoratedSender != null) decoratedSender.testEnded(host);
+	}
+
+	public void sampleOccurred(SampleEvent event) {
+		//Strip the response data before writing, but only for a successful request.
+	    SampleResult result = event.getResult();
+		if(result.isSuccessful()) {
+		    result.setResponseData(new byte[0]);
+		}
+		if(decoratedSender == null)
+		{
+			try {
+				listener.sampleOccurred(event);
+			} catch (RemoteException e) {
+				log.error("Error sending sample result over network ",e);
+			}
+		}
+		else
+		{
+			decoratedSender.sampleOccurred(event);
+		}
+	}
+
+}
