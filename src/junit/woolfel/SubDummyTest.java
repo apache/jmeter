9dcc32c4b773e0f8368ef2b016355afcd64b8cc3
Sebastian Bazley <sebb@apache.org>
2009-09-21 20:15:25 +0000

    Add AL headers Update tests to improve coverage
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@817395 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 43535bc28185b26999ee6cf0090f0b666e9ddfe5

--- src/junit/woolfel/SubDummyTest.java
+++ src/junit/woolfel/SubDummyTest.java
@@ -1,40 +1,40 @@
 /*
- * Created on Jul 28, 2005
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  */
+
 package woolfel;
 
-/**
- * @author pete
- *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
- */
 public class SubDummyTest extends DummyTestCase {
 
-	/**
-	 * 
-	 */
 	public SubDummyTest() {
 		super();
-		// TODO Auto-generated constructor stub
+        System.out.println("public SubDummyTest()");
 	}
 
-	/**
-	 * @param arg0
-	 */
 	public SubDummyTest(String arg0) {
 		super(arg0);
-		// TODO Auto-generated constructor stub
+        System.out.println("public SubDummyTest("+arg0+")");
 	}
 
     public void oneTimeSetUp() {
-        System.out.println("oneTimeSetUp called -- ");
+        System.out.println("SubDummyTest#oneTimeSetUp(): "+getName());
     }
     
     public void oneTimeTearDown() {
-        System.out.println("oneTimeTearDown called -- ");
+        System.out.println("SubDummyTest#oneTimeTearDown(): "+getName());
     }
 }
