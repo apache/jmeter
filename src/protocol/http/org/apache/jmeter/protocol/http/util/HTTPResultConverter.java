6da316603260867fd808e8842262846318767eae
Sebastian Bazley <sebb@apache.org>
2009-12-10 00:18:03 +0000

    Javadoc fixes; no need for method to be protected
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@889030 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 20d084f802e7840ad04978a07c5f721f9a7f8b97

--- src/protocol/http/org/apache/jmeter/protocol/http/util/HTTPResultConverter.java
+++ src/protocol/http/org/apache/jmeter/protocol/http/util/HTTPResultConverter.java
@@ -55,24 +55,14 @@ public class HTTPResultConverter extends SampleResultConverter {
         super(arg0);
     }
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see com.thoughtworks.xstream.converters.Converter#canConvert(java.lang.Class)
-     */
+    /** {@inheritDoc} */
     @SuppressWarnings("unchecked") // superclass does not support types
     @Override
     public boolean canConvert(Class arg0) {
         return HTTPSampleResult.class.equals(arg0);
     }
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see com.thoughtworks.xstream.converters.Converter#marshal(java.lang.Object,
-     *      com.thoughtworks.xstream.io.HierarchicalStreamWriter,
-     *      com.thoughtworks.xstream.converters.MarshallingContext)
-     */
+    /** {@inheritDoc} */
     @Override
     public void marshal(Object obj, HierarchicalStreamWriter writer, MarshallingContext context) {
         HTTPSampleResult res = (HTTPSampleResult) obj;
@@ -86,14 +76,7 @@ public class HTTPResultConverter extends SampleResultConverter {
         saveSamplerData(writer, context, res, save);
     }
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see org.apache.jmeter.save.converters.SampleResultConverter#saveSamplerData(com.thoughtworks.xstream.io.HierarchicalStreamWriter,
-     *      org.apache.jmeter.samplers.SampleResult,
-     *      org.apache.jmeter.samplers.SampleSaveConfiguration)
-     */
-    protected void saveSamplerData(HierarchicalStreamWriter writer, MarshallingContext context, HTTPSampleResult res,
+    private void saveSamplerData(HierarchicalStreamWriter writer, MarshallingContext context, HTTPSampleResult res,
             SampleSaveConfiguration save) {
         if (save.saveSamplerData(res)) {
             writeString(writer, TAG_COOKIES, res.getCookies());
@@ -106,12 +89,7 @@ public class HTTPResultConverter extends SampleResultConverter {
         }
     }
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see com.thoughtworks.xstream.converters.Converter#unmarshal(com.thoughtworks.xstream.io.HierarchicalStreamReader,
-     *      com.thoughtworks.xstream.converters.UnmarshallingContext)
-     */
+    /** {@inheritDoc} */
     @Override
     public Object unmarshal(HierarchicalStreamReader reader, UnmarshallingContext context) {
         HTTPSampleResult res = (HTTPSampleResult) createCollection(context.getRequiredType());
@@ -134,7 +112,7 @@ public class HTTPResultConverter extends SampleResultConverter {
         return res;
     }
 
-    protected void retrieveHTTPItem(HierarchicalStreamReader reader, UnmarshallingContext context,
+    private void retrieveHTTPItem(HierarchicalStreamReader reader, UnmarshallingContext context,
             HTTPSampleResult res, Object subItem) {
         if (subItem instanceof URL) {
             res.setURL((URL) subItem);
