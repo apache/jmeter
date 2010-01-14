4a828c37965359adf935f0d8f4e2719c3f304127
Sebastian Bazley <sebb@apache.org>
2010-01-14 14:08:37 +0000

    Bug 48542 - SoapSampler uses wrong response header field to decide if response is gzip encoded
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@899207 13f79535-47bb-0310-9956-ffa450edef68

--- src/protocol/http/org/apache/jmeter/protocol/http/sampler/SoapSampler.java
+++ src/protocol/http/org/apache/jmeter/protocol/http/sampler/SoapSampler.java
@@ -260,7 +260,7 @@ public class SoapSampler extends HTTPSampler2 implements Interruptible { // Impl
 
             if (instream != null) {// will be null for HEAD
 
-                org.apache.commons.httpclient.Header responseHeader = httpMethod.getResponseHeader(TRANSFER_ENCODING);
+                org.apache.commons.httpclient.Header responseHeader = httpMethod.getResponseHeader(HEADER_CONTENT_ENCODING);
                 if (responseHeader != null && ENCODING_GZIP.equals(responseHeader.getValue())) {
                     instream = new GZIPInputStream(instream);
                 }
