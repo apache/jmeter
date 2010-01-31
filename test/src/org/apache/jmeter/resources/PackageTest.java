0ccac4fdc953acc605235a24d6c2b1dafdd3785a
Sebastian Bazley <sebb@apache.org>
2010-01-31 13:10:37 +0000

    Unused
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@905032 13f79535-47bb-0310-9956-ffa450edef68

--- test/src/org/apache/jmeter/resources/PackageTest.java
+++ test/src/org/apache/jmeter/resources/PackageTest.java
@@ -24,7 +24,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Enumeration;
 import java.util.Iterator;
 import java.util.List;
 import java.util.MissingResourceException;
