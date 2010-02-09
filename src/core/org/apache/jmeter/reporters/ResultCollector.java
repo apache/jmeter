3ff3a44d2c315ed8484eb61049061aff312aa5cb
Sebastian Bazley <sebb@apache.org>
2010-02-09 17:50:00 +0000

    Thread-safety fixes
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@908128 13f79535-47bb-0310-9956-ffa450edef68

--- src/core/org/apache/jmeter/reporters/ResultCollector.java
+++ src/core/org/apache/jmeter/reporters/ResultCollector.java
@@ -122,7 +122,7 @@ public class ResultCollector extends AbstractListenerElement implements SampleLi
     //@GuardedBy("LOCK")
     private static int instanceCount; // Keep track of how many instances are active
 
-    // Instance variables
+    // Instance variables (guarded by volatile)
     
     private transient volatile PrintWriter out;
 
@@ -131,7 +131,7 @@ public class ResultCollector extends AbstractListenerElement implements SampleLi
     private volatile boolean isStats = false;
     
     /** the summarizer to which this result collector will forward the samples */
-    private Summariser summariser;
+    private volatile Summariser summariser;
 
     /**
      * No-arg constructor.
@@ -170,11 +170,11 @@ public class ResultCollector extends AbstractListenerElement implements SampleLi
         return getPropertyAsBoolean(ERROR_LOGGING);
     }
 
-    public void setErrorLogging(boolean errorLogging) {
+    public final void setErrorLogging(boolean errorLogging) {
         setProperty(new BooleanProperty(ERROR_LOGGING, errorLogging));
     }
 
-    public void setSuccessOnlyLogging(boolean value) {
+    public final void setSuccessOnlyLogging(boolean value) {
         if (value) {
             setProperty(new BooleanProperty(SUCCESS_ONLY_LOGGING, true));
         } else {
@@ -248,7 +248,7 @@ public class ResultCollector extends AbstractListenerElement implements SampleLi
         }
     }
 
-    public synchronized void testStarted(String host) {
+    public void testStarted(String host) {
         synchronized(LOCK){
             instanceCount++;
             try {
