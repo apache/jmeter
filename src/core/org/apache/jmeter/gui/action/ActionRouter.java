29f9d96144d46d30192f39777c48c24777966b93
Sebastian Bazley <sebb@apache.org>
2010-06-06 19:56:43 +0000

    Bug 49394 - Classcast Exception in ActionRouter.postActionPerformed
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@951940 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 6f9033b6ba366cd0f76e1ade42855c4e66338f6b

--- src/core/org/apache/jmeter/gui/action/ActionRouter.java
+++ src/core/org/apache/jmeter/gui/action/ActionRouter.java
@@ -237,9 +237,9 @@ public final class ActionRouter implements ActionListener {
         if (action != null) {
             HashSet<ActionListener> listenerSet = preActionListeners.get(action.getName());
             if (listenerSet != null && listenerSet.size() > 0) {
-                Object[] listeners = listenerSet.toArray();
+                ActionListener[] listeners = listenerSet.toArray(new ActionListener[listenerSet.size()]);
                 for (int i = 0; i < listeners.length; i++) {
-                    ((ActionListener) listeners[i]).actionPerformed(e);
+                    listeners[i].actionPerformed(e);
                 }
             }
         }
@@ -249,7 +249,7 @@ public final class ActionRouter implements ActionListener {
         if (action != null) {
             HashSet<ActionListener> listenerSet = postActionListeners.get(action.getName());
             if (listenerSet != null && listenerSet.size() > 0) {
-                ActionListener[] listeners = (ActionListener[]) listenerSet.toArray();
+                ActionListener[] listeners = listenerSet.toArray(new ActionListener[listenerSet.size()]);
                 for (int i = 0; i < listeners.length; i++) {
                     listeners[i].actionPerformed(e);
                 }
