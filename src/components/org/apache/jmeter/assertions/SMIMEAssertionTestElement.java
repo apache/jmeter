eb2e9e1a480bddc5b33705d4d309ffedfa4d7281
Sebastian Bazley <sebb@apache.org>
2010-06-25 16:35:27 +0000

    Tab police
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@958030 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: b5a9dcc96239604d3a6df3ecb2105e1c0cf77a3c

--- src/components/org/apache/jmeter/assertions/SMIMEAssertionTestElement.java
+++ src/components/org/apache/jmeter/assertions/SMIMEAssertionTestElement.java
@@ -27,36 +27,36 @@ import org.apache.jmeter.testelement.property.StringProperty;
 import org.apache.jmeter.util.JMeterUtils;
 
 public class SMIMEAssertionTestElement extends AbstractTestElement implements
-		Serializable, Assertion {
+        Serializable, Assertion {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private static final String VERIFY_SIGNATURE_KEY = "SMIMEAssertionTestElement.verifySignature";
+    private static final String VERIFY_SIGNATURE_KEY = "SMIMEAssertionTestElement.verifySignature";
 
-	private static final String NOT_SIGNED_KEY = "SMIMEAssertionTestElement.notSigned";
+    private static final String NOT_SIGNED_KEY = "SMIMEAssertionTestElement.notSigned";
 
-	private static final String SIGNER_NO_CHECK_KEY = "SMIMEAssertionTestElement.signerNoCheck";
+    private static final String SIGNER_NO_CHECK_KEY = "SMIMEAssertionTestElement.signerNoCheck";
 
-	private static final String SIGNER_CHECK_BY_FILE_KEY = "SMIMEAssertionTestElement.signerCheckByFile";
+    private static final String SIGNER_CHECK_BY_FILE_KEY = "SMIMEAssertionTestElement.signerCheckByFile";
 
-	private static final String SIGNER_CERT_FILE_KEY = "SMIMEAssertionTestElement.signerCertFile";
+    private static final String SIGNER_CERT_FILE_KEY = "SMIMEAssertionTestElement.signerCertFile";
 
-	private static final String SINGER_CHECK_CONSTRAINTS_KEY = "SMIMEAssertionTestElement.signerCheckConstraints";
+    private static final String SINGER_CHECK_CONSTRAINTS_KEY = "SMIMEAssertionTestElement.signerCheckConstraints";
 
-	private static final String SIGNER_SERIAL_KEY = "SMIMEAssertionTestElement.signerSerial";
+    private static final String SIGNER_SERIAL_KEY = "SMIMEAssertionTestElement.signerSerial";
 
-	private static final String SIGNER_EMAIL_KEY = "SMIMEAssertionTestElement.signerEmail";
+    private static final String SIGNER_EMAIL_KEY = "SMIMEAssertionTestElement.signerEmail";
 
-	private static final String SIGNER_DN_KEY = "SMIMEAssertionTestElement.signerDn";
+    private static final String SIGNER_DN_KEY = "SMIMEAssertionTestElement.signerDn";
 
-	private static final String ISSUER_DN_KEY = "SMIMEAssertionTestElement.issuerDn";
+    private static final String ISSUER_DN_KEY = "SMIMEAssertionTestElement.issuerDn";
 
-	public SMIMEAssertionTestElement() {
-		super();
-	}
+    public SMIMEAssertionTestElement() {
+        super();
+    }
 
-	public AssertionResult getResult(SampleResult response) {
-		try {
+    public AssertionResult getResult(SampleResult response) {
+        try {
             return SMIMEAssertion.getResult(this, response, getName());
         } catch (NoClassDefFoundError e) {
             AssertionResult assertionResult = new AssertionResult(getName());
@@ -65,88 +65,88 @@ public class SMIMEAssertionTestElement extends AbstractTestElement implements
                 .getResString("bouncy_castle_unavailable_message"));
             return assertionResult;
         }
-	}
+    }
 
-	public boolean isVerifySignature() {
-		return getPropertyAsBoolean(VERIFY_SIGNATURE_KEY);
-	}
+    public boolean isVerifySignature() {
+        return getPropertyAsBoolean(VERIFY_SIGNATURE_KEY);
+    }
 
-	public void setVerifySignature(boolean verifySignature) {
-		setProperty(new BooleanProperty(VERIFY_SIGNATURE_KEY, verifySignature));
-	}
+    public void setVerifySignature(boolean verifySignature) {
+        setProperty(new BooleanProperty(VERIFY_SIGNATURE_KEY, verifySignature));
+    }
 
-	public String getIssuerDn() {
-		return getPropertyAsString(ISSUER_DN_KEY);
-	}
+    public String getIssuerDn() {
+        return getPropertyAsString(ISSUER_DN_KEY);
+    }
 
-	public void setIssuerDn(String issuertDn) {
-		setProperty(new StringProperty(ISSUER_DN_KEY, issuertDn));
-	}
+    public void setIssuerDn(String issuertDn) {
+        setProperty(new StringProperty(ISSUER_DN_KEY, issuertDn));
+    }
 
-	public boolean isSignerCheckByFile() {
-		return getPropertyAsBoolean(SIGNER_CHECK_BY_FILE_KEY);
-	}
+    public boolean isSignerCheckByFile() {
+        return getPropertyAsBoolean(SIGNER_CHECK_BY_FILE_KEY);
+    }
 
-	public void setSignerCheckByFile(boolean signerCheckByFile) {
-		setProperty(new BooleanProperty(SIGNER_CHECK_BY_FILE_KEY,
-				signerCheckByFile));
-	}
+    public void setSignerCheckByFile(boolean signerCheckByFile) {
+        setProperty(new BooleanProperty(SIGNER_CHECK_BY_FILE_KEY,
+                signerCheckByFile));
+    }
 
-	public boolean isSignerCheckConstraints() {
-		return getPropertyAsBoolean(SINGER_CHECK_CONSTRAINTS_KEY);
-	}
+    public boolean isSignerCheckConstraints() {
+        return getPropertyAsBoolean(SINGER_CHECK_CONSTRAINTS_KEY);
+    }
 
-	public void setSignerCheckConstraints(boolean signerCheckConstraints) {
-		setProperty(new BooleanProperty(SINGER_CHECK_CONSTRAINTS_KEY,
-				signerCheckConstraints));
-	}
+    public void setSignerCheckConstraints(boolean signerCheckConstraints) {
+        setProperty(new BooleanProperty(SINGER_CHECK_CONSTRAINTS_KEY,
+                signerCheckConstraints));
+    }
 
-	public boolean isSignerNoCheck() {
-		return getPropertyAsBoolean(SIGNER_NO_CHECK_KEY);
-	}
+    public boolean isSignerNoCheck() {
+        return getPropertyAsBoolean(SIGNER_NO_CHECK_KEY);
+    }
 
-	public void setSignerNoCheck(boolean signerNoCheck) {
-		setProperty(new BooleanProperty(SIGNER_NO_CHECK_KEY, signerNoCheck));
-	}
+    public void setSignerNoCheck(boolean signerNoCheck) {
+        setProperty(new BooleanProperty(SIGNER_NO_CHECK_KEY, signerNoCheck));
+    }
 
-	public String getSignerCertFile() {
-		return getPropertyAsString(SIGNER_CERT_FILE_KEY);
-	}
+    public String getSignerCertFile() {
+        return getPropertyAsString(SIGNER_CERT_FILE_KEY);
+    }
 
-	public void setSignerCertFile(String signerCertFile) {
-		setProperty(new StringProperty(SIGNER_CERT_FILE_KEY, signerCertFile));
-	}
+    public void setSignerCertFile(String signerCertFile) {
+        setProperty(new StringProperty(SIGNER_CERT_FILE_KEY, signerCertFile));
+    }
 
-	public String getSignerDn() {
-		return getPropertyAsString(SIGNER_DN_KEY);
-	}
+    public String getSignerDn() {
+        return getPropertyAsString(SIGNER_DN_KEY);
+    }
 
-	public void setSignerDn(String signerDn) {
-		setProperty(new StringProperty(SIGNER_DN_KEY, signerDn));
-	}
+    public void setSignerDn(String signerDn) {
+        setProperty(new StringProperty(SIGNER_DN_KEY, signerDn));
+    }
 
-	public String getSignerSerial() {
-		return getPropertyAsString(SIGNER_SERIAL_KEY);
-	}
+    public String getSignerSerial() {
+        return getPropertyAsString(SIGNER_SERIAL_KEY);
+    }
 
-	public void setSignerSerial(String signerSerial) {
-		setProperty(new StringProperty(SIGNER_SERIAL_KEY, signerSerial));
-	}
+    public void setSignerSerial(String signerSerial) {
+        setProperty(new StringProperty(SIGNER_SERIAL_KEY, signerSerial));
+    }
 
-	public String getSignerEmail() {
-		return getPropertyAsString(SIGNER_EMAIL_KEY);
-	}
+    public String getSignerEmail() {
+        return getPropertyAsString(SIGNER_EMAIL_KEY);
+    }
 
-	public void setSignerEmail(String signerEmail) {
-		setProperty(new StringProperty(SIGNER_EMAIL_KEY, signerEmail));
-	}
+    public void setSignerEmail(String signerEmail) {
+        setProperty(new StringProperty(SIGNER_EMAIL_KEY, signerEmail));
+    }
 
-	public boolean isNotSigned() {
-		return getPropertyAsBoolean(NOT_SIGNED_KEY);
-	}
+    public boolean isNotSigned() {
+        return getPropertyAsBoolean(NOT_SIGNED_KEY);
+    }
 
-	public void setNotSigned(boolean notSigned) {
-		setProperty(new BooleanProperty(NOT_SIGNED_KEY, notSigned));
-	}
+    public void setNotSigned(boolean notSigned) {
+        setProperty(new BooleanProperty(NOT_SIGNED_KEY, notSigned));
+    }
 
 }
