0c3d9c978883ec2cea63d50544372aba4676510f
Sebastian Bazley <sebb@apache.org>
2010-07-02 21:40:47 +0000

    Tab police; correct indentation
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@960116 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: fc8e4b7aa66a7505795210946d9f78e103d77cf3

--- test/src/org/apache/jmeter/protocol/http/util/TestHTTPUtils.java
+++ test/src/org/apache/jmeter/protocol/http/util/TestHTTPUtils.java
@@ -23,61 +23,61 @@ import java.net.URL;
 import junit.framework.TestCase;
 
 public class TestHTTPUtils extends TestCase {
-        public TestHTTPUtils(String name) {
-            super(name);
-        }
+    public TestHTTPUtils(String name) {
+        super(name);
+    }
 
-        public void testgetEncoding() throws Exception {
-            assertNull(ConversionUtils.getEncodingFromContentType("xyx"));
-            assertEquals("utf8",ConversionUtils.getEncodingFromContentType("charset=utf8"));
-            assertEquals("utf8",ConversionUtils.getEncodingFromContentType("charset=\"utf8\""));
-            assertEquals("utf8",ConversionUtils.getEncodingFromContentType("text/plain ;charset=utf8"));
-            assertEquals("utf8",ConversionUtils.getEncodingFromContentType("text/html ;charset=utf8;charset=def"));
-            assertNull(ConversionUtils.getEncodingFromContentType("charset="));
-            assertNull(ConversionUtils.getEncodingFromContentType(";charset=;"));
-            assertNull(ConversionUtils.getEncodingFromContentType(";charset=no-such-charset;"));
-        }
-        
-        public void testMakeRelativeURL() throws Exception {
-            URL base = new URL("http://192.168.0.1/a/b/c"); // Trailing file
-            assertEquals(new URL("http://192.168.0.1/a/b/d"),ConversionUtils.makeRelativeURL(base,"d"));
-            assertEquals(new URL("http://192.168.0.1/a/d"),ConversionUtils.makeRelativeURL(base,"../d"));
-            assertEquals(new URL("http://192.168.0.1/d"),ConversionUtils.makeRelativeURL(base,"../../d"));
-            assertEquals(new URL("http://192.168.0.1/d"),ConversionUtils.makeRelativeURL(base,"../../../d"));
-            assertEquals(new URL("http://192.168.0.1/d"),ConversionUtils.makeRelativeURL(base,"../../../../d"));
-            assertEquals(new URL("http://192.168.0.1/../d"),ConversionUtils.makeRelativeURL(base,"/../d"));
-            assertEquals(new URL("http://192.168.0.1/a/b/d"),ConversionUtils.makeRelativeURL(base,"./d"));
-        }
+    public void testgetEncoding() throws Exception {
+        assertNull(ConversionUtils.getEncodingFromContentType("xyx"));
+        assertEquals("utf8",ConversionUtils.getEncodingFromContentType("charset=utf8"));
+        assertEquals("utf8",ConversionUtils.getEncodingFromContentType("charset=\"utf8\""));
+        assertEquals("utf8",ConversionUtils.getEncodingFromContentType("text/plain ;charset=utf8"));
+        assertEquals("utf8",ConversionUtils.getEncodingFromContentType("text/html ;charset=utf8;charset=def"));
+        assertNull(ConversionUtils.getEncodingFromContentType("charset="));
+        assertNull(ConversionUtils.getEncodingFromContentType(";charset=;"));
+        assertNull(ConversionUtils.getEncodingFromContentType(";charset=no-such-charset;"));
+    }
+    
+    public void testMakeRelativeURL() throws Exception {
+        URL base = new URL("http://192.168.0.1/a/b/c"); // Trailing file
+        assertEquals(new URL("http://192.168.0.1/a/b/d"),ConversionUtils.makeRelativeURL(base,"d"));
+        assertEquals(new URL("http://192.168.0.1/a/d"),ConversionUtils.makeRelativeURL(base,"../d"));
+        assertEquals(new URL("http://192.168.0.1/d"),ConversionUtils.makeRelativeURL(base,"../../d"));
+        assertEquals(new URL("http://192.168.0.1/d"),ConversionUtils.makeRelativeURL(base,"../../../d"));
+        assertEquals(new URL("http://192.168.0.1/d"),ConversionUtils.makeRelativeURL(base,"../../../../d"));
+        assertEquals(new URL("http://192.168.0.1/../d"),ConversionUtils.makeRelativeURL(base,"/../d"));
+        assertEquals(new URL("http://192.168.0.1/a/b/d"),ConversionUtils.makeRelativeURL(base,"./d"));
+    }
 
-        public void testMakeRelativeURL2() throws Exception {
-            URL base = new URL("http://192.168.0.1/a/b/c/"); // Trailing directory
-            assertEquals(new URL("http://192.168.0.1/a/b/c/d"),ConversionUtils.makeRelativeURL(base,"d"));
-            assertEquals(new URL("http://192.168.0.1/a/b/d"),ConversionUtils.makeRelativeURL(base,"../d"));
-            assertEquals(new URL("http://192.168.0.1/a/d"),ConversionUtils.makeRelativeURL(base,"../../d"));
-            assertEquals(new URL("http://192.168.0.1/d"),ConversionUtils.makeRelativeURL(base,"../../../d"));
-            assertEquals(new URL("http://192.168.0.1/d"),ConversionUtils.makeRelativeURL(base,"../../../../d"));
-            assertEquals(new URL("http://192.168.0.1/../d"),ConversionUtils.makeRelativeURL(base,"/../d"));
-            assertEquals(new URL("http://192.168.0.1/a/b/c/d"),ConversionUtils.makeRelativeURL(base,"./d"));
-        }
-		
-		public void testRemoveSlashDotDot()
-		{
-			assertEquals("/path/", ConversionUtils.removeSlashDotDot("/path/"));
-			assertEquals("http://host/", ConversionUtils.removeSlashDotDot("http://host/"));
-            assertEquals("http://host/one", ConversionUtils.removeSlashDotDot("http://host/one"));
-			assertEquals("/two", ConversionUtils.removeSlashDotDot("/one/../two"));
-			assertEquals("http://host:8080/two", ConversionUtils.removeSlashDotDot("http://host:8080/one/../two"));
-			assertEquals("http://host:8080/two/", ConversionUtils.removeSlashDotDot("http://host:8080/one/../two/"));
-			assertEquals("http://usr@host:8080/two/", ConversionUtils.removeSlashDotDot("http://usr@host:8080/one/../two/"));
-			assertEquals("http://host:8080/two/?query#anchor", ConversionUtils.removeSlashDotDot("http://host:8080/one/../two/?query#anchor"));
-			assertEquals("one", ConversionUtils.removeSlashDotDot("one/two/.."));
-			assertEquals("../../path", ConversionUtils.removeSlashDotDot("../../path"));
-			assertEquals("/", ConversionUtils.removeSlashDotDot("/one/.."));
-			assertEquals("/", ConversionUtils.removeSlashDotDot("/one/../"));
-			assertEquals("/?a", ConversionUtils.removeSlashDotDot("/one/..?a"));
-			assertEquals("http://host/one", ConversionUtils.removeSlashDotDot("http://host/one/../one"));
-			assertEquals("http://host/one/two", ConversionUtils.removeSlashDotDot("http://host/one/two/../../one/two"));
-            assertEquals("http://host/..", ConversionUtils.removeSlashDotDot("http://host/.."));
-            assertEquals("http://host/../abc", ConversionUtils.removeSlashDotDot("http://host/../abc"));
-		}
+    public void testMakeRelativeURL2() throws Exception {
+        URL base = new URL("http://192.168.0.1/a/b/c/"); // Trailing directory
+        assertEquals(new URL("http://192.168.0.1/a/b/c/d"),ConversionUtils.makeRelativeURL(base,"d"));
+        assertEquals(new URL("http://192.168.0.1/a/b/d"),ConversionUtils.makeRelativeURL(base,"../d"));
+        assertEquals(new URL("http://192.168.0.1/a/d"),ConversionUtils.makeRelativeURL(base,"../../d"));
+        assertEquals(new URL("http://192.168.0.1/d"),ConversionUtils.makeRelativeURL(base,"../../../d"));
+        assertEquals(new URL("http://192.168.0.1/d"),ConversionUtils.makeRelativeURL(base,"../../../../d"));
+        assertEquals(new URL("http://192.168.0.1/../d"),ConversionUtils.makeRelativeURL(base,"/../d"));
+        assertEquals(new URL("http://192.168.0.1/a/b/c/d"),ConversionUtils.makeRelativeURL(base,"./d"));
+    }
+
+    public void testRemoveSlashDotDot()
+    {
+        assertEquals("/path/", ConversionUtils.removeSlashDotDot("/path/"));
+        assertEquals("http://host/", ConversionUtils.removeSlashDotDot("http://host/"));
+        assertEquals("http://host/one", ConversionUtils.removeSlashDotDot("http://host/one"));
+        assertEquals("/two", ConversionUtils.removeSlashDotDot("/one/../two"));
+        assertEquals("http://host:8080/two", ConversionUtils.removeSlashDotDot("http://host:8080/one/../two"));
+        assertEquals("http://host:8080/two/", ConversionUtils.removeSlashDotDot("http://host:8080/one/../two/"));
+        assertEquals("http://usr@host:8080/two/", ConversionUtils.removeSlashDotDot("http://usr@host:8080/one/../two/"));
+        assertEquals("http://host:8080/two/?query#anchor", ConversionUtils.removeSlashDotDot("http://host:8080/one/../two/?query#anchor"));
+        assertEquals("one", ConversionUtils.removeSlashDotDot("one/two/.."));
+        assertEquals("../../path", ConversionUtils.removeSlashDotDot("../../path"));
+        assertEquals("/", ConversionUtils.removeSlashDotDot("/one/.."));
+        assertEquals("/", ConversionUtils.removeSlashDotDot("/one/../"));
+        assertEquals("/?a", ConversionUtils.removeSlashDotDot("/one/..?a"));
+        assertEquals("http://host/one", ConversionUtils.removeSlashDotDot("http://host/one/../one"));
+        assertEquals("http://host/one/two", ConversionUtils.removeSlashDotDot("http://host/one/two/../../one/two"));
+        assertEquals("http://host/..", ConversionUtils.removeSlashDotDot("http://host/.."));
+        assertEquals("http://host/../abc", ConversionUtils.removeSlashDotDot("http://host/../abc"));
+    }
 }
