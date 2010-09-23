d36afb80496413af666c8041a590bb1d84470c10
Sebastian Bazley <sebb@apache.org>
2010-09-23 21:28:12 +0000

    Bug 49975 - New function returning the name of the current sampler
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@1000632 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/functions/org/apache/jmeter/functions/SamplerName.java
@@ -0,0 +1,94 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.apache.jmeter.functions;
+
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.jmeter.engine.util.CompoundVariable;
+import org.apache.jmeter.functions.AbstractFunction;
+import org.apache.jmeter.functions.InvalidVariableException;
+import org.apache.jmeter.samplers.SampleResult;
+import org.apache.jmeter.samplers.Sampler;
+import org.apache.jmeter.threads.JMeterVariables;
+import org.apache.jmeter.util.JMeterUtils;
+import org.apache.jorphan.logging.LoggingManager;
+import org.apache.log.Logger;
+
+/**
+ * Function to return the name of the current sampler.
+ */
+public class SamplerName extends AbstractFunction {
+
+    private static final Logger log = LoggingManager.getLoggerForClass();
+
+    private static final String KEY = "__samplerName"; //$NON-NLS-1$
+
+	private static final List<String> desc = new LinkedList<String>();
+
+    static {
+        // desc.add("Use fully qualified host name: TRUE/FALSE (Default FALSE)");
+        desc.add(JMeterUtils.getResString("function_name_paropt")); //$NON-NLS-1$
+    }
+
+    private Object[] values;
+
+	/** {@inheritDoc} */
+	@Override
+	public String execute(SampleResult previousResult, Sampler currentSampler)
+			throws InvalidVariableException {
+		// return JMeterContextService.getContext().getCurrentSampler().getName();
+		String name = "";
+		if (currentSampler != null) { // will be null if function is used on TestPlan
+		    name = currentSampler.getName();
+		}
+		log.info("execute: ***********************************************");
+		if (values.length > 0){
+            JMeterVariables vars = getVariables();
+            if (vars != null) {// May be null if function is used on TestPlan
+                String varName = ((CompoundVariable) values[0]).execute().trim();
+                if (varName.length() > 0) {
+                    log.info("name="+name+", varName="+varName+".");
+                    vars.put(varName, name);
+                }
+            }
+		}
+        return name;
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	public synchronized void setParameters(Collection<CompoundVariable> parameters)
+			throws InvalidVariableException {
+        checkParameterCount(parameters, 0, 1);
+        values = parameters.toArray();
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	public String getReferenceKey() {
+		return KEY;
+	}
+
+	/** {@inheritDoc} */
+	public List<String> getArgumentDesc() {
+		return desc;
+	}
+}
