15e3bb80912c3c24076e1a742b0039977b444a74
Sebastian Bazley <sebb@apache.org>
2010-09-23 21:29:30 +0000

    Document where default charset is used
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@1000635 13f79535-47bb-0310-9956-ffa450edef68

--- src/protocol/http/org/apache/jmeter/protocol/http/proxy/Proxy.java
+++ src/protocol/http/org/apache/jmeter/protocol/http/proxy/Proxy.java
@@ -197,7 +197,7 @@ public class Proxy extends Thread {
             if ((request.getMethod().startsWith(HTTPConstants.CONNECT)) && (outStreamClient != null)) {
                 log.debug("Method CONNECT => SSL");
                 // write a OK reponse to browser, to engage SSL exchange
-                outStreamClient.write(("HTTP/1.0 200 OK\r\n\r\n").getBytes()); // $NON-NLS-1$
+                outStreamClient.write(("HTTP/1.0 200 OK\r\n\r\n").getBytes()); // $NON-NLS-1$ // TODO charset?
                 outStreamClient.flush();
                // With ssl request, url is host:port (without https:// or path)
                 String[] param = request.getUrl().split(":");  // $NON-NLS-1$
