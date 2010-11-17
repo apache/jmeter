c635a4794e7bd5aff70a6335b7d02569f828828a
Sebastian Bazley <sebb@apache.org>
2010-11-17 22:06:34 +0000

    Simplify - save DestinationSetup as a boolean
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@1036249 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 8350799ccc121889c4870a0a25c2f4d2d3ef72d0

--- src/protocol/jms/org/apache/jmeter/protocol/jms/control/gui/JMSPublisherGui.java
+++ src/protocol/jms/org/apache/jmeter/protocol/jms/control/gui/JMSPublisherGui.java
@@ -63,10 +63,6 @@ public class JMSPublisherGui extends AbstractSamplerGui implements ChangeListene
     public static final String MAP_MSG_RSC = "jms_map_message"; //$NON-NLS-1$
     /** Create an ObjectMessage */
     public static final String OBJECT_MSG_RSC = "jms_object_message"; //$NON-NLS-1$
-
-    public final static String DEST_SETUP_STATIC = "jms_dest_setup_static"; // $NON-NLS-1$
-
-    public final static String DEST_SETUP_DYNAMIC = "jms_dest_setup_dynamic"; // $NON-NLS-1$
     //-- End of names used in JMX files
 
     // Button group resources
@@ -102,6 +98,11 @@ public class JMSPublisherGui extends AbstractSamplerGui implements ChangeListene
 
     private final JLabeledRadioI18N msgChoice = new JLabeledRadioI18N("jms_message_type", MSGTYPES_ITEMS, TEXT_MSG_RSC); //$NON-NLS-1$
     
+
+    // These are the names of properties used to define the labels
+    private final static String DEST_SETUP_STATIC = "jms_dest_setup_static"; // $NON-NLS-1$
+
+    private final static String DEST_SETUP_DYNAMIC = "jms_dest_setup_dynamic"; // $NON-NLS-1$
     // Button group resources
     private final static String[] DEST_SETUP_ITEMS = { DEST_SETUP_STATIC, DEST_SETUP_DYNAMIC };
 
@@ -164,7 +165,7 @@ public class JMSPublisherGui extends AbstractSamplerGui implements ChangeListene
         sampler.setMessageChoice(msgChoice.getText());
         sampler.setIterations(iterations.getText());
         sampler.setUseAuth(useAuth.isSelected());
-        sampler.setDestinationSetup(destSetup.getText());
+        sampler.setDestinationStatic(destSetup.getText().equals(DEST_SETUP_STATIC));
     }
 
     /**
@@ -248,7 +249,7 @@ public class JMSPublisherGui extends AbstractSamplerGui implements ChangeListene
         updateConfig(sampler.getConfigChoice());
         iterations.setText(sampler.getIterations());
         useAuth.setSelected(sampler.isUseAuth());
-        destSetup.setText(sampler.getDestinationSetup());
+        destSetup.setText(sampler.isDestinationStatic() ? DEST_SETUP_STATIC : DEST_SETUP_DYNAMIC);
     }
 
     /**
