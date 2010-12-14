7f6d31f9a7d3cd8d78471f8fb31f5160f7e676d8
Sebastian Bazley <sebb@apache.org>
2010-12-14 11:57:11 +0000

    Unused import
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@1049053 13f79535-47bb-0310-9956-ffa450edef68

--- src/core/org/apache/jmeter/engine/StandardJMeterEngine.java
+++ src/core/org/apache/jmeter/engine/StandardJMeterEngine.java
@@ -25,7 +25,6 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
