c51d5d592cc3c958dd7590baa513786353f9447a
Bruno Demion <milamber@apache.org>
2011-01-30 09:40:55 +0000

    Bug 50686 - HeaderManager logging to verbose when merging instances Downgrade to debug
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@1065240 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 26ea6605991ff63c698f61bc3475699e67358ce7

--- src/protocol/http/org/apache/jmeter/protocol/http/sampler/HTTPSamplerBase.java
+++ src/protocol/http/org/apache/jmeter/protocol/http/sampler/HTTPSamplerBase.java
@@ -681,7 +681,7 @@ public abstract class HTTPSamplerBase extends AbstractSampler
     public void setHeaderManager(HeaderManager value) {
         HeaderManager mgr = getHeaderManager();
         if (mgr != null) {
-            log.warn("Existing HeaderManager '" + mgr.getName() + "' merged with '" + value.getName() + "'");
+            log.debug("Existing HeaderManager '" + mgr.getName() + "' merged with '" + value.getName() + "'");
             value = mgr.merge(value, true);
             if (log.isDebugEnabled()) {
                 log.debug("HeaderManager merged: " + value.getName());
