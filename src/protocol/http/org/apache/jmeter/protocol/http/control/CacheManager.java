82d24f3d38d8b4e7c16a782633f997fc6f5efe2a
Bruno Demion <milamber@apache.org>
2011-09-07 13:20:47 +0000

    Bug 51752 - HTTP Cache is broken when using "Retrieve all embedded resources" with concurrent pool
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@1166165 13f79535-47bb-0310-9956-ffa450edef68

--- src/protocol/http/org/apache/jmeter/protocol/http/control/CacheManager.java
+++ src/protocol/http/org/apache/jmeter/protocol/http/control/CacheManager.java
@@ -57,7 +57,7 @@ public class CacheManager extends ConfigTestElement implements TestListener, Ser
     public static final String USE_EXPIRES = "useExpires"; // $NON-NLS-1$
     //-
 
-    private transient ThreadLocal<Map<String, CacheEntry>> threadCache;
+    private transient InheritableThreadLocal<Map<String, CacheEntry>> threadCache;
 
     private transient boolean useExpires; // Cached value
 
@@ -325,7 +325,7 @@ public class CacheManager extends ConfigTestElement implements TestListener, Ser
 
     private void clearCache() {
         log.debug("Clear cache");
-        threadCache = new ThreadLocal<Map<String, CacheEntry>>(){
+        threadCache = new InheritableThreadLocal<Map<String, CacheEntry>>(){
             @Override
             protected Map<String, CacheEntry> initialValue(){
                 return new HashMap<String, CacheEntry>();
