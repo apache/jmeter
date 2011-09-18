3755c969daf9dcf7d9c259438402ac0931c6a86c
Bruno Demion <milamber@apache.org>
2011-09-18 16:01:51 +0000

    little update for JavaDoc
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@1172298 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: aa5a3e581a18ee09d2de08f751fae38787783c83

--- src/core/org/apache/jmeter/engine/JMeterEngineException.java
+++ src/core/org/apache/jmeter/engine/JMeterEngineException.java
@@ -21,7 +21,7 @@ package org.apache.jmeter.engine;
 import java.io.Serializable;
 
 /**
- * Exception class for use by {@link JMeterEngine#runTest()} and {@link RemoteJMeterEngine#runTest()}
+ * Exception class for use by {@link JMeterEngine#runTest()} and {@link RemoteJMeterEngine#rrunTest()}
  */
 public class JMeterEngineException extends Exception implements Serializable {
     private static final long serialVersionUID = 240L;
