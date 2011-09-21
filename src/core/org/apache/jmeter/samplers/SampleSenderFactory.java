2194739e06815b3573d4a50d6ce632f548ec93a7
Sebastian Bazley <sebb@apache.org>
2011-09-21 15:20:11 +0000

    Propagate original error
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@1173705 13f79535-47bb-0310-9956-ffa450edef68

--- src/core/org/apache/jmeter/samplers/SampleSenderFactory.java
+++ src/core/org/apache/jmeter/samplers/SampleSenderFactory.java
@@ -80,7 +80,7 @@ public class SampleSenderFactory {
             } catch (Exception e) {
                 // houston we have a problem !!
                 log.error("Unable to create a sample sender from class "+type);
-                throw new IllegalArgumentException(e.getMessage());
+                throw new IllegalArgumentException(e.getMessage(), e);
             }
 
             return s;
