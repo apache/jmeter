816ce48d6616011deb6c57296aaba930b40cb6f8
Philippe Mouawad <pmouawad@apache.org>
2011-10-19 20:52:58 +0000

    Changed thread name to better identiy Thread children
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@1186483 13f79535-47bb-0310-9956-ffa450edef68

--- src/protocol/jms/org/apache/jmeter/protocol/jms/sampler/Receiver.java
+++ src/protocol/jms/org/apache/jmeter/protocol/jms/sampler/Receiver.java
@@ -78,7 +78,7 @@ public class Receiver implements Runnable {
             String principal, String credentials, boolean useResMsgIdAsCorrelId)
             throws JMSException {
         Receiver receiver = new Receiver(factory, receiveQueue, principal, credentials, useResMsgIdAsCorrelId);
-        Thread thread = new Thread(receiver, "JMS Receiever");
+        Thread thread = new Thread(receiver, Thread.currentThread().getName()+"-JMS-Receiver");
         thread.start();
         return receiver;
     }
