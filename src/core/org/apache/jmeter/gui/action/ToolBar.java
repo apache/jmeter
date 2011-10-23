9d43cfd40bc18dc4cbe56ddd664dfdae7c466eb8
Bruno Demion <milamber@apache.org>
2011-10-23 18:41:08 +0000

    Bug 52040 - Add a toolbar in JMeter main window
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@1187947 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/core/org/apache/jmeter/gui/action/ToolBar.java
@@ -0,0 +1,70 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.apache.jmeter.gui.action;
+
+import java.awt.event.ActionEvent;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.jmeter.gui.GuiPackage;
+
+/**
+ * Hide / unhide toolbar.
+ *
+ */
+public class ToolBar implements Command {
+
+    private static final Set<String> commands = new HashSet<String>();
+
+    static {
+        commands.add(ActionNames.TOOLBAR);
+    }
+
+    /**
+     * Constructor for object.
+     */
+    public ToolBar() {
+    }
+
+    /**
+     * Gets the ActionNames attribute of the action
+     *
+     * @return the ActionNames value
+     */
+    public Set<String> getActionNames() {
+        return commands;
+    }
+
+    /**
+     * This method performs the actual command processing.
+     *
+     * @param e
+     *            the generic UI action event
+     */
+    public void doAction(ActionEvent e) {
+        if (ActionNames.TOOLBAR.equals(e.getActionCommand())) {
+            GuiPackage guiInstance = GuiPackage.getInstance();
+            if (guiInstance.getMenuItemToolbar().getModel().isSelected()) {
+                guiInstance.getMainToolbar().setVisible(true);
+            } else {
+                guiInstance.getMainToolbar().setVisible(false);
+            }
+        }
+    }
+}
---
+++ src/core/org/apache/jmeter/gui/util/JMeterToolBar.java
@@ -0,0 +1,134 @@
+/* 
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed  under the  License is distributed on an "AS IS" BASIS,
+ * WITHOUT  WARRANTIES OR CONDITIONS  OF ANY KIND, either  express  or
+ * implied.
+ * 
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.jmeter.gui.util;
+
+import java.net.URL;
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.Properties;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+import javax.swing.JToolBar;
+
+import org.apache.jmeter.gui.action.ActionRouter;
+import org.apache.jmeter.util.JMeterUtils;
+import org.apache.jorphan.logging.LoggingManager;
+import org.apache.jorphan.util.JMeterException;
+import org.apache.log.Logger;
+
+/**
+ * The JMeter main toolbar class
+ *
+ */
+public class JMeterToolBar {
+    
+    private static final Logger log = LoggingManager.getLoggerForClass();
+
+    // protected fields: JMeterToolBar class can be use to create another toolbar (plugin, etc.)    
+    protected static final String defaultIconProp = "org/apache/jmeter/images/toolbar/icons-toolbar.properties"; //$NON-NLS-1$
+    
+    protected static final String keyIconProp = "jmeter.toolbar.icons"; //$NON-NLS-1$
+    
+    /**
+     * Create the default JMeter toolbar
+     * @return the JMeter toolbar
+     */
+    public static JToolBar createToolbar(boolean visible) {
+        JToolBar toolBar = new JToolBar();
+        toolBar.setFloatable(false);
+        toolBar.setVisible(visible);
+
+        LinkedHashMap<String, IconToolbarBean> icons = getIconMappings();
+        if (icons != null) {
+            Collection<IconToolbarBean> enumIcons = icons.values();
+            for (IconToolbarBean iconToolbarBean : enumIcons) {
+                if (iconToolbarBean == null) {
+                    toolBar.addSeparator();
+                } else {
+                    toolBar.add(makeButtonItemRes(iconToolbarBean));
+                }
+            }
+        }
+        // implicit return empty toolbar if icons == null
+        return toolBar;
+    }
+    
+    /**
+     * Generate a button component from icon bean
+     * @param iconBean contains I18N key, ActionNames, icon path, optional icon path pressed
+     * @return a button for toolbar
+     */
+    private static JButton makeButtonItemRes(IconToolbarBean iconBean) {
+        final URL imageURL = JMeterUtils.class.getClassLoader().getResource(iconBean.getIconPath());
+        JButton button = new JButton(new ImageIcon(imageURL));
+        button.setToolTipText(JMeterUtils.getResString(iconBean.getI18nKey()));
+        final URL imageURLPressed = JMeterUtils.class.getClassLoader().getResource(iconBean.getIconPathPressed());
+        button.setPressedIcon(new ImageIcon(imageURLPressed));
+        button.addActionListener(ActionRouter.getInstance());
+        button.setActionCommand(iconBean.getActionNameResolve());
+        return button;
+    }
+    
+    /**
+     * Parse icon set file.
+     * @return List of icons/action definition
+     */
+    private static LinkedHashMap<String, IconToolbarBean> getIconMappings() {
+        String iconProp = JMeterUtils.getPropDefault(keyIconProp, defaultIconProp); //$NON-NLS-1$
+        Properties p = JMeterUtils.loadProperties(iconProp);
+        if (p == null && !iconProp.equals(defaultIconProp)) {
+            log.info(iconProp + " not found - using " + defaultIconProp);
+            iconProp = defaultIconProp;
+            p = JMeterUtils.loadProperties(defaultIconProp);
+        }
+        if (p == null) {
+            JOptionPane.showMessageDialog(null, JMeterUtils
+                    .getResString("toolbar_icon_set_not_found"), // $NON-NLS-1$
+                    JMeterUtils.getResString("toolbar_icon_set_not_found"), // $NON-NLS-1$
+                    JOptionPane.WARNING_MESSAGE);
+            return null;
+        }
+        log.info("Loading toolbar icons properties from " + iconProp); //$NON-NLS-1$
+        
+        String order = p.getProperty("toolbar"); //$NON-NLS-1$
+        p.remove("toolbar"); //$NON-NLS-1$
+        String[] oList = order.split(","); //$NON-NLS-1$
+        
+        LinkedHashMap<String, IconToolbarBean> listIcons = new LinkedHashMap<String, IconToolbarBean>();
+        int ctr = 1;
+        for (String key : oList) {
+            log.debug("Toolbar icon key: " + key); //$NON-NLS-1$
+            if (key.trim().equals("|")) { //$NON-NLS-1$
+                listIcons.put("space" + ctr++, null); //$NON-NLS-1$
+            } else {
+                try {
+                    IconToolbarBean itb = new IconToolbarBean(p.getProperty(key));
+                    listIcons.put(key, itb);
+                } catch (JMeterException je) {
+                    log.error("Toolbar icon loading error - key: " + key); //$NON-NLS-1$
+                }
+            }
+        }
+        return listIcons;
+    }
+
+}
