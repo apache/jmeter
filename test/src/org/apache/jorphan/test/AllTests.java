4a926ed1d4e587201def1d9a871796c7808a11d1
Sebastian Bazley <sebb@apache.org>
2011-10-24 16:39:20 +0000

    Unused imports
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/trunk@1188214 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 74c125f5899933011d93bfb4379f656b1029b65f

--- test/src/org/apache/jorphan/test/AllTests.java
+++ test/src/org/apache/jorphan/test/AllTests.java
@@ -24,7 +24,6 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.lang.reflect.Method;
 import java.nio.charset.Charset;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
 import java.util.Properties;
