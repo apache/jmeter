cde7fa2dc2f0583f82ea0fbab3bf20009b55d5b2
Sebastian Bazley <sebb@apache.org>
2011-11-26 14:24:53 +0000

    GuiEditor was the wrong name; they are really type editors
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1206442 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 7bbb62f3061644a70aa4f4d8dc33eee15164549c

--- src/core/org/apache/jmeter/testbeans/BeanInfoSupport.java
+++ src/core/org/apache/jmeter/testbeans/BeanInfoSupport.java
@@ -29,7 +29,7 @@ import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 import org.apache.jmeter.testbeans.gui.GenericTestBeanCustomizer;
-import org.apache.jmeter.testbeans.gui.GuiEditor;
+import org.apache.jmeter.testbeans.gui.TypeEditor;
 import org.apache.jmeter.util.JMeterUtils;
 import org.apache.jorphan.logging.LoggingManager;
 import org.apache.log.Logger;
@@ -167,7 +167,7 @@ public abstract class BeanInfoSupport extends SimpleBeanInfo {
      *            property name
      * @return descriptor for a property of that name, or null if there's none
      */
-    protected PropertyDescriptor property(String name, GuiEditor editor) {
+    protected PropertyDescriptor property(String name, TypeEditor editor) {
         PropertyDescriptor property = property(name);
         if (property != null) {
             property.setValue(GenericTestBeanCustomizer.GUITYPE, editor);
