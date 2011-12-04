ececa3dc8bae1cc82c778e7fcb06a90d4f8f13a4
Bruno Demion <milamber@apache.org>
2011-12-04 12:48:24 +0000

    Fix a little GUI issue when Concurrent download is checked but not enabled, the Nb pool was editable
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1210102 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: bae108344c55f065d359cd6ddca82b4e95cffd46

--- src/protocol/http/org/apache/jmeter/protocol/http/control/gui/HttpTestSampleGui.java
+++ src/protocol/http/org/apache/jmeter/protocol/http/control/gui/HttpTestSampleGui.java
@@ -168,7 +168,7 @@ public class HttpTestSampleGui extends AbstractSamplerGui
         concurrentDwn = new JCheckBox(JMeterUtils.getResString("web_testing_concurrent_download")); // $NON-NLS-1$
         concurrentDwn.addItemListener(new ItemListener() {
             public void itemStateChanged(final ItemEvent e) {
-                if (e.getStateChange() == ItemEvent.SELECTED) { concurrentPool.setEnabled(true); }
+                if (getImages.isSelected() && e.getStateChange() == ItemEvent.SELECTED) { concurrentPool.setEnabled(true); }
                 else { concurrentPool.setEnabled(false); }
             }
         });
