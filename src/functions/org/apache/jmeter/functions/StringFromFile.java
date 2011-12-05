e01914c9f54d6a750008805e0c312295ec9496e7
Sebastian Bazley <sebb@apache.org>
2011-12-05 00:12:04 +0000

    Bug 52266 - Code:Inconsistent synchronization Ensure safe publication of variables
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1210289 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: a6f71b0a7f3152bdc2c574f77941a3713b613182

--- src/functions/org/apache/jmeter/functions/StringFromFile.java
+++ src/functions/org/apache/jmeter/functions/StringFromFile.java
@@ -91,11 +91,13 @@ public class StringFromFile extends AbstractFunction implements TestListener {
     // @GuardedBy("this")
     private Object[] values;
 
+    // @GuardedBy("this")
     private BufferedReader myBread = null; // Buffered reader
 
     // @GuardedBy("this")
     private boolean firstTime = false; // should we try to open the file?
 
+    // @GuardedBy("this")
     private String fileName; // needed for error messages
 
     // @GuardedBy("this")
@@ -116,7 +118,7 @@ public class StringFromFile extends AbstractFunction implements TestListener {
     /**
      * Close file and log
      */
-    private void closeFile() {
+    private synchronized void closeFile() {
         if (myBread == null) {
             return;
         }
