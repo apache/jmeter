71b9aa50662e0fa5a95a81da9d5f982993930376
Sebastian Bazley <sebb@apache.org>
2011-12-20 20:50:28 +0000

    Bug 52339 - JMeter Statistical mode in distributed testing shows wrong response time<
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1221486 13f79535-47bb-0310-9956-ffa450edef68

--- src/jorphan/org/apache/jorphan/math/StatCalculatorLong.java
+++ src/jorphan/org/apache/jorphan/math/StatCalculatorLong.java
@@ -27,16 +27,32 @@ public class StatCalculatorLong extends StatCalculator<Long> {
         super(Long.valueOf(0L), Long.valueOf(Long.MIN_VALUE), Long.valueOf(Long.MAX_VALUE));
     }
 
+    /**
+     * Add a single value (normally elapsed time)
+     * 
+     * @param val the value to add, which should correspond with a single sample
+     */
     public void addValue(long val){
         super.addValue(Long.valueOf(val));
     }
 
+    /**
+     * Update the calculator with the value for an aggregated sample.
+     * 
+     * @param val the aggregate value, normally the elapsed time
+     * @param sampleCount the number of samples contributing to the aggregate value
+     */
     public void addValue(long val, int sampleCount){
         super.addValue(Long.valueOf(val), sampleCount);
     }
 
     @Override
     protected Long divide(Long val, int n) {
+        return Long.valueOf(val.longValue() / n);
+    }
+
+    @Override
+    protected Long divide(Long val, long n) {
         return Long.valueOf(val.longValue() / n);
     }
 }
