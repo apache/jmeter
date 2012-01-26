04052cccdeea39ff72b7c4a635d13847914eef26
Sebastian Bazley <sebb@apache.org>
2012-01-26 20:12:10 +0000

    Bug 52497 - Improve DebugSampler and DebugPostProcessor
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1236372 13f79535-47bb-0310-9956-ffa450edef68

--- src/components/org/apache/jmeter/extractor/DebugPostProcessorBeanInfo.java
+++ src/components/org/apache/jmeter/extractor/DebugPostProcessorBeanInfo.java
@@ -18,12 +18,40 @@
 
 package org.apache.jmeter.extractor;
 
+import java.beans.PropertyDescriptor;
+
 import org.apache.jmeter.testbeans.BeanInfoSupport;
 
 public class DebugPostProcessorBeanInfo extends BeanInfoSupport {
 
     public DebugPostProcessorBeanInfo() {
         super(DebugPostProcessor.class);
+
+        PropertyDescriptor p;
+
+        p = property("displaySamplerProperties");
+        p.setValue(NOT_UNDEFINED, Boolean.TRUE);
+        p.setValue(NOT_EXPRESSION, Boolean.TRUE);
+        p.setValue(NOT_OTHER, Boolean.TRUE);
+        p.setValue(DEFAULT, Boolean.TRUE);
+
+        p = property("displayJMeterVariables");
+        p.setValue(NOT_UNDEFINED, Boolean.TRUE);
+        p.setValue(NOT_EXPRESSION, Boolean.TRUE);
+        p.setValue(NOT_OTHER, Boolean.TRUE);
+        p.setValue(DEFAULT, Boolean.TRUE);
+
+        p = property("displayJMeterProperties");
+        p.setValue(NOT_UNDEFINED, Boolean.TRUE);
+        p.setValue(NOT_EXPRESSION, Boolean.TRUE);
+        p.setValue(NOT_OTHER, Boolean.TRUE);
+        p.setValue(DEFAULT, Boolean.FALSE);
+
+        p = property("displaySystemProperties");
+        p.setValue(NOT_UNDEFINED, Boolean.TRUE);
+        p.setValue(NOT_EXPRESSION, Boolean.TRUE);
+        p.setValue(NOT_OTHER, Boolean.TRUE);
+        p.setValue(DEFAULT, Boolean.FALSE);
     }
 
 }
