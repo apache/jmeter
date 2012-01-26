baaf012f1988d96f919cd72a2c0f89de18aca449
Sebastian Bazley <sebb@apache.org>
2012-01-26 20:12:10 +0000

    Bug 52497 - Improve DebugSampler and DebugPostProcessor
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1236372 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: d56684415ae034a35708f7915dd02749a84aa07f

--- src/components/org/apache/jmeter/extractor/DebugPostProcessor.java
+++ src/components/org/apache/jmeter/extractor/DebugPostProcessor.java
@@ -18,6 +18,13 @@
 
 package org.apache.jmeter.extractor;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
+
 import org.apache.jmeter.processor.PostProcessor;
 import org.apache.jmeter.samplers.SampleResult;
 import org.apache.jmeter.testbeans.TestBean;
@@ -25,32 +32,123 @@ import org.apache.jmeter.testelement.AbstractTestElement;
 import org.apache.jmeter.testelement.property.JMeterProperty;
 import org.apache.jmeter.testelement.property.PropertyIterator;
 import org.apache.jmeter.threads.JMeterContext;
+import org.apache.jmeter.util.JMeterUtils;
 
 /**
  * Debugging Post-Processor: creates a subSample containing the variables defined in the previous sampler.
  */
 public class DebugPostProcessor extends AbstractTestElement implements PostProcessor, TestBean {
 
-    private static final long serialVersionUID = 240L;
+    private static final long serialVersionUID = 260L;
+
+    private boolean displaySamplerProperties;
+
+    private boolean displayJMeterVariables;
+
+    private boolean displayJMeterProperties;
+
+    private boolean displaySystemProperties;
 
     public void process(){
         StringBuilder sb = new StringBuilder(100);
+        StringBuilder rd = new StringBuilder(20); // for request Data
+        SampleResult sr = new SampleResult();
+        sr.setSampleLabel(getName());
+        sr.sampleStart();
         JMeterContext threadContext = getThreadContext();
-        PropertyIterator i = threadContext.getCurrentSampler().propertyIterator();
-        while(i.hasNext())
-        {
-            JMeterProperty prop = i.next();
-            sb.append(prop.getName());
-            sb.append("=");
-            sb.append(prop.getStringValue());
+        if (isDisplaySamplerProperties()){
+            rd.append("SamplerProperties\n");
+            sb.append("SamplerProperties:\n");
+            formatPropertyIterator(sb, threadContext.getCurrentSampler().propertyIterator());
             sb.append("\n");
-         }
-         SampleResult sr = new SampleResult();
-         sr.setResponseData(sb.toString(), null);
-         sr.setDataType(SampleResult.TEXT);
-         sr.setSampleLabel(getName());
-         sr.setSuccessful(true);
-         sr.setSamplerData("SamplerPropertyValues");
-         threadContext.getPreviousResult().addSubResult(sr);
+        }
+
+        if (isDisplayJMeterVariables()){
+            rd.append("JMeterVariables\n");
+            sb.append("JMeterVariables:\n");
+            formatSet(sb, threadContext.getVariables().entrySet());
+            sb.append("\n");
+        }
+
+        if (isDisplayJMeterProperties()){
+            rd.append("JMeterProperties\n");
+            sb.append("JMeterProperties:\n");
+            formatSet(sb, JMeterUtils.getJMeterProperties().entrySet());
+            sb.append("\n");
+        }
+
+        if (isDisplaySystemProperties()){
+            rd.append("SystemProperties\n");
+            sb.append("SystemProperties:\n");
+            formatSet(sb, System.getProperties().entrySet());
+            sb.append("\n");
+        }
+
+        sr.setResponseData(sb.toString(), null);
+        sr.setDataType(SampleResult.TEXT);
+        sr.setSamplerData(rd.toString());
+        sr.setResponseOK();
+        sr.sampleEnd();
+        threadContext.getPreviousResult().addSubResult(sr);
+    }
+
+    private void formatPropertyIterator(StringBuilder sb, PropertyIterator iter) {
+        Map<String, String> map = new HashMap<String, String>();
+        while (iter.hasNext()) {
+            JMeterProperty item = iter.next();
+            map.put(item.getName(), item.getStringValue());
+        }
+        formatSet(sb, map.entrySet());
+    }
+
+    private void formatSet(StringBuilder sb, @SuppressWarnings("rawtypes") Set s) {
+        @SuppressWarnings("unchecked")
+        ArrayList<Map.Entry<Object, Object>> al = new ArrayList<Map.Entry<Object, Object>>(s);
+        Collections.sort(al, new Comparator<Map.Entry<Object, Object>>(){
+            public int compare(Map.Entry<Object, Object> o1, Map.Entry<Object, Object> o2) {
+                String m1,m2;
+                m1=(String)o1.getKey();
+                m2=(String)o2.getKey();
+                return m1.compareTo(m2);
+            }
+        });
+        for(Map.Entry<Object, Object> me : al){
+            sb.append(me.getKey());
+            sb.append("=");
+            sb.append(me.getValue());
+            sb.append("\n");
+        }
+    }
+
+    public boolean isDisplayJMeterVariables() {
+        return displayJMeterVariables;
+    }
+
+    public void setDisplayJMeterVariables(boolean displayJMeterVariables) {
+        this.displayJMeterVariables = displayJMeterVariables;
+    }
+
+    public boolean isDisplayJMeterProperties() {
+        return displayJMeterProperties;
+    }
+
+    public void setDisplayJMeterProperties(boolean displayJMeterPropterties) {
+        this.displayJMeterProperties = displayJMeterPropterties;
+    }
+
+    public boolean isDisplaySamplerProperties() {
+        return displaySamplerProperties;
+    }
+
+    public void setDisplaySamplerProperties(boolean displaySamplerProperties) {
+        this.displaySamplerProperties = displaySamplerProperties;
+    }
+
+    public boolean isDisplaySystemProperties() {
+        return displaySystemProperties;
+    }
+
+    public void setDisplaySystemProperties(boolean displaySystemProperties) {
+        this.displaySystemProperties = displaySystemProperties;
     }
 }
