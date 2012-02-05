5ab94260765c1f7abb7546557071a4c986e31981
Philippe Mouawad <pmouawad@apache.org>
2012-02-05 13:27:02 +0000

    Bug 52603 - MailerVisualizer : Enable SSL , TLS and Authentication Javadocs+fix to missing case
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1240725 13f79535-47bb-0310-9956-ffa450edef68

--- src/components/org/apache/jmeter/reporters/MailerModel.java
+++ src/components/org/apache/jmeter/reporters/MailerModel.java
@@ -231,7 +231,9 @@ public class MailerModel extends AbstractTestElement implements Serializable {
 
                 try {
                     sendMail(getFromAddress(), addressList, getSuccessSubject(), "URL Restarted: "
-                            + sample.getSampleLabel(), getSmtpHost());
+                            + sample.getSampleLabel(), getSmtpHost(),
+                            getSmtpPort(), getLogin(), getPassword(),
+                            getMailAuthType());
                 } catch (Exception e) {
                     log.error("Problem sending mail", e);
                 }
@@ -314,6 +316,12 @@ public class MailerModel extends AbstractTestElement implements Serializable {
      *            the message-body.
      * @param smtpHost
      *            the smtp-server used to send the mail.
+     * @param smtpPort the smtp-server port used to send the mail.
+     * @param user the login used to authenticate
+     * @param password the password used to authenticate
+     * @param mailAuthType {@link MailAuthType} Security policy
+     * @throws AddressException
+     * @throws MessagingException
      */
     public void sendMail(String from, List<String> vEmails, String subject,
             String attText, String smtpHost, 
@@ -381,6 +389,11 @@ public class MailerModel extends AbstractTestElement implements Serializable {
         Transport.send(msg);
     }
 
+    /**
+     * Send a Test Mail to check configuration
+     * @throws AddressException
+     * @throws MessagingException
+     */
     public synchronized void sendTestMail() throws AddressException, MessagingException {
         String to = getToAddress();
         String from = getFromAddress();
