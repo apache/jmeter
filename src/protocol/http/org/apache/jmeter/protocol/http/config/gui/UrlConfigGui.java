29fec478b56736d688c17228990df3a4f522f80a
Philippe Mouawad <pmouawad@apache.org>
2012-02-07 06:24:05 +0000

    Bug 52613 - Using Raw Post Body option, text is considered as encoded
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1241359 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: a7c606151941c7337aadb7bb741b5e5d95fd53b9

--- src/protocol/http/org/apache/jmeter/protocol/http/config/gui/UrlConfigGui.java
+++ src/protocol/http/org/apache/jmeter/protocol/http/config/gui/UrlConfigGui.java
@@ -199,7 +199,7 @@ public class UrlConfigGui extends JPanel implements ChangeListener {
              * On retrival, CRLF is converted back to LF for storage in the text field.
              * See
              */
-            HTTPArgument arg = new HTTPArgument("", text.replaceAll("\n","\r\n"), true);
+            HTTPArgument arg = new HTTPArgument("", text.replaceAll("\n","\r\n"), false);
             arg.setAlwaysEncoded(false);
             args.addArgument(arg);
         } else {
