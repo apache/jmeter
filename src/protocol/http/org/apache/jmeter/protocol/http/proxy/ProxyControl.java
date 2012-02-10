2d133c1875b740117e132be3cbd6d402addd82ba
Philippe Mouawad <pmouawad@apache.org>
2012-02-10 18:31:29 +0000

    Fixed typo Fixed wrong log
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1242879 13f79535-47bb-0310-9956-ffa450edef68

--- src/protocol/http/org/apache/jmeter/protocol/http/proxy/ProxyControl.java
+++ src/protocol/http/org/apache/jmeter/protocol/http/proxy/ProxyControl.java
@@ -492,7 +492,7 @@ public class ProxyControl extends GenericController {
         if(log.isDebugEnabled()) {
             log.debug("Content-type to filter : " + sampleContentType);
         }
-        // Check if the include pattern is mathed
+        // Check if the include pattern is matched
         if(includeExp != null && includeExp.length() > 0) {
             if(log.isDebugEnabled()) {
                 log.debug("Include expression : " + includeExp);
@@ -509,7 +509,7 @@ public class ProxyControl extends GenericController {
             }
         }
 
-        // Check if the exclude pattern is mathed
+        // Check if the exclude pattern is matched
         if(excludeExp != null && excludeExp.length() > 0) {
             if(log.isDebugEnabled()) {
                 log.debug("Exclude expression : " + excludeExp);
@@ -522,7 +522,7 @@ public class ProxyControl extends GenericController {
                     return false;
                 }
             } catch (MalformedCachePatternException e) {
-                log.warn("Skipped invalid content exclude pattern: " + includeExp, e);
+                log.warn("Skipped invalid content exclude pattern: " + excludeExp, e);
             }
         }
 
