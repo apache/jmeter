2d61eaa94d6c15e9d254396b25b34b4384ed02ed
Sebastian Bazley <sebb@apache.org>
2012-03-09 10:59:40 +0000

    Bug 52868 - BSF language list should be sorted
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1298788 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 8f2e9af886b47e9bf62ded62b1af3d99742aacf8

--- src/core/org/apache/jmeter/util/BSFBeanInfoSupport.java
+++ src/core/org/apache/jmeter/util/BSFBeanInfoSupport.java
@@ -19,6 +19,7 @@
 package org.apache.jmeter.util;
 
 import java.beans.PropertyDescriptor;
+import java.util.Arrays;
 import java.util.Properties;
 
 import org.apache.jmeter.testbeans.BeanInfoSupport;
@@ -39,7 +40,8 @@ public abstract class BSFBeanInfoSupport extends BeanInfoSupport {
         for (Object language : languages.keySet()) {
             LANGUAGE_TAGS[i++] = language.toString();
         }
-        LANGUAGE_TAGS[i] = "jexl";
+        LANGUAGE_TAGS[i] = "jexl"; // $NON-NLS-1$
+        Arrays.sort(LANGUAGE_TAGS);
     }
 
     protected BSFBeanInfoSupport(Class<?> beanClass) {
