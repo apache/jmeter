a8faef585ed6272d1db9c0c7242107b1fea1fb22
Sebastian Bazley <sebb@apache.org>
2012-04-23 22:25:54 +0000

    Bug 53136 - Catching Throwable needs to be carefully handled No need to catch Throwable here
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1329484 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 3ef5637747f95e870175615728d4f20820dea7c7

--- src/reports/org/apache/jmeter/gui/util/ReportMenuFactory.java
+++ src/reports/org/apache/jmeter/gui/util/ReportMenuFactory.java
@@ -93,7 +93,7 @@ public final class ReportMenuFactory {
             }
 
             initializeMenus();
-        } catch (Throwable e) {
+        } catch (Exception e) {
             log.error("", e);
         }
     }
@@ -280,7 +280,7 @@ public final class ReportMenuFactory {
                 } catch (NoClassDefFoundError e) {
                     log.warn("Missing jar? Could not create " + name + ". " + e);
                     continue;
-                } catch (Throwable e) {
+                } catch (Exception e) {
                     log.warn("Could not instantiate " + name, e);
                     continue;
                 }
