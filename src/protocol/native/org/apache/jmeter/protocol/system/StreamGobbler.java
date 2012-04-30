3870d0118999e4e5be2d86fa7ee7edd8d3ee2202
Philippe Mouawad <pmouawad@apache.org>
2012-04-30 15:05:46 +0000

    Bug 53164 - New System Sampler
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1332231 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 480d00a0ddc470b96fdf26a31780126eb452ed3c

---
+++ src/protocol/native/org/apache/jmeter/protocol/system/StreamGobbler.java
@@ -0,0 +1,75 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.apache.jmeter.protocol.system;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * Thread that eats Output and Error Stream to avoid Deadlock on Windows Machines
+ */
+class StreamGobbler extends Thread {
+	private InputStream is;
+	private StringBuilder buffer = new StringBuilder();
+	/**
+	 * @param is {@link InputStream}
+	 */
+	StreamGobbler(InputStream is) {
+		this.is = is;
+	}
+
+	/**
+	 * @see java.lang.Thread#run()
+	 */
+	public void run() {
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new InputStreamReader(is));
+			String line = null;
+			while ((line = br.readLine()) != null)
+			{
+				buffer.append(line);
+				buffer.append("\r\n");
+			}
+		} catch (IOException e) {
+			buffer.append(e.getMessage());
+		}
+		finally
+		{
+			if(br != null)
+			{
+				try {
+					br.close();
+				} catch (Throwable e) {
+					// NOOP
+				}
+			}
+		}
+	}
+	
+	/**
+	 * @return
+	 */
+	public String getResult()
+	{
+		return buffer.toString();
+	}
+}
---
+++ src/protocol/native/org/apache/jmeter/protocol/system/SystemSampler.java
@@ -0,0 +1,230 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.apache.jmeter.protocol.system;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.jmeter.config.Argument;
+import org.apache.jmeter.config.Arguments;
+import org.apache.jmeter.samplers.AbstractSampler;
+import org.apache.jmeter.samplers.Entry;
+import org.apache.jmeter.samplers.SampleResult;
+import org.apache.jmeter.services.FileServer;
+import org.apache.jmeter.testelement.property.TestElementProperty;
+import org.apache.jorphan.logging.LoggingManager;
+import org.apache.log.Logger;
+
+/**
+ * A sampler for executing a System function. 
+ */
+public class SystemSampler extends AbstractSampler {
+    private static final long serialVersionUID = 1;
+    
+    public static final String COMMAND = "SystemSampler.command";
+    
+    public static final String DIRECTORY = "SystemSampler.directory";
+
+    public static final String ARGUMENTS = "SystemSampler.arguments";
+    
+    public static final String CHECK_RETURN_CODE = "SystemSampler.checkReturnCode";
+    
+    public static final String EXPECTED_RETURN_CODE = "SystemSampler.expectedReturnCode";
+
+    /**
+     * Logging
+     */
+    private static final Logger log = LoggingManager.getLoggerForClass();
+
+    /**
+     * Create a SystemSampler.
+     */
+    public SystemSampler() {
+        super();
+    }
+    
+    /**
+     * Performs a test sample.
+     * 
+     * @param entry
+     *            the Entry for this sample
+     * @return test SampleResult
+     */
+    public SampleResult sample(Entry entry) {
+        SampleResult results = new SampleResult();
+        results.setDataType(SampleResult.TEXT);
+        
+        try {
+            String command = getCommand();
+            Arguments args = getArguments();
+            boolean checkReturnCode = getCheckReturnCode();
+            int expectedReturnCode = getExpectedReturnCode();
+            
+            List<String> cmds = new ArrayList<String>(args.getArgumentCount()+1);
+            StringBuilder cmdLine = new StringBuilder((null == command) ? "" : command);
+            cmds.add(command);
+            for (int i=0;i<args.getArgumentCount();i++) {
+                Argument arg = args.getArgument(i);
+                cmds.add(arg.getPropertyAsString(Argument.VALUE));
+                cmdLine.append(" ");
+                cmdLine.append(cmds.get(i+1));
+            }
+
+            results.setSamplerData(cmdLine.toString());
+            
+            File directory = null;
+            if(StringUtils.isEmpty(getDirectory())) {
+                directory = new File(FileServer.getDefaultBase());
+                if(log.isDebugEnabled()) {
+                    log.debug("Using default directory:"+directory.getAbsolutePath());
+                }
+            } else {
+                directory = new File(getDirectory());
+                if(log.isDebugEnabled()) {
+                    log.debug("Using configured directory:"+directory.getAbsolutePath());
+                }
+            }
+            NativeCommand nativeCommand = new NativeCommand(directory);
+            
+            String responseData = null;
+            try {
+                if(log.isDebugEnabled()) {
+                    log.debug("Will run :"+cmdLine);
+                }
+                results.sampleStart();
+                int returnCode = nativeCommand.run(cmds);   
+                results.sampleEnd();
+
+                if (checkReturnCode && (returnCode != expectedReturnCode)) {
+                    results.setSuccessful(false);
+                    responseData = "System did not return expected return code.  Expected ["+expectedReturnCode+"]. Returned ["+returnCode+"].";
+                    results.setSampleLabel("FAILED: " + getName());
+                } else {
+                    results.setSuccessful(true);
+                    responseData = "System Call Complete.";
+                    results.setSampleLabel(getName());
+                }
+            } catch (IOException ioe) {
+                results.setSuccessful(false);
+                responseData = "Exception occured whilst executing System Call: "+ioe;
+                results.setSampleLabel("ERROR: " + getName());
+            } catch (InterruptedException ie) {
+                results.setSuccessful(false);
+                responseData = "System Sampler Interupted whilst executing System Call: "+ie;
+                results.setSampleLabel("ERROR: " + getName());
+            }
+    
+            results.setResponseData((responseData+"\nProcess Output:\n"+nativeCommand.getOutResult()).getBytes());
+            
+        } catch (Exception e) {
+            results.setSuccessful(false);
+            results.setResponseData(("Unknown Exception caught: "+e).getBytes());
+            results.setSampleLabel("ERROR: " + getName());
+        }
+        return results;
+    }
+
+    /**
+     * 
+     * @return
+     */
+    public String getDirectory() {
+        return getPropertyAsString(DIRECTORY, FileServer.getDefaultBase());
+    }
+    
+    /**
+     * 
+     * @param directory
+     */
+    public void setDirectory(String directory) {
+        setProperty(DIRECTORY, directory, FileServer.getDefaultBase());
+    }
+
+    /**
+     * Sets the Command attribute of the JavaConfig object
+     * 
+     * @param command
+     *            the new Command value
+     */
+    public void setCommand(String command) {
+        setProperty(COMMAND, command);
+    }
+
+    /**
+     * Gets the Command attribute of the JavaConfig object
+     * 
+     * @return the Command value
+     */
+    public String getCommand() {
+        return getPropertyAsString(COMMAND);
+    }
+    
+    /**
+     * Set the arguments (parameters) for the JavaSamplerClient to be executed
+     * with.
+     * 
+     * @param args
+     *            the new arguments. These replace any existing arguments.
+     */
+    public void setArguments(Arguments args) {
+        setProperty(new TestElementProperty(ARGUMENTS, args));
+    }
+
+    /**
+     * Get the arguments (parameters) for the JavaSamplerClient to be executed
+     * with.
+     * 
+     * @return the arguments
+     */
+    public Arguments getArguments() {
+        return (Arguments) getProperty(ARGUMENTS).getObjectValue();
+    }
+    
+    /**
+     * @param checkit boolean indicates if we check or not return code
+     */
+    public void setCheckReturnCode(boolean checkit) {
+        setProperty(CHECK_RETURN_CODE, (new Boolean(checkit)).toString());
+    }
+    
+    /**
+     * @return boolean indicating if we check or not return code
+     */
+    public boolean getCheckReturnCode() {
+        return getPropertyAsBoolean(CHECK_RETURN_CODE);
+    }
+    
+    /**
+     * @param code expected return code
+     */
+    public void setExpectedReturnCode(int code) {
+        setProperty(EXPECTED_RETURN_CODE, Integer.toString(code));
+    }
+    
+    /**
+     * @return expected return code
+     */
+    public int getExpectedReturnCode() {
+        return getPropertyAsInt(EXPECTED_RETURN_CODE);
+    }
+
+}
