1436854a6ba24b4688d601e59663434e671f20fc
Philippe Mouawad <pmouawad@apache.org>
2012-05-17 22:19:27 +0000

    Bug 53256 - Make Clear All command clean LogViewer content
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1339887 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 6b48a0e974b38c89d72a05c2169fcf679de4fb32

--- src/core/org/apache/jmeter/gui/MainFrame.java
+++ src/core/org/apache/jmeter/gui/MainFrame.java
@@ -755,6 +755,7 @@ public class MainFrame extends JFrame implements TestListener, Remoteable, DropT
 
     
     public void clearData() {
+        logPanel.clear();
         if(DISPLAY_ERROR_FATAL_COUNTER) {
             errorsAndFatalsCounterLogTarget.clearData();
         }
