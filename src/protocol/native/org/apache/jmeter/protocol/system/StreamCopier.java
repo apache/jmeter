9a8893139153a3c6e330daa2173c5877b4c2bb67
Sebastian Bazley <sebb@apache.org>
2012-09-02 22:35:25 +0000

    OS Process - allow specification of stdout/stderr/stdin Bugzilla Id: 53168
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1380074 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 774f7fb0cf9132ea76b9d1502f07cafb67100eda

---
+++ src/protocol/native/org/apache/jmeter/protocol/system/StreamCopier.java
@@ -0,0 +1,65 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.apache.jmeter.protocol.system;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.jorphan.logging.LoggingManager;
+import org.apache.log.Logger;
+
+/**
+ * Thread that copies a stream in the background; closes both input and output streams.
+ */
+class StreamCopier extends Thread {
+
+    private static final Logger log = LoggingManager.getLoggerForClass();
+
+    private final InputStream is;
+    private final OutputStream os;
+
+	/**
+	 * @param is {@link InputStream}
+	 * @throws IOException 
+	 */
+	StreamCopier(InputStream is, OutputStream os) throws IOException {
+		this.is = is;
+		this.os = os;
+	}
+
+	/**
+	 * @see java.lang.Thread#run()
+	 */
+	@Override
+    public void run() {
+	    try {
+            IOUtils.copyLarge(is, os);
+            os.close();
+            is.close();
+        } catch (IOException e) {
+            log.warn("Error writing stream", e);
+        } finally {
+            IOUtils.closeQuietly(is);
+            IOUtils.closeQuietly(os);
+        }
+	}
+	
+}
