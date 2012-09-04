1a3195d8ddc8e2f53b98277bcd987de643cf96bb
Philippe Mouawad <pmouawad@apache.org>
2012-09-04 19:59:44 +0000

    Add TODO
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1380833 13f79535-47bb-0310-9956-ffa450edef68

--- src/functions/org/apache/jmeter/functions/SamplerName.java
+++ src/functions/org/apache/jmeter/functions/SamplerName.java
@@ -45,6 +45,7 @@ public class SamplerName extends AbstractFunction {
 
     private Object[] values;
 
+    // TODO Should this method be synchronized ? all other function execute are
     /** {@inheritDoc} */
     @Override
     public String execute(SampleResult previousResult, Sampler currentSampler)
