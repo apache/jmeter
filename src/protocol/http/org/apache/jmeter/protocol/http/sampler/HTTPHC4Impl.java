b9888ccace0b14ba05ad076402bfc1c9629eb93a
Philippe Mouawad <pmouawad@apache.org>
2012-09-04 20:40:02 +0000

    Remove unused import
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1380870 13f79535-47bb-0310-9956-ffa450edef68

--- src/protocol/http/org/apache/jmeter/protocol/http/sampler/HTTPHC4Impl.java
+++ src/protocol/http/org/apache/jmeter/protocol/http/sampler/HTTPHC4Impl.java
@@ -23,7 +23,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.UnsupportedEncodingException;
 import java.net.InetAddress;
 import java.net.URI;
 import java.net.URL;
