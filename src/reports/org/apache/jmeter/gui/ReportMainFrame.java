66a8cda7952c1ddfd5f0e1147592c1370041691f
Philippe Mouawad <pmouawad@apache.org>
2012-09-05 20:31:54 +0000

    use indexOf(char) remove unused import
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1381348 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 826cb94cb3462757e271d3f016241540ed0488e9

--- src/reports/org/apache/jmeter/gui/ReportMainFrame.java
+++ src/reports/org/apache/jmeter/gui/ReportMainFrame.java
@@ -24,7 +24,6 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -298,7 +297,7 @@ public class ReportMainFrame extends JFrame {
 
         // allow for windows / chars in filename
         String temp = fname.replace('\\', '/'); // $NON-NLS-1$ // $NON-NLS-2$
-        String simpleName = temp.substring(temp.lastIndexOf("/") + 1);// $NON-NLS-1$
+        String simpleName = temp.substring(temp.lastIndexOf('/') + 1);// $NON-NLS-1$
         setTitle(simpleName + " (" + fname + ") - " + DEFAULT_TITLE); // $NON-NLS-1$ // $NON-NLS-2$
     }
 
