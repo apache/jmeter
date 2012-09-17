0d7d7a0b67a7228f400365d807b1489c6443f26b
Philippe Mouawad <pmouawad@apache.org>
2012-09-17 20:08:45 +0000

    Bug 53879 - GUI : Allow Popups to be closed with ESC key
    Use String as less as possible
    Bugzilla Id: 53879
    
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1386800 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 3a69122e525c8530aecd2e2111626f9b9a89b909

--- src/core/org/apache/jmeter/gui/action/SearchTreeDialog.java
+++ src/core/org/apache/jmeter/gui/action/SearchTreeDialog.java
@@ -87,24 +87,35 @@ public class SearchTreeDialog extends JDialog implements ActionListener {
     @Override
     protected JRootPane createRootPane() {
         JRootPane rootPane = new JRootPane();
-        InputMap inputMap = rootPane.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);  
-        ActionMap actionMap = rootPane.getActionMap();
-        inputMap.put(KeyStrokes.ESC, "ESCAPE");
-        inputMap.put(KeyStrokes.ENTER, "ENTER");
         // Hide Window on ESC
-        Action escapeAction = new AbstractAction() { 
+        Action escapeAction = new AbstractAction("ESCAPE") { 
+            /**
+             * 
+             */
+            private static final long serialVersionUID = -6543764044868772971L;
+
             public void actionPerformed(ActionEvent actionEvent) { 
                 setVisible(false);
             } 
         };
-        actionMap.put("ESCAPE", escapeAction);
         // Do search on Enter
-        Action enterAction = new AbstractAction() { 
+        Action enterAction = new AbstractAction("ENTER") { 
+            /**
+             * 
+             */
+            private static final long serialVersionUID = -3661361497864527363L;
+
             public void actionPerformed(ActionEvent actionEvent) { 
                 doSearch(actionEvent);
             } 
         };
-        actionMap.put("ENTER", enterAction);
+        ActionMap actionMap = rootPane.getActionMap();
+        actionMap.put(escapeAction.getValue(Action.NAME), escapeAction);
+        actionMap.put(enterAction.getValue(Action.NAME), enterAction);
+        InputMap inputMap = rootPane.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);  
+        inputMap.put(KeyStrokes.ESC, escapeAction.getValue(Action.NAME));
+        inputMap.put(KeyStrokes.ENTER, enterAction.getValue(Action.NAME));
+
         return rootPane;
     }
 
