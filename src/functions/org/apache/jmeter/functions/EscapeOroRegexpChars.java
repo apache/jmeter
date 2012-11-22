37bb27a775db76b4a30f3127460f5410783e4b0d
Philippe Mouawad <pmouawad@apache.org>
2012-11-22 12:23:44 +0000

    Bug 54189 - Add a function to quote ORO regexp meta characters Bugzilla Id: 54189
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1412519 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 18e0b73190b56a6874d85098156865fe2398bfd7

---
+++ src/functions/org/apache/jmeter/functions/EscapeOroRegexpChars.java
@@ -0,0 +1,120 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.apache.jmeter.functions;
+
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.jmeter.engine.util.CompoundVariable;
+import org.apache.jmeter.samplers.SampleResult;
+import org.apache.jmeter.samplers.Sampler;
+import org.apache.jmeter.threads.JMeterVariables;
+import org.apache.jmeter.util.JMeterUtils;
+import org.apache.jorphan.logging.LoggingManager;
+import org.apache.log.Logger;
+import org.apache.oro.text.regex.Perl5Compiler;
+
+/**
+ * Escape ORO meta characters
+ * @since 2.9
+ */
+public class EscapeOroRegexpChars extends AbstractFunction {
+    private static final Logger log = LoggingManager.getLoggerForClass();
+
+    private static final List<String> desc = new LinkedList<String>();
+
+    private static final String KEY = "__escapeOroRegexpChars"; //$NON-NLS-1$
+
+    static {
+        desc.add(JMeterUtils.getResString("value_to_quote_meta")); //$NON-NLS-1$
+        desc.add(JMeterUtils.getResString("function_name_paropt")); //$NON-NLS-1$
+    }
+
+    private CompoundVariable[] values;
+
+    private static final int MAX_PARAM_COUNT = 2;
+
+    private static final int MIN_PARAM_COUNT = 1;
+    
+    private static final int CHARS = 1;
+
+    private static final int PARAM_NAME = 2;
+
+    /**
+     * No-arg constructor.
+     */
+    public EscapeOroRegexpChars() {
+        super();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public synchronized String execute(SampleResult previousResult, Sampler currentSampler)
+            throws InvalidVariableException {
+
+        String charsToUse = null;//means no restriction
+        if (values.length >= CHARS) {
+            charsToUse = (values[CHARS - 1]).execute().trim();
+            if (charsToUse.length() <= 0) { // empty chars, return to null
+                charsToUse = null;
+            }
+        }
+
+        String myName = "";//$NON-NLS-1$
+        if (values.length >= PARAM_NAME) {
+            myName = (values[PARAM_NAME - 1]).execute().trim();
+        }
+
+        String myValue = Perl5Compiler.quotemeta(charsToUse);
+         
+        if (myName.length() > 0) {
+            JMeterVariables vars = getVariables();
+            if (vars != null) {// Can be null if called from Config item testEnded() method
+                vars.put(myName, myValue);
+            }
+        }
+
+        if (log.isDebugEnabled()) {
+            String tn = Thread.currentThread().getName();
+            log.debug(tn + " name:" //$NON-NLS-1$
+                    + myName + " value:" + myValue);//$NON-NLS-1$
+        }
+
+        return myValue;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public synchronized void setParameters(Collection<CompoundVariable> parameters) throws InvalidVariableException {
+        checkParameterCount(parameters, MIN_PARAM_COUNT, MAX_PARAM_COUNT);
+        values = parameters.toArray(new CompoundVariable[parameters.size()]);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String getReferenceKey() {
+        return KEY;
+    }
+
+    /** {@inheritDoc} */
+    public List<String> getArgumentDesc() {
+        return desc;
+    }
+}
