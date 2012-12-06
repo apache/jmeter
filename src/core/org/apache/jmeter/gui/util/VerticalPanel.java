b5ad9f97331f73a5a2a9ba5bdc233323622cdc6f
Sebastian Bazley <sebb@apache.org>
2012-12-06 01:02:10 +0000

    Remove spurious comments
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1417720 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 8bd6c40ef834ea0a7b035a7f750570ac82e90ff1

--- src/core/org/apache/jmeter/gui/util/VerticalPanel.java
+++ src/core/org/apache/jmeter/gui/util/VerticalPanel.java
@@ -16,12 +16,6 @@
  *
  */
 
-/*
- * Created on Apr 25, 2003
- *
- * To change the template for this generated file go to
- * Window>Preferences>Java>Code Generation>Code and Comments
- */
 package org.apache.jmeter.gui.util;
 
 import java.awt.Color;
