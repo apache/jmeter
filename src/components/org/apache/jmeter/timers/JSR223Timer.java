032970fa2e594e56d115c461ea01b6353b7172cf
Philippe Mouawad <pmouawad@apache.org>
2012-12-27 15:16:53 +0000

    Change log level when file is missing of scripting error occurs
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1426222 13f79535-47bb-0310-9956-ffa450edef68

--- src/components/org/apache/jmeter/timers/JSR223Timer.java
+++ src/components/org/apache/jmeter/timers/JSR223Timer.java
@@ -46,11 +46,11 @@ public class JSR223Timer extends JSR223TestElement implements Cloneable, Timer, 
             }
             delay = Long.parseLong(o.toString());
         } catch (NumberFormatException e) {
-            log.warn("Problem in JSR223 script ", e);
+            log.error("Problem in JSR223 script ", e);
         } catch (IOException e) {
-            log.warn("Problem in JSR223 script ", e);
+            log.error("Problem in JSR223 script ", e);
         } catch (ScriptException e) {
-            log.warn("Problem in JSR223 script ", e);
+            log.error("Problem in JSR223 script ", e);
         }
         return delay;
     }
