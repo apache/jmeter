f40c2e8e5d590b2e814f57ac88ac221f556060cc
Philippe Mouawad <pmouawad@apache.org>
2013-03-02 23:45:21 +0000

    Bug 54584 - MongoDB plugin Bugzilla Id: 54584
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1451961 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/protocol/mongodb/org/apache/jmeter/protocol/mongodb/sampler/MongoScriptSamplerBeanInfo.java
@@ -0,0 +1,75 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.apache.jmeter.protocol.mongodb.sampler;
+
+import java.beans.PropertyDescriptor;
+
+import org.apache.jmeter.testbeans.BeanInfoSupport;
+import org.apache.jmeter.testbeans.gui.TextAreaEditor;
+import org.apache.jmeter.testbeans.gui.TypeEditor;
+import org.apache.jorphan.logging.LoggingManager;
+import org.apache.log.Logger;
+
+/**
+  */
+public class MongoScriptSamplerBeanInfo
+    extends BeanInfoSupport {
+
+    private static final Logger log = LoggingManager.getLoggerForClass();
+
+    public MongoScriptSamplerBeanInfo() {
+        super(MongoScriptSampler.class);
+
+        //http://api.mongodb.org/java/2.7.2/com/mongodb/Mongo.html
+        createPropertyGroup("mongodb", new String[] {
+                "source",
+                "database",
+                "username",
+                "password" });
+
+        createPropertyGroup("sampler", new String[]{
+                "script"});
+
+        PropertyDescriptor p = property("database");
+        p.setValue(NOT_UNDEFINED, Boolean.TRUE);
+        p.setValue(DEFAULT, "");
+        p = property("username");
+        p.setValue(NOT_UNDEFINED, Boolean.TRUE);
+        p.setValue(DEFAULT, "");
+        p = property("password", TypeEditor.PasswordEditor);
+        p.setValue(NOT_UNDEFINED, Boolean.TRUE);
+        p.setValue(DEFAULT, "");
+        p = property("source");
+        p.setValue(NOT_UNDEFINED, Boolean.TRUE);
+        p.setValue(DEFAULT, "");
+
+        p = property("script");
+        p.setValue(NOT_UNDEFINED, Boolean.FALSE);
+        p.setValue(DEFAULT, "");
+        p.setValue(NOT_EXPRESSION, Boolean.TRUE);
+        p.setPropertyEditorClass(TextAreaEditor.class);
+
+        if(log.isDebugEnabled()) {
+            for (PropertyDescriptor pd : getPropertyDescriptors()) {
+                log.debug(pd.getName());
+                log.debug(pd.getDisplayName());
+            }
+        }
+    }
+}
