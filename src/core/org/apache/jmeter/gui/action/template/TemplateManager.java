a16f3e90c22539e7b08abd38a43389bc80348c08
Philippe Mouawad <pmouawad@apache.org>
2013-06-29 20:08:15 +0000

    Bug 55085 - UX Improvement : Ability to create New Test Plan from Templates Bugzilla Id: 55085
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1498018 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 59abba4ace7b45df4f481f8e0d943c7d0a6881ac

---
+++ src/core/org/apache/jmeter/gui/action/template/TemplateManager.java
@@ -0,0 +1,140 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.apache.jmeter.gui.action.template;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.jmeter.util.JMeterUtils;
+import org.apache.jorphan.logging.LoggingManager;
+import org.apache.jorphan.util.JOrphanUtils;
+import org.apache.log.Logger;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
+
+/**
+ * Manages Test Plan templates
+ * @since 2.10
+ */
+public class TemplateManager {
+    private class Templates {
+        private Map<String, Template> templates = new HashMap<String, Template>();
+    }
+    private static final String TEMPLATE_FILES = JMeterUtils.getPropDefault("template.files", // $NON-NLS-1$
+            "/bin/templates/templates.xml");
+
+    private static final Logger log = LoggingManager.getLoggerForClass();
+    
+    private static final TemplateManager SINGLETON = new TemplateManager();
+    
+    private Map<String, Template> templates = new HashMap<String, Template>();
+
+    private XStream xstream = initXStream();
+
+    /**
+     * 
+     * @return
+     */
+    public static final TemplateManager getInstance() {
+        return SINGLETON;
+    }
+    
+    private TemplateManager()  {
+        try {
+            templates = readTemplates();            
+        } catch(IOException e) {
+            log.error("Error loading templates from files referenced in templates.files property:"+
+                    TEMPLATE_FILES, e);
+        }
+    }
+    
+    private XStream initXStream() {
+        XStream xstream = new XStream(new DomDriver());
+        xstream.alias("template", Template.class);
+        xstream.alias("templates", Templates.class);
+        
+        // templates i
+        xstream.addImplicitMap(Templates.class, 
+                // field TemplateManager#templates 
+                "templates", // $NON-NLS-1$
+                Template.class,     
+                // field Template#name 
+                "name" // $NON-NLS-1$
+                );
+                
+        return xstream;
+    }
+
+    public void addTemplate(Template template) {
+        templates.put(template.getName(), template);
+    }
+
+    /**
+     * @return Set<String> the templates names
+     */
+    public Set<String> getTemplateNames() {
+        return templates.keySet();
+    }
+
+
+    private Map<String, Template> readTemplates() throws FileNotFoundException {
+        Map<String, Template> templates = new HashMap<String, Template>();
+       
+        String[] templateFiles = TEMPLATE_FILES.split(",");
+        for (int i = 0; i < templateFiles.length; i++) {
+            if(!StringUtils.isEmpty(templateFiles[i])) {
+                InputStream inputStream = null;
+                File f = new File(JMeterUtils.getJMeterHome(),
+                        templateFiles[i]); 
+                try {
+                    if(f.exists() && f.canRead()) {
+                        log.info("Reading templates from:"+f.getAbsolutePath());
+                        inputStream = new BufferedInputStream(new FileInputStream(f)); 
+                                
+                        templates.putAll(((Templates) xstream.fromXML(inputStream)).templates);
+                    } else {
+                        log.warn("Ignoring template file:'"+f.getAbsolutePath()+"' as it does not exist or is not readable");
+                    }
+                } catch(Exception ex) {                    
+                    log.warn("Ignoring template file:'"+f.getAbsolutePath()+"', an error occured parsing the file", ex);
+                } finally {
+                    JOrphanUtils.closeQuietly(inputStream);
+                } 
+            }
+        }
+        return templates;
+    }
+
+    /**
+     * @param selectedTemplate Template name
+     * @return {@link Template}
+     */
+    public Template getTemplateByName(String selectedTemplate) {
+        return templates.get(selectedTemplate);
+    }
+}
