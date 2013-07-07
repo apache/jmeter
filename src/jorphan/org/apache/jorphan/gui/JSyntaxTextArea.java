fa2405cca4679366fff2d651500c64b850851c32
Sebastian Bazley <sebb@apache.org>
2013-07-07 02:36:50 +0000

    Opps, forgot JMeterUtils was in core
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1500355 13f79535-47bb-0310-9956-ffa450edef68

--- src/jorphan/org/apache/jorphan/gui/JSyntaxTextArea.java
+++ src/jorphan/org/apache/jorphan/gui/JSyntaxTextArea.java
@@ -18,7 +18,8 @@
 
 package org.apache.jorphan.gui;
 
-import org.apache.jmeter.util.JMeterUtils;
+// jorphan cannot call methods in core because it is built first
+//import org.apache.jmeter.util.JMeterUtils;
 import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
 import org.fife.ui.rsyntaxtextarea.SyntaxConstants;
 
@@ -29,9 +30,9 @@ public class JSyntaxTextArea extends RSyntaxTextArea {
 
     private static final long serialVersionUID = 210L;
 
-    private final boolean WRAP_STYLE_WORD = JMeterUtils.getPropDefault("jsyntaxtextarea.wrapstyleword", true);
-    private final boolean LINE_WRAP = JMeterUtils.getPropDefault("jsyntaxtextarea.linewrap", true);
-    private final boolean CODE_FOLDING = JMeterUtils.getPropDefault("jsyntaxtextarea.codefolding", true);
+    private final boolean WRAP_STYLE_WORD = true;//JMeterUtils.getPropDefault("jsyntaxtextarea.wrapstyleword", true);
+    private final boolean LINE_WRAP = true;//JMeterUtils.getPropDefault("jsyntaxtextarea.linewrap", true);
+    private final boolean CODE_FOLDING = true;//JMeterUtils.getPropDefault("jsyntaxtextarea.codefolding", true);
 
     /**
      * Creates the default syntax highlighting text area.
