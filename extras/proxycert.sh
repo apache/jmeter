231f06cc8dda6ee7a25aea7f3b5ee2a14dfdc5c4
Sebastian Bazley <sebb@apache.org>
2013-08-22 09:08:28 +0000

    Make validity an optional parameter; check if output file exists
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1516391 13f79535-47bb-0310-9956-ffa450edef68

--- extras/proxycert.sh
+++ extras/proxycert.sh
@@ -17,16 +17,25 @@
 
 
 ## Generate temporary proxyserver for use with JMeter Proxy recorder
+## Usage:  sh proxycert.sh [validity(default 1)]
+## e.g. sh proxyxcert.sh 7
+
+KEYSTORE=proxyserver.jks
+if [ -r ${KEYSTORE} ]
+then
+    echo "${KEYSTORE} exists; please rename or delete it before creating a replacement"
+    exit 1
+fi
 
 DNAME="cn=JMeter Proxy (DO NOT TRUST)"
-VALIDITY=1
+VALIDITY=${1:-1}
 # Must agree with property proxy.cert.keystorepass
 STOREPASSWORD=password
 # Must agree with proxy.cert.keypassword
 KEYPASSWORD=password
 
 ## generate the keystore with the certificate
-keytool -genkeypair -alias jmeter -keystore proxyserver.jks -keypass ${KEYPASSWORD} -storepass ${STOREPASSWORD} -validity ${VALIDITY} -keyalg RSA -dname ${DNAME}
+keytool -genkeypair -alias jmeter -keystore ${KEYSTORE} -keypass ${KEYPASSWORD} -storepass ${STOREPASSWORD} -validity ${VALIDITY} -keyalg RSA -dname ${DNAME}
 
 ## show the contents
-keytool -list -v -keystore proxyserver.jks -storepass ${STOREPASSWORD}
+keytool -list -v -keystore ${KEYSTORE} -storepass ${STOREPASSWORD}
