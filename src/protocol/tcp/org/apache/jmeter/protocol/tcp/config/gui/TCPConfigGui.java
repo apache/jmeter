e8351f3a19d817784ab4c953df566cc61bac7a84
Philippe Mouawad <pmouawad@apache.org>
2013-09-27 22:10:59 +0000

    Bug 55600 - JSyntaxTextArea : Strange behaviour on first undo Bugzilla Id: 55600
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1527102 13f79535-47bb-0310-9956-ffa450edef68

--- src/protocol/tcp/org/apache/jmeter/protocol/tcp/config/gui/TCPConfigGui.java
+++ src/protocol/tcp/org/apache/jmeter/protocol/tcp/config/gui/TCPConfigGui.java
@@ -95,7 +95,7 @@ public class TCPConfigGui extends AbstractConfigGui {
         serverPanel.setConnectTimeout(element.getPropertyAsString(TCPSampler.TIMEOUT_CONNECT));
         setNoDelay.setTristateFromProperty(element, TCPSampler.NODELAY);
 //        setNoDelay.setSelected(element.getPropertyAsBoolean(TCPSampler.NODELAY));
-        requestData.setText(element.getPropertyAsString(TCPSampler.REQUEST));
+        requestData.setInitialText(element.getPropertyAsString(TCPSampler.REQUEST));
         requestData.setCaretPosition(0);
         closeConnection.setTristateFromProperty(element, TCPSampler.CLOSE_CONNECTION);
 //        closeConnection.setSelected(element.getPropertyAsBoolean(TCPSampler.CLOSE_CONNECTION, TCPSampler.CLOSE_CONNECTION_DEFAULT));
@@ -144,7 +144,7 @@ public class TCPConfigGui extends AbstractConfigGui {
 
         serverPanel.clear();
         classname.setText(""); //$NON-NLS-1$
-        requestData.setText(""); //$NON-NLS-1$
+        requestData.setInitialText(""); //$NON-NLS-1$
         reUseConnection.setSelected(true);
         setNoDelay.setSelected(false); // TODO should this be indeterminate?
         closeConnection.setSelected(TCPSampler.CLOSE_CONNECTION_DEFAULT); // TODO should this be indeterminate?
