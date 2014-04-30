86b1154cff470499270aba4d79a8683a26559dc8
dzmitrykashlach <dzmitrykashlach@gmail.com>
2014-04-30 14:43:56 +0300

    continue.on.remote_engine_fail=true

--- src/core/org/apache/jmeter/EngineReInitializer.java
+++ src/core/org/apache/jmeter/EngineReInitializer.java
@@ -1,20 +1,47 @@
 package org.apache.jmeter;
 
+import org.apache.jmeter.engine.ClientJMeterEngine;
+import org.apache.jmeter.engine.JMeterEngine;
+import org.apache.jorphan.collections.HashTree;
+import org.apache.jorphan.logging.LoggingManager;
+import org.apache.log.Logger;
+
 /**
  * Created by dzmitrykashlach on 4/30/14.
  */
 public class EngineReInitializer implements Runnable {
-    public final String engine;
-    public int numberOfRetries;
+    private static final Logger log = LoggingManager.getLoggerForClass();
+    private final String engineStr;
+    private JMeterEngine engine;
+    private int numberOfRetries;
+    private int delayBetweenRetries;
+    private HashTree tree;
 
-    public EngineReInitializer(int numberOfRetries, String engine) {
+
+    public EngineReInitializer(int numberOfRetries, String engineStr, int delayBetweenRetries, HashTree tree) {
         this.numberOfRetries = numberOfRetries;
-        this.engine = engine;
+        this.engineStr = engineStr;
+        this.delayBetweenRetries = delayBetweenRetries;
+        this.tree = tree;
+
     }
 
     @Override
     public void run() {
-//        JMeterEngine eng = doRemoteInit(engine, tree);
+        for (int i = 0; i < this.numberOfRetries; i++) {
+            try {
+                this.engine = new ClientJMeterEngine(this.engineStr);
+                Thread.sleep(this.delayBetweenRetries);
+            } catch (Exception e) {
+                log.fatalError("Failure connecting to remote host: " + this.engineStr, e);
+                System.err.println("Failure connecting to remote host: " + this.engineStr + " " + e);
+                return;
+            }
+            engine.configure(this.tree);
+        }
+    }
 
+    public JMeterEngine getEngine() {
+        return engine;
     }
 }
