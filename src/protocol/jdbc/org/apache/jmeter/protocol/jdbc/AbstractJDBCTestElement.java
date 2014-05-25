76a776f9f412cb5e685b8ef7623a70e9dc317ac7
Philippe Mouawad <pmouawad@apache.org>
2014-05-25 20:34:37 +0000

    Bug 46932 - Alias given in select statement is not used as column header in response data for a JDBC request Bugzilla Id: 46932
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1597469 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 42847fd2ceea42c53ff9f25b9281a308f94068be

--- src/protocol/jdbc/org/apache/jmeter/protocol/jdbc/AbstractJDBCTestElement.java
+++ src/protocol/jdbc/org/apache/jmeter/protocol/jdbc/AbstractJDBCTestElement.java
@@ -386,7 +386,7 @@ public abstract class AbstractJDBCTestElement extends AbstractTestElement implem
 
         int numColumns = meta.getColumnCount();
         for (int i = 1; i <= numColumns; i++) {
-            sb.append(meta.getColumnName(i));
+            sb.append(meta.getColumnLabel(i));
             if (i==numColumns){
                 sb.append('\n');
             } else {
@@ -414,7 +414,7 @@ public abstract class AbstractJDBCTestElement extends AbstractTestElement implem
                         row = new HashMap<String, Object>(numColumns);
                         results.add(row);
                     }
-                    row.put(meta.getColumnName(i), o);
+                    row.put(meta.getColumnLabel(i), o);
                 }
                 if (o instanceof byte[]) {
                     o = new String((byte[]) o, ENCODING);
