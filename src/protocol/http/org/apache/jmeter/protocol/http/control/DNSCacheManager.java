8e7d338bc75603c90473690aa29d88c1ef42e662
dzmitrykashlach <dzmitrykashlach@gmail.com>
2014-08-02 19:11:08 +0300

    remove ARecords&DNSServers

--- src/protocol/http/org/apache/jmeter/protocol/http/control/DNSCacheManager.java
+++ src/protocol/http/org/apache/jmeter/protocol/http/control/DNSCacheManager.java
@@ -2,14 +2,10 @@ package org.apache.jmeter.protocol.http.control;
 
 import org.apache.jmeter.config.ConfigTestElement;
 import org.apache.jmeter.engine.event.LoopIterationEvent;
-import org.apache.jmeter.protocol.http.control.dnscachemanager.ARecord;
-import org.apache.jmeter.protocol.http.control.dnscachemanager.DNSServer;
 import org.apache.jmeter.testelement.TestIterationListener;
 import org.apache.jmeter.testelement.TestStateListener;
 import org.apache.jmeter.testelement.property.BooleanProperty;
 import org.apache.jmeter.testelement.property.CollectionProperty;
-import org.apache.jmeter.testelement.property.PropertyIterator;
-import org.apache.jmeter.testelement.property.TestElementProperty;
 import org.apache.jmeter.util.JMeterUtils;
 import org.apache.jorphan.logging.LoggingManager;
 import org.apache.log.Logger;
@@ -69,14 +65,6 @@ public class DNSCacheManager extends ConfigTestElement implements TestStateListe
         return (CollectionProperty) getProperty(DNS_SERVERS);
     }
 
-    public CollectionProperty getARecords() {
-        return (CollectionProperty) getProperty(A_RECORDS);
-    }
-
-    public int getDNSServersCount() {// Used by GUI
-        return getDNSServers().size();
-    }
-
     public boolean getClearEachIteration() {
         return getPropertyAsBoolean(CLEAR);
     }
@@ -86,18 +74,6 @@ public class DNSCacheManager extends ConfigTestElement implements TestStateListe
     }
 
 
-    public String getPredefinedARecord(String host) {
-        List<TestElementProperty> aRecords = (List<TestElementProperty>) getARecords().getObjectValue();
-        Iterator<TestElementProperty> iterator = aRecords.iterator();
-        long curTime = System.currentTimeMillis();
-        while (iterator.hasNext()) {
-            ARecord aRecord = (ARecord) iterator.next().getObjectValue();
-            if (aRecord.getName().equals(host) && curTime <= aRecord.getExpires()) {
-                return aRecord.getIP();
-            }
-        }
-        return "";
-    }
 
     public String doRequest(Lookup lookup) {
         Record[] records = lookup.run();
@@ -110,47 +86,8 @@ public class DNSCacheManager extends ConfigTestElement implements TestStateListe
         return "";
     }
     public String resolve(String host) throws TextParseException, UnknownHostException {
-        // if A record is in table, then use it.
-        /*
-        1. Get list of names from A Records table
-        2. if host is in list -> take IP and return it. Otherwise, continue with dnsjava
-         */
-        String aRecord = getPredefinedARecord(host);
-        if (!aRecord.isEmpty()) {
-            return aRecord;
-        }
-        List<TestElementProperty> dnsServers = (List<TestElementProperty>) getDNSServers().getObjectValue();
 
-        Comparator<TestElementProperty> dnsServerComparator = new Comparator<TestElementProperty>() {
-            @Override
-            public int compare(TestElementProperty o1, TestElementProperty o2) {
-                int prior1 = ((DNSServer) o1.getObjectValue()).getPriority();
-                int prior2 = ((DNSServer) o2.getObjectValue()).getPriority();
-                return prior2 - prior1;
-            }
-        };
-
-
-        Collections.sort(dnsServers, dnsServerComparator);
-        Iterator<TestElementProperty> iterator = dnsServers.iterator();
-        StringBuilder ip = new StringBuilder();
-        while (iterator.hasNext()) {
-            TestElementProperty dnsServer = iterator.next();
-            ip.append(((DNSServer) dnsServer.getObjectValue()).getIP());
-            InetSocketAddress dns = new InetSocketAddress(ip.toString(), 53);
-            ((SimpleResolver) resolver).setAddress(dns);
-            Lookup.setDefaultResolver(resolver);
-            Lookup.setDefaultCache(cache, DClass.IN);
-            Lookup lookup = new Lookup(host, Type.A, DClass.IN);
-            String recordStr = doRequest(lookup);
-            if (!recordStr.isEmpty()) {
-                return recordStr;
-            }
-
-        }
-        Lookup.refreshDefault();
-        Lookup lookup = new Lookup(host, Type.A, DClass.IN);
-        return doRequest(lookup);
+        return null;
     }
 
     /**
@@ -161,43 +98,8 @@ public class DNSCacheManager extends ConfigTestElement implements TestStateListe
         // do nothing, the DNS cache manager has to accept changes.
     }
 
-    /**
-     * Add a server.
-     */
-    public void addDNSServer(DNSServer dnsServer) {
-        String dnsIP = dnsServer.getIP();
-        removeMatchingServers(dnsServer); // Can't have two matching servers
 
-        if (DELETE_NULL_SERVERS && (null == dnsIP || dnsIP.length() == 0)) {
-            if (log.isDebugEnabled()) {
-                log.debug("Dropping dnsServer with null IP " + dnsServer.toString());
-            }
-        } else {
-            if (log.isDebugEnabled()) {
-                log.debug("Add dnsServer to store " + dnsServer.toString());
-            }
-            getDNSServers().addItem(dnsServer);
-        }
-    }
 
-    /**
-     * Add an A Record.
-     */
-    public void addARecord(ARecord aRecord) {
-        String aRIP = aRecord.getIP();
-        removeMatchingARecords(aRecord); // Can't have two matching servers
-
-        if (DELETE_NULL_SERVERS && (null == aRIP || aRIP.length() == 0)) {
-            if (log.isDebugEnabled()) {
-                log.debug("Dropping dnsServer with null IP " + aRecord.toString());
-            }
-        } else {
-            if (log.isDebugEnabled()) {
-                log.debug("Add dnsServer to store " + aRecord.toString());
-            }
-            getARecords().addItem(aRecord);
-        }
-    }
 
     /**
      * {@inheritDoc}
@@ -205,113 +107,7 @@ public class DNSCacheManager extends ConfigTestElement implements TestStateListe
     @Override
     public void clear() {
         super.clear();
-        clearDNSServers(); // ensure data is set up OK initially
-        clearARecords();
     }
-
-    /*
-     * Remove all the servers.
-     */
-    private void clearDNSServers() {
-        log.debug("Clear all servers from store");
-        setProperty(new CollectionProperty(DNS_SERVERS, new ArrayList<Object>()));
-    }
-
-    /*
-     * Remove all the aRecords
-     */
-    private void clearARecords() {
-        log.debug("Clear all aRecords from store");
-        setProperty(new CollectionProperty(A_RECORDS, new ArrayList<Object>()));
-    }
-
-    /**
-     * Remove a server.
-     */
-    public void remove(int index) {// TODO not used by GUI
-        getDNSServers().remove(index);
-    }
-
-    /**
-     * Return the server at index i.
-     */
-    public DNSServer get(int i) {// Only used by GUI
-        return (DNSServer) getDNSServers().get(i).getObjectValue();
-    }
-
-    /**
-     * Check if servers match, i.e. name, path and domain are equal.
-     * <br/>
-     * TODO - should we compare secure too?
-     *
-     * @param a
-     * @param b
-     * @return true if servers match
-     */
-    private boolean matchServer(DNSServer a, DNSServer b) {
-        return
-                a.getName().equals(b.getName())
-                        &&
-                        a.getIP().equals(b.getIP())
-                        &&
-                        a.getPriority() == b.getPriority();
-    }
-
-
-    /**
-     * Check if servers match, i.e. name, path and domain are equal.
-     * <br/>
-     * TODO - should we compare secure too?
-     *
-     * @param a
-     * @param b
-     * @return true if servers match
-     */
-    private boolean matchRecord(ARecord a, ARecord b) {
-        return
-                a.getName().equals(b.getName())
-                        &&
-                        a.getIP().equals(b.getIP())
-                        &&
-                        a.getExpires() == b.getExpires();
-    }
-
-    void removeMatchingServers(DNSServer dnsServer) {
-        // Scan for any matching dnsServers
-        PropertyIterator iter = getDNSServers().iterator();
-        while (iter.hasNext()) {
-            DNSServer dnsServIter = (DNSServer) iter.next().getObjectValue();
-            if (dnsServer == null) {// TODO is this possible?
-                continue;
-            }
-            if (matchServer(dnsServIter, dnsServer)) {
-                if (log.isDebugEnabled()) {
-                    log.debug("New DNSServer = " + dnsServer.toString()
-                            + " removing matching DNSServer " + dnsServer.toString());
-                }
-                iter.remove();
-            }
-        }
-    }
-
-    void removeMatchingARecords(ARecord aRecord) {
-        // Scan for any matching dnsServers
-        PropertyIterator iter = getARecords().iterator();
-        while (iter.hasNext()) {
-            ARecord aRecordIter = (ARecord) iter.next().getObjectValue();
-            if (aRecord == null) {// TODO is this possible?
-                continue;
-            }
-            if (matchRecord(aRecordIter, aRecord)) {
-                if (log.isDebugEnabled()) {
-                    log.debug("New DNSServer = " + aRecord.toString()
-                            + " removing matching DNSServer " + aRecord.toString());
-                }
-                iter.remove();
-            }
-        }
-    }
-
 
     /**
      * {@inheritDoc}
@@ -354,5 +150,4 @@ public class DNSCacheManager extends ConfigTestElement implements TestStateListe
             this.cache.clearCache();
         }
     }
-
 }
--- src/protocol/http/org/apache/jmeter/protocol/http/gui/DNSPanel.java
+++ src/protocol/http/org/apache/jmeter/protocol/http/gui/DNSPanel.java
@@ -1,16 +1,8 @@
 package org.apache.jmeter.protocol.http.gui;
 
 import org.apache.jmeter.config.gui.AbstractConfigGui;
-import org.apache.jmeter.gui.util.PowerTableModel;
 import org.apache.jmeter.protocol.http.control.DNSCacheManager;
-import org.apache.jmeter.protocol.http.control.dnscachemanager.ARecord;
-import org.apache.jmeter.protocol.http.control.dnscachemanager.Constants;
-import org.apache.jmeter.protocol.http.control.dnscachemanager.DNSServer;
-import org.apache.jmeter.protocol.http.gui.dnspanel.ARecordsPanel;
-import org.apache.jmeter.protocol.http.gui.dnspanel.DNSServersPanel;
 import org.apache.jmeter.testelement.TestElement;
-import org.apache.jmeter.testelement.property.PropertyIterator;
-import org.apache.jorphan.gui.GuiUtils;
 import org.apache.jorphan.gui.layout.VerticalLayout;
 import org.apache.jorphan.logging.LoggingManager;
 import org.apache.log.Logger;
@@ -22,14 +14,13 @@ import java.awt.*;
  * Created by dzmitrykashlach on 6/13/14.
  */
 public class DNSPanel extends AbstractConfigGui {
+    public static final String CLEAR_CACHE_EACH_ITER = "Clear cache each iteration";
+    public static final String OPTIONS = "Options";
 
     private static final long serialVersionUID = 240L;
 
     private static final Logger log = LoggingManager.getLoggerForClass();
 
-    private JPanel dnsServersPanel;
-
-    private JPanel aRecordsPanel;
 
 
     private JCheckBox clearEachIteration;
@@ -41,25 +32,13 @@ public class DNSPanel extends AbstractConfigGui {
         init();
     }
 
-    @Override
-    public String getStaticLabel() {
-        return Constants.DNS_CACHE_MANAGER; //$NON-NLS-1$
-    }
 
     @Override
     public String getLabelResource() {
-        return null;
+        return "dns_cache_manager_title";
     }
 
-    private void addDNSToTable(DNSServer dnsServer) {
-        ((DNSServersPanel) dnsServersPanel).getTableModel().
-                addRow(new Object[]{dnsServer.getName(), dnsServer.getIP(), dnsServer.getPriority()});
-    }
 
-    private void addARecordToTable(ARecord aRecord) {
-        ((ARecordsPanel) aRecordsPanel).getTableModel().
-                addRow(new Object[]{aRecord.getName(), aRecord.getIP(), aRecord.getExpires()});
-    }
 
     /**
      * Modifies a given TestElement to mirror the data in the gui components.
@@ -68,24 +47,10 @@ public class DNSPanel extends AbstractConfigGui {
      */
     @Override
     public void modifyTestElement(TestElement dnsCm) {
-        GuiUtils.stopTableEditing(((DNSServersPanel) dnsServersPanel).getDnsServersTable());
-        GuiUtils.stopTableEditing(((ARecordsPanel) aRecordsPanel).getARecordsTable());
         dnsCm.clear();
         configureTestElement(dnsCm);
         if (dnsCm instanceof DNSCacheManager) {
             DNSCacheManager dnsCacheManager = (DNSCacheManager) dnsCm;
-            PowerTableModel dnsServersModel = ((DNSServersPanel) dnsServersPanel).getTableModel();
-            for (int i = 0; i < dnsServersModel.getRowCount(); i++) {
-                DNSServer dnsServer = createDNSServer(dnsServersModel.getRowData(i));
-                dnsCacheManager.addDNSServer(dnsServer);
-            }
-            PowerTableModel aRecordsModel = ((ARecordsPanel) aRecordsPanel).getTableModel();
-
-            for (int i = 0; i < aRecordsModel.getRowCount(); i++) {
-                ARecord aRecord = createARecord(aRecordsModel.getRowData(i));
-                dnsCacheManager.addARecord(aRecord);
-            }
-
             dnsCacheManager.setClearEachIteration(clearEachIteration.isSelected());
         }
     }
@@ -96,48 +61,9 @@ public class DNSPanel extends AbstractConfigGui {
     @Override
     public void clearGui() {
         super.clearGui();
-        ((DNSServersPanel) dnsServersPanel).resetButtons();
-        ((ARecordsPanel) aRecordsPanel).resetButtons();
-        PowerTableModel dnsServersModel = ((DNSServersPanel) dnsServersPanel).getTableModel();
-        dnsServersModel.clearData();
-        PowerTableModel aRecordsModel = ((ARecordsPanel) aRecordsPanel).getTableModel();
-        aRecordsModel.clearData();
         clearEachIteration.setSelected(false);
     }
 
-    private DNSServer createDNSServer(Object[] rowData) {
-        DNSServer dnsServer = new DNSServer(
-                (String) rowData[0],
-                (String) rowData[1],
-                (Integer) rowData[2]); // Non-expiring
-        return dnsServer;
-    }
-
-    private ARecord createARecord(Object[] rowData) {
-        ARecord aRecord = new ARecord(
-                (String) rowData[0],
-                (String) rowData[1],
-                (Long) rowData[2]); // Non-expiring
-        return aRecord;
-    }
-
-    private void populateTables(DNSCacheManager manager) {
-        PowerTableModel dnsServersModel = ((DNSServersPanel) dnsServersPanel).getTableModel();
-        dnsServersModel.clearData();
-        PowerTableModel aRecordsModel = ((ARecordsPanel) aRecordsPanel).getTableModel();
-        aRecordsModel.clearData();
-        PropertyIterator iter;
-
-        iter = manager.getDNSServers().iterator();
-        while (iter.hasNext()) {
-            addDNSToTable((DNSServer) iter.next().getObjectValue());
-        }
-
-        iter = manager.getARecords().iterator();
-        while (iter.hasNext()) {
-            addARecordToTable((ARecord) iter.next().getObjectValue());
-        }
-    }
 
     @Override
     public TestElement createTestElement() {
@@ -151,7 +77,6 @@ public class DNSPanel extends AbstractConfigGui {
         super.configure(el);
 
         DNSCacheManager dnsCacheManager = (DNSCacheManager) el;
-        populateTables(dnsCacheManager);
         clearEachIteration.setSelected((dnsCacheManager).getClearEachIteration());
     }
 
@@ -160,7 +85,7 @@ public class DNSPanel extends AbstractConfigGui {
      */
     private void init() {
         clearEachIteration =
-                new JCheckBox(Constants.CLEAR_CACHE_EACH_ITER, false); //$NON-NLS-1$
+                new JCheckBox(CLEAR_CACHE_EACH_ITER, false); //$NON-NLS-1$
         setLayout(new BorderLayout());
         setBorder(makeBorder());
         JPanel northPanel = new JPanel();
@@ -168,7 +93,7 @@ public class DNSPanel extends AbstractConfigGui {
         northPanel.add(makeTitlePanel());
         JPanel optionsPane = new JPanel();
         optionsPane.setBorder(BorderFactory.createTitledBorder(
-                BorderFactory.createEtchedBorder(), Constants.OPTIONS)); // $NON-NLS-1$
+                BorderFactory.createEtchedBorder(), OPTIONS)); // $NON-NLS-1$
         optionsPane.setLayout(new VerticalLayout(5, VerticalLayout.BOTH));
         optionsPane.add(clearEachIteration);
         JPanel policyTypePane = new JPanel();
@@ -176,10 +101,6 @@ public class DNSPanel extends AbstractConfigGui {
         optionsPane.add(policyTypePane);
         northPanel.add(optionsPane);
         add(northPanel, BorderLayout.NORTH);
-        dnsServersPanel = new DNSServersPanel();
-        aRecordsPanel = new ARecordsPanel();
-        add(dnsServersPanel, BorderLayout.CENTER);
-        add(aRecordsPanel, BorderLayout.SOUTH);
     }
 }
 
