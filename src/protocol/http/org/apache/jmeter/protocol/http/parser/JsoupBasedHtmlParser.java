abcd82cd8752d46fb527107448cdc075061fa102
Sebastian Bazley <sebb@apache.org>
2014-08-18 13:48:44 +0000

    Log currently unused
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1618607 13f79535-47bb-0310-9956-ffa450edef68

--- src/protocol/http/org/apache/jmeter/protocol/http/parser/JsoupBasedHtmlParser.java
+++ src/protocol/http/org/apache/jmeter/protocol/http/parser/JsoupBasedHtmlParser.java
@@ -24,8 +24,8 @@ import java.util.Iterator;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.jmeter.protocol.http.util.ConversionUtils;
-import org.apache.jorphan.logging.LoggingManager;
-import org.apache.log.Logger;
+//import org.apache.jorphan.logging.LoggingManager;
+//import org.apache.log.Logger;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -39,7 +39,7 @@ import org.jsoup.select.NodeVisitor;
  * TODO Factor out common code between {@link LagartoBasedHtmlParser} and this one (adapter pattern)
  */
 public class JsoupBasedHtmlParser extends HTMLParser {
-    private static final Logger log = LoggingManager.getLoggerForClass();
+//    private static final Logger log = LoggingManager.getLoggerForClass();
 
     /*
      * A dummy class to pass the pointer of URL.
