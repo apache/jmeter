04be5e012c7bc2796bdfbd4e919e911f289a53b1
Philippe Mouawad <pmouawad@apache.org>
2014-09-07 08:31:46 +0000

    Bug 42248 - Undo-redo support on Test Plan tree modification Temporary disable as I missed the right place to call it Bugzilla Id: 42248
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1622984 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 9a8ad8a2688a0c6fc38d5ec1510ded0b89c2879e

--- src/core/org/apache/jmeter/gui/action/UndoCommand.java
+++ src/core/org/apache/jmeter/gui/action/UndoCommand.java
@@ -47,12 +47,16 @@ public class UndoCommand implements Command {
 
         if (command.equals(ActionNames.UNDO)) {
             guiPackage.goInHistory(-1);
+            /*
             guiPackage.getMainFrame().updateUndoRedoIcons(guiPackage.canUndo()
                     ,guiPackage.canRedo());
+                    */
         } else if (command.equals(ActionNames.REDO)) {
             guiPackage.goInHistory(1);
+            /*
             guiPackage.getMainFrame().updateUndoRedoIcons(guiPackage.canUndo()
                     ,guiPackage.canRedo());
+                   */
         } else {
             throw new IllegalArgumentException("Wrong action called: " + command);
         }
