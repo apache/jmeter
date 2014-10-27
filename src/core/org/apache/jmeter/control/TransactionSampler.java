faf1dffc0fe48c17a56c029f140a5b745710bfdb
Philippe Mouawad <pmouawad@apache.org>
2014-10-27 15:20:12 +0000

    Javadocs
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1634569 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: a271eaafea966696e3db88c3f43468820558530a

--- src/core/org/apache/jmeter/control/TransactionSampler.java
+++ src/core/org/apache/jmeter/control/TransactionSampler.java
@@ -36,7 +36,7 @@ import org.apache.jmeter.testelement.TestElement;
 
 /**
  * Transaction Sampler class to measure transaction times
- * (not exposed a a GUI class, as it is only used internally)
+ * (not exposed a a GUI class, as it is only used internally by TransactionController in Generate Parent sample mode)
  */
 public class TransactionSampler extends AbstractSampler {
     private static final long serialVersionUID = 240L;
