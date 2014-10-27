a829042b1cbcdaebc0cce2991760f088f13c50ed
Philippe Mouawad <pmouawad@apache.org>
2014-10-27 13:42:36 +0000

    Bug 57114 - Performance : Functions that only have values as instance variable should not synchronize execute Keep synchronized where it is needed Bugzilla Id: 57114
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1634536 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 8cd3077396be2b32c4649fc6858458c729b945be

--- src/functions/org/apache/jmeter/functions/LogFunction.java
+++ src/functions/org/apache/jmeter/functions/LogFunction.java
@@ -76,7 +76,7 @@ public class LogFunction extends AbstractFunction {
 
     /** {@inheritDoc} */
     @Override
-    public synchronized String execute(SampleResult previousResult, Sampler currentSampler)
+    public String execute(SampleResult previousResult, Sampler currentSampler)
             throws InvalidVariableException {
         // The method is synchronized to avoid interference of messages from multiple threads
         String stringToLog = ((CompoundVariable) values[0]).execute();
@@ -134,7 +134,7 @@ public class LogFunction extends AbstractFunction {
     }
 
     // Routine to perform the output (also used by __logn() function)
-    static void logDetails(Logger l, String s, String prio, Throwable t, String c) {
+    static synchronized void logDetails(Logger l, String s, String prio, Throwable t, String c) {
         if (prio.equalsIgnoreCase("OUT")) //$NON-NLS-1
         {
             printDetails(System.out, s, t, c);
