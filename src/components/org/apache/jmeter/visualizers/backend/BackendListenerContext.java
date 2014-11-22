3d54ec68ac4ea8fd9b35961b251f13c03419f03a
Philippe Mouawad <pmouawad@apache.org>
2014-11-22 19:15:09 +0000

    Rename log to LOGGER as per naming rules and felix notes Fix warning on wrapper
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1641107 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: fabf95777a26f89ad0caea872cddafa3837c3966

--- src/components/org/apache/jmeter/visualizers/backend/BackendListenerContext.java
+++ src/components/org/apache/jmeter/visualizers/backend/BackendListenerContext.java
@@ -45,7 +45,7 @@ public class BackendListenerContext {
      */
 
     /** Logging */
-    private static final Logger log = LoggingManager.getLoggerForClass();
+    private static final Logger LOGGER = LoggingManager.getLoggerForClass();
 
     /**
      * Map containing the initialization parameters for the BackendListenerClient.
@@ -164,7 +164,7 @@ public class BackendListenerContext {
         try {
             return Integer.decode(params.get(name)).intValue();
         } catch (NumberFormatException e) {
-            log.warn("Value for parameter '" + name + "' not an integer: '" + params.get(name) + "'.  Using default: '"
+            LOGGER.warn("Value for parameter '" + name + "' not an integer: '" + params.get(name) + "'.  Using default: '"
                     + defaultValue + "'.", e);
             return defaultValue;
         }
@@ -216,7 +216,7 @@ public class BackendListenerContext {
         try {
             return Long.decode(params.get(name)).longValue();
         } catch (NumberFormatException e) {
-            log.warn("Value for parameter '" + name + "' not a long: '" + params.get(name) + "'.  Using default: '"
+            LOGGER.warn("Value for parameter '" + name + "' not a long: '" + params.get(name) + "'.  Using default: '"
                     + defaultValue + "'.", e);
             return defaultValue;
         }
@@ -232,6 +232,6 @@ public class BackendListenerContext {
         if (params == null || !params.containsKey(name)) {
             return defaultValue;
         }
-        return Boolean.valueOf(params.get(name));
+        return Boolean.parseBoolean(params.get(name));
     }
 }
