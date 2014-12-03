96f43ea05ee7a6daefba337400d99a3cb314e436
Andrey Pohilko <apc4@ya.ru>
2014-12-03 17:07:02 +0300

    Use connect time in appropriate places

--- src/core/org/apache/jmeter/samplers/SampleSaveConfiguration.java
+++ src/core/org/apache/jmeter/samplers/SampleSaveConfiguration.java
@@ -303,7 +303,7 @@ public class SampleSaveConfiguration implements Cloneable, Serializable {
         _subresults      = TRUE.equalsIgnoreCase(props.getProperty(SUBRESULTS_PROP, TRUE));
         _assertions      = TRUE.equalsIgnoreCase(props.getProperty(ASSERTIONS_PROP, TRUE));
         _latency         = TRUE.equalsIgnoreCase(props.getProperty(LATENCY_PROP, TRUE));
-        _connectTime     = TRUE.equalsIgnoreCase(props.getProperty(CONNECT_TIME_PROP, TRUE));
+        _connectTime     = TRUE.equalsIgnoreCase(props.getProperty(CONNECT_TIME_PROP, FALSE));
         _samplerData     = TRUE.equalsIgnoreCase(props.getProperty(SAMPLERDATA_PROP, FALSE));
         _responseHeaders = TRUE.equalsIgnoreCase(props.getProperty(RESPONSEHEADERS_PROP, FALSE));
         _requestHeaders  = TRUE.equalsIgnoreCase(props.getProperty(REQUESTHEADERS_PROP, FALSE));
@@ -644,7 +644,7 @@ public class SampleSaveConfiguration implements Cloneable, Serializable {
     }
 
     public void setConnectTime(boolean connectTime) {
-        this.connectTime= connectTime;
+        this.connectTime = connectTime;
     }
 
     public boolean saveMessage() {
