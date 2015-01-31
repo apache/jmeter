be8120170c3ece30c168485bbef00f446f9f6ccf
Philippe Mouawad <pmouawad@apache.org>
2015-01-31 20:36:42 +0000

    Bug 57269 - Drop org.apache.jmeter.reports package Update FILEVERSION Bugzilla Id: 57269
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1656253 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: bd4937e3cf2650c280c6c6dd31dc16a5b3fac079

--- src/core/org/apache/jmeter/save/SaveService.java
+++ src/core/org/apache/jmeter/save/SaveService.java
@@ -182,7 +182,7 @@ public class SaveService {
     private static String fileVersion = ""; // read from saveservice.properties file// $NON-NLS-1$
     // Must match Revision id value in saveservice.properties, 
     // used to ensure saveservice.properties and SaveService are updated simultaneously
-    private static final String FILEVERSION = "1656250"; // Expected value $NON-NLS-1$
+    private static final String FILEVERSION = "1656252"; // Expected value $NON-NLS-1$
     private static String fileEncoding = ""; // read from properties file// $NON-NLS-1$
 
     static {
