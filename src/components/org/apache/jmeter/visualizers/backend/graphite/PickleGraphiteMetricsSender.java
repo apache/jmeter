bd3222ed039e3c1d072e5ebdb9b312b35e8ba56c
Philippe Mouawad <pmouawad@apache.org>
2015-03-01 13:47:19 +0000

    Document why writer/stream is not closed
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1663099 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: cfca7179487c800e66646636c95f65f47b88dfe3

--- src/components/org/apache/jmeter/visualizers/backend/graphite/PickleGraphiteMetricsSender.java
+++ src/components/org/apache/jmeter/visualizers/backend/graphite/PickleGraphiteMetricsSender.java
@@ -109,6 +109,7 @@ class PickleGraphiteMetricsSender extends AbstractGraphiteMetricsSender {
 
                 out = socketOutputStreamPool.borrowObject(socketConnectionInfos);
                 out.write(header);
+                //pickleWriter is not close as it would close the underlying pooled out
                 Writer pickleWriter = new OutputStreamWriter(out, CHARSET_NAME);
                 pickleWriter.write(payload);
                 pickleWriter.flush();
