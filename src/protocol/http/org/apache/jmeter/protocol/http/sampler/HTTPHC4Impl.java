cde7612f77acbf5cd862efcbc9e41b1726af705e
Felix Schumacher <fschumacher@apache.org>
2015-04-26 11:51:09 +0000

    Don't call sampleEnd twice in HTTPHC4Impl when a RuntimeException or an IOException occurs in the sample method.
    Bugzilla Id: 57858
    
    
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1676099 13f79535-47bb-0310-9956-ffa450edef68

--- src/protocol/http/org/apache/jmeter/protocol/http/sampler/HTTPHC4Impl.java
+++ src/protocol/http/org/apache/jmeter/protocol/http/sampler/HTTPHC4Impl.java
@@ -411,15 +411,22 @@ public class HTTPHC4Impl extends HTTPHCAbstractImpl {
             res = resultProcessing(areFollowingRedirect, frameDepth, res);
 
         } catch (IOException e) {
-            res.sampleEnd();
             log.debug("IOException", e);
-           // pick up headers if failed to execute the request
+            if (res.getEndTime() == 0) {
+                res.sampleEnd();
+            }
+            // pick up headers if failed to execute the request
+            if (res.getRequestHeaders() != null) {
+                log.debug("Overwriting request old headers: " + res.getRequestHeaders());
+            }
             res.setRequestHeaders(getConnectionHeaders((HttpRequest) localContext.getAttribute(ExecutionContext.HTTP_REQUEST)));
             errorResult(e, res);
             return res;
         } catch (RuntimeException e) {
-            res.sampleEnd();
             log.debug("RuntimeException", e);
+            if (res.getEndTime() == 0) {
+                res.sampleEnd();
+            }
             errorResult(e, res);
             return res;
         } finally {
