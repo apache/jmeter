88448a845957ee1601b5dd75fee1bc2ec32b0aec
Philippe Mouawad <pmouawad@apache.org>
2015-06-28 20:15:43 +0000

    Java7 multiple exception catching
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1688058 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: fb9efa0c3d6119807203c69dddde39920ef0b465

--- src/core/org/apache/jmeter/reporters/ResultCollector.java
+++ src/core/org/apache/jmeter/reporters/ResultCollector.java
@@ -404,17 +404,10 @@ public class ResultCollector extends AbstractListenerElement implements SampleLi
                         }
                     }
                 }
-            } catch (IOException e) {
+            } catch (IOException|JMeterError|RuntimeException|OutOfMemoryError e) {
+                // FIXME Why do we catch OOM ?
                 log.warn("Problem reading JTL file: "+file);
-            } catch (JMeterError e){
-                log.warn("Problem reading JTL file: "+file);
-            } catch (RuntimeException e){ // e.g. NullPointerException
-                log.warn("Problem reading JTL file: "+file,e);
-            } catch (OutOfMemoryError e) {
-                log.warn("Problem reading JTL file: "+file,e);
-            } catch (ConfigurationException e) { // Avalon only
-                log.warn("Problem reading Avalon JTL file: "+file,e);
-            } catch (SAXException e) { // Avalon only
+            } catch (ConfigurationException|SAXException e) { // Avalon only
                 log.warn("Problem reading Avalon JTL file: "+file,e);
             } finally {
                 JOrphanUtils.closeQuietly(dataReader);
