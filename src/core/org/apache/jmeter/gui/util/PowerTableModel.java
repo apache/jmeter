21d3d6e2f0034040470179426b5bf8ac08b7d708
Philippe Mouawad <pmouawad@apache.org>
2015-06-28 20:16:19 +0000

    Java7 multiple exception catching
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1688059 13f79535-47bb-0310-9956-ffa450edef68

--- src/core/org/apache/jmeter/gui/util/PowerTableModel.java
+++ src/core/org/apache/jmeter/gui/util/PowerTableModel.java
@@ -141,74 +141,47 @@ public class PowerTableModel extends DefaultTableModel {
             try {
                 Constructor<?> constr = colClass.getConstructor(new Class[] { String.class });
                 return constr.newInstance(new Object[] { "" });
-            } catch (NoSuchMethodException err) {
-            } catch (InstantiationException err) {
-            } catch (IllegalAccessException err) {
-            } catch (InvocationTargetException err) {
+            } catch (NoSuchMethodException|InstantiationException|IllegalAccessException|InvocationTargetException ignored) {
             }
             try {
                 Constructor<?> constr = colClass.getConstructor(new Class[] { Integer.TYPE });
                 return constr.newInstance(new Object[] { Integer.valueOf(0) });
-            } catch (NoSuchMethodException err) {
-            } catch (InstantiationException err) {
-            } catch (IllegalAccessException err) {
-            } catch (InvocationTargetException err) {
+            } catch (NoSuchMethodException|InstantiationException|IllegalAccessException|InvocationTargetException ignored) {
             }
             try {
                 Constructor<?> constr = colClass.getConstructor(new Class[] { Long.TYPE });
                 return constr.newInstance(new Object[] { Long.valueOf(0L) });
-            } catch (NoSuchMethodException err) {
-            } catch (InstantiationException err) {
-            } catch (IllegalAccessException err) {
-            } catch (InvocationTargetException err) {
+            } catch (NoSuchMethodException|InstantiationException|IllegalAccessException|InvocationTargetException ignored) {
             }
             try {
                 Constructor<?> constr = colClass.getConstructor(new Class[] { Boolean.TYPE });
                 return constr.newInstance(new Object[] { Boolean.FALSE });
-            } catch (NoSuchMethodException err) {
-            } catch (InstantiationException err) {
-            } catch (IllegalAccessException err) {
-            } catch (InvocationTargetException err) {
+            } catch (NoSuchMethodException|InstantiationException|IllegalAccessException|InvocationTargetException ignored) {
             }
             try {
                 Constructor<?> constr = colClass.getConstructor(new Class[] { Float.TYPE });
                 return constr.newInstance(new Object[] { Float.valueOf(0F) });
-            } catch (NoSuchMethodException err) {
-            } catch (InstantiationException err) {
-            } catch (IllegalAccessException err) {
-            } catch (InvocationTargetException err) {
+            } catch (NoSuchMethodException|InstantiationException|IllegalAccessException|InvocationTargetException ignored) {
             }
             try {
                 Constructor<?> constr = colClass.getConstructor(new Class[] { Double.TYPE });
                 return constr.newInstance(new Object[] { Double.valueOf(0D) });
-            } catch (NoSuchMethodException err) {
-            } catch (InstantiationException err) {
-            } catch (IllegalAccessException err) {
-            } catch (InvocationTargetException err) {
+            } catch (NoSuchMethodException|InstantiationException|IllegalAccessException|InvocationTargetException ignored) {
             }
             try {
                 Constructor<?> constr = colClass.getConstructor(new Class[] { Character.TYPE });
                 return constr.newInstance(new Object[] { Character.valueOf(' ') });
-            } catch (NoSuchMethodException err) {
-            } catch (InstantiationException err) {
-            } catch (IllegalAccessException err) {
-            } catch (InvocationTargetException err) {
+            } catch (NoSuchMethodException|InstantiationException|IllegalAccessException|InvocationTargetException ignored) {
             }
             try {
                 Constructor<?> constr = colClass.getConstructor(new Class[] { Byte.TYPE });
                 return constr.newInstance(new Object[] { Byte.valueOf(Byte.MIN_VALUE) });
-            } catch (NoSuchMethodException err) {
-            } catch (InstantiationException err) {
-            } catch (IllegalAccessException err) {
-            } catch (InvocationTargetException err) {
+            } catch (NoSuchMethodException|InstantiationException|IllegalAccessException|InvocationTargetException ignored) {
             }
             try {
                 Constructor<?> constr = colClass.getConstructor(new Class[] { Short.TYPE });
                 return constr.newInstance(new Object[] { Short.valueOf(Short.MIN_VALUE) });
-            } catch (NoSuchMethodException err) {
-            } catch (InstantiationException err) {
-            } catch (IllegalAccessException err) {
-            } catch (InvocationTargetException err) {
+            } catch (NoSuchMethodException|InstantiationException|IllegalAccessException|InvocationTargetException ignored) {
             }
         }
         return "";
