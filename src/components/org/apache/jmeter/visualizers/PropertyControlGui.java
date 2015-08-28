8294de08344c581b9d444ed0e43542eb5e50183f
Sebastian Bazley <sebb@apache.org>
2015-08-28 10:06:05 +0000

    Unused import
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1698287 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: afd0116ee89266b68bb07f6ba2b3b4021304756c

--- src/components/org/apache/jmeter/visualizers/PropertyControlGui.java
+++ src/components/org/apache/jmeter/visualizers/PropertyControlGui.java
@@ -28,7 +28,6 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
