6a893711ab8f58bc1345b941faa3ba89cbe2dfc9
Philippe Mouawad <pmouawad@apache.org>
2015-10-03 20:29:27 +0000

    Bug 58477 - __javaScript function : Allow use of Nashorn engine for Java8 and upper versions Bugzilla Id: 58477
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1706624 13f79535-47bb-0310-9956-ffa450edef68

--- src/functions/org/apache/jmeter/functions/JavaScript.java
+++ src/functions/org/apache/jmeter/functions/JavaScript.java
@@ -22,6 +22,12 @@ import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 
+import javax.script.Bindings;
+import javax.script.ScriptContext;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.SimpleScriptContext;
+
 import org.apache.jmeter.engine.util.CompoundVariable;
 import org.apache.jmeter.samplers.SampleResult;
 import org.apache.jmeter.samplers.Sampler;
@@ -40,7 +46,27 @@ import org.mozilla.javascript.Scriptable;
  * @since 1.9
  */
 public class JavaScript extends AbstractFunction {
+    private static final String NASHORN_ENGINE_NAME = "nashorn"; //$NON-NLS-1$
 
+    private static final String USE_RHINO_ENGINE_PROPERTY = "javascript.use_rhino"; //$NON-NLS-1$
+
+    /**
+     * Initialization On Demand Holder pattern
+     */
+    private static class LazyHolder {
+        public static final ScriptEngineManager INSTANCE = new ScriptEngineManager();
+    }
+ 
+    private static final boolean USE_RHINO_ENGINE = 
+            getInstance().getEngineByName(JavaScript.NASHORN_ENGINE_NAME) == null || //$NON-NLS-1$
+            JMeterUtils.getPropDefault(USE_RHINO_ENGINE_PROPERTY, true) ;
+
+    /**
+     * @return ScriptEngineManager singleton
+     */
+    public static ScriptEngineManager getInstance() {
+            return LazyHolder.INSTANCE;
+    }
     private static final List<String> desc = new LinkedList<>();
 
     private static final String KEY = "__javaScript"; //$NON-NLS-1$
@@ -70,7 +96,77 @@ public class JavaScript extends AbstractFunction {
         String varName = values.length < 2 ? null : ((CompoundVariable) values[1]).execute().trim();
         String resultStr = "";
 
+        if(USE_RHINO_ENGINE) {
+            resultStr = executeWithRhino(previousResult, currentSampler, jmctx,
+                vars, script, varName);
+        } else {
+            resultStr = executeWithNashorn(previousResult, currentSampler, jmctx,
+                    vars, script, varName);
+        }
+
+        return resultStr;
+
+    }
+
+    /**
+     * 
+     * @param previousResult {@link SampleResult}
+     * @param currentSampler {@link Sampler}
+     * @param jmctx {@link JMeterContext}
+     * @param vars {@link JMeterVariables}
+     * @param script Javascript code
+     * @param varName variable name
+     * @return result as String
+     * @throws InvalidVariableException
+     */
+    private String executeWithNashorn(SampleResult previousResult,
+            Sampler currentSampler, JMeterContext jmctx, JMeterVariables vars,
+            String script, String varName)
+            throws InvalidVariableException {
+        String resultStr = null;
+        try {
+            ScriptContext newContext = new SimpleScriptContext();
+            ScriptEngine engine = getInstance().getEngineByName(JavaScript.NASHORN_ENGINE_NAME);
+            Bindings bindings = engine.createBindings();
+
+            // Set up some objects for the script to play with
+            bindings.put("log", log); //$NON-NLS-1$
+            bindings.put("ctx", jmctx); //$NON-NLS-1$
+            bindings.put("vars", vars); //$NON-NLS-1$
+            bindings.put("props", JMeterUtils.getJMeterProperties()); //$NON-NLS-1$
+
+            bindings.put("threadName", Thread.currentThread().getName()); //$NON-NLS-1$
+            bindings.put("sampler", currentSampler); //$NON-NLS-1$
+            bindings.put("sampleResult", previousResult); //$NON-NLS-1$
+            newContext.setBindings(bindings, ScriptContext.ENGINE_SCOPE);
+            Object result = engine.eval(script, newContext);
+            resultStr = result.toString();
+            if (varName != null && vars != null) {// vars can be null if run from TestPlan
+                vars.put(varName, resultStr);
+            }
+        } catch (Exception e) {
+            log.error("Error processing Javascript: [" + script + "]\n", e);
+            throw new InvalidVariableException("Error processing Javascript: [" + script + "]", e);
+        } 
+        return resultStr;
+    }
+    
+    /**
+     * @param previousResult {@link SampleResult}
+     * @param currentSampler {@link Sampler}
+     * @param jmctx {@link JMeterContext}
+     * @param vars {@link JMeterVariables}
+     * @param script Javascript code
+     * @param varName variable name
+     * @return result as String
+     * @throws InvalidVariableException
+     */
+    private String executeWithRhino(SampleResult previousResult,
+            Sampler currentSampler, JMeterContext jmctx, JMeterVariables vars,
+            String script, String varName)
+            throws InvalidVariableException {
         Context cx = Context.enter();
+        String resultStr = null;
         try {
 
             Scriptable scope = cx.initStandardObjects(null);
@@ -91,16 +187,13 @@ public class JavaScript extends AbstractFunction {
             if (varName != null && vars != null) {// vars can be null if run from TestPlan
                 vars.put(varName, resultStr);
             }
-
         } catch (RhinoException e) {
             log.error("Error processing Javascript: [" + script + "]\n", e);
             throw new InvalidVariableException("Error processing Javascript: [" + script + "]", e);
         } finally {
             Context.exit();
         }
-
         return resultStr;
-
     }
 
     /** {@inheritDoc} */
