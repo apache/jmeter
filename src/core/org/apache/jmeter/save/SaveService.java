194e69dcbd342d498fa61deea9ecebf8b588d206
Philippe Mouawad <pmouawad@apache.org>
2015-10-21 21:26:58 +0000

    Bug 58515 - New JSON related components : JSON-PATH Extractor and JSON-PATH Renderer in View Results Tree Bugzilla Id: 58515
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1709921 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 2d53d25cd253361bb1a1fed2088edf8d6bf63372

--- src/core/org/apache/jmeter/save/SaveService.java
+++ src/core/org/apache/jmeter/save/SaveService.java
@@ -177,7 +177,7 @@ public class SaveService {
     
     // Must match _version property value in saveservice.properties
     // used to ensure saveservice.properties and SaveService are updated simultaneously
-    static final String PROPVERSION = "2.8";// Expected version $NON-NLS-1$
+    static final String PROPVERSION = "2.9";// Expected version $NON-NLS-1$
 
     // Internal information only
     private static String fileVersion = ""; // read from saveservice.properties file// $NON-NLS-1$
