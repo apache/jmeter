18c2b81211fd13faccf73a41b70cf2f368f6ec9f
ubikfsabbe <f.sabbe@ubik-ingenierie.com>
2015-11-10 15:29:33 +0100

    Corrects #21

--- src/core/org/apache/jmeter/report/config/ReportGeneratorConfiguration.java
+++ src/core/org/apache/jmeter/report/config/ReportGeneratorConfiguration.java
@@ -22,12 +22,11 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Properties;
 
 import org.apache.jorphan.logging.LoggingManager;
 import org.apache.log.Logger;
-
-import com.sun.org.apache.xalan.internal.utils.Objects;
 
 import jodd.props.Props;
 
