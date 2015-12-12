apache/jmeter

commit 080b0f691ccee39283623376d6b1ab26dcee881e

Method names should not begin with upper case.
git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1719666 13f79535-47bb-0310-9956-ffa450edef68

src/core/org/apache/jmeter/report/core/JsonUtil.java
@@ -47,7 +47,7 @@ public final class JsonUtil {
      * @return the string
      */
     public static String toJsonObject(Map<String, String> map) {
-        String result = "{";
+        StringBuilder result = new StringBuilder("{");
         if (map != null) {
             String[] array = new String[map.size()];
             int index = 0;

src/core/org/apache/jmeter/report/processor/ApdexSummaryConsumer.java
@@ -117,10 +117,10 @@ public class ApdexSummaryConsumer extends
         }
 
         // Increment the total count of samples with the current name
-        data.IncTotalCount();
+        data.incTotalCount();
 
         // Increment the total count of samples
-        overallData.IncTotalCount();
+        overallData.incTotalCount();
 
         // Process only succeeded samples
         if (sample.getSuccess()) {
@@ -129,19 +129,19 @@ public class ApdexSummaryConsumer extends
             // Increment the counters depending on the elapsed time.
             ApdexThresholdsInfo thresholdsInfo = data.getApdexThresholdInfo();
             if (elapsedTime <= thresholdsInfo.getSatisfiedThreshold()) {
-                data.IncSatisfiedCount();
+                data.incSatisfiedCount();
             } else if (elapsedTime <= thresholdsInfo.getToleratedThreshold()) {
-                data.IncToleratedCount();
+                data.incToleratedCount();
             }
 
             // Increment the overall counters depending on the elapsed time.
             ApdexThresholdsInfo overallThresholdsInfo = overallData
                     .getApdexThresholdInfo();
             if (elapsedTime <= overallThresholdsInfo.getSatisfiedThreshold()) {
-                overallData.IncSatisfiedCount();
+                overallData.incSatisfiedCount();
             } else if (elapsedTime <= overallThresholdsInfo
                     .getToleratedThreshold()) {
-                overallData.IncToleratedCount();
+                overallData.incToleratedCount();
             }
         }
 

src/core/org/apache/jmeter/report/processor/ApdexSummaryData.java
@@ -96,15 +96,15 @@ public class ApdexSummaryData {
         apdexThresholdInfo = info;
     }
 
-    public void IncSatisfiedCount() {
+    public void incSatisfiedCount() {
         satisfiedCount++;
     }
 
-    public void IncToleratedCount() {
+    public void incToleratedCount() {
         toleratedCount++;
     }
 
-    public void IncTotalCount() {
+    public void incTotalCount() {
         totalCount++;
     }
 }

commit 12ac2e9adffc10483ead8a91379310cd24e8fe14

Correct mistake, that was introduced with r1719666.
git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1719675 13f79535-47bb-0310-9956-ffa450edef68

Former-commit-id: 8ddbf93445865a45e39a665d9586101c27d3acb0

src/core/org/apache/jmeter/report/core/JsonUtil.java
@@ -47,7 +47,7 @@ public final class JsonUtil {
      * @return the string
      */
     public static String toJsonObject(Map<String, String> map) {
-        StringBuilder result = new StringBuilder("{");
+        String result = "{";
         if (map != null) {
             String[] array = new String[map.size()];
             int index = 0;