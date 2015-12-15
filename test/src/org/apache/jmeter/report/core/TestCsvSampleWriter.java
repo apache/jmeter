a3b83931d5ae6fcfb441d8c01fd8827082a27dca
Felix Schumacher <fschumacher@apache.org>
2015-12-15 20:39:12 +0000

    Use Validate methods from commons lang3 and throw NPE instead of ArgumentNullException. Add test cases.
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1720242 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 92dc94c85364ee6bc18e00ea2a0e90e486acf223

---
+++ test/src/org/apache/jmeter/report/core/TestCsvSampleWriter.java
@@ -0,0 +1,104 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package org.apache.jmeter.report.core;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
+
+import org.apache.jmeter.util.JMeterUtils;
+
+import junit.framework.TestCase;
+
+public class TestCsvSampleWriter extends TestCase {
+
+    protected void setUp() throws Exception {
+        // We have to initialize JMeterUtils
+        JMeterUtils.loadJMeterProperties("jmeter.properties");
+    };
+
+    SampleMetadata metadata = new SampleMetadata(',', "a", "b");
+
+    public void testCsvSampleWriterConstructorWithNull() throws Exception {
+        try {
+            CsvSampleWriter dummy = new CsvSampleWriter(null);
+            dummy.close(); // We should never get here, but it would be a
+                           // writer, so close it
+            fail("NPE expected");
+        } catch (NullPointerException e) {
+            // OK, we should land here
+        }
+    }
+
+    public void testCsvSampleWriterConstructorWithWriter() throws Exception {
+        try (Writer writer = new StringWriter();
+                CsvSampleWriter csvWriter = new CsvSampleWriter(writer,
+                        metadata)) {
+            csvWriter.writeHeader();
+            // need to replace the writer to flush the original one
+            replaceWriter(csvWriter);
+            assertEquals("a,b\n", writer.toString());
+        }
+    }
+
+    private void replaceWriter(CsvSampleWriter csvWriter) throws IOException {
+        try (Writer replacement = new StringWriter()) {
+            csvWriter.setWriter(replacement);
+        }
+    }
+
+    public void testWriteWithoutWriter() throws Exception {
+        try (CsvSampleWriter csvWriter = new CsvSampleWriter(metadata)) {
+            Sample sample = new SampleBuilder(metadata).add("a1").add("b1")
+                    .build();
+            try {
+                csvWriter.write(sample);
+                fail("ISE expected");
+            } catch (IllegalStateException e) {
+                // OK, we should land here
+            }
+        }
+    }
+
+    public void testWriteWithoutSample() throws Exception {
+        try (Writer writer = new StringWriter();
+                CsvSampleWriter csvWriter = new CsvSampleWriter(writer,
+                        metadata)) {
+            try {
+                csvWriter.write(null);
+                fail("NPE expected");
+            } catch (NullPointerException e) {
+                assertEquals("sample must not be null", e.getMessage());
+            }
+        }
+    }
+
+    public void testWrite() throws Exception {
+        try (Writer writer = new StringWriter();
+                CsvSampleWriter csvWriter = new CsvSampleWriter(writer,
+                        metadata)) {
+            Sample sample = new SampleBuilder(metadata).add("a1").add("b1")
+                    .build();
+            csvWriter.write(sample);
+            // need to replace the writer to flush the original one
+            replaceWriter(csvWriter);
+            assertEquals("a1,b1\n", writer.toString());
+        }
+    }
+
+}
