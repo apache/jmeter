e16076355dc1a1f5100cb1f9a8327039cba0e884
Philippe Mouawad <pmouawad@apache.org>
2016-01-03 22:08:20 +0000

    Drop previously deprecated elements in 2.13: - WebService(SOAP) Request - HTML Parameter Mask
    Mention revision in upgrade
    Fix sha
    
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1722758 13f79535-47bb-0310-9956-ffa450edef68

--- src/core/org/apache/jmeter/save/SaveService.java
+++ src/core/org/apache/jmeter/save/SaveService.java
@@ -192,7 +192,7 @@ public class SaveService {
     private static String fileVersion = ""; // computed from saveservice.properties file// $NON-NLS-1$
     // Must match the sha1 checksum of the file saveservice.properties (without newline character),
     // used to ensure saveservice.properties and SaveService are updated simultaneously
-    static final String FILEVERSION = "1e7d41832b89d6436a366b621d03d7888d9dfd1d"; // Expected value $NON-NLS-1$
+    static final String FILEVERSION = "59d3dfc5e81300143d090a6e8cf86496b006fb33"; // Expected value $NON-NLS-1$
 
     private static String fileEncoding = ""; // read from properties file// $NON-NLS-1$
 
