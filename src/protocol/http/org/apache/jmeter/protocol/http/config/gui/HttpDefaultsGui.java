680977179b25ab9b2077705e7ce11b6f3e725ae4
Bruno Demion <milamber@apache.org>
2016-01-03 17:50:15 +0000

    Don't use a fixed height for the concurrent number box, use the current (ui system) height Bugzilla Id: 58426
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1722736 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 76f7cfb86c53944829c1ac98aa482ce52ca7965a

--- src/protocol/http/org/apache/jmeter/protocol/http/config/gui/HttpDefaultsGui.java
+++ src/protocol/http/org/apache/jmeter/protocol/http/config/gui/HttpDefaultsGui.java
@@ -172,8 +172,8 @@ public class HttpDefaultsGui extends AbstractConfigGui {
             }
         });
         concurrentPool = new JTextField(2); // 2 columns size
-        concurrentPool.setMinimumSize(new Dimension(10,20));
-        concurrentPool.setMaximumSize(new Dimension(30,20));
+        concurrentPool.setMinimumSize(new Dimension(10, (int) concurrentPool.getPreferredSize().getHeight()));
+        concurrentPool.setMaximumSize(new Dimension(30, (int) concurrentPool.getPreferredSize().getHeight()));
         embeddedRsrcPanel.add(concurrentDwn);
         embeddedRsrcPanel.add(concurrentPool);
         
