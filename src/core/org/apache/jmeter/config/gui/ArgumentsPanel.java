6e91150b6bf1e22bbfbf8ca4f330359d9c61c72f
Philippe Mouawad <pmouawad@apache.org>
2016-01-14 21:18:56 +0000

    Bug 58860 - HTTP Request : Add automatic variable generation in HTTP parameters table by right click As per Felix note on mailing list, I am stupid, rollback Bugzilla Id: 58860
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1724688 13f79535-47bb-0310-9956-ffa450edef68

--- src/core/org/apache/jmeter/config/gui/ArgumentsPanel.java
+++ src/core/org/apache/jmeter/config/gui/ArgumentsPanel.java
@@ -680,7 +680,7 @@ public class ArgumentsPanel extends AbstractConfigGui implements ActionListener 
     /**
      * Initialize the components and layout of this component.
      */
-    protected final void init() {
+    protected void init() {
         JPanel p = this;
 
         if (standalone) {
