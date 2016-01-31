2b1e1b7a54df81006e6ab810401faab347f7f8d4
Felix Schumacher <fschumacher@apache.org>
2016-01-31 11:48:59 +0000

    Space police. Part of patch by Benoit Wiart.
    Bugzilla Id: 58949
    
    
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1727794 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 20c181a07c6cc2417af56e6ae6306aaf7e0c904c

--- src/protocol/ldap/org/apache/jmeter/protocol/ldap/config/gui/LDAPArguments.java
+++ src/protocol/ldap/org/apache/jmeter/protocol/ldap/config/gui/LDAPArguments.java
@@ -37,7 +37,6 @@ import org.apache.jmeter.config.ConfigTestElement;
  *
  * author Michael Stover author Mark Walsh
  */
-
 public class LDAPArguments extends ConfigTestElement implements Serializable {
     private static final long serialVersionUID = 240L;
 
