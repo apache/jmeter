1f48368bf1abf6432b4e88bcc082773d3c58449d
Philippe Mouawad <pmouawad@apache.org>
2016-02-01 20:25:22 +0000

    remove commented code (patch by Benoit Wiart) #resolve #97
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1727998 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 034470684c2ac81b12420c2568c68399f1c48426

--- src/protocol/http/org/apache/jmeter/protocol/http/proxy/HttpRequestHdr.java
+++ src/protocol/http/org/apache/jmeter/protocol/http/proxy/HttpRequestHdr.java
@@ -390,24 +390,6 @@ public class HttpRequestHdr {
         return "";// $NON-NLS-1$
     }
 
-//    /**
-//     * Returns the remainder of a tokenized string.
-//     *
-//     * @param tk
-//     *            String that is partially tokenized.
-//     * @return The remainder
-//     */
-//    private String getRemainder(StringTokenizer tk) {
-//        StringBuilder strBuff = new StringBuilder();
-//        if (tk.hasMoreTokens()) {
-//            strBuff.append(tk.nextToken());
-//        }
-//        while (tk.hasMoreTokens()) {
-//            strBuff.append(" "); // $NON-NLS-1$
-//            strBuff.append(tk.nextToken());
-//        }
-//        return strBuff.toString();
-//    }
 
     public String getUrlWithoutQuery(URL _url) {
         String fullUrl = _url.toString();
