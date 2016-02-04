d24a2d5ac1c64b8ace1f408b863d1c2e3a6bddb7
Philippe Mouawad <pmouawad@apache.org>
2016-02-04 08:14:13 +0000

    Bug 58897 - Improve JUnit Test code STEP 5 patch by B. Wiart #resolve #104 Bugzilla Id: 58897
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1728418 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 1ac5e02fc0d34920a3b24b3422bc0317a7696bf4

---
+++ test/src/org/apache/jmeter/functions/RandomFunctionTest.java
@@ -0,0 +1,48 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+
+package org.apache.jmeter.functions;
+
+import static org.apache.jmeter.functions.FunctionTestHelper.makeParams;
+
+import java.util.Collection;
+
+import org.apache.jmeter.engine.util.CompoundVariable;
+import org.apache.jmeter.junit.JMeterTestCase;
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+public class RandomFunctionTest extends JMeterTestCase {
+
+    @Test
+    public void randomTest1() throws Exception {
+        Random r = new Random();
+        Collection<CompoundVariable> parms = makeParams("0","10000000000","VAR");
+        r.setParameters(parms);
+        String s = r.execute(null,null);
+        long l = Long.parseLong(s);
+        assertTrue(l>=0 && l<=10000000000L);
+        
+        parms = makeParams("1","1","VAR");
+        r.setParameters(parms);
+        s = r.execute(null,null);
+        l = Long.parseLong(s);
+        assertEquals(1, l);
+    }
+}
