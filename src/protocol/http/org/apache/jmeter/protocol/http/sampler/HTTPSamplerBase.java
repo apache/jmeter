a30282f58aee1da8ab2fd0d83c0cf3819c725cc9
Sebastian Bazley <sebb@apache.org>
2016-02-04 13:19:38 +0000

    Unused import
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1728469 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 94174258f636ed4866213946d7afee1852269536

--- src/protocol/http/org/apache/jmeter/protocol/http/sampler/HTTPSamplerBase.java
+++ src/protocol/http/org/apache/jmeter/protocol/http/sampler/HTTPSamplerBase.java
@@ -22,7 +22,6 @@ import java.io.InputStream;
 import java.io.PrintStream;
 import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.security.MessageDigest;
