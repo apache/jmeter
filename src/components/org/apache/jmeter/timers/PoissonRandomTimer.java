4867b9e300fb094632b2b17bb4627c4cad2c2032
Philippe Mouawad <pmouawad@apache.org>
2016-02-12 21:59:48 +0000

    remove some redundant modifiers Part 1 Patch by Benoit Wiart https://github.com/apache/jmeter/pull/116
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1730123 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 78a2bfc4e73d062a6d8ce2344670a4c22170f86b

--- src/components/org/apache/jmeter/timers/PoissonRandomTimer.java
+++ src/components/org/apache/jmeter/timers/PoissonRandomTimer.java
@@ -329,7 +329,7 @@ public class PoissonRandomTimer extends RandomTimer implements Serializable {
      * @param lambda Lambda in Poisson
      * @return random
      */
-    private static final int poissonRandomNumberLowEq30(int lambda) {
+    private static int poissonRandomNumberLowEq30(int lambda) {
         double L = Math.exp(-lambda);
         int k = 0;
         double p = 1;
@@ -346,7 +346,7 @@ public class PoissonRandomTimer extends RandomTimer implements Serializable {
      * @param lambda Lambda in Poisson
      * @return random
      */
-    private static final int poissonRandomNumberSup30(int lambda) {
+    private static int poissonRandomNumberSup30(int lambda) {
         double c = 0.767 - 3.36/lambda;
         double beta = Math.PI/Math.sqrt(3.0*lambda);
         double alpha = beta*lambda;
@@ -374,7 +374,7 @@ public class PoissonRandomTimer extends RandomTimer implements Serializable {
      * @param n Number for which we want log(n!)
      * @return Log factorial
      */
-    private static final double logFactorial(int n)
+    private static double logFactorial(int n)
     {
         if (n < 0) {
             throw new IllegalArgumentException();
