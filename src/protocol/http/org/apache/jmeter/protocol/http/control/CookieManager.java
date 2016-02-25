8e8a9349ba38eab3cfb2f216d51904457810b71e
Philippe Mouawad <pmouawad@apache.org>
2016-02-25 23:03:26 +0000

    Bug 59071 - CookieManager : Defaults used by CookieManager (after Bug 58756) are wrong Fix test failure Bugzilla Id: 59071
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1732391 13f79535-47bb-0310-9956-ffa450edef68

--- src/protocol/http/org/apache/jmeter/protocol/http/control/CookieManager.java
+++ src/protocol/http/org/apache/jmeter/protocol/http/control/CookieManager.java
@@ -119,11 +119,11 @@ public class CookieManager extends ConfigTestElement implements TestStateListene
     }
 
     public String getPolicy() {
-        return getPropertyAsString(POLICY);
+        return getPropertyAsString(POLICY, HC4CookieHandler.DEFAULT_POLICY_NAME);
     }
 
     public void setCookiePolicy(String policy){
-        setProperty(POLICY, policy);
+        setProperty(POLICY, policy, HC4CookieHandler.DEFAULT_POLICY_NAME);
     }
 
     public CollectionProperty getCookies() {
@@ -143,11 +143,11 @@ public class CookieManager extends ConfigTestElement implements TestStateListene
     }
 
     public String getImplementation() {
-        return getPropertyAsString(IMPLEMENTATION);
+        return getPropertyAsString(IMPLEMENTATION, DEFAULT_IMPLEMENTATION);
     }
 
     public void setImplementation(String implementation){
-        setProperty(IMPLEMENTATION, implementation);
+        setProperty(IMPLEMENTATION, implementation, DEFAULT_IMPLEMENTATION);
     }
 
     /**
