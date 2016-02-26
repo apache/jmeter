564705a40948f1b64b06b2682d0246e7aaa6bb5f
Philippe Mouawad <pmouawad@apache.org>
2016-02-26 20:12:33 +0000

    Bug 59060 - HTTP Request GUI : Move File Upload to a new Tab to have more space for parameters and prevent incoherent configuration Contributed by Benoit Wiart #resolve #140 https://github.com/apache/jmeter/pull/140 Bugzilla Id: 59060
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1732554 13f79535-47bb-0310-9956-ffa450edef68

--- src/protocol/http/org/apache/jmeter/protocol/http/config/gui/MultipartUrlConfigGui.java
+++ src/protocol/http/org/apache/jmeter/protocol/http/config/gui/MultipartUrlConfigGui.java
@@ -28,6 +28,10 @@ import org.apache.jmeter.protocol.http.gui.HTTPFileArgsPanel;
 import org.apache.jmeter.testelement.TestElement;
 import org.apache.jmeter.util.JMeterUtils;
 
+/**
+ * @deprecated since 3.0 will be removed in the next version 3.1
+ */
+@Deprecated
 public class MultipartUrlConfigGui extends UrlConfigGui {
 
     private static final long serialVersionUID = 240L;
