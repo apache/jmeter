fa507f8117fa080e7c15dacf3971fb28bb99761a
benoit <b.wiart@ubik-ingenierie.com>
2016-03-02 11:57:03 +0100

    BUG 59095 drop UserParameterXMLParser (was deprecated 8 years ago) and associated classes deprecate UserSequence

--- src/protocol/http/org/apache/jmeter/protocol/http/modifier/UserSequence.java
+++ src/protocol/http/org/apache/jmeter/protocol/http/modifier/UserSequence.java
@@ -31,8 +31,8 @@ import org.apache.log.Logger;
  * This module controls the Sequence in which user details are returned. This
  * module uses round robin allocation of users.
  *
- * @version $Revision$
  */
+@Deprecated
 public class UserSequence implements Serializable {
     private static final long serialVersionUID = 233L;
 
