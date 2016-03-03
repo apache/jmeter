4e7a1f4387e8d0d6aeb997200fe7953fa48bb1de
Felix Schumacher <fschumacher@apache.org>
2016-03-03 20:55:33 +0000

    HTTP Request : Make Method field editable so that additional methods (Webdav) can be added easily
    Bugzilla Id: 59083
    
    
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1733521 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 6a78f780db397ca43f03a9406212a5494a54a65e

--- src/protocol/http/org/apache/jmeter/protocol/http/sampler/HTTPHC4Impl.java
+++ src/protocol/http/org/apache/jmeter/protocol/http/sampler/HTTPHC4Impl.java
@@ -487,13 +487,11 @@ public class HTTPHC4Impl extends HTTPHCAbstractImpl {
     protected void handleMethod(String method, HTTPSampleResult result,
             HttpRequestBase httpRequest, HttpContext localContext) throws IOException {
         // Handle the various methods
-        if (method.equals(HTTPConstants.POST)) {
+        if (httpRequest instanceof HttpPost) {
             String postBody = sendPostData((HttpPost)httpRequest);
             result.setQueryString(postBody);
-        } else if (method.equals(HTTPConstants.PUT) || method.equals(HTTPConstants.PATCH)
-                || HttpWebdav.isWebdavMethod(method)
-                || method.equals(HTTPConstants.DELETE)) {
-            String entityBody = sendEntityData(( HttpEntityEnclosingRequestBase)httpRequest);
+        } else if (httpRequest instanceof HttpEntityEnclosingRequestBase) {
+            String entityBody = sendEntityData((HttpEntityEnclosingRequestBase) httpRequest);
             result.setQueryString(entityBody);
         }
     }
