68773fb340f71fe58d0ce83682997ba13745178e
Felix Schumacher <fschumacher@apache.org>
2016-03-18 11:17:47 +0000

    Unittests for BaseParser.
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1735575 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 2d8208c546c06bc69e6ac6ab0016068730714f97

---
+++ test/src/org/apache/jmeter/protocol/http/parser/NotReusableParser.java
@@ -0,0 +1,43 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package org.apache.jmeter.protocol.http.parser;
+
+import java.net.URL;
+import java.util.Iterator;
+
+import org.apache.commons.lang3.NotImplementedException;
+
+/**
+ * Test class, that implements an dummy {@link LinkExtractorParser} that is not
+ * reusable
+ */
+public class NotReusableParser implements LinkExtractorParser {
+
+    @Override
+    public Iterator<URL> getEmbeddedResourceURLs(String userAgent,
+            byte[] responseData, URL baseUrl, String encoding)
+            throws LinkExtractorParseException {
+        throw new NotImplementedException("Test class");
+    }
+
+    @Override
+    public boolean isReusable() {
+        return false;
+    }
+
+}
