13883657e7ea642decd7f76b6b0304422bbaf668
Philippe Mouawad <pmouawad@apache.org>
2016-03-23 14:17:05 +0000

    Bug 59205 TCP Sampler: Set connect time in sampler when connection is established As per dev mailing list discussion, update documentation and handle to connection error Bugzilla Id: 59205
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1736324 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: dbc451a10f1651b08d77429d79936130832ee8e7

--- src/protocol/tcp/org/apache/jmeter/protocol/tcp/sampler/TCPSampler.java
+++ src/protocol/tcp/org/apache/jmeter/protocol/tcp/sampler/TCPSampler.java
@@ -392,8 +392,12 @@ public class TCPSampler extends AbstractSampler implements ThreadListener, Inter
         res.setSamplerData(sb.toString()); 
         res.sampleStart();
         try {
-            Socket sock = getSocket(socketKey);
-            res.connectEnd();
+            Socket sock;
+            try {
+                sock = getSocket(socketKey);
+            } finally {
+                res.connectEnd();
+            }
             if (sock == null) {
                 res.setResponseCode("500"); //$NON-NLS-1$
                 res.setResponseMessage(getError());
