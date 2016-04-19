d05be36804d78de956f5bf7530bf5412c09f7b4c
Vladimir Sitnikov <vladimirsitnikov@apache.org>
2016-04-19 21:38:42 +0000

    Refactor TestCacheManager: replace duplicated code with inheritance
    Reduce timeouts from 5 to 1 seconds to speedup test execution
    
    closes #178
    
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1740017 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 6d5044a0814572f34579364d5761b6eca6d65362

---
+++ test/src/org/apache/jmeter/protocol/http/control/TestCacheManagerUrlConnection.java
@@ -0,0 +1,75 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.apache.jmeter.protocol.http.control;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.jmeter.protocol.http.sampler.HTTPSampleResult;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class TestCacheManagerUrlConnection extends TestCacheManagerUrlConnectionBase {
+
+    @Override
+    protected void cacheResult(HTTPSampleResult result) throws Exception {
+        this.cacheManager.saveDetails(this.urlConnection, result);
+    }
+
+    @Override
+    protected void setExpires(String expires) {
+        ((URLConnectionStub) urlConnection).expires = expires;
+    }
+
+    @Override
+    protected void setCacheControl(String cacheControl) {
+        ((URLConnectionStub) urlConnection).cacheControl = cacheControl;
+    }
+
+    @Override
+    protected void setLastModified(String lastModified) {
+        ((URLConnectionStub) urlConnection).lastModifiedHeader = lastModified;
+    }
+
+    @Override
+    protected void checkRequestHeader(String requestHeader, String expectedValue) {
+        Map<String, List<String>> properties = this.httpUrlConnection.getRequestProperties();
+        checkProperty(properties, requestHeader, expectedValue);
+    }
+
+    @Override
+    protected void addRequestHeader(String requestHeader, String value) {
+        // no-op
+    }
+
+    @Override
+    protected void setRequestHeaders() {
+        this.cacheManager.setHeaders(this.httpUrlConnection, this.url);
+    }
+
+    private static void checkProperty(Map<String, List<String>> properties, String property, String expectedPropertyValue) {
+        assertNotNull("Properties should not be null. Expected to find within it property = " + property + " with expected value = " + expectedPropertyValue, properties);
+        List<String> listOfPropertyValues = properties.get(property);
+        assertNotNull("No property entry found for property " + property, listOfPropertyValues);
+        assertEquals("Did not find single property for property " + property, 1, listOfPropertyValues.size());
+        assertEquals("Unexpected value for property " + property, expectedPropertyValue, listOfPropertyValues.get(0));
+    }
+
+}
