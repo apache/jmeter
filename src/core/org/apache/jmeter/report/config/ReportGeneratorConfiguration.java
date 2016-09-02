6b6bb8bca7ba69b9507cc636254e30f4f455d60a
Philippe Mouawad <pmouawad@apache.org>
2016-09-02 19:36:44 +0000

    Bug 59956 - Web Report / Dashboard : Add ability to generate a graph for a range of data Replace date format by one without separator for easier use from command line Bugzilla Id: 59956
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1759023 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 31d669721697f20a576cabc9b2518f16c5996a00

--- src/core/org/apache/jmeter/report/config/ReportGeneratorConfiguration.java
+++ src/core/org/apache/jmeter/report/config/ReportGeneratorConfiguration.java
@@ -42,7 +42,7 @@ public class ReportGeneratorConfiguration {
 
     private static final Logger LOG = LoggingManager.getLoggerForClass();
 
-    private static final String DATE_FORMAT = "dd/MM/yyyy HH:mm:ss"; //$NON-NLS-1$
+    private static final String RANGE_DATE_FORMAT = "yyyyMMddHHmmss"; //$NON-NLS-1$
 
     public static final char KEY_DELIMITER = '.';
     public static final String REPORT_GENERATOR_KEY_PREFIX = "jmeter.reportgenerator";
@@ -634,7 +634,7 @@ public class ReportGeneratorConfiguration {
                 REPORT_GENERATOR_KEY_START_DATE, String.class);
         final String endDateValue = getOptionalProperty(props,
                 REPORT_GENERATOR_KEY_END_DATE, String.class);
-        SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT);
+        SimpleDateFormat dateFormat = new SimpleDateFormat(RANGE_DATE_FORMAT);
         try {
             if(!StringUtils.isEmpty(startDateValue)) {
                 reportStartDate = dateFormat.parse(startDateValue);
@@ -642,7 +642,7 @@ public class ReportGeneratorConfiguration {
             }
         } catch (ParseException e) {
             LOG.error("Error parsing property 'REPORT_GENERATOR_KEY_START_DATE' with value:" + startDateValue
-                    +" using format:"+DATE_FORMAT, e);
+                    +" using format:"+RANGE_DATE_FORMAT, e);
         }
         try {
             if(!StringUtils.isEmpty(endDateValue)) {
@@ -651,7 +651,7 @@ public class ReportGeneratorConfiguration {
             }
         } catch (ParseException e) {
             LOG.error("Error parsing property 'REPORT_GENERATOR_KEY_START_DATE' with value:" + endDateValue 
-                    +" using format:"+DATE_FORMAT, e);
+                    +" using format:"+RANGE_DATE_FORMAT, e);
         }
         
         LOG.info("Will use date range start date: "+startDateValue+", end date: "+endDateValue);
