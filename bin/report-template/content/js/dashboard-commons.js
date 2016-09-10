c35e9f5d6948f9e896245886f12e714f95147a07
Philippe Mouawad <pmouawad@apache.org>
2016-09-10 12:48:25 +0000

    Bug 60108 - Report / Dashboard : In Requests Summary rounding is too aggressive Bugzilla Id: 60108
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1760157 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: c13a721f53a63d2aa84a4a62097e7f042763eab5

--- bin/report-template/content/js/dashboard-commons.js
+++ bin/report-template/content/js/dashboard-commons.js
@@ -14,6 +14,60 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 */
+
+/**
+ * From https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Math/round
+ * Licensed under https://creativecommons.org/licenses/by-sa/2.5/
+ */
+// Closure
+(function() {
+  /**
+   * Decimal adjustment of a number.
+   *
+   * @param {String}  type  The type of adjustment.
+   * @param {Number}  value The number.
+   * @param {Integer} exp   The exponent (the 10 logarithm of the adjustment base).
+   * @returns {Number} The adjusted value.
+   */
+  function decimalAdjust(type, value, exp) {
+    // If the exp is undefined or zero...
+    if (typeof exp === 'undefined' || +exp === 0) {
+      return Math[type](value);
+    }
+    value = +value;
+    exp = +exp;
+    // If the value is not a number or the exp is not an integer...
+    if (isNaN(value) || !(typeof exp === 'number' && exp % 1 === 0)) {
+      return NaN;
+    }
+    // Shift
+    value = value.toString().split('e');
+    value = Math[type](+(value[0] + 'e' + (value[1] ? (+value[1] - exp) : -exp)));
+    // Shift back
+    value = value.toString().split('e');
+    return +(value[0] + 'e' + (value[1] ? (+value[1] + exp) : exp));
+  }
+
+  // Decimal round
+  if (!Math.round10) {
+    Math.round10 = function(value, exp) {
+      return decimalAdjust('round', value, exp);
+    };
+  }
+  // Decimal floor
+  if (!Math.floor10) {
+    Math.floor10 = function(value, exp) {
+      return decimalAdjust('floor', value, exp);
+    };
+  }
+  // Decimal ceil
+  if (!Math.ceil10) {
+    Math.ceil10 = function(value, exp) {
+      return decimalAdjust('ceil', value, exp);
+    };
+  }
+})();
+
 /*
  * Suffixes the specified value with a unit
  * The spaced argument defines whether a space character is introduced.
