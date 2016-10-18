9d2cc4d5df627d4c94d4a0bf8e271424de2f318f
Philippe Mouawad <pmouawad@apache.org>
2016-10-18 18:40:52 +0000

    Bug 60252 - Add sent kbytes/s to Aggregate Report and Summary report Remove use of deprecated setBytes(int) Bugzilla Id: 60252
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1765492 13f79535-47bb-0310-9956-ffa450edef68

--- test/src/org/apache/jmeter/save/TestCSVSaveService.java
+++ test/src/org/apache/jmeter/save/TestCSVSaveService.java
@@ -173,7 +173,7 @@ public class TestCSVSaveService extends JMeterTestCase {
         result.setThreadName("6");
         result.setDataType("7");
         result.setSuccessful(true);
-        result.setBytes(8);
+        result.setBytes(8L);
         result.setSentBytes(9);
         result.setGroupThreads(10);
         result.setAllThreads(11);
