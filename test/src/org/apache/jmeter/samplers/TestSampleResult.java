15efd82caaef4fa825761e6d59970b6aae6774c2
Philippe Mouawad <pmouawad@apache.org>
2016-10-18 18:40:52 +0000

    Bug 60252 - Add sent kbytes/s to Aggregate Report and Summary report Remove use of deprecated setBytes(int) Bugzilla Id: 60252
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1765492 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 9d2cc4d5df627d4c94d4a0bf8e271424de2f318f

--- test/src/org/apache/jmeter/samplers/TestSampleResult.java
+++ test/src/org/apache/jmeter/samplers/TestSampleResult.java
@@ -143,7 +143,7 @@ public class TestSampleResult {
             SampleResult res = new SampleResult();
             
             res.sampleStart();
-            res.setBytes(100);
+            res.setBytes(100L);
             res.setSampleLabel("sample of size 100 bytes");
             res.sampleEnd();
             assertEquals(100, res.getBytesAsLong());
@@ -217,7 +217,7 @@ public class TestSampleResult {
 
             parent.sampleStart();
             Thread.sleep(100);
-            parent.setBytes(300);
+            parent.setBytes(300L);
             parent.setSampleLabel("Parent Sample");
             parent.setSuccessful(true);
             parent.sampleEnd();
@@ -227,7 +227,7 @@ public class TestSampleResult {
             SampleResult child1 = new SampleResult(nanoTime);            
             child1.sampleStart();
             Thread.sleep(100);
-            child1.setBytes(100);
+            child1.setBytes(100L);
             child1.setSampleLabel("Child1 Sample");
             child1.setSuccessful(true);
             child1.sampleEnd();
@@ -250,7 +250,7 @@ public class TestSampleResult {
             SampleResult child2 = new SampleResult(nanoTime);            
             child2.sampleStart();
             Thread.sleep(100);
-            child2.setBytes(200);
+            child2.setBytes(200L);
             child2.setSampleLabel("Child2 Sample");
             child2.setSuccessful(true);
             child2.sampleEnd();
