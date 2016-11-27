d1dac2de358f2b2557d95c3bd4783f270b65aa9c
Felix Schumacher <fschumacher@apache.org>
2016-11-27 13:35:45 +0000

    Fix race-conditions in CssParser. Based on a patch by Jerome Loisel (loisel.jerome at gmail.com)
    This closes github pr #221
    Bugzilla Id: 59934
    
    
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1771589 13f79535-47bb-0310-9956-ffa450edef68

--- src/protocol/http/org/apache/jmeter/protocol/http/parser/CssParser.java
+++ src/protocol/http/org/apache/jmeter/protocol/http/parser/CssParser.java
@@ -20,169 +20,72 @@ package org.apache.jmeter.protocol.http.parser;
 
 import java.net.URL;
 import java.nio.charset.Charset;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
 
-import org.apache.commons.codec.digest.DigestUtils;
-import org.apache.commons.collections.map.LRUMap;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.Validate;
+import org.apache.commons.lang3.tuple.ImmutableTriple;
+import org.apache.commons.lang3.tuple.Triple;
 import org.apache.jmeter.util.JMeterUtils;
 import org.apache.jorphan.logging.LoggingManager;
 import org.apache.log.Logger;
 
-import com.helger.css.ECSSVersion;
-import com.helger.css.decl.CSSDeclaration;
-import com.helger.css.decl.CSSExpressionMemberTermURI;
-import com.helger.css.decl.CSSImportRule;
-import com.helger.css.decl.CascadingStyleSheet;
-import com.helger.css.decl.ICSSTopLevelRule;
-import com.helger.css.decl.visit.CSSVisitor;
-import com.helger.css.decl.visit.DefaultCSSUrlVisitor;
-import com.helger.css.handler.LoggingCSSParseExceptionCallback;
-import com.helger.css.parser.ParseException;
-import com.helger.css.reader.CSSReader;
-import com.helger.css.reader.CSSReaderSettings;
-import com.helger.css.reader.errorhandler.DoNothingCSSInterpretErrorHandler;
-import com.helger.css.reader.errorhandler.ICSSInterpretErrorHandler;
-import com.helger.css.reader.errorhandler.LoggingCSSParseErrorHandler;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.LoadingCache;
 
 /**
  * CSS Parser used to extract from CSS files external urls
+ *
  * @since 3.0
  */
 public class CssParser implements LinkExtractorParser {
-    private static final boolean IGNORE_UNRECOVERABLE_PARSING_ERROR = JMeterUtils.getPropDefault("httpsampler.ignore_failed_embedded_resource", false); //$NON-NLS-1$
+    private static final URLCollection EMPTY_URL_COLLECTION = new URLCollection(Collections.emptyList());
     private static final Logger LOG = LoggingManager.getLoggerForClass();
 
     /**
      *
      */
-    private static final int CSS_URL_CACHE_MAX_SIZE = JMeterUtils.getPropDefault("css.parser.cache.size", 400);
-    private static final boolean IGNORE_ALL_CSS_ERRORS = JMeterUtils.getPropDefault("css.parser.ignore_all_css_errors", true);
-
-    /**
-     *
-     */
-    @SuppressWarnings("unchecked")
-    private static final Map<String, URLCollection> CSS_URL_CACHE =
-            CSS_URL_CACHE_MAX_SIZE > 0 ? Collections.synchronizedMap(new LRUMap(CSS_URL_CACHE_MAX_SIZE)) : null;
-
-
-    private static final class CustomLoggingCSSParseExceptionCallback extends LoggingCSSParseExceptionCallback {
-        /**
-         *
-         */
-        private static final long serialVersionUID = -9111232037888068394L;
-        private URL cssUrl;
-
-        /**
-         * @param cssUrl {@link URL}
-         */
-        public CustomLoggingCSSParseExceptionCallback(URL cssUrl) {
-            this.cssUrl = cssUrl;
-        }
-        /**
-         * @see com.helger.css.handler.LoggingCSSParseExceptionCallback#onException(com.helger.css.parser.ParseException)
-         */
-        @Override
-        public void onException(ParseException ex) {
-            if(IGNORE_UNRECOVERABLE_PARSING_ERROR) {
-                LOG.warn("Failed to parse CSS: " + cssUrl + ", " + LoggingCSSParseErrorHandler.createLoggingStringParseError (ex));
-            } else {
-                throw new IllegalStateException("Failed to parse CSS: " + cssUrl + ", " + LoggingCSSParseErrorHandler.createLoggingStringParseError (ex));
-            }
-        }
+    private static final LoadingCache<Triple<String, URL, Charset>, URLCollection> CSS_URL_CACHE;
+    static {
+        final int cacheSize = JMeterUtils.getPropDefault(
+                "css.parser.cache.size", 400);
+        CSS_URL_CACHE = Caffeine.newBuilder().maximumSize(cacheSize)
+                .build(new CssParserCacheLoader());
     }
 
     /**
      *
-     */
-    public CssParser() {
-    }
-
-    /**
-     *
-     * @see
-     * org.apache.jmeter.protocol.http.parser.LinkExtractorParser#getEmbeddedResourceURLs
-     * (java.lang.String, byte[], java.net.URL, java.lang.String)
+     * @see org.apache.jmeter.protocol.http.parser.LinkExtractorParser#getEmbeddedResourceURLs
+     *      (java.lang.String, byte[], java.net.URL, java.lang.String)
      */
     @Override
     public Iterator<URL> getEmbeddedResourceURLs(String userAgent, byte[] data,
-            final URL baseUrl, String encoding) throws LinkExtractorParseException {
+            final URL baseUrl, String encoding)
+            throws LinkExtractorParseException {
         try {
-            boolean cacheEnabled = CSS_URL_CACHE_MAX_SIZE > 0;
-            String md5Key = null;
-            URLCollection urlCollection = null;
-            if(cacheEnabled) {
-                md5Key = DigestUtils.md5Hex(data);
-                urlCollection = CSS_URL_CACHE.get(md5Key);
-            }
+            final String cssContent = new String(data, encoding);
+            final Charset charset = Charset.forName(encoding);
+            final Triple<String, URL, Charset> triple = ImmutableTriple.of(
+                    cssContent, baseUrl, charset);
+            final URLCollection urlCollection = orDefault(CSS_URL_CACHE.get(triple), EMPTY_URL_COLLECTION);
 
-            if(urlCollection == null) {
-                String cssContent = new String(data, encoding);
-                final CSSReaderSettings cssSettings = new CSSReaderSettings()
-                        .setBrowserCompliantMode(true)
-                        .setFallbackCharset(Charset.forName(encoding))
-                        .setCSSVersion(ECSSVersion.CSS30)
-                        .setCustomErrorHandler(
-                                new LoggingCSSParseErrorHandler())
-                        .setCustomExceptionHandler(
-                                new CustomLoggingCSSParseExceptionCallback(
-                                        baseUrl));
-                if (IGNORE_ALL_CSS_ERRORS) {
-                    cssSettings
-                            .setInterpretErrorHandler(new DoNothingCSSInterpretErrorHandler());
-                }
-                final CascadingStyleSheet aCSS = CSSReader
-                        .readFromStringStream(cssContent, cssSettings);
-                final List<URLString> list = new ArrayList<>();
-                urlCollection = new URLCollection(list);
-                final URLCollection localCollection = urlCollection;
-                if(aCSS != null) {
-                    CSSVisitor.visitCSSUrl(aCSS, new DefaultCSSUrlVisitor() {
-                        @Override
-                        public void onImport(final CSSImportRule importRule) {
-                            String location = importRule.getLocationString();
-                            if(!StringUtils.isEmpty(location)) {
-                                localCollection.addURL(location, baseUrl);
-                            }
-                        }
-                        // Call for URLs outside of URLs
-                        @Override
-                        public void onUrlDeclaration(
-                                final ICSSTopLevelRule aTopLevelRule,
-                                final CSSDeclaration aDeclaration,
-                                final CSSExpressionMemberTermURI aURITerm) {
-                            // NOOP
-                            // Browser fetch such urls only when CSS rule matches
-                            // so we disable this code
-                            //urlCollection.addURL(aURITerm.getURIString(), baseUrl);
-                        }
-                    });
-                    if(cacheEnabled) {
-                        CSS_URL_CACHE.put(md5Key, urlCollection);
-                    }
-                } else {
-                   LOG.warn("Failed parsing url:"+baseUrl+", got null CascadingStyleSheet");
-                }
-            }
-
-            if(LOG.isDebugEnabled()) {
-                StringBuilder builder = new StringBuilder();
-                for (Iterator<URL> iterator = urlCollection.iterator(); iterator.hasNext();) {
-                    URL urlString = iterator.next();
-                    builder.append(urlString).append(',');
-                }
-                LOG.debug("Parsed:"+baseUrl+", got:"+builder.toString());
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("Parsed:" + baseUrl + ", got:" + StringUtils.join(urlCollection, ","));
             }
 
             return urlCollection.iterator();
         } catch (Exception e) {
             throw new LinkExtractorParseException(e);
         }
+    }
+
+    private URLCollection orDefault(URLCollection urlCollection,
+            URLCollection defaultValue) {
+        if (urlCollection == null) {
+            return Validate.notNull(defaultValue);
+        }
+        return urlCollection;
     }
 
     @Override
