db937c5ebfe600d44bdf733280ffa51c98e3d468
Philippe Mouawad <pmouawad@apache.org>
2016-12-21 19:10:15 +0000

    Sonar : Fix errors and code smells
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1775539 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: f4740718d71e1e6edf23229d140121e38213aa03

--- src/core/org/apache/jmeter/gui/action/Save.java
+++ src/core/org/apache/jmeter/gui/action/Save.java
@@ -122,7 +122,7 @@ public class Save extends AbstractAction {
 
     @Override
     public void doAction(ActionEvent e) throws IllegalUserActionException {
-        HashTree subTree = null;
+        HashTree subTree;
         boolean fullSave = false; // are we saving the whole tree?
         if (!commands.contains(e.getActionCommand())) {
             throw new IllegalUserActionException("Invalid user command:" + e.getActionCommand());
@@ -238,10 +238,10 @@ public class Save extends AbstractAction {
                     log.warn("Failed to delete backup file " + expiredBackupFile.getName()); //$NON-NLS-1$
                 }
             }
-        } catch(RuntimeException | Error ex) {
+        } catch(RuntimeException ex) {
             throw ex;
         }
-        catch (Throwable ex) {
+        catch (Exception ex) {
             log.error("Error saving tree:", ex);
             throw new IllegalUserActionException("Couldn't save test plan to file: " + updateFile, ex);
         } 
