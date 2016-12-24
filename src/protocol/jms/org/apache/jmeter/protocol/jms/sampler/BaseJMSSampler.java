19bcecd9caad67ea9b69b866ead91643b14c893b
Philippe Mouawad <pmouawad@apache.org>
2016-12-24 14:45:14 +0000

    Init logger 1 time for perfs
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1775970 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: c8abf1925da7696b2938acb74b7bfbe564676371

--- src/protocol/jms/org/apache/jmeter/protocol/jms/sampler/BaseJMSSampler.java
+++ src/protocol/jms/org/apache/jmeter/protocol/jms/sampler/BaseJMSSampler.java
@@ -29,6 +29,7 @@ import org.apache.jmeter.samplers.Entry;
 import org.apache.jmeter.samplers.SampleResult;
 import org.apache.jmeter.util.JMeterUtils;
 import org.apache.jorphan.logging.LoggingManager;
+import org.apache.log.Logger;
 
 /**
  *
@@ -38,6 +39,8 @@ import org.apache.jorphan.logging.LoggingManager;
 public abstract class BaseJMSSampler extends AbstractSampler {
 
     private static final long serialVersionUID = 240L;
+    
+    private static final Logger LOGGER = LoggingManager.getLoggerForClass(); 
 
     //++ These are JMX file attribute names and must not be changed
     private static final String JNDI_INITIAL_CONTEXT_FAC = "jms.initial_context_factory"; // $NON-NLS-1$
@@ -372,7 +375,7 @@ public abstract class BaseJMSSampler extends AbstractSampler {
             response.append("\n\n");
 
         } catch (JMSException e) {
-            LoggingManager.getLoggerForClass().warn(
+            LOGGER.warn(
                     "Can't extract message headers", e);
         }
 
