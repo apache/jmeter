eca634acae4f8c40297983b1148b8641fb7718ae
Philippe Mouawad <pmouawad@apache.org>
2016-12-28 18:08:20 +0000

    sonar: fix errors and mention change in changes.xml and component reference
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1776315 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 3b46f06abe15d63c22e7d8f5d27131775eae6f59

--- src/protocol/native/org/apache/jmeter/protocol/system/SystemSampler.java
+++ src/protocol/native/org/apache/jmeter/protocol/system/SystemSampler.java
@@ -127,7 +127,7 @@ public class SystemSampler extends AbstractSampler {
             env.put(arg.getName(), arg.getPropertyAsString(Argument.VALUE));
         }
         
-        File directory = null;
+        File directory;
         if(StringUtils.isEmpty(getDirectory())) {
             directory = new File(FileServer.getDefaultBase());
             if(log.isDebugEnabled()) {
@@ -155,7 +155,7 @@ public class SystemSampler extends AbstractSampler {
             results.sampleStart();
             int returnCode = nativeCommand.run(cmds);
             results.sampleEnd();
-            results.setResponseCode(Integer.toString(returnCode)); // TODO is this the best way to do this?
+            results.setResponseCode(Integer.toString(returnCode));
             if(log.isDebugEnabled()) {
                 log.debug("Ran : "+cmdLine + " using working directory: "+directory.getAbsolutePath()+
                         " with execution environment: "+nativeCommand.getExecutionEnvironment()+ " => " + returnCode);
@@ -171,13 +171,14 @@ public class SystemSampler extends AbstractSampler {
         } catch (IOException ioe) {
             results.sampleEnd();
             results.setSuccessful(false);
-            // results.setResponseCode("???"); TODO what code should be set here?
+            results.setResponseCode("500"); //$NON-NLS-1$
             results.setResponseMessage("Exception occurred whilst executing system call: " + ioe);
         } catch (InterruptedException ie) {
             results.sampleEnd();
             results.setSuccessful(false);
-            // results.setResponseCode("???"); TODO what code should be set here?
+            results.setResponseCode("500"); //$NON-NLS-1$
             results.setResponseMessage("System Sampler interrupted whilst executing system call: " + ie);
+            Thread.currentThread().interrupt();
         }
 
         if (nativeCommand != null) {
