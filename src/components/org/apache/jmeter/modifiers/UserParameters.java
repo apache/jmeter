eae0797ade5d0abed1525c81935cfc247bc4547f
Philippe Mouawad <pmouawad@apache.org>
2016-12-28 17:17:04 +0000

    Fix doc
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1776306 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 532af7f57c2c4859caffa1a608be4644336e5abd

--- src/components/org/apache/jmeter/modifiers/UserParameters.java
+++ src/components/org/apache/jmeter/modifiers/UserParameters.java
@@ -44,13 +44,13 @@ public class UserParameters extends AbstractTestElement implements Serializable,
 
     public static final String PER_ITERATION = "UserParameters.per_iteration";// $NON-NLS-1$
 
-    /*
+    /**
      * Although the lock appears to be an instance lock, in fact the lock is
-     * shared between all threads in a thread group, but different thread groups
-     * have different locks - see the clone() method below
+     * shared between all threads see the clone() method below
      *
      * The lock ensures that all the variables are processed together, which is
      * important for functions such as __CSVRead and _StringFromFile.
+     * But it has a performance drawback.
      */
     private transient Object lock = new Object();
 
