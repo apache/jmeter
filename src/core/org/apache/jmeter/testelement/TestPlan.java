c2093a0fa0b87f7ce98403d7f8093f81799a1993
Philippe Mouawad <pmouawad@apache.org>
2016-12-30 23:47:25 +0000

    sonar : fix security errors Now we fail if issues happens on JAR loading
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1776662 13f79535-47bb-0310-9956-ffa450edef68

--- src/core/org/apache/jmeter/testelement/TestPlan.java
+++ src/core/org/apache/jmeter/testelement/TestPlan.java
@@ -20,6 +20,7 @@ package org.apache.jmeter.testelement;
 
 import java.io.IOException;
 import java.io.Serializable;
+import java.net.MalformedURLException;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -253,8 +254,13 @@ public class TestPlan extends AbstractTestElement implements Serializable, TestS
         // we set the classpath
         String[] paths = this.getTestPlanClasspathArray();
         for (String path : paths) {
-            NewDriver.addURL(path);
-            log.info("add " + path + " to classpath");
+            try {
+                NewDriver.addURL(path);
+                log.info("added " + path + " to classpath");
+            } catch (MalformedURLException e) {
+                // TODO Should we continue the test or fail ?
+                log.error("Error adding " + path + " to classpath", e);
+            }
         }
     }
 
