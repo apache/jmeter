e8d20d77faa1234f29d277904e7c538d43d1b98c
Philippe Mouawad <pmouawad@apache.org>
2016-12-31 14:45:50 +0000

    Sonar : fix squid:CommentedOutCodeLine "This block of commented-out lines of code should be removed. " Also fix false positive on catching NoClassDefFoundError
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1776761 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: f8c89de297c4dcd089902c66dacc1a18081051b8

--- src/components/org/apache/jmeter/assertions/BeanShellAssertion.java
+++ src/components/org/apache/jmeter/assertions/BeanShellAssertion.java
@@ -109,14 +109,7 @@ public class BeanShellAssertion extends BeanShellTestElement implements Assertio
                     .toString()));
             result.setError(false);
         }
-        /*
-         * To avoid class loading problems when the BSH jar is missing, we don't
-         * try to catch this error separately catch (bsh.EvalError ex) {
-         * log.debug("",ex); result.setError(true);
-         * result.setFailureMessage(ex.toString()); }
-         */
-        // but we do trap this error to make tests work better
-        catch (NoClassDefFoundError ex) {
+        catch (NoClassDefFoundError ex) { // NOSONAR explicitely trap this error to make tests work better 
             log.error("BeanShell Jar missing? " + ex.toString());
             result.setError(true);
             result.setFailureMessage("BeanShell Jar missing? " + ex.toString());
