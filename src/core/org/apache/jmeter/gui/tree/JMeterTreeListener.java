d7ffb1cd52fcc81be3dba930a854dea03968ecb6
Philippe Mouawad <pmouawad@apache.org>
2016-12-31 15:10:46 +0000

    Remove useless code
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1776776 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 2e37e029e56c2699e3eeb5faf5a42a752b0ecfc9

--- src/core/org/apache/jmeter/gui/tree/JMeterTreeListener.java
+++ src/core/org/apache/jmeter/gui/tree/JMeterTreeListener.java
@@ -18,7 +18,6 @@
 
 package org.apache.jmeter.gui.tree;
 
-import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.InputEvent;
@@ -94,16 +93,6 @@ public class JMeterTreeListener implements TreeSelectionListener, MouseListener,
      */
     public void setJTree(JTree tree) {
         this.tree = tree;
-    }
-
-    /**
-     * Sets the EndWindow attribute of the JMeterTreeListener object.
-     *
-     * @param window
-     *            the new EndWindow value
-     */
-    public void setEndWindow(Container window) {
-        // endWindow = window;
     }
 
     /**
