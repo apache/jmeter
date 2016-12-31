00761c1c824c039297261876d961701415728ee2
Philippe Mouawad <pmouawad@apache.org>
2016-12-31 15:09:56 +0000

    Sonar : fix fix false positive squid:CommentedOutCodeLine "This block of commented-out lines of code should be removed. "
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1776774 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 7f833c8abe32a18c3c3da916e225299d4793e089

--- src/core/org/apache/jmeter/gui/util/FilePanelEntry.java
+++ src/core/org/apache/jmeter/gui/util/FilePanelEntry.java
@@ -82,7 +82,7 @@ public class FilePanelEntry extends HorizontalPanel implements ActionListener {
             listeners.add(listener);
         }
         if (exts != null && 
-          !(exts.length == 1 && exts[0] == null) // String null is converted to String[]{null}
+          !(exts.length == 1 && exts[0] == null) // String null is converted to String[]{null} NOSONAR it's not code
             ) {
             this.filetypes = new String[exts.length];
             System.arraycopy(exts, 0, this.filetypes, 0, exts.length);
