4f9277161ee636f88d61259a67662673ee4a1f5a
Philippe Mouawad <pmouawad@apache.org>
2016-12-31 16:04:14 +0000

    Sonar : fix squid:S00115 "Rename this constant name to match the regular expression '^[A-Z][A-Z0-9]*(_[A-Z0-9]+)*$'" Mention breaking change in changes.xml
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1776792 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 762fda431925b16ab1581f6568dc9f549dab4084

--- src/protocol/http/org/apache/jmeter/protocol/http/sampler/HTTPHC3Impl.java
+++ src/protocol/http/org/apache/jmeter/protocol/http/sampler/HTTPHC3Impl.java
@@ -100,7 +100,7 @@ public class HTTPHC3Impl extends HTTPHCAbstractImpl {
 
     private static final String HTTP_AUTHENTICATION_PREEMPTIVE = "http.authentication.preemptive"; // $NON-NLS-1$
 
-    private static final boolean canSetPreEmptive; // OK to set pre-emptive auth?
+    private static final boolean CAN_SET_PREEMPTIVE; // OK to set pre-emptive auth?
 
     private static final ThreadLocal<Map<HostConfiguration, HttpClient>> httpClients = 
         new ThreadLocal<Map<HostConfiguration, HttpClient>>(){
@@ -144,7 +144,7 @@ public class HTTPHC3Impl extends HTTPHCAbstractImpl {
 
         // If the pre-emptive parameter is undefined, then we can set it as needed
         // otherwise we should do what the user requested.
-        canSetPreEmptive =  params.getParameter(HTTP_AUTHENTICATION_PREEMPTIVE) == null;
+        CAN_SET_PREEMPTIVE =  params.getParameter(HTTP_AUTHENTICATION_PREEMPTIVE) == null;
 
         // Handle old-style JMeter properties
         try {
@@ -493,7 +493,7 @@ public class HTTPHC3Impl extends HTTPHCAbstractImpl {
             if (user.length() > 0){
                 httpClient.getState().setProxyCredentials(
                         new AuthScope(proxyHost,proxyPort,null,AuthScope.ANY_SCHEME),
-                        new NTCredentials(user,getProxyPass(),localHost,PROXY_DOMAIN)
+                        new NTCredentials(user,getProxyPass(),LOCALHOST,PROXY_DOMAIN)
                     );
             } else {
                 httpClient.getState().clearProxyCredentials();
@@ -503,7 +503,7 @@ public class HTTPHC3Impl extends HTTPHCAbstractImpl {
                 if (PROXY_USER.length() > 0){
                     httpClient.getState().setProxyCredentials(
                         new AuthScope(PROXY_HOST,PROXY_PORT,null,AuthScope.ANY_SCHEME),
-                        new NTCredentials(PROXY_USER,PROXY_PASS,localHost,PROXY_DOMAIN)
+                        new NTCredentials(PROXY_USER,PROXY_PASS,LOCALHOST,PROXY_DOMAIN)
                     );
                 }
             } else {
@@ -699,17 +699,17 @@ public class HTTPHC3Impl extends HTTPHCAbstractImpl {
                             new NTCredentials(
                                     username,
                                     auth.getPass(),
-                                    localHost,
+                                    LOCALHOST,
                                     domain
                             ));
                     // We have credentials - should we set pre-emptive authentication?
-                    if (canSetPreEmptive){
+                    if (CAN_SET_PREEMPTIVE){
                         log.debug("Setting Pre-emptive authentication");
                         params.setAuthenticationPreemptive(true);
                     }
             } else {
                 state.clearCredentials();
-                if (canSetPreEmptive){
+                if (CAN_SET_PREEMPTIVE){
                     params.setAuthenticationPreemptive(false);
                 }
             }
