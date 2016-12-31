7dd6d0f28b117424e25be70795dcd5513af4db15
Philippe Mouawad <pmouawad@apache.org>
2016-12-31 15:51:17 +0000

    Sonar : fix squid:S00115 "Rename this constant name to match the regular expression '^[A-Z][A-Z0-9]*(_[A-Z0-9]+)*$'"
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1776781 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: aa9970aa44a3870a8158f18f54295cf4c39cd4c9

--- src/components/org/apache/jmeter/visualizers/TreeNodeRenderer.java
+++ src/components/org/apache/jmeter/visualizers/TreeNodeRenderer.java
@@ -37,16 +37,16 @@ public class TreeNodeRenderer extends DefaultTreeCellRenderer {
 
     private static final long serialVersionUID = 240L;
 
-    private static final String iconSize = JMeterUtils.getPropDefault(JMeter.TREE_ICON_SIZE, JMeter.DEFAULT_TREE_ICON_SIZE);
+    private static final String ICON_SIZE = JMeterUtils.getPropDefault(JMeter.TREE_ICON_SIZE, JMeter.DEFAULT_TREE_ICON_SIZE);
 
     // Same ViewResultsTree
     private static final ImageIcon imageSuccess = JMeterUtils.getImage(
             JMeterUtils.getPropDefault("viewResultsTree.success",  //$NON-NLS-1$
-                    "vrt/" + iconSize + "/security-high-2.png")); //$NON-NLS-1$ $NON-NLS-2$
+                    "vrt/" + ICON_SIZE + "/security-high-2.png")); //$NON-NLS-1$ $NON-NLS-2$
 
     private static final ImageIcon imageFailure = JMeterUtils.getImage(
             JMeterUtils.getPropDefault("viewResultsTree.failure",  //$NON-NLS-1$
-                    "vrt/" + iconSize + "/security-low-2.png")); //$NON-NLS-1$ $NON-NLS-2$
+                    "vrt/" + ICON_SIZE + "/security-low-2.png")); //$NON-NLS-1$ $NON-NLS-2$
 
     public TreeNodeRenderer() {
         super();
