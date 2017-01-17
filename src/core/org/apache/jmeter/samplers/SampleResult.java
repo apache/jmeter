e9054b65a946657f4526a5b6ceb5639c357402c6
Antonio Gomes Rodrigues <agomes@apache.org>
2017-01-17 21:14:44 +0000

    This closes #243 : Fix warning prone default charset in SampleResult.java
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1779267 13f79535-47bb-0310-9956-ffa450edef68

--- src/core/org/apache/jmeter/samplers/SampleResult.java
+++ src/core/org/apache/jmeter/samplers/SampleResult.java
@@ -711,7 +711,7 @@ public class SampleResult implements Serializable, Cloneable, Searchable {
             responseData = response.getBytes(getDataEncodingWithDefault());
         } catch (UnsupportedEncodingException e) {
             log.warn("Could not convert string, using default encoding. "+e.getLocalizedMessage());
-            responseData = response.getBytes(); // N.B. default charset is used deliberately here
+            responseData = response.getBytes(Charset.defaultCharset()); // N.B. default charset is used deliberately here
         }
     }
 
@@ -731,7 +731,7 @@ public class SampleResult implements Serializable, Cloneable, Searchable {
         } catch (UnsupportedEncodingException e) {
             log.warn("Could not convert string using '"+encodeUsing+
                     "', using default encoding: "+DEFAULT_CHARSET,e);
-            responseData = response.getBytes(); // N.B. default charset is used deliberately here
+            responseData = response.getBytes(Charset.defaultCharset()); // N.B. default charset is used deliberately here
             setDataEncoding(DEFAULT_CHARSET);
         }
     }
@@ -763,7 +763,7 @@ public class SampleResult implements Serializable, Cloneable, Searchable {
             return responseDataAsString;
         } catch (UnsupportedEncodingException e) {
             log.warn("Using platform default as "+getDataEncodingWithDefault()+" caused "+e);
-            return new String(responseData); // N.B. default charset is used deliberately here
+            return new String(responseData,Charset.defaultCharset()); // N.B. default charset is used deliberately here
         }
     }
 
