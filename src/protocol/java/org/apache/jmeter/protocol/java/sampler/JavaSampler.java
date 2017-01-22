0c33a09cf326685b9bbf3072bc2007f2bb041612
Felix Schumacher <fschumacher@apache.org>
2017-01-22 12:49:15 +0000

    Utilised Java 8 (and 7) features to tidy up code. Removed unnecessary types. Contributed by Graham Russell (graham at ham1.co.uk). Part 2/8 of github pr #255
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1779802 13f79535-47bb-0310-9956-ffa450edef68

--- src/protocol/java/org/apache/jmeter/protocol/java/sampler/JavaSampler.java
+++ src/protocol/java/org/apache/jmeter/protocol/java/sampler/JavaSampler.java
@@ -120,7 +120,7 @@ public class JavaSampler extends AbstractSampler implements TestStateListener, I
         String name = getClassname().trim();
         try {
             javaClass = Class.forName(name, false, Thread.currentThread().getContextClassLoader());
-            Method method = javaClass.getMethod("teardownTest", new Class[]{JavaSamplerContext.class});
+            Method method = javaClass.getMethod("teardownTest", JavaSamplerContext.class);
             isToBeRegistered = !method.getDeclaringClass().equals(AbstractJavaSamplerClient.class);
             log.info("Created class: " + name + ". Uses tearDownTest: " + isToBeRegistered);
         } catch (Exception e) {
