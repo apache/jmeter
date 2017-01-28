4ecd04b0c20ef4b6e14640c27be9b8a4b9882a4e
Felix Schumacher <fschumacher@apache.org>
2017-01-28 12:39:26 +0000

    Use lambda expression instead of anonymous class.
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1780672 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 8e9cb495f7b16b5df34c07e7aff0b187131c9f68

--- src/components/org/apache/jmeter/visualizers/AssertionVisualizer.java
+++ src/components/org/apache/jmeter/visualizers/AssertionVisualizer.java
@@ -58,13 +58,10 @@ public class AssertionVisualizer extends AbstractVisualizer implements Clearable
         sb.append(sample.getSampleLabel());
         sb.append(getAssertionResult(sample));
         sb.append("\n"); // $NON-NLS-1$
-        JMeterUtils.runSafe(false, new Runnable() {
-            @Override
-            public void run() {
-                synchronized (textArea) {
-                    textArea.append(sb.toString());
-                    textArea.setCaretPosition(textArea.getText().length());
-                }                
+        JMeterUtils.runSafe(false, () -> {
+            synchronized (textArea) {
+                textArea.append(sb.toString());
+                textArea.setCaretPosition(textArea.getText().length());
             }
         });
     }
