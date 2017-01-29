59b162c9d2d9167113dfeabc66931b79edb70a05
Philippe Mouawad <pmouawad@apache.org>
2017-01-29 13:17:50 +0000

    Avoid import *
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1780792 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 80beaa2a0f690c77804aafb247f970bbd4477cf3

--- test/src/org/apache/jmeter/protocol/http/control/TestAuthorization.java
+++ test/src/org/apache/jmeter/protocol/http/control/TestAuthorization.java
@@ -18,7 +18,7 @@
 
 package org.apache.jmeter.protocol.http.control;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 import org.apache.jmeter.protocol.http.control.AuthManager.Mechanism;
 import org.hamcrest.CoreMatchers;
