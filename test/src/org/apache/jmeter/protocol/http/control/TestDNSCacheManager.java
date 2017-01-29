d258c32ae5264f87af956a393a908e3ba9e8e05b
Philippe Mouawad <pmouawad@apache.org>
2017-01-29 20:35:59 +0000

    Avoid NPE if init of resolver fails Add Test cases
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1780852 13f79535-47bb-0310-9956-ffa450edef68

--- test/src/org/apache/jmeter/protocol/http/control/TestDNSCacheManager.java
+++ test/src/org/apache/jmeter/protocol/http/control/TestDNSCacheManager.java
@@ -20,18 +20,92 @@ package org.apache.jmeter.protocol.http.control;
 
 import static org.junit.Assert.fail;
 
+import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 import org.apache.jmeter.junit.JMeterTestCase;
+import org.junit.Assert;
 import org.junit.Test;
+import org.xbill.DNS.ExtendedResolver;
 
 public class TestDNSCacheManager extends JMeterTestCase {
-
+    private static final String INVALID_DNS_SERVER = "8.8.8.8.9"; //$NON-NLS-1$
+    
+    private static final String VALID_DNS_SERVER = "8.8.8.8"; //$NON-NLS-1$
+    @Test
+    public void testWithCustomResolverAnd1WrongServer() throws UnknownHostException {
+        DNSCacheManager original = new DNSCacheManager();
+        original.addServer(INVALID_DNS_SERVER);
+        original.setCustomResolver(true);
+        original.setTimeoutMs(100);
+        try {
+            original.resolve("jmeter.apache.org");
+            fail("Should have failed as DNS server does not exist");
+        } catch (UnknownHostException e) {
+            Assert.assertNull(original.resolver);
+            Assert.assertTrue(original.initFailed);
+        }
+        
+        try {
+            original.resolve("www.apache.org");
+            fail("Should have failed as DNS server does not exist");
+            // OK
+        } catch (UnknownHostException e) {
+            Assert.assertNull(original.resolver);
+            Assert.assertTrue(original.initFailed);
+        }
+    }
+    
+    @Test
+    public void testWithCustomResolverAnd1Server() throws UnknownHostException {
+        DNSCacheManager original = new DNSCacheManager();
+        original.addServer(VALID_DNS_SERVER);
+        original.setCustomResolver(true);
+        original.setTimeoutMs(100);
+        try {
+            original.resolve("jmeter.apache.org");
+            Assert.assertNotNull(original.resolver);
+            Assert.assertTrue(((ExtendedResolver)original.resolver).getResolvers().length==1);
+            // OK
+        } catch (UnknownHostException e) {
+            fail("System DNS server should have been used");
+        }
+    }
+    
+    @Test
+    public void testWithCustomResolverAndNoServer() throws UnknownHostException {
+        DNSCacheManager original = new DNSCacheManager();
+        original.setCustomResolver(true);
+        original.setTimeoutMs(100);
+        try {
+            // This will use Default System DNS resolver
+            original.resolve("jmeter.apache.org");
+            Assert.assertNotNull(original.resolver);
+            Assert.assertTrue(((ExtendedResolver)original.resolver).getResolvers().length==0);
+        } catch (UnknownHostException e) {
+            fail("Should have failed as no DNS server provided");
+        }
+    }
+    
+    @Test
+    public void testWithCustomResolverAndInvalidNameserver() throws UnknownHostException {
+        DNSCacheManager original = new DNSCacheManager();
+        original.setCustomResolver(true);
+        original.addServer(INVALID_DNS_SERVER);
+        original.setTimeoutMs(100);
+        try {
+            original.resolve("jmeter.apache.org");
+            fail();
+        } catch (UnknownHostException e) {
+            // OK
+        }
+    }
+    
     @Test
     public void testCloneWithCustomResolverAndInvalidNameserver() throws UnknownHostException {
         DNSCacheManager original = new DNSCacheManager();
         original.setCustomResolver(true);
-        original.addServer("127.0.0.99");
+        original.addServer(INVALID_DNS_SERVER);
         DNSCacheManager clone = (DNSCacheManager) original.clone();
         clone.setTimeoutMs(100);
         try {
@@ -41,5 +115,32 @@ public class TestDNSCacheManager extends JMeterTestCase {
             // OK
         }
     }
-
+    
+    @Test
+    public void testResolveExistingHostWithSystemDefaultDnsServer() throws UnknownHostException {
+        DNSCacheManager original = new DNSCacheManager();
+        original.setCustomResolver(false);
+        try {
+            InetAddress[] result = original.resolve("www.example.org");
+            Assert.assertNotNull(result);
+            Assert.assertNull(original.resolver);
+            // IPv4 and IPv6
+            Assert.assertTrue(result.length == 2);
+        } catch (UnknownHostException e) {
+            Assert.fail("Should not have failed");
+        }
+    }
+    
+    @Test
+    public void testResolveNonExistingHostWithSystemDefaultDnsServer() throws UnknownHostException {
+        DNSCacheManager original = new DNSCacheManager();
+        original.setCustomResolver(false);
+        try {
+            original.resolve("jmeterxxx.apache.org");
+            fail();
+        } catch (UnknownHostException e) {
+            Assert.assertNull(original.resolver);
+            // OK
+        }
+    }
 }
