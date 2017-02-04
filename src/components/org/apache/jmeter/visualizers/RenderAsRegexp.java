7882f2dd399aa9dd4822c74842a3f5c42f0a141d
Philippe Mouawad <pmouawad@apache.org>
2017-02-04 14:55:09 +0000

    Bug 60691 - View Results Tree : In Renderers (XPath, JSON Path Tester, RegExp Tester and CSS/JQuery Tester) lower panel is sometimes not visible as upper panel is too big and cannot be resized Bugzilla Id: 60691
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1781678 13f79535-47bb-0310-9956-ffa450edef68

--- src/components/org/apache/jmeter/visualizers/RenderAsRegexp.java
+++ src/components/org/apache/jmeter/visualizers/RenderAsRegexp.java
@@ -174,13 +174,14 @@ public class RenderAsRegexp implements ResultRenderer, ActionListener {
         regexpDataField.setWrapStyleWord(true);
 
         JScrollPane regexpDataPane = GuiUtils.makeScrollPane(regexpDataField);
-        regexpDataPane.setMinimumSize(new Dimension(0, 200));
+        regexpDataPane.setPreferredSize(new Dimension(0, 200));
 
         JPanel pane = new JPanel(new BorderLayout(0, 5));
 
         JSplitPane mainSplit = new JSplitPane(JSplitPane.VERTICAL_SPLIT,
                 regexpDataPane, createRegexpTasksPanel());
-        mainSplit.setDividerLocation(300);
+        mainSplit.setDividerLocation(0.6d);
+        mainSplit.setOneTouchExpandable(true);
         pane.add(mainSplit, BorderLayout.CENTER);
         return pane;
     }
