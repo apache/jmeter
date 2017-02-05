06b480df1a0516b82d15883fc684279c1700ea83
Philippe Mouawad <pmouawad@apache.org>
2017-02-05 22:39:54 +0000

    Bug 60564 - Migrating LogKit to SLF4J - Replace logkit loggers with slf4j ones with keeping the current logkit binding solution
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1781826 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: b43d5d44280bb132a1c65ae00a2cd8360d9673d8

--- src/protocol/tcp/org/apache/jmeter/protocol/tcp/sampler/BinaryTCPClientImpl.java
+++ src/protocol/tcp/org/apache/jmeter/protocol/tcp/sampler/BinaryTCPClientImpl.java
@@ -31,9 +31,9 @@ import java.io.OutputStream;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.jmeter.util.JMeterUtils;
-import org.apache.jorphan.logging.LoggingManager;
 import org.apache.jorphan.util.JOrphanUtils;
-import org.apache.log.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * TCPClient implementation.
@@ -45,7 +45,7 @@ import org.apache.log.Logger;
  * Input data is assumed to be in hex, and is converted to binary
  */
 public class BinaryTCPClientImpl extends AbstractTCPClient {
-    private static final Logger log = LoggingManager.getLoggerForClass();
+    private static final Logger log = LoggerFactory.getLogger(BinaryTCPClientImpl.class);
 
     private static final int EOM_INT = JMeterUtils.getPropDefault("tcp.BinaryTCPClient.eomByte", 1000); // $NON_NLS-1$
 
