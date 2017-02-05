619635eb5bffe88c9339d54b6059ff471c575d19
Philippe Mouawad <pmouawad@apache.org>
2017-02-05 20:31:55 +0000

    Fix sonar warning
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1781799 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 9432e403ef7f8917596f259411fdc97ef548f78f

--- src/core/org/apache/jmeter/util/JMeterUtils.java
+++ src/core/org/apache/jmeter/util/JMeterUtils.java
@@ -52,24 +52,24 @@ import javax.swing.UIManager;
 import org.apache.commons.io.IOUtils;
 import org.apache.jmeter.gui.GuiPackage;
 import org.apache.jmeter.threads.JMeterContextService;
-import org.apache.jorphan.logging.LoggingManager;
 import org.apache.jorphan.reflect.ClassFinder;
 import org.apache.jorphan.test.UnitTestManager;
 import org.apache.jorphan.util.JMeterError;
 import org.apache.jorphan.util.JOrphanUtils;
-import org.apache.log.Logger;
 import org.apache.oro.text.MalformedCachePatternException;
 import org.apache.oro.text.PatternCacheLRU;
 import org.apache.oro.text.regex.Pattern;
 import org.apache.oro.text.regex.Perl5Compiler;
 import org.apache.oro.text.regex.Perl5Matcher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * This class contains the static utility methods used by JMeter.
  *
  */
 public class JMeterUtils implements UnitTestManager {
-    private static final Logger log = LoggingManager.getLoggerForClass();
+    private static final Logger log = LoggerFactory.getLogger(JMeterUtils.class);
     
     // Note: cannot use a static variable here, because that would be processed before the JMeter properties
     // have been defined (Bug 52783)
@@ -155,6 +155,7 @@ public class JMeterUtils implements UnitTestManager {
      */
     @Deprecated
     public static void initLogging() {
+        // NOOP
     }
 
     /**
