6fa472dca35fc49b30a2b3246b376b962597e30c
Philippe Mouawad <pmouawad@apache.org>
2017-02-16 21:32:02 +0000

    Bug 60727 - Drop commons-httpclient-3.1 and related elements Fix test failure Bugzilla Id: 60727
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1783307 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: c9900c448edb097d47d8b3f8edb16d3630c85fd3

--- src/protocol/http/org/apache/jmeter/protocol/http/sampler/HTTPSamplerFactory.java
+++ src/protocol/http/org/apache/jmeter/protocol/http/sampler/HTTPSamplerFactory.java
@@ -93,7 +93,7 @@ public final class HTTPSamplerFactory {
         }
         if (IMPL_JAVA.equals(impl) || HTTP_SAMPLER_JAVA.equals(impl)) {
             return new HTTPJavaImpl(base);
-        } else if (IMPL_HTTP_CLIENT4.equals(impl)) {
+        } else if (IMPL_HTTP_CLIENT4.equals(impl) || IMPL_HTTP_CLIENT3_1.equals(impl)) {
             return new HTTPHC4Impl(base);
         } else {
             throw new IllegalArgumentException("Unknown implementation type: '"+impl+"'");
