f729fcaa563a5c43872abd7d91a900b9be302b4b
Felix Schumacher <fschumacher@apache.org>
2017-02-19 15:51:14 +0000

    Remove unused imports.
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1783657 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 89106e19dc42537e55c0873007e8296ad063857a

--- src/components/org/apache/jmeter/visualizers/ViewResultsFullVisualizer.java
+++ src/components/org/apache/jmeter/visualizers/ViewResultsFullVisualizer.java
@@ -33,7 +33,6 @@ import java.awt.event.ItemListener;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
