9f4f4fc30d16172bc1f1db1a7849ac3f22573b63
Philippe Mouawad <pmouawad@apache.org>
2017-02-21 19:52:07 +0000

    Format
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1783926 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 237de6076a10e0da44440ae568426b09f4390d32

--- test/src/org/apache/jmeter/assertions/SizeAssertionTest.java
+++ test/src/org/apache/jmeter/assertions/SizeAssertionTest.java
@@ -31,146 +31,146 @@ import org.apache.jmeter.threads.JMeterVariables;
 import org.junit.Before;
 import org.junit.Test;
 
-public class SizeAssertionTest extends JMeterTestCase{
+public class SizeAssertionTest extends JMeterTestCase {
 
-      private SizeAssertion assertion;
-      private SampleResult sample1,sample0;
-      private AssertionResult result;
-      private final String data1 = "response Data\n" +  "line 2\n\nEOF";
-      private final int data1Len=data1.length();
-      
-      @Before
+    private SizeAssertion assertion;
+    private SampleResult sample1, sample0;
+    private AssertionResult result;
+    private final String data1 = "response Data\n" + "line 2\n\nEOF";
+    private final int data1Len = data1.length();
+
+    @Before
     public void setUp() {
-          JMeterContext jmctx = JMeterContextService.getContext();
-          assertion = new SizeAssertion();
-          assertion.setThreadContext(jmctx);
-          assertion.setTestFieldResponseBody();
-          JMeterVariables vars = new JMeterVariables();
-          jmctx.setVariables(vars);
-          sample0 = new SampleResult();
-          sample1 = new SampleResult();
-          sample1.setResponseData(data1, null);
-      }
+        JMeterContext jmctx = JMeterContextService.getContext();
+        assertion = new SizeAssertion();
+        assertion.setThreadContext(jmctx);
+        assertion.setTestFieldResponseBody();
+        JMeterVariables vars = new JMeterVariables();
+        jmctx.setVariables(vars);
+        sample0 = new SampleResult();
+        sample1 = new SampleResult();
+        sample1.setResponseData(data1, null);
+    }
 
-      @Test
-    public void testSizeAssertionEquals() throws Exception{
-          assertion.setCompOper(SizeAssertion.EQUAL);
-          assertion.setAllowedSize(0);
-          result = assertion.getResult(sample1);
-          assertFailed();
+    @Test
+    public void testSizeAssertionEquals() throws Exception {
+        assertion.setCompOper(SizeAssertion.EQUAL);
+        assertion.setAllowedSize(0);
+        result = assertion.getResult(sample1);
+        assertFailed();
 
-          result = assertion.getResult(sample0);
-          assertPassed();
+        result = assertion.getResult(sample0);
+        assertPassed();
 
-          assertion.setAllowedSize(data1Len);
-          result = assertion.getResult(sample1);
-          assertPassed();
+        assertion.setAllowedSize(data1Len);
+        result = assertion.getResult(sample1);
+        assertPassed();
 
-          result = assertion.getResult(sample0);
-          assertFailed();
-        }
-      
-      @Test
-    public void testSizeAssertionNotEquals() throws Exception{
-          assertion.setCompOper(SizeAssertion.NOTEQUAL);
-          assertion.setAllowedSize(0);
-          result = assertion.getResult(sample1);
-          assertPassed();
+        result = assertion.getResult(sample0);
+        assertFailed();
+    }
 
-          result = assertion.getResult(sample0);
-          assertFailed();
+    @Test
+    public void testSizeAssertionNotEquals() throws Exception {
+        assertion.setCompOper(SizeAssertion.NOTEQUAL);
+        assertion.setAllowedSize(0);
+        result = assertion.getResult(sample1);
+        assertPassed();
 
-          assertion.setAllowedSize(data1Len);
-          result = assertion.getResult(sample1);
-          assertFailed();
+        result = assertion.getResult(sample0);
+        assertFailed();
 
-          result = assertion.getResult(sample0);
-          assertPassed();
-        }
+        assertion.setAllowedSize(data1Len);
+        result = assertion.getResult(sample1);
+        assertFailed();
 
-      @Test
-    public void testSizeAssertionGreaterThan() throws Exception{
-          assertion.setCompOper(SizeAssertion.GREATERTHAN);
-          assertion.setAllowedSize(0);
-          result = assertion.getResult(sample1);
-          assertPassed();
+        result = assertion.getResult(sample0);
+        assertPassed();
+    }
 
-          result = assertion.getResult(sample0);
-          assertFailed();
+    @Test
+    public void testSizeAssertionGreaterThan() throws Exception {
+        assertion.setCompOper(SizeAssertion.GREATERTHAN);
+        assertion.setAllowedSize(0);
+        result = assertion.getResult(sample1);
+        assertPassed();
 
-          assertion.setAllowedSize(data1Len);
-          result = assertion.getResult(sample1);
-          assertFailed();
+        result = assertion.getResult(sample0);
+        assertFailed();
 
-          result = assertion.getResult(sample0);
-          assertFailed();
-        }
-      
-      @Test
-    public void testSizeAssertionGreaterThanEqual() throws Exception{
-          assertion.setCompOper(SizeAssertion.GREATERTHANEQUAL);
-          assertion.setAllowedSize(0);
-          result = assertion.getResult(sample1);
-          assertPassed();
+        assertion.setAllowedSize(data1Len);
+        result = assertion.getResult(sample1);
+        assertFailed();
 
-          result = assertion.getResult(sample0);
-          assertPassed();
+        result = assertion.getResult(sample0);
+        assertFailed();
+    }
 
-          assertion.setAllowedSize(data1Len);
-          result = assertion.getResult(sample1);
-          assertPassed();
+    @Test
+    public void testSizeAssertionGreaterThanEqual() throws Exception {
+        assertion.setCompOper(SizeAssertion.GREATERTHANEQUAL);
+        assertion.setAllowedSize(0);
+        result = assertion.getResult(sample1);
+        assertPassed();
 
-          result = assertion.getResult(sample0);
-          assertFailed();
-        }
-      
-      @Test
-    public void testSizeAssertionLessThan() throws Exception{
-          assertion.setCompOper(SizeAssertion.LESSTHAN);
-          assertion.setAllowedSize(0);
-          result = assertion.getResult(sample1);
-          assertFailed();
+        result = assertion.getResult(sample0);
+        assertPassed();
 
-          result = assertion.getResult(sample0);
-          assertFailed();
+        assertion.setAllowedSize(data1Len);
+        result = assertion.getResult(sample1);
+        assertPassed();
 
-          assertion.setAllowedSize(data1Len+1);
-          result = assertion.getResult(sample1);
-          assertPassed();
+        result = assertion.getResult(sample0);
+        assertFailed();
+    }
 
-          result = assertion.getResult(sample0);
-          assertPassed();
-        }
+    @Test
+    public void testSizeAssertionLessThan() throws Exception {
+        assertion.setCompOper(SizeAssertion.LESSTHAN);
+        assertion.setAllowedSize(0);
+        result = assertion.getResult(sample1);
+        assertFailed();
 
-      @Test
-    public void testSizeAssertionLessThanEqual() throws Exception{
-          assertion.setCompOper(SizeAssertion.LESSTHANEQUAL);
-          assertion.setAllowedSize(0);
-          result = assertion.getResult(sample1);
-          assertFailed();
+        result = assertion.getResult(sample0);
+        assertFailed();
 
-          result = assertion.getResult(sample0);
-          assertPassed();
+        assertion.setAllowedSize(data1Len + 1);
+        result = assertion.getResult(sample1);
+        assertPassed();
 
-          assertion.setAllowedSize(data1Len+1);
-          result = assertion.getResult(sample1);
-          assertPassed();
+        result = assertion.getResult(sample0);
+        assertPassed();
+    }
 
-          result = assertion.getResult(sample0);
-          assertPassed();
-        }
-// TODO - need a lot more tests
-      
-      private void assertPassed() throws Exception{
-          assertNull("Failure message should be null",result.getFailureMessage());
-          assertFalse(result.isError());
-          assertFalse(result.isFailure());        
-      }
-      
-      private void assertFailed() throws Exception{
-          assertNotNull("Failure message should not be null",result.getFailureMessage());
-          assertFalse("Should not be: Response was null","Response was null".equals(result.getFailureMessage()));
-          assertFalse(result.isError());
-          assertTrue(result.isFailure());     
-      }
+    @Test
+    public void testSizeAssertionLessThanEqual() throws Exception {
+        assertion.setCompOper(SizeAssertion.LESSTHANEQUAL);
+        assertion.setAllowedSize(0);
+        result = assertion.getResult(sample1);
+        assertFailed();
+
+        result = assertion.getResult(sample0);
+        assertPassed();
+
+        assertion.setAllowedSize(data1Len + 1);
+        result = assertion.getResult(sample1);
+        assertPassed();
+
+        result = assertion.getResult(sample0);
+        assertPassed();
+    }
+    // TODO - need a lot more tests
+
+    private void assertPassed() throws Exception {
+        assertNull("Failure message should be null", result.getFailureMessage());
+        assertFalse(result.isError());
+        assertFalse(result.isFailure());
+    }
+
+    private void assertFailed() throws Exception {
+        assertNotNull("Failure message should not be null", result.getFailureMessage());
+        assertFalse("Should not be: Response was null", "Response was null".equals(result.getFailureMessage()));
+        assertFalse(result.isError());
+        assertTrue(result.isFailure());
+    }
 }
