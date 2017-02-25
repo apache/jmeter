d91c384e161f16595aa5ae6a73211af4e72bac1c
Philippe Mouawad <pmouawad@apache.org>
2017-02-25 14:43:32 +0000

    Bug 56939 - Parameters are not passed with OPTIONS HTTP Request Bugzilla Id: 56939
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1784383 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: d421595542c15bfede5748f7ef1f310c81dec655

--- src/protocol/http/org/apache/jmeter/protocol/http/sampler/HTTPSamplerBase.java
+++ src/protocol/http/org/apache/jmeter/protocol/http/sampler/HTTPSamplerBase.java
@@ -975,7 +975,9 @@ public abstract class HTTPSamplerBase extends AbstractSampler
         pathAndQuery.append(path);
 
         // Add the query string if it is a HTTP GET or DELETE request
-        if (HTTPConstants.GET.equals(getMethod()) || HTTPConstants.DELETE.equals(getMethod())) {
+        if (HTTPConstants.GET.equals(getMethod()) 
+                || HTTPConstants.DELETE.equals(getMethod())
+                || HTTPConstants.OPTIONS.equals(getMethod())) {
             // Get the query string encoded in specified encoding
             // If no encoding is specified by user, we will get it
             // encoded in UTF-8, which is what the HTTP spec says
