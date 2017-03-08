993278a7b29df1fc3109444ddf04bb91232476d5
Philippe Mouawad <pmouawad@apache.org>
2017-03-08 20:39:21 +0000

    Bug 60837 - Since version 3.1, HttpGet with a body may not be retried even if retrycount is higher than 0 Bugzilla Id: 60837
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1786077 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 8e02c01bfe86f5bc394d3bcc63945748bba8634e

--- src/protocol/http/org/apache/jmeter/protocol/http/sampler/HTTPHC4Impl.java
+++ src/protocol/http/org/apache/jmeter/protocol/http/sampler/HTTPHC4Impl.java
@@ -101,6 +101,7 @@ import org.apache.http.impl.client.DefaultClientConnectionReuseStrategy;
 import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.client.DefaultHttpRequestRetryHandler;
+import org.apache.http.impl.client.EntityEnclosingRequestWrapper;
 import org.apache.http.impl.conn.SystemDefaultDnsResolver;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.message.BufferedHeader;
@@ -823,7 +824,19 @@ public class HTTPHC4Impl extends HTTPHCAbstractImpl {
             httpClient = new DefaultHttpClient(connManager, clientParams) {
                 @Override
                 protected HttpRequestRetryHandler createHttpRequestRetryHandler() {
-                    return new DefaultHttpRequestRetryHandler(RETRY_COUNT, false); // set retry count
+                    return new DefaultHttpRequestRetryHandler(RETRY_COUNT, false) { // set retry count
+                        @Override
+                        protected boolean handleAsIdempotent(HttpRequest request) {
+                            if(request instanceof EntityEnclosingRequestWrapper) {
+                                EntityEnclosingRequestWrapper enclosingRequest =
+                                        (EntityEnclosingRequestWrapper) request;
+                                if(HTTPConstants.GET.equals(enclosingRequest.getMethod())) {
+                                    return true;
+                                }
+                            }
+                            return super.handleAsIdempotent(request);
+                        }
+                    };
                 }
             };
             
