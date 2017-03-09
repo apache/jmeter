fb7c0012b868974ebf857908fabfeab3dd58be9b
Philippe Mouawad <pmouawad@apache.org>
2017-03-09 20:34:05 +0000

    add httpclient4.request_sent_retry_enabled to allow retrying even if request was sent
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1786236 13f79535-47bb-0310-9956-ffa450edef68

--- src/protocol/http/org/apache/jmeter/protocol/http/sampler/HTTPHC4Impl.java
+++ src/protocol/http/org/apache/jmeter/protocol/http/sampler/HTTPHC4Impl.java
@@ -152,6 +152,10 @@ public class HTTPHC4Impl extends HTTPHCAbstractImpl {
 
     /** retry count to be used (default 0); 0 = disable retries */
     private static final int RETRY_COUNT = JMeterUtils.getPropDefault("httpclient4.retrycount", 0);
+    
+    /** true if it's OK to retry requests that have been sent */
+    private static final boolean REQUEST_SENT_RETRY_ENABLED = 
+            JMeterUtils.getPropDefault("httpclient4.request_sent_retry_enabled", false);
 
     /** Idle timeout to be applied to connections if no Keep-Alive header is sent by the server (default 0 = disable) */
     private static final int IDLE_TIMEOUT = JMeterUtils.getPropDefault("httpclient4.idletimeout", 0);
@@ -823,7 +827,8 @@ public class HTTPHC4Impl extends HTTPHCAbstractImpl {
             httpClient = new DefaultHttpClient(connManager, clientParams) {
                 @Override
                 protected HttpRequestRetryHandler createHttpRequestRetryHandler() {
-                    return new StandardHttpRequestRetryHandler(RETRY_COUNT, false);
+                    return new StandardHttpRequestRetryHandler(RETRY_COUNT, 
+                            REQUEST_SENT_RETRY_ENABLED);
                 }
             };
             
