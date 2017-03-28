142418f3ff364c015cf82a7187298413ea72cb71
Philippe Mouawad <pmouawad@apache.org>
2017-03-28 18:59:34 +0000

    Bug 60112 - Report / Dashboard : Add ability to customize APDEX thresholds per Transaction name Contributed by Stephane Leplus This closes #287 Bugzilla Id: 60112
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1789193 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 06760bd0de23d19c360e3f84de2c34e88399e07f

---
+++ test/src/org/apache/jmeter/report/dashboard/ApdexPerTransactionTest.java
@@ -0,0 +1,144 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.apache.jmeter.report.dashboard;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
+import org.apache.jmeter.report.config.ReportGeneratorConfiguration;
+import org.apache.jmeter.util.JMeterUtils;
+import org.apache.oro.text.regex.PatternMatcher;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import jodd.props.Props;
+
+public class ApdexPerTransactionTest {
+	
+	private static final Logger log = LoggerFactory.getLogger(ApdexPerTransactionTest.class);
+	
+	// prop in the file mixes comma, semicolon and spans several lines.
+	// it also includes hardcoded sample names mixed with regexes 
+	private static final String apdexString = "sample(\\d+):1000|2000;samples12:3000|4000;scenar01-12:5000|6000";
+	
+	@Test
+	public void testgetApdexPerTransactionProperty() {
+		final Properties merged = new Properties();
+		final Props props = new Props();
+		final String REPORT_GENERATOR_KEY_PREFIX = "jmeter.reportgenerator";
+		final char KEY_DELIMITER = '.';
+		final String REPORT_GENERATOR_KEY_APDEX_PER_TRANSACTION = REPORT_GENERATOR_KEY_PREFIX
+	            + KEY_DELIMITER + "apdex_per_transaction";
+		
+		File rgp = new File("test/resources/", "reportgenerator_test.properties");
+		merged.putAll(loadProps(rgp));
+		props.load(merged);
+		final String apdexPerTransaction = getOptionalProperty(props, 
+        		REPORT_GENERATOR_KEY_APDEX_PER_TRANSACTION, 
+        		String.class);
+		assertEquals(apdexString, apdexPerTransaction);
+		
+	}
+	
+	@Test
+	public void testGetApdexPerTransactionParts() {
+		Map<String, Long[]> apdex = ReportGeneratorConfiguration.getApdexPerTransactionParts(apdexString);
+		assertNotNull("map should not be null", apdex);
+		assertEquals(3, apdex.size());
+		Set<String> keys = apdex.keySet();
+		assertTrue(keys.contains("samples12"));
+		assertTrue(keys.contains("scenar01-12"));
+		assertTrue(keys.contains("sample(\\d+)"));
+		assertArrayEquals(new Long[] {1000L,  2000L}, apdex.get("sample(\\d+)"));
+	}
+
+   @Test
+    public void testGetApdexPerTransactionPartsOneCustomization() {
+        Map<String, Long[]> apdex = ReportGeneratorConfiguration.getApdexPerTransactionParts("sample(\\d+):1000|2000");
+        assertNotNull("map should not be null", apdex);
+        assertEquals(1, apdex.size());
+        Set<String> keys = apdex.keySet();
+        assertTrue(keys.contains("sample(\\d+)"));
+        assertArrayEquals(new Long[] {1000L,  2000L}, apdex.get("sample(\\d+)"));
+    }
+	
+	@Test
+	public void testSampleNameMatching() {
+		/* matching pairs : 
+		 * sample(\d+) sample2
+		 * sample(\d+) sample12
+		 * scenar01-12 scenar01-12
+		 * samples12 samples12
+		 * */
+		
+		String[] sampleNames = {"sample2","sample12", "scenar01-12", "samples12"};
+		
+		Map<String, Long[]> apdex = ReportGeneratorConfiguration.getApdexPerTransactionParts(apdexString);
+		for (String sampleName : sampleNames) {
+			boolean hasMatched = false;
+			for (Map.Entry<String, Long[]> entry : apdex.entrySet()) {
+				org.apache.oro.text.regex.Pattern regex = JMeterUtils.getPatternCache().getPattern(entry.getKey());
+    			PatternMatcher matcher = JMeterUtils.getMatcher();
+    			if(matcher.matches(sampleName, regex)) {
+    				hasMatched= true;
+    			}
+    		}
+			assertTrue(hasMatched);
+    	}
+		
+	}
+	
+	private static Properties loadProps(File file) {
+        final Properties props = new Properties();
+        try (FileInputStream inStream = new FileInputStream(file)) {
+            props.load(inStream);
+        } catch (IOException e) {
+            log.error("Problem loading properties. " + e); // NOSONAR
+        }
+        return props;
+    }
+	
+	private static String getOptionalProperty(Props props,
+            String key, Class clazz) {
+        String property = getProperty(props, key, null, clazz);
+        if (property != null) {
+        }
+        return property;
+    }
+	
+	private static String getProperty(Props props, String key,
+            String defaultValue, Class clazz)
+             {
+        String value = props.getValue(key);
+        if (value == null) {
+            return defaultValue;
+        }
+        return value;
+    }
+}
