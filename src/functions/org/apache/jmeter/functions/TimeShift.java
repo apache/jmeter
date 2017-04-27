59c179d2366f910d968ea10bb4b5e943988dc8ae
Chassagneux Maxime <4163013@airfrance.fr>
2017-04-27 17:31:25 +0200

    Add a cache for the DateTimeFormatter object

--- src/functions/org/apache/jmeter/functions/TimeShift.java
+++ src/functions/org/apache/jmeter/functions/TimeShift.java
@@ -41,22 +41,22 @@ import org.apache.jmeter.util.JMeterUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+
 /**
  * timeShifting Function permit to shift a date
  *
- * Parameters: 
- * - format date @see
+ * Parameters: - format date @see
  * https://docs.oracle.com/javase/8/docs/api/java/time/format/DateTimeFormatter.html
- * (optional - defaults to epoch time in millisecond) 
- * - date to shift formated
- * as first param (optional - defaults now) 
- * - amount of (seconds, minutes,
- * hours, days or months ) to add (optional - default nothing is add ) 
- * - variable name ( optional )
+ * (optional - defaults to epoch time in millisecond) - date to shift formated
+ * as first param (optional - defaults now) - amount of (seconds, minutes,
+ * hours, days or months ) to add (optional - default nothing is add ) -
+ * variable name ( optional )
  *
- * Returns: a formatted date with the specified number of (seconds,
- * minutes, hours, days or months ) added. - value is also saved in the
- * variable for later re-use.
+ * Returns: a formatted date with the specified number of (seconds, minutes,
+ * hours, days or months ) added. - value is also saved in the variable for
+ * later re-use.
  *
  * @since 3.3
  */
@@ -65,10 +65,8 @@ public class TimeShift extends AbstractFunction {
 
     private static final String KEY = "__timeShift"; // $NON-NLS-1$
 
-    private static final List<String> desc = Arrays.asList(
-            JMeterUtils.getResString("time_format_shift"),
-            JMeterUtils.getResString("date_to_shift"),
-            JMeterUtils.getResString("value_to_shift"),
+    private static final List<String> desc = Arrays.asList(JMeterUtils.getResString("time_format_shift"),
+            JMeterUtils.getResString("date_to_shift"), JMeterUtils.getResString("value_to_shift"),
             JMeterUtils.getResString("function_name_paropt"));
 
     // Ensure that these are set, even if no paramters are provided
@@ -76,7 +74,11 @@ public class TimeShift extends AbstractFunction {
     private String dateToShift = ""; //$NON-NLS-1$
     private String amountToShift = ""; //$NON-NLS-1$
     private String variableName = ""; //$NON-NLS-1$
-    
+    private ZoneId systemDefaultZoneID = ZoneId.systemDefault();
+
+    /** Date time format cache handler **/
+    private Cache<Object, Object> dateTimeFormatterCache = null;
+
     public TimeShift() {
         super();
     }
@@ -85,17 +87,11 @@ public class TimeShift extends AbstractFunction {
     @Override
     public String execute(SampleResult previousResult, Sampler currentSampler) throws InvalidVariableException {
         String dateString;
-        LocalDateTime localDateTimeToShift = LocalDateTime.now(ZoneId.systemDefault());
+        LocalDateTime localDateTimeToShift = LocalDateTime.now(systemDefaultZoneID);
         DateTimeFormatter formatter = null;
         if (!StringUtils.isEmpty(format)) {
             try {
-                formatter = new DateTimeFormatterBuilder().appendPattern(format)
-                        .parseDefaulting(ChronoField.NANO_OF_SECOND, 0).parseDefaulting(ChronoField.MILLI_OF_SECOND, 0)
-                        .parseDefaulting(ChronoField.SECOND_OF_MINUTE, 0).parseDefaulting(ChronoField.MINUTE_OF_HOUR, 0)
-                        .parseDefaulting(ChronoField.HOUR_OF_DAY, 0).parseDefaulting(ChronoField.DAY_OF_MONTH, 1)
-                        .parseDefaulting(ChronoField.MONTH_OF_YEAR, 1)
-                        .parseDefaulting(ChronoField.YEAR_OF_ERA, Year.now().getValue())
-                        .toFormatter(JMeterUtils.getLocale());
+                formatter = (DateTimeFormatter) dateTimeFormatterCache.get(format, key -> createFormatter((String)key));
             } catch (IllegalArgumentException ex) {
                 log.error("Pattern '{}' is invalid", format, ex); // $NON-NLS-1$
                 return "";
@@ -120,7 +116,7 @@ public class TimeShift extends AbstractFunction {
             try {
                 Duration duration = Duration.parse(amountToShift);
                 localDateTimeToShift = localDateTimeToShift.plus(duration);
-            }catch (DateTimeParseException ex) {
+            } catch (DateTimeParseException ex) {
                 log.error("Failed to parse the amount duration '{}' to shift ", amountToShift, ex); // $NON-NLS-1$
             }
         }
@@ -132,13 +128,29 @@ public class TimeShift extends AbstractFunction {
             dateString = String.valueOf(localDateTimeToShift.toInstant(offset).toEpochMilli());
         }
 
-        if (variableName.length() > 0) {
+        if (!StringUtils.isEmpty(variableName)) {
             JMeterVariables vars = getVariables();
             if (vars != null) {// vars will be null on TestPlan
                 vars.put(variableName, dateString);
             }
         }
         return dateString;
+    }
+
+    private DateTimeFormatter createFormatter(String format) throws IllegalArgumentException {
+
+        log.debug("Create a new instance of DateTimeFormatter for format '{}' in the cache", format);
+        return new DateTimeFormatterBuilder().appendPattern(format).parseDefaulting(ChronoField.NANO_OF_SECOND, 0)
+                .parseDefaulting(ChronoField.MILLI_OF_SECOND, 0).parseDefaulting(ChronoField.SECOND_OF_MINUTE, 0)
+                .parseDefaulting(ChronoField.MINUTE_OF_HOUR, 0).parseDefaulting(ChronoField.HOUR_OF_DAY, 0)
+                .parseDefaulting(ChronoField.DAY_OF_MONTH, 1).parseDefaulting(ChronoField.MONTH_OF_YEAR, 1)
+                .parseDefaulting(ChronoField.YEAR_OF_ERA, Year.now().getValue()).toFormatter(JMeterUtils.getLocale());
+    }
+
+    protected static Cache<Object, Object> buildCache() {
+        Caffeine<Object, Object> cacheBuilder = Caffeine.newBuilder();
+        cacheBuilder.maximumSize(100);
+        return cacheBuilder.build();
     }
 
     /** {@inheritDoc} */
@@ -148,10 +160,15 @@ public class TimeShift extends AbstractFunction {
         checkParameterCount(parameters, 0, 4);
         Object[] values = parameters.toArray();
 
-        format = ((CompoundVariable) values[0]).execute();
-        dateToShift = ((CompoundVariable) values[1]).execute();
-        amountToShift = ((CompoundVariable) values[2]).execute();
+        format = ((CompoundVariable) values[0]).execute().trim();
+        dateToShift = ((CompoundVariable) values[1]).execute().trim();
+        amountToShift = ((CompoundVariable) values[2]).execute().trim();
         variableName = ((CompoundVariable) values[3]).execute().trim();
+
+        // Create the cache
+        if (dateTimeFormatterCache == null) {
+            dateTimeFormatterCache =  buildCache();
+        }
 
     }
 
