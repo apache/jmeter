dc6331ceea1c7ba4444918860c22210f9ee02cbc
Chassagneux Maxime <4163013@airfrance.fr>
2017-04-27 14:12:16 +0200

    Use assertThat with hamcrest-date in JUnit test

--- test/src/org/apache/jmeter/functions/TestTimeShiftFunction.java
+++ test/src/org/apache/jmeter/functions/TestTimeShiftFunction.java
@@ -19,11 +19,18 @@
 package org.apache.jmeter.functions;
 
 import static org.apache.jmeter.functions.FunctionTestHelper.makeParams;
-import static org.junit.Assert.assertTrue;
+import static org.exparity.hamcrest.date.LocalDateMatchers.sameDay;
+import static org.exparity.hamcrest.date.LocalDateTimeMatchers.within;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.Collection;
-import java.util.LinkedList;
+import java.util.TimeZone;
 
 import org.apache.jmeter.engine.util.CompoundVariable;
 import org.apache.jmeter.junit.JMeterTestCase;
@@ -35,127 +42,99 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class TestTimeShiftFunction extends JMeterTestCase {
-        private Function function;
+    private Function function;
 
-        private SampleResult result;
+    private SampleResult result;
 
-        private Collection<CompoundVariable> params;
+    private JMeterVariables vars;
 
-        private JMeterVariables vars;
+    private JMeterContext jmctx = null;
 
-        private JMeterContext jmctx = null;
+    private String value;
 
-        private String value;
-        
-        @Before
-        public void setUp() {
-            jmctx = JMeterContextService.getContext();
-            vars = new JMeterVariables();
-            jmctx.setVariables(vars);
-            jmctx.setPreviousResult(result);
-            params = new LinkedList<>();
-            result = new SampleResult();
-            function = new TimeShift();
-        }
+    @Before
+    public void setUp() {
+        jmctx = JMeterContextService.getContext();
+        vars = new JMeterVariables();
+        jmctx.setVariables(vars);
+        jmctx.setPreviousResult(result);
+        result = new SampleResult();
+        function = new TimeShift();
+    }
 
-        @Test
-        public void testDatePlusOneDay() throws Exception {
-            Collection<CompoundVariable> params = makeParams("yyyy-dd-MM","2017-01-01","1d","");
-            function.setParameters(params);
-            value = function.execute(result, null);
-            assertTrue(value.equalsIgnoreCase("2017-02-01"));
-        }
-        
-        @Test
-        public void testDatePlusOneDayInVariable() throws Exception {
-            Collection<CompoundVariable> params = makeParams("yyyy-dd-MM","2017-01-01","1d","VAR");
-            function.setParameters(params);
-            function.execute(result, null);
-            assertTrue(vars.get("VAR").equalsIgnoreCase("2017-02-01"));
-        }
-        
-        @Test
-        public void testDateLessOneDay() throws Exception {
-            Collection<CompoundVariable> params = makeParams("yyyy-dd-MM","2017-01-01","-1d","");
-            function.setParameters(params);
-            value = function.execute(result, null);
-            assertTrue(value.equalsIgnoreCase("2016-31-12"));
-        }
-        
-        @Test
-        public void testDatePlusOneHour() throws Exception {
-            Collection<CompoundVariable> params = makeParams("HH:mm:ss","14:00:00","1H","");
-            function.setParameters(params);
-            value = function.execute(result, null);
-            assertTrue(value.equalsIgnoreCase("15:00:00"));
-        }
-        
-        @Test
-        public void testDateLessOneMinute() throws Exception {
-            Collection<CompoundVariable> params = makeParams("HH:mm:ss","14:00:00","-1m","");
-            function.setParameters(params);
-            value = function.execute(result, null);
-            assertTrue(value.equalsIgnoreCase("13:59:00"));
-        }
-        
-        @Test
-        public void testDefault() throws Exception {
-            Collection<CompoundVariable> params = makeParams("","","","");
-            function.setParameters(params);
-            long before = Instant.now().toEpochMilli();
-            value = function.execute(result, null);
-            long after = Instant.now().toEpochMilli();
-            long now = Long.parseLong(value);
-            assertTrue(now >= before && now <= after);
-        }
-        
-        @Test
-        public void testDefaultPlusOneDay() throws Exception {
-            Collection<CompoundVariable> params = makeParams("","","1d","");
-            function.setParameters(params);
-            long before = Instant.now().toEpochMilli();
-            before = before + 86_400_000;
-            value = function.execute(result, null);
-            long after = Instant.now().toEpochMilli();
-            after = after + 86_400_000;
-            long now = Long.parseLong(value);
-            assertTrue(now >= before && now <= after);
-        }
-        
-        @Test
-        public void testWrongAmountToAdd() throws Exception {
-            // Nothin is add with wrong value
-            Collection<CompoundVariable> params = makeParams("","","1dsd","");
-            function.setParameters(params);
-            long before = Instant.now().toEpochMilli();
-            value = function.execute(result, null);
-            long after = Instant.now().toEpochMilli();
-            long now = Long.parseLong(value);
-            assertTrue(now >= before && now <= after);
-        }
-        
-        @Test
-        public void testWrongFormatDate() throws Exception {
-            Collection<CompoundVariable> params = makeParams("hjfdjyra:fd","","1d","");
-            function.setParameters(params);
-            long before = Instant.now().toEpochMilli();
-            before = before + 86_400_000;
-            value = function.execute(result, null);
-            long after = Instant.now().toEpochMilli();
-            after = after + 86_400_000;
-            long now = Long.parseLong(value);
-            assertTrue(now >= before && now <= after);
-        }
-        
-        @Test
-        public void testAllWrongParameter() throws Exception {
-            Collection<CompoundVariable> params = makeParams("hjfdjyra:fd","2014-35-2","-ad51r","");
-            function.setParameters(params);
-            long before = Instant.now().toEpochMilli();
-            value = function.execute(result, null);
-            long after = Instant.now().toEpochMilli();
-            long now = Long.parseLong(value);
-            assertTrue(now >= before && now <= after);
-        }
-        
+    @Test
+    public void testDatePlusOneDay() throws Exception {
+        Collection<CompoundVariable> params = makeParams("yyyy-dd-MM", "2017-01-01", "1d", "");
+        function.setParameters(params);
+        value = function.execute(result, null);
+        assertThat(value, is(equalTo("2017-02-01")));
+    }
+
+    @Test
+    public void testDatePlusOneDayInVariable() throws Exception {
+        Collection<CompoundVariable> params = makeParams("yyyy-dd-MM", "2017-01-01", "1d", "VAR");
+        function.setParameters(params);
+        function.execute(result, null);
+        assertThat(vars.get("VAR"), is(equalTo("2017-02-01")));
+    }
+
+    @Test
+    public void testDatePlusComplexPeriod() throws Exception {
+        Collection<CompoundVariable> params = makeParams("yyyy-dd-MM HH:m", "2017-01-01 12:00", "+1M+1d-1H-5m", "VAR");
+        function.setParameters(params);
+        String value = function.execute(result, null);
+        assertThat(value, is(equalTo("2017-02-02 10:55")));
+    }
+
+    @Test
+    public void testDefault() throws Exception {
+        Collection<CompoundVariable> params = makeParams("", "", "", "");
+        function.setParameters(params);
+        value = function.execute(result, null);
+        long resultat = Long.parseLong(value);
+        LocalDateTime  nowFromFunction = LocalDateTime.ofInstant(Instant.ofEpochMilli(resultat), TimeZone
+                .getDefault().toZoneId());
+        assertThat(nowFromFunction, within(5, ChronoUnit.SECONDS, LocalDateTime.now()));
+    }
+
+    @Test
+    public void testNowPlusOneDay() throws Exception {
+        Collection<CompoundVariable> params = makeParams("YYYY-MM-dd", "", "1d", "");
+        function.setParameters(params);
+        value = function.execute(result, null);
+        LocalDate tomorrow = LocalDate.now().plusDays(1);
+        LocalDate tomorrowFromFunction = LocalDate.parse(value);
+        assertThat(tomorrowFromFunction, sameDay(tomorrow));
+    }
+    
+    @Test
+    public void testNowWithComplexPeriod() throws Exception {
+        Collection<CompoundVariable> params = makeParams("YYYY-MM-dd'T'HH:mm:ss", "", "+1M+1d-1H-5m5s", "");
+        function.setParameters(params);
+        value = function.execute(result, null);
+        LocalDateTime futureDate = LocalDateTime.now().plusMonths(1).plusDays(1).plusHours(-1).plusMinutes(-5).plusSeconds(5);
+        LocalDateTime futureDateFromFunction = LocalDateTime.parse(value);
+        assertThat(futureDateFromFunction, within(1, ChronoUnit.SECONDS, futureDate));
+    }
+
+    @Test
+    public void testWrongAmountToAdd() throws Exception {
+        // Nothing is add with wrong value, so check if return is now
+        Collection<CompoundVariable> params = makeParams("", "", "1Psd", "");
+        function.setParameters(params);
+        value = function.execute(result, null);
+        long resultat = Long.parseLong(value);
+        LocalDateTime  nowFromFunction = LocalDateTime.ofInstant(Instant.ofEpochMilli(resultat), TimeZone
+                .getDefault().toZoneId());
+        assertThat(nowFromFunction, within(5, ChronoUnit.SECONDS, LocalDateTime.now()));
+    }
+
+    @Test
+    public void testWrongFormatDate() throws Exception {
+        Collection<CompoundVariable> params = makeParams("hjfdjyra:fd", "", "1d", "");
+        function.setParameters(params);
+        value = function.execute(result, null);
+        assertThat(value, is(equalTo("")));
+    }
+
 }
