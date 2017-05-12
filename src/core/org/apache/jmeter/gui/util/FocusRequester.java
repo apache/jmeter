359a7d8f11507cc650ede8361d884a1b6858b1bb
Felix Schumacher <fschumacher@apache.org>
2017-05-12 19:38:56 +0000

    Use method reference.
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1795016 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 60213f5fde68580acce78d6d6b3bca911f50706f

--- src/core/org/apache/jmeter/gui/util/FocusRequester.java
+++ src/core/org/apache/jmeter/gui/util/FocusRequester.java
@@ -30,6 +30,6 @@ import javax.swing.SwingUtilities;
 public class FocusRequester {
 
     public static void requestFocus(final Component comp) {
-        SwingUtilities.invokeLater(() -> comp.requestFocusInWindow());
+        SwingUtilities.invokeLater(comp::requestFocusInWindow);
     }
 }
