a37df9d1edd04c7c27f71b191e033228c9101b1c
Philippe Mouawad <pmouawad@apache.org>
2017-06-02 20:04:13 +0000

    Bug 61151 - Report/Dashboard : Top 5 Errors by Sampler and Errors : If assertion contains html code, the html part is hidden Bugzilla Id: 61151
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1797423 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 2de23e69a1254c687363d24da258fb195f5f1959

--- test/src/org/apache/jmeter/report/processor/ErrorsSummaryConsumerTest.java
+++ test/src/org/apache/jmeter/report/processor/ErrorsSummaryConsumerTest.java
@@ -35,6 +35,16 @@ public class ErrorsSummaryConsumerTest {
         sample = new Sample(0, metadata, new String[] { "false", "200", "", "FailureMessage" });
         Assert.assertEquals("FailureMessage", ErrorsSummaryConsumer.getErrorKey(sample));
         
+        sample = new Sample(0, metadata, new String[] { "false", "200", "", 
+                "Test failed: text expected to contain /<title>Some html text</title>/" });
+        Assert.assertEquals("Test failed: text expected to contain \\/&lt;title&gt;Some html text&lt;\\/title&gt;\\/", 
+                ErrorsSummaryConsumer.getErrorKey(sample));
+        
+        sample = new Sample(0, metadata, new String[] { "false", "200", "", 
+                "Test failed: text expected to contain /{\"glossary\": { \"title\": \"example glossary\"}}/" });
+        Assert.assertEquals("Test failed: text expected to contain \\/{&quot;glossary&quot;: { &quot;title&quot;: &quot;example glossary&quot;}}\\/", 
+                ErrorsSummaryConsumer.getErrorKey(sample));
+        
         sample = new Sample(0, metadata, new String[] { "true", "200", "", "" });
         Assert.assertEquals(ErrorsSummaryConsumer.ASSERTION_FAILED, ErrorsSummaryConsumer.getErrorKey(sample));
 
