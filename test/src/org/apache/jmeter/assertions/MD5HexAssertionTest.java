a505a66ef65fa15c7149168d599ad2e6e77ddbb4
Felix Schumacher <fschumacher@apache.org>
2017-07-15 12:05:49 +0000

    Unit tests for MD5HexAssertion
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1802020 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: a8bf8ef0ff1d40b2d93be75036f8aaccc2442b3d

--- test/src/org/apache/jmeter/assertions/MD5HexAssertionTest.java
+++ test/src/org/apache/jmeter/assertions/MD5HexAssertionTest.java
@@ -18,14 +18,72 @@
 
 package org.apache.jmeter.assertions;
 
+import java.nio.charset.StandardCharsets;
+
+import org.apache.jmeter.samplers.SampleResult;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class MD5HexAssertionTest {
+    
+    private MD5HexAssertion assertion;
+
+    @Before
+    public void setUp() {
+        assertion = new MD5HexAssertion();
+    }
+
+    @Test
+    public void testEmptyResponse() throws Exception {
+        AssertionResult result = assertion.getResult(sampleResult(""));
+        Assert.assertTrue(result.isFailure());
+        Assert.assertNotEquals(result.getFailureMessage(), "");
+    }
+
+    @Test
+    public void testEmptyMD5() throws Exception {
+        assertion.setAllowedMD5Hex("");
+        AssertionResult result = assertion.getResult(sampleResult("anything"));
+        Assert.assertTrue(result.isFailure());
+        Assert.assertNotEquals(result.getFailureMessage(), "");
+    }
+
+    @Test
+    public void testWrongMD5() throws Exception {
+        assertion.setAllowedMD5Hex("a");
+        AssertionResult result = assertion.getResult(sampleResult("anything"));
+        Assert.assertTrue(result.isFailure());
+        Assert.assertNotEquals(result.getFailureMessage(), "");
+    }
+
+    @Test
+    public void testCorrectMD5LowerCase() throws Exception {
+        assertion.setAllowedMD5Hex("f0e166dc34d14d6c228ffac576c9a43c");
+        AssertionResult result = assertion.getResult(sampleResult("anything"));
+        Assert.assertFalse(result.isFailure());
+        Assert.assertFalse(result.isError());
+        Assert.assertNull(result.getFailureMessage());
+    }
+
+    @Test
+    public void testCorrectMD5MixedCase() throws Exception {
+        assertion.setAllowedMD5Hex("F0e166Dc34D14d6c228ffac576c9a43c");
+        AssertionResult result = assertion.getResult(sampleResult("anything"));
+        Assert.assertFalse(result.isFailure());
+        Assert.assertFalse(result.isError());
+        Assert.assertNull(result.getFailureMessage());
+    }
 
     @Test
     public void testMD5() throws Exception {
         Assert.assertEquals("D41D8CD98F00B204E9800998ECF8427E", MD5HexAssertion.baMD5Hex(new byte[] {}).toUpperCase(java.util.Locale.ENGLISH));
+    }
+
+    private SampleResult sampleResult(String data) {
+        SampleResult response = new SampleResult();
+        response.setResponseData(data.getBytes(StandardCharsets.UTF_8));
+        return response;
     }
 
 }
