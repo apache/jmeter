70150c4a766f0d16b782349a5078ac2475827c0b
Philippe Mouawad <pmouawad@apache.org>
2017-07-22 12:55:58 +0000

    Bug 61259 - JDBC Request : since JMeter 3.0, when JDBC auto-commit is false, a rollback statement happens each time a Request is executed Bugzilla Id: 61259
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1802688 13f79535-47bb-0310-9956-ffa450edef68

--- src/protocol/jdbc/org/apache/jmeter/protocol/jdbc/config/DataSourceElement.java
+++ src/protocol/jdbc/org/apache/jmeter/protocol/jdbc/config/DataSourceElement.java
@@ -221,6 +221,7 @@ public class DataSourceElement extends AbstractTestElement
         int poolSize = Integer.parseInt(maxPool);
         dataSource.setMinIdle(0);
         dataSource.setInitialSize(poolSize);
+        dataSource.setEnableAutoCommitOnReturn(false);
         dataSource.setRollbackOnReturn(false);
         dataSource.setMaxIdle(poolSize);
         dataSource.setMaxTotal(poolSize);
