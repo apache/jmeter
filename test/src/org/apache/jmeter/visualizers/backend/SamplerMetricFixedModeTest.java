b611d53b2ef634494abf7a321d05424ad9524eee
Chassagneux Maxime <4163013@airfrance.fr>
2017-07-25 14:15:05 +0200

    Back to old method for change static field

--- test/src/org/apache/jmeter/visualizers/backend/SamplerMetricFixedModeTest.java
+++ test/src/org/apache/jmeter/visualizers/backend/SamplerMetricFixedModeTest.java
@@ -15,17 +15,15 @@
  * limitations under the License.
  *
  */
- 
+
 package org.apache.jmeter.visualizers.backend;
 
 import static org.junit.Assert.assertEquals;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.Locale;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 
 import org.apache.jmeter.samplers.SampleResult;
-import org.apache.jmeter.util.JMeterUtils;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -33,73 +31,76 @@ public class SamplerMetricFixedModeTest {
 
     private static final int DEFAULT_ELAPSED_TIME = 1_000;
 
-    public static void createJmeterEnv() throws IOException {
-        File propsFile;
-        try {
-            propsFile = File.createTempFile("jmeter", ".properties");
-            propsFile.deleteOnExit();
-            JMeterUtils.loadJMeterProperties(propsFile.getAbsolutePath());
-        } catch (IOException ex) {
-            ex.printStackTrace(System.err);
-        }
-        JMeterUtils.setLocale(new Locale("ignoreResources"));
+    /**
+     * Method to change a static final field
+     * 
+     * @param field
+     * @param newValue
+     * @throws Exception
+     */
+    static void setFinalStatic(Field field, Object newValue) throws Exception {
+        field.setAccessible(true);
+        Field modifiersField = Field.class.getDeclaredField("modifiers");
+        modifiersField.setAccessible(true);
+        modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
+        field.set(null, newValue);
     }
-    
+
     @Before
-    public void initMode() throws IOException  {
-        createJmeterEnv();
-        JMeterUtils.setProperty("backend_metrics_window_mode", "FIXED");
-     }
+    public void initMode() throws NoSuchFieldException, SecurityException, Exception {
+        setFinalStatic(SamplerMetric.class.getDeclaredField("WINDOW_MODE"), WindowMode.FIXED);
+    }
 
     @Test
     public void checkResetOkAndAllStats() throws NoSuchFieldException, SecurityException, Exception {
 
         SamplerMetric metric = new SamplerMetric();
         metric.add(createSampleResult(true));
-        assertEquals("Before reset  ok.max", DEFAULT_ELAPSED_TIME, metric.getOkMaxTime() , 0.001);
+        assertEquals("Before reset  ok.max", DEFAULT_ELAPSED_TIME, metric.getOkMaxTime(), 0.001);
         assertEquals("Before reset all.max", DEFAULT_ELAPSED_TIME, metric.getAllMaxTime(), 0.001);
         assertEquals("Before reset failure", 1, metric.getHits(), 0.0);
-        
-        // In fixed mode DescriptiveStatistics are not reset, just sliding on a window
+
+        // In fixed mode DescriptiveStatistics are not reset, just sliding on a
+        // window
         metric.resetForTimeInterval();
-        
-        assertEquals("After reset in FIXED mode ok.max", DEFAULT_ELAPSED_TIME, metric.getOkMaxTime() , 0.001);
+
+        assertEquals("After reset in FIXED mode ok.max", DEFAULT_ELAPSED_TIME, metric.getOkMaxTime(), 0.001);
         assertEquals("After reset in FIXED mode all.max", DEFAULT_ELAPSED_TIME, metric.getAllMaxTime(), 0.0);
         assertEquals("After reset failure", 0, metric.getHits(), 0.0);
-        
+        metric = null;
     }
 
     @Test
     public void checkResetKoAndAllStats() throws NoSuchFieldException, SecurityException, Exception {
-       
+
         SamplerMetric metric = new SamplerMetric();
         metric.add(createSampleResult(false));
-        assertEquals("Before reset  ko.max", DEFAULT_ELAPSED_TIME, metric.getKoMaxTime() , 0.001);
+        assertEquals("Before reset  ko.max", DEFAULT_ELAPSED_TIME, metric.getKoMaxTime(), 0.001);
         assertEquals("Before reset all.max", DEFAULT_ELAPSED_TIME, metric.getAllMaxTime(), 0.001);
         assertEquals("Before reset failure", 1, metric.getFailures(), 0.0);
-        
-        // In fixed mode DescriptiveStatistics are not reset, just sliding on a window
+
+        // In fixed mode DescriptiveStatistics are not reset, just sliding on a
+        // window
         metric.resetForTimeInterval();
-        
-        assertEquals("After reset in FIXED mode  ko.max", DEFAULT_ELAPSED_TIME, metric.getKoMaxTime() , 0.0);
+
+        assertEquals("After reset in FIXED mode  ko.max", DEFAULT_ELAPSED_TIME, metric.getKoMaxTime(), 0.0);
         assertEquals("After reset in FIXED mode all.max", DEFAULT_ELAPSED_TIME, metric.getAllMaxTime(), 0.0);
         assertEquals("After reset failure", 0, metric.getFailures(), 0.001);
-        
+        metric = null;
     }
-    
 
     @Test
     public void checkErrorsDetailStat() {
-            
+
         SamplerMetric metric = new SamplerMetric();
         metric.add(createSampleResult("400", "bad request"));
         metric.add(createSampleResult("400", "Bad Request "));
         metric.add(createSampleResult("500", "Internal Server Error"));
-        ErrorMetric error = new ErrorMetric( createSampleResult("400", "Bad request") );
-        assertEquals("Count for '400 - bad request' error ", 2,  metric.getErrors().get(error) , 0.0);
-        error = new ErrorMetric( createSampleResult("500", "Internal Server Error") );
-        assertEquals("Count for '500 - Internal Server Error' error ", 1,  metric.getErrors().get(error) , 0.0);
-        
+        ErrorMetric error = new ErrorMetric(createSampleResult("400", "Bad request"));
+        assertEquals("Count for '400 - bad request' error ", 2, metric.getErrors().get(error), 0.0);
+        error = new ErrorMetric(createSampleResult("500", "Internal Server Error"));
+        assertEquals("Count for '500 - Internal Server Error' error ", 1, metric.getErrors().get(error), 0.0);
+        metric = null;
     }
 
     private SampleResult createSampleResult(boolean success) {
@@ -111,11 +112,11 @@ public class SamplerMetricFixedModeTest {
         result.setEndTime(result.getStartTime() + DEFAULT_ELAPSED_TIME);
         return result;
     }
-    
+
     private SampleResult createSampleResult(String errorCode, String errorMessage) {
         SampleResult result = createSampleResult(false);
         result.setResponseCode(errorCode);
-        result.setResponseMessage(errorMessage);   
+        result.setResponseMessage(errorMessage);
         return result;
     }
 }
