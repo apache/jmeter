d336b6968aa89eb9d9587c57496625564c65032b
Maxime Chassagneux <mchassagneux@apache.org>
2017-07-25 12:59:56 +0000

    Fix reset metric on timed mode and split JUnit test ( for timed et fixed mode ) to handle correctly the real behavior of JMeter Bugzilla Id: 61339
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1802933 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 1410ca644614d55ce0f930b7eb2a203c24d17f43

--- test/src/org/apache/jmeter/visualizers/backend/SamplerMetricFixedModeTest.java
+++ test/src/org/apache/jmeter/visualizers/backend/SamplerMetricFixedModeTest.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  *
  */
- 
+
 package org.apache.jmeter.visualizers.backend;
 
 import static org.junit.Assert.assertEquals;
@@ -24,14 +24,16 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 
 import org.apache.jmeter.samplers.SampleResult;
+import org.junit.Before;
 import org.junit.Test;
 
-public class SamplerMetricTest {
+public class SamplerMetricFixedModeTest {
 
     private static final int DEFAULT_ELAPSED_TIME = 1_000;
 
     /**
      * Method to change a static final field
+     * 
      * @param field
      * @param newValue
      * @throws Exception
@@ -42,71 +44,63 @@ public class SamplerMetricTest {
         modifiersField.setAccessible(true);
         modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
         field.set(null, newValue);
-     }
+    }
+
+    @Before
+    public void initMode() throws NoSuchFieldException, SecurityException, Exception {
+        setFinalStatic(SamplerMetric.class.getDeclaredField("WINDOW_MODE"), WindowMode.FIXED);
+    }
 
     @Test
     public void checkResetOkAndAllStats() throws NoSuchFieldException, SecurityException, Exception {
-        setFinalStatic(SamplerMetric.class.getDeclaredField("WINDOW_MODE"), WindowMode.FIXED);
+
         SamplerMetric metric = new SamplerMetric();
         metric.add(createSampleResult(true));
-        assertEquals("Before reset  ok.max", DEFAULT_ELAPSED_TIME, metric.getOkMaxTime() , 0.001);
+        assertEquals("Before reset  ok.max", DEFAULT_ELAPSED_TIME, metric.getOkMaxTime(), 0.001);
         assertEquals("Before reset all.max", DEFAULT_ELAPSED_TIME, metric.getAllMaxTime(), 0.001);
         assertEquals("Before reset failure", 1, metric.getHits(), 0.0);
-        
-        // In fixed mode DescriptiveStatistics are not reset, just sliding on a window
+
+        // In fixed mode DescriptiveStatistics are not reset, just sliding on a
+        // window
         metric.resetForTimeInterval();
-        
-        assertEquals("After reset in FIXED mode ok.max", DEFAULT_ELAPSED_TIME, metric.getOkMaxTime() , 0.0);
+
+        assertEquals("After reset in FIXED mode ok.max", DEFAULT_ELAPSED_TIME, metric.getOkMaxTime(), 0.001);
         assertEquals("After reset in FIXED mode all.max", DEFAULT_ELAPSED_TIME, metric.getAllMaxTime(), 0.0);
         assertEquals("After reset failure", 0, metric.getHits(), 0.0);
-        
-        // Change mode to TIMED, now all metric are reset on each interval
-        setFinalStatic(SamplerMetric.class.getDeclaredField("WINDOW_MODE"), WindowMode.TIMED);
-        metric.resetForTimeInterval();
-        
-        assertEquals("After reset in TIMED mode ok.max", Double.NaN, metric.getOkMaxTime() , 0.0);
-        assertEquals("After reset in TIMED mode all.max", Double.NaN, metric.getAllMaxTime(), 0.0);
-        
+        metric = null;
     }
 
     @Test
     public void checkResetKoAndAllStats() throws NoSuchFieldException, SecurityException, Exception {
-       
-        setFinalStatic(SamplerMetric.class.getDeclaredField("WINDOW_MODE"), WindowMode.FIXED);
+
         SamplerMetric metric = new SamplerMetric();
         metric.add(createSampleResult(false));
-        assertEquals("Before reset  ko.max", DEFAULT_ELAPSED_TIME, metric.getKoMaxTime() , 0.001);
+        assertEquals("Before reset  ko.max", DEFAULT_ELAPSED_TIME, metric.getKoMaxTime(), 0.001);
         assertEquals("Before reset all.max", DEFAULT_ELAPSED_TIME, metric.getAllMaxTime(), 0.001);
         assertEquals("Before reset failure", 1, metric.getFailures(), 0.0);
-        
-        // In fixed mode DescriptiveStatistics are not reset, just sliding on a window
+
+        // In fixed mode DescriptiveStatistics are not reset, just sliding on a
+        // window
         metric.resetForTimeInterval();
-        
-        assertEquals("After reset in FIXED mode  ko.max", DEFAULT_ELAPSED_TIME, metric.getKoMaxTime() , 0.0);
+
+        assertEquals("After reset in FIXED mode  ko.max", DEFAULT_ELAPSED_TIME, metric.getKoMaxTime(), 0.0);
         assertEquals("After reset in FIXED mode all.max", DEFAULT_ELAPSED_TIME, metric.getAllMaxTime(), 0.0);
         assertEquals("After reset failure", 0, metric.getFailures(), 0.001);
-        
-        // Change mode to TIMED, now all metric are reset on each interval
-        setFinalStatic(SamplerMetric.class.getDeclaredField("WINDOW_MODE"), WindowMode.TIMED);
-        metric.resetForTimeInterval();
-        
-        assertEquals("After reset in TIMED mode  ko.max", Double.NaN, metric.getKoMaxTime() , 0.0);
-        assertEquals("After reset in TIMED mode all.max", Double.NaN, metric.getAllMaxTime(), 0.0);
+        metric = null;
     }
-    
 
     @Test
     public void checkErrorsDetailStat() {
-            
+
         SamplerMetric metric = new SamplerMetric();
         metric.add(createSampleResult("400", "bad request"));
         metric.add(createSampleResult("400", "Bad Request "));
         metric.add(createSampleResult("500", "Internal Server Error"));
-        ErrorMetric error = new ErrorMetric( createSampleResult("400", "Bad request") );
-        assertEquals("Count for '400 - bad request' error ", 2,  metric.getErrors().get(error) , 0.0);
-        error = new ErrorMetric( createSampleResult("500", "Internal Server Error") );
-        assertEquals("Count for '500 - Internal Server Error' error ", 1,  metric.getErrors().get(error) , 0.0);
-        
+        ErrorMetric error = new ErrorMetric(createSampleResult("400", "Bad request"));
+        assertEquals("Count for '400 - bad request' error ", 2, metric.getErrors().get(error), 0.0);
+        error = new ErrorMetric(createSampleResult("500", "Internal Server Error"));
+        assertEquals("Count for '500 - Internal Server Error' error ", 1, metric.getErrors().get(error), 0.0);
+        metric = null;
     }
 
     private SampleResult createSampleResult(boolean success) {
@@ -118,11 +112,11 @@ public class SamplerMetricTest {
         result.setEndTime(result.getStartTime() + DEFAULT_ELAPSED_TIME);
         return result;
     }
-    
+
     private SampleResult createSampleResult(String errorCode, String errorMessage) {
         SampleResult result = createSampleResult(false);
         result.setResponseCode(errorCode);
-        result.setResponseMessage(errorMessage);   
+        result.setResponseMessage(errorMessage);
         return result;
     }
 }
