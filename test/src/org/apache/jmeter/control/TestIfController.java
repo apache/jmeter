719eddaa53824ab3cb3dba81aa86a05c96d6a523
Antonio Gomes Rodrigues <agomes@apache.org>
2017-10-09 22:53:09 +0000

    Bug 61602 - Fix broken tests - part1
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1811623 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 5a8c32006a6bc47a6b05abbb934e8430a61080cb

--- test/src/org/apache/jmeter/control/TestIfController.java
+++ test/src/org/apache/jmeter/control/TestIfController.java
@@ -102,8 +102,8 @@ public class TestIfController extends JMeterTestCase {
         ifCont2.setEvaluateAll(false);
 
         CounterConfig counterConfig = new CounterConfig();
-        counterConfig.setStart(1);
-        counterConfig.setIncrement(1);
+        counterConfig.setStartValue("1");
+        counterConfig.setIncrement("1");
         counterConfig.setVarName("VAR1");
 
         DebugSampler debugSampler2 = new DebugSampler();
