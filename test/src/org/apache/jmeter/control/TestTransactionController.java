49255f63c8bc4dcd2b40cd248ae0d8180d43bcae
Vincent Herilier <vherilier@gmail.com>
2017-10-10 18:49:54 +0200

    Add related JUnit test

--- test/src/org/apache/jmeter/control/TestTransactionController.java
+++ test/src/org/apache/jmeter/control/TestTransactionController.java
@@ -12,11 +12,14 @@ import org.apache.jmeter.threads.JMeterVariables;
 import org.apache.jmeter.threads.ListenerNotifier;
 import org.apache.jmeter.threads.TestCompiler;
 import org.apache.jmeter.threads.ThreadGroup;
+import org.apache.jmeter.util.JMeterUtils;
 import org.apache.jorphan.collections.ListedHashTree;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+
+
 
 import static org.junit.Assert.assertEquals;
 
@@ -72,7 +75,142 @@ public class TestTransactionController extends JMeterTestCase {
         assertEquals("Number of samples in transaction : 1, number of failing samples : 1", listener.events.get(0).getResult().getResponseMessage());
     }
 
+    /**
+     * @see "http://bz.apache.org/bugzilla/show_bug.cgi?id=61466"
+     */
+    @Test
+    public void testIssue61466_dont_global_use() throws Exception {
+        JMeterContextService.getContext().setVariables(new JMeterVariables());
+        JMeterUtils.getJMeterProperties();
 
+
+        // transactioncontroller.use_comment_on_all value defined as "false" per default
+        JMeterUtils.setProperty("transactioncontroller.use_comment_on_all", "false");
+
+        TestSampleListener listener1 = new TestSampleListener();
+        TestSampleListener listener2 = new TestSampleListener();
+        TestSampleListener listener3 = new TestSampleListener();
+
+        TransactionController transactionController1 = new TransactionController();
+        transactionController1.setComment("Test with default use of comment");
+        transactionController1.setGenerateParentSample(true);
+
+        TransactionController transactionController2 = new TransactionController();
+        transactionController2.setComment("Test with forced use of comment");
+        transactionController2.setGenerateParentSample(true);
+        transactionController2.setUseComment(true);
+
+        TransactionController transactionController3 = new TransactionController();
+        transactionController3.setComment("Test with discarded use of comment");
+        transactionController3.setGenerateParentSample(true);
+        transactionController3.setUseComment(false);
+
+        DebugSampler debugSampler1 = new DebugSampler();
+        DebugSampler debugSampler2 = new DebugSampler();
+        DebugSampler debugSampler3 = new DebugSampler();
+
+        LoopController loop = new LoopController();
+        loop.setLoops(1);
+        loop.setContinueForever(false);
+
+        ListedHashTree hashTree = new ListedHashTree();
+        hashTree.add(loop);
+        hashTree.add(loop, transactionController1);
+        hashTree.add(transactionController1, debugSampler1);
+        hashTree.add(transactionController1, listener1);
+        hashTree.add(loop, transactionController2);
+        hashTree.add(transactionController2);
+        hashTree.add(transactionController2, debugSampler2);
+        hashTree.add(transactionController2, listener2);
+        hashTree.add(loop, transactionController3);
+        hashTree.add(transactionController3);
+        hashTree.add(transactionController3, debugSampler3);
+        hashTree.add(transactionController3, listener3);
+        
+        TestCompiler compiler = new TestCompiler(hashTree);
+        hashTree.traverse(compiler);
+
+        ThreadGroup threadGroup = new ThreadGroup();
+        threadGroup.setNumThreads(1);
+
+        ListenerNotifier notifier = new ListenerNotifier();
+
+        JMeterThread thread = new JMeterThread(hashTree, threadGroup, notifier);
+        thread.setThreadGroup(threadGroup);
+        thread.setOnErrorStopThread(true);
+        thread.run();
+
+        assertEquals("First TC must discard 'Comment' value from its SampleResult", "", listener1.events.get(0).getResult().getSampleComment());
+        assertEquals("Second TC must force 'Comment' value from its SampleResult", "Test with forced use of comment", listener2.events.get(0).getResult().getSampleComment());
+        assertEquals("Third TC must discard 'Comment' value from its SampleResult", "", listener3.events.get(0).getResult().getSampleComment());
+    }
+    
+    @Test
+    public void testIssue61466_do_global_use() throws Exception {
+        JMeterContextService.getContext().setVariables(new JMeterVariables());
+        JMeterUtils.getJMeterProperties();
+
+
+        // transactioncontroller.use_comment_on_all value defined as "true"
+        JMeterUtils.setProperty("transactioncontroller.use_comment_on_all", "true");
+
+        TestSampleListener listener1 = new TestSampleListener();
+        TestSampleListener listener2 = new TestSampleListener();
+        TestSampleListener listener3 = new TestSampleListener();
+        
+        TransactionController transactionController1 = new TransactionController();
+        transactionController1.setComment("Test with default use of comment");
+        transactionController1.setGenerateParentSample(true);
+
+        TransactionController transactionController2 = new TransactionController();
+        transactionController2.setComment("Test with forced use of comment");
+        transactionController2.setGenerateParentSample(true);
+        transactionController2.setUseComment(true);
+
+        TransactionController transactionController3 = new TransactionController();
+        transactionController3.setComment("Test with discarded use of comment");
+        transactionController3.setGenerateParentSample(true);
+        transactionController3.setUseComment(false);
+
+        DebugSampler debugSampler1 = new DebugSampler();
+        DebugSampler debugSampler2 = new DebugSampler();
+        DebugSampler debugSampler3 = new DebugSampler();
+
+        LoopController loop = new LoopController();
+        loop.setLoops(1);
+        loop.setContinueForever(false);
+
+        ListedHashTree hashTree = new ListedHashTree();
+        hashTree.add(loop);
+        hashTree.add(loop, transactionController1);
+        hashTree.add(transactionController1, debugSampler1);
+        hashTree.add(transactionController1, listener1);
+        hashTree.add(loop, transactionController2);
+        hashTree.add(transactionController2);
+        hashTree.add(transactionController2, debugSampler2);
+        hashTree.add(transactionController2, listener2);
+        hashTree.add(loop, transactionController3);
+        hashTree.add(transactionController3);
+        hashTree.add(transactionController3, debugSampler3);
+        hashTree.add(transactionController3, listener3);
+        
+        TestCompiler compiler = new TestCompiler(hashTree);
+        hashTree.traverse(compiler);
+
+        ThreadGroup threadGroup = new ThreadGroup();
+        threadGroup.setNumThreads(1);
+
+        ListenerNotifier notifier = new ListenerNotifier();
+
+        JMeterThread thread = new JMeterThread(hashTree, threadGroup, notifier);
+        thread.setThreadGroup(threadGroup);
+        thread.setOnErrorStopThread(true);
+        thread.run();
+
+        assertEquals("First TC must force 'Comment' value from its SampleResult", "Test with default use of comment", listener1.events.get(0).getResult().getSampleComment());
+        assertEquals("Second TC must force 'Comment' value from its SampleResult", "Test with forced use of comment", listener2.events.get(0).getResult().getSampleComment());
+        assertEquals("Third TC must discard 'Comment' value from its SampleResult", "", listener3.events.get(0).getResult().getSampleComment());
+    }
 
     public class TestSampleListener extends ResultCollector implements SampleListener {
         public List<SampleEvent> events = new ArrayList<>();
