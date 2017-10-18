eb0309393e389e19ec6b6a04dceda77f0975f836
Philippe Mouawad <pmouawad@apache.org>
2017-10-18 20:20:27 +0000

    Bug 60213 - Boundary based extractor Fix version Bugzilla Id: 60213
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1812573 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 9cba8e7e6a9ba42b1a4338564a0e7145b8ccf334

--- src/core/org/apache/jmeter/save/SaveService.java
+++ src/core/org/apache/jmeter/save/SaveService.java
@@ -149,7 +149,7 @@ public class SaveService {
     
     // Must match _version property value in saveservice.properties
     // used to ensure saveservice.properties and SaveService are updated simultaneously
-    static final String PROPVERSION = "3.2";// Expected version $NON-NLS-1$
+    static final String PROPVERSION = "3.4";// Expected version $NON-NLS-1$
 
     // Internal information only
     private static String fileVersion = ""; // computed from saveservice.properties file// $NON-NLS-1$
