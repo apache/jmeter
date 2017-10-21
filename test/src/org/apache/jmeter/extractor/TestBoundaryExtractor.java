6d8e38c74fe5155228263a7f01c697a135eef6c4
Philippe Mouawad <pmouawad@apache.org>
2017-10-21 10:42:00 +0000

    Fix wrong test
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1812818 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 6d343b61cf062751664f0fd29033b9abb9561276

--- test/src/org/apache/jmeter/extractor/TestBoundaryExtractor.java
+++ test/src/org/apache/jmeter/extractor/TestBoundaryExtractor.java
@@ -156,10 +156,11 @@ public class TestBoundaryExtractor {
         assertEquals("PARENT", vars.get("regVal"));
         extractor.setLeftBoundary("<title>");
         extractor.setRightBoundary("</title>");
-        extractor.setScopeAll();
+        extractor.setScopeChildren();
         extractor.process();
         final String found = vars.get("regVal");
-        assertTrue(found.equals("ONE") || found.equals("TWO"));
+        assertTrue(found.equals("ONE") || found.equals("TWO")
+                || found.equals("THREE"));
     }
     
     @Test
