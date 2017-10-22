f7a16c067552d6d5ac1038422e8232f4e00b497b
Philippe Mouawad <pmouawad@apache.org>
2017-10-22 21:06:58 +0000

    Bug 61642 - Improve FTP test coverage Bugzilla Id: 61642
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1812953 13f79535-47bb-0310-9956-ffa450edef68

--- test/src/org/apache/jmeter/JMeterVersionTest.java
+++ test/src/org/apache/jmeter/JMeterVersionTest.java
@@ -115,7 +115,8 @@ public class JMeterVersionTest extends JMeterTestCase {
         propNames.remove("rat-tasks");
         versions.remove("rat-tasks");
         // remove optional hsqldb, jacoco and sonar jars (required for coverage reporting, not required for jmeter)
-        for (String optLib : Arrays.asList("jacocoant", "sonarqube-ant-task", "hsqldb", "activemq-all")) {
+        for (String optLib : Arrays.asList("jacocoant", "sonarqube-ant-task", "hsqldb", "activemq-all",
+                "mina-core", "ftplet-api", "ftpserver-core")) {
             propNames.remove(optLib);
             versions.remove(optLib);
         }
