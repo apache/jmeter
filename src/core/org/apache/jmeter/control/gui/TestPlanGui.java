4a92d4da60dfb0b63fe7133eb84a4047fbfff8b7
Philippe Mouawad <pmouawad@apache.org>
2017-10-23 20:26:08 +0000

    Bug 61656 - tearDown Thread Group should run by default at stop or shutdown of test Bugzilla Id: 61656
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1813084 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: efeabeb53a40f3da99bb3f3e605b35ca510a8d53

--- src/core/org/apache/jmeter/control/gui/TestPlanGui.java
+++ src/core/org/apache/jmeter/control/gui/TestPlanGui.java
@@ -70,7 +70,7 @@ public class TestPlanGui extends AbstractJMeterGuiComponent {
         argsPanel = new ArgumentsPanel(JMeterUtils.getResString("user_defined_variables")); // $NON-NLS-1$
         serializedMode = new JCheckBox(JMeterUtils.getResString("testplan.serialized")); // $NON-NLS-1$
         functionalMode = new JCheckBox(JMeterUtils.getResString("functional_mode")); // $NON-NLS-1$
-        tearDownOnShutdown = new JCheckBox(JMeterUtils.getResString("teardown_on_shutdown")); // $NON-NLS-1$
+        tearDownOnShutdown = new JCheckBox(JMeterUtils.getResString("teardown_on_shutdown"), true); // $NON-NLS-1$
         init();
     }
 
@@ -198,7 +198,7 @@ public class TestPlanGui extends AbstractJMeterGuiComponent {
         super.clearGui();
         functionalMode.setSelected(false);
         serializedMode.setSelected(false);
-        tearDownOnShutdown.setSelected(false);
+        tearDownOnShutdown.setSelected(true);
         argsPanel.clear();
         browseJar.clearFiles();
     }
