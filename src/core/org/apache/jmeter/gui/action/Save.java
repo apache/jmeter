ccf7d0b503c0f1fc53952d36e1e01b9cec089aa4
Graham Russell <graham@ham1.co.uk>
2017-11-09 12:26:07 +0000

    removed unused import

--- src/core/org/apache/jmeter/gui/action/Save.java
+++ src/core/org/apache/jmeter/gui/action/Save.java
@@ -30,7 +30,6 @@ import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import javax.swing.JFileChooser;
