22ac75896977e2450cb31f53d68afa0dd40dc720
Antonio Gomes Rodrigues <agomes@apache.org>
2017-11-18 19:32:23 +0000

    [Bug 61775] Add a link to help menu to download nighty builds (it open the browser with the correct link) - add missing file
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1815689 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/core/org/apache/jmeter/gui/action/LinkNightlyBuild.java
@@ -0,0 +1,64 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.apache.jmeter.gui.action;
+
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LinkNightlyBuild extends AbstractAction {
+    
+    private static final Logger log = LoggerFactory.getLogger(LinkNightlyBuild.class);
+
+    private static final Set<String> commands = new HashSet<>();
+
+    static {
+        commands.add(ActionNames.LINK_NIGHTLY_BUILD);
+    }
+    
+    /**
+     * @see org.apache.jmeter.gui.action.Command#doAction(ActionEvent)
+     */
+    @Override
+    public void doAction(ActionEvent e) {
+        String url = "http://jmeter.apache.org/nightly.html";
+        try {
+            java.awt.Desktop.getDesktop().browse(java.net.URI.create(url));
+        } catch (IOException err) {
+            log.error("LinkNightlyBuild: User default browser is not found, or it fails to be launched, or the default handler application failed to be launched on {}", err);
+        } catch (UnsupportedOperationException err) {
+            log.error("LinkNightlyBuild: Current platform does not support the Desktop.Action.BROWSE actionon {}", err);
+        } catch (SecurityException err) {
+            log.error("LinkNightlyBuild: Security problem on {}", err);
+        } catch (Exception err) {
+            log.error("LinkNightlyBuild on {}", err);
+        }
+    }
+
+    @Override
+    public Set<String> getActionNames() {
+        return commands;
+    }
+    
+
+}
