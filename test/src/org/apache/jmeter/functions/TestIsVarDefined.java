5c6a7115d2341180fb0ab7d54efed92ea095404d
Felix Schumacher <fschumacher@apache.org>
2017-11-19 12:00:46 +0000

    Whitespace police
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1815728 13f79535-47bb-0310-9956-ffa450edef68

--- test/src/org/apache/jmeter/functions/TestIsVarDefined.java
+++ test/src/org/apache/jmeter/functions/TestIsVarDefined.java
@@ -31,6 +31,7 @@ import org.apache.jmeter.threads.JMeterContextService;
 import org.apache.jmeter.threads.JMeterVariables;
 import org.junit.Before;
 import org.junit.Test;
+
 /**
  * 
  * Test {@link IsVarDefined} Function
@@ -39,63 +40,63 @@ import org.junit.Test;
  *
  */
 public class TestIsVarDefined extends JMeterTestCase {
-	protected AbstractFunction isVarDefined;
+    protected AbstractFunction isVarDefined;
 
-	private SampleResult result;
+    private SampleResult result;
 
-	private Collection<CompoundVariable> params;
+    private Collection<CompoundVariable> params;
 
-	private JMeterVariables vars;
+    private JMeterVariables vars;
 
-	private JMeterContext jmctx;
+    private JMeterContext jmctx;
 
-	@Before
-	public void setUp() {
-		isVarDefined = new IsVarDefined();
-		result = new SampleResult();
-		jmctx = JMeterContextService.getContext();
-		String data = "dummy data";
-		result.setResponseData(data, null);
-		vars = new JMeterVariables();
-		jmctx.setVariables(vars);
-		jmctx.setPreviousResult(result);
-		params = new LinkedList<>();
-	}
+    @Before
+    public void setUp() {
+        isVarDefined = new IsVarDefined();
+        result = new SampleResult();
+        jmctx = JMeterContextService.getContext();
+        String data = "dummy data";
+        result.setResponseData(data, null);
+        vars = new JMeterVariables();
+        jmctx.setVariables(vars);
+        jmctx.setPreviousResult(result);
+        params = new LinkedList<>();
+    }
 
-	@Test
-	public void testParameterCountIsPropDefined() throws Exception {
-		checkInvalidParameterCounts(isVarDefined, 1, 1);
-	}
+    @Test
+    public void testParameterCountIsPropDefined() throws Exception {
+        checkInvalidParameterCounts(isVarDefined, 1, 1);
+    }
 
-	@Test
-	public void testIsVarNotDefinedOnlyPropDefined() throws Exception {
-		params.add(new CompoundVariable("file.encoding"));
-		isVarDefined.setParameters(params);
-		String returnValue = isVarDefined.execute(result, null);
-		assertEquals("false", returnValue);
-	}
+    @Test
+    public void testIsVarNotDefinedOnlyPropDefined() throws Exception {
+        params.add(new CompoundVariable("file.encoding"));
+        isVarDefined.setParameters(params);
+        String returnValue = isVarDefined.execute(result, null);
+        assertEquals("false", returnValue);
+    }
 
-	@Test
-	public void testIsVarDefined() throws Exception {
-		vars.put("varName", "");
-		params.add(new CompoundVariable("varName"));
-		isVarDefined.setParameters(params);
-		String returnValue = isVarDefined.execute(result, null);
-		assertEquals("true", returnValue);
-	}
+    @Test
+    public void testIsVarDefined() throws Exception {
+        vars.put("varName", "");
+        params.add(new CompoundVariable("varName"));
+        isVarDefined.setParameters(params);
+        String returnValue = isVarDefined.execute(result, null);
+        assertEquals("true", returnValue);
+    }
 
-	@Test
-	public void testIsVarNotDefined() throws Exception {
-		params.add(new CompoundVariable("emptyProperty"));
-		isVarDefined.setParameters(params);
-		String returnValue = isVarDefined.execute(result, null);
-		assertEquals("false", returnValue);
-	}
+    @Test
+    public void testIsVarNotDefined() throws Exception {
+        params.add(new CompoundVariable("emptyProperty"));
+        isVarDefined.setParameters(params);
+        String returnValue = isVarDefined.execute(result, null);
+        assertEquals("false", returnValue);
+    }
 
-	@Test(expected = InvalidVariableException.class)
-	public void testIsVarDefinedError() throws Exception {
-		isVarDefined.setParameters(params);
-		isVarDefined.execute(result, null);
-	}
+    @Test(expected = InvalidVariableException.class)
+    public void testIsVarDefinedError() throws Exception {
+        isVarDefined.setParameters(params);
+        isVarDefined.execute(result, null);
+    }
 
 }
