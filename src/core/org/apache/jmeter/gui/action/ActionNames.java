d22da25741f948b7cc2a390c107ba4beb404818b
Antonio Gomes Rodrigues <agomes@apache.org>
2017-11-20 20:56:56 +0000

    Fix comment in src/core/org/apache/jmeter/gui/action/ActionNames.java
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1815857 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 8f2000f09534e851962e13a873fca976db932cfb

--- src/core/org/apache/jmeter/gui/action/ActionNames.java
+++ src/core/org/apache/jmeter/gui/action/ActionNames.java
@@ -62,7 +62,7 @@ public final class ActionNames {
     public static final String LAF_PREFIX       = "laf:"; // Look and Feel prefix
     public static final String LINK_BUG_TRACKER = "link_bug_tracker:"; // URI of bug tracker
     public static final String LINK_NIGHTLY_BUILD = "link_nightly_build:"; // URI of nightly build
-    public static final String LINK_RELEASE_NOTES = "link_release_notes"; // URI of nightly build
+    public static final String LINK_RELEASE_NOTES = "link_release_notes"; // URI of the release note
     public static final String LOGGER_PANEL_ENABLE_DISABLE     = "logger_panel_enable_disable"; // $NON-NLS-1$
     public static final String LOG_LEVEL_PREFIX = "loglevel:"; // Logger level prefix
     public static final String MERGE            = "merge"; // $NON-NLS-1$
