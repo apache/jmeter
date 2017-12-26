afddd802b70219516384ef8afd522763df99a469
Philippe Mouawad <pmouawad@apache.org>
2017-12-26 20:40:32 +0000

    Bug 61931 - Exponential Timer : timer that produces poisson arrivals with given constant throughput This closes #231 Bugzilla Id: 61931
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1819293 13f79535-47bb-0310-9956-ffa450edef68

--- src/core/org/apache/jmeter/save/SaveService.java
+++ src/core/org/apache/jmeter/save/SaveService.java
@@ -149,13 +149,13 @@ public class SaveService {
     
     // Must match _version property value in saveservice.properties
     // used to ensure saveservice.properties and SaveService are updated simultaneously
-    static final String PROPVERSION = "3.4";// Expected version $NON-NLS-1$
+    static final String PROPVERSION = "4.0";// Expected version $NON-NLS-1$
 
     // Internal information only
     private static String fileVersion = ""; // computed from saveservice.properties file// $NON-NLS-1$
     // Must match the sha1 checksum of the file saveservice.properties (without newline character),
     // used to ensure saveservice.properties and SaveService are updated simultaneously
-    static final String FILEVERSION = "0acd8200bf252acf41d5eeca6aa56b0eaee06f0f"; // Expected value $NON-NLS-1$
+    static final String FILEVERSION = "c616712e26251d15588fbe615d1c31a34dbbf854"; // Expected value $NON-NLS-1$
 
     private static String fileEncoding = ""; // read from properties file// $NON-NLS-1$
 
