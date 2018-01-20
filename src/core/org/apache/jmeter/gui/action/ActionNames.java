765e018028ead378fc2736b76c5d5a60cff7330e
Philippe Mouawad <pmouawad@apache.org>
2018-01-20 14:49:12 +0000

    BUG 61785 Add Menu Item in Help > Useful links for functions reference Bugzilla Id: 61785
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1821746 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 3aff45abf6f8d1710314210e46d395da9913a045

--- src/core/org/apache/jmeter/gui/action/ActionNames.java
+++ src/core/org/apache/jmeter/gui/action/ActionNames.java
@@ -62,6 +62,7 @@ public final class ActionNames {
     public static final String LAF_PREFIX       = "laf:"; // Look and Feel prefix
     public static final String LINK_BUG_TRACKER = "link_bug_tracker:"; // URI of bug tracker
     public static final String LINK_COMP_REF    = "link_component_reference:"; // URI of component reference
+    public static final String LINK_FUNC_REF    = "link_function_reference:"; // URI of functions reference
     public static final String LINK_NIGHTLY_BUILD = "link_nightly_build:"; // URI of nightly build
     public static final String LINK_RELEASE_NOTES = "link_release_notes"; // URI of the release note
     public static final String LOGGER_PANEL_ENABLE_DISABLE     = "logger_panel_enable_disable"; // $NON-NLS-1$
