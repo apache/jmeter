0469cb73f96b8d675b267b653ec160e774192a22
Philippe Mouawad <pmouawad@apache.org>
2018-01-23 21:49:53 +0000

    Fix checkstyle issues
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1822061 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: b43f9b3c9599e28fca7151b56746fb6d3a662ec6

--- src/core/org/apache/jmeter/rmi/RMIServerSocketFactoryImpl.java
+++ src/core/org/apache/jmeter/rmi/RMIServerSocketFactoryImpl.java
@@ -56,18 +56,23 @@ public class RMIServerSocketFactoryImpl implements RMIServerSocketFactory, Seria
 
     @Override
     public boolean equals(Object obj) {
-        if (this == obj)
+        if (this == obj) {
             return true;
-        if (obj == null)
+        }
+        if (obj == null) {
             return false;
-        if (getClass() != obj.getClass())
+        }
+        if (getClass() != obj.getClass()) {
             return false;
+        }
         RMIServerSocketFactoryImpl other = (RMIServerSocketFactoryImpl) obj;
         if (localAddress == null) {
-            if (other.localAddress != null)
+            if (other.localAddress != null) {
                 return false;
-        } else if (!localAddress.equals(other.localAddress))
+            }
+        } else if (!localAddress.equals(other.localAddress)) {
             return false;
+        }
         return true;
     }
 
