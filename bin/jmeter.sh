1d56d06f60e99c1fa3a9c20da64e976cdc51633a
Felix Schumacher <fschumacher@apache.org>
2018-01-31 05:51:47 +0000

    Followup to r1822700. Add back java 9 detection as noted by pmouawad.
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1822757 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: a13f610bb945ce8f5f287799124899810ab64dfe

--- bin/jmeter.sh
+++ bin/jmeter.sh
@@ -45,6 +45,65 @@ done
 
 PRGDIR=`dirname "$PRG"`
 
-export JMETER_COMPLETE_ARGS=true
+# Make sure prerequisite environment variables are set
+if [ -z "$JAVA_HOME" -a -z "$JRE_HOME" ]; then
+  if [ "`uname`" = "Darwin" ]; then
+    #
+    if [ -x '/usr/libexec/java_home' ] ; then
+      export JAVA_HOME=`/usr/libexec/java_home`
+    #
+    elif [ -d "/System/Library/Frameworks/JavaVM.framework/Versions/CurrentJDK/Home" ]; then
+      export JAVA_HOME="/System/Library/Frameworks/JavaVM.framework/Versions/CurrentJDK/Home"
+    fi
+  else
+    JAVA_PATH=`which java 2>/dev/null`
+    if [ "x$JAVA_PATH" != "x" ]; then
+      JAVA_PATH=`dirname $JAVA_PATH 2>/dev/null`
+      JRE_HOME=`dirname $JAVA_PATH 2>/dev/null`
+    fi
+    if [ "x$JRE_HOME" = "x" ]; then
+      # XXX: Should we try other locations?
+      if [ -x /usr/bin/java ]; then
+        JRE_HOME=/usr
+      fi
+    fi
+  fi
+  if [ -z "$JAVA_HOME" -a -z "$JRE_HOME" ]; then
+    echo "Neither the JAVA_HOME nor the JRE_HOME environment variable is defined"
+    echo "At least one of these environment variable is needed to run this program"
+    exit 1
+  fi
+fi
+if [ -z "$JAVA_HOME" -a "$1" = "debug" ]; then
+  echo "JAVA_HOME should point to a JDK in order to run in debug mode."
+  exit 1
+fi
+if [ -z "$JRE_HOME" ]; then
+  JRE_HOME="$JAVA_HOME"
+fi
+if [ -z "$JAVA_HOME" ]; then
+  JAVA_HOME="$JRE_HOME"
+fi
+
+#--add-modules java.activation if JAVA 9
+JAVA9_OPTS=
+
+# Minimal version to run JMeter
+MINIMAL_VERSION=8
+
+# Check if version is from OpenJDK or Oracle Hotspot JVM prior to 9 containing 1.${version}.x
+CURRENT_VERSION=`"${JAVA_HOME}/bin/java" -version 2>&1 | awk -F'"' '/version/ {gsub("^1[.]", "", $2); gsub("[^0-9].*$", "", $2); print $2}'`
+
+# Check if Java is present and the minimal version requirement
+if [ "$CURRENT_VERSION" -gt "$MINIMAL_VERSION" ]; then
+    JAVA9_OPTS="--add-modules java.activation --add-opens java.desktop/sun.awt=ALL-UNNAMED --add-opens java.desktop/javax.swing.text.html=ALL-UNNAMED --add-opens java.desktop/sun.swing=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.lang.invoke=ALL-UNNAMED"
+fi
+
+# Don't add additional arguments to the JVM start, except those needed for Java 9
+JMETER_COMPLETE_ARGS=true
+
+# add the Java9 args before the user given ones
+JVM_ARGS="$JAVA9_OPTS $JVM_ARGS"
+export JVM_ARGS JMETER_COMPLETE_ARGS
 
 "${PRGDIR}/jmeter" "$@"
