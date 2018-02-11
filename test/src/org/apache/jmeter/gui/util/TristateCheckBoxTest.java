1a8641cebd00e469f2fb747a0dc9a49ec802aa6b
Felix Schumacher <fschumacher@apache.org>
2018-02-11 18:09:15 +0000

    Use one statement per line
    In preparation for #365 on github. Contributed by Graham Russell (graham at ham1.co.uk)
    
    
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1823869 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: fd797b7b6eee6910e64b54555a80f5674cd86b9e

--- test/src/org/apache/jmeter/gui/util/TristateCheckBoxTest.java
+++ test/src/org/apache/jmeter/gui/util/TristateCheckBoxTest.java
@@ -101,13 +101,17 @@ public class TristateCheckBoxTest {
     private static void switchOnAction(TristateCheckBox tristateBox) {
         switch(tristateBox.getState()) {
         case SELECTED:
-            System.out.println("Selected"); break;
+            System.out.println("Selected");
+            break;
         case DESELECTED:
-            System.out.println("Not Selected"); break;
+            System.out.println("Not Selected");
+            break;
         case INDETERMINATE:
-            System.out.println("Tristate Selected"); break;
+            System.out.println("Tristate Selected");
+            break;
         default:
-            System.err.println("Unexpected state: " + tristateBox.getState()); break;
+            System.err.println("Unexpected state: " + tristateBox.getState());
+            break;
         }
     }
 }
