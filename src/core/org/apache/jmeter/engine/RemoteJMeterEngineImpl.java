06a97286774d6fab597928f265a3c37e3cc5e98f
Felix Schumacher <fschumacher@apache.org>
2018-02-12 16:22:06 +0000

    Use lambda instead of subclassing Thread.
    Part of #356 on Github. Contributed by Graham Russell (graham at ham1.co.uk)
    
    
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1824005 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: f20884b1878473c811efdf9ae1f7fe38afa372c5

--- src/core/org/apache/jmeter/engine/RemoteJMeterEngineImpl.java
+++ src/core/org/apache/jmeter/engine/RemoteJMeterEngineImpl.java
@@ -186,13 +186,10 @@ public final class RemoteJMeterEngineImpl extends java.rmi.server.UnicastRemoteO
     public void rexit() throws RemoteException {
         log.info("Exiting");
         // Bug 59400 - allow rexit() to return
-        Thread et = new Thread() {
-            @Override
-            public void run() {
-                log.info("Stopping the backing engine");
-                backingEngine.exit();
-            }  
-        };
+        Thread et = new Thread(() -> {
+            log.info("Stopping the backing engine");
+            backingEngine.exit();
+        });
         et.setDaemon(false);
         // Tidy up any objects we created
         Registry reg = LocateRegistry.getRegistry(
