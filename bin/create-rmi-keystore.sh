7a5e1ef1f3c4a5135b19d6b7f36408f7ea9faf05
Logan Mauzaize (T524467) <t524467@airfrance.fr>
2018-05-23 14:26:31 +0200

    Adds parameter supports for RMI keystore creation scripts
    Helps to integrate script into non-interactive process.

--- bin/create-rmi-keystore.sh
+++ bin/create-rmi-keystore.sh
@@ -15,6 +15,6 @@
 ##   See the License for the specific language governing permissions and
 ##   limitations under the License.
 ##
-keytool -genkey -keyalg RSA -alias rmi -keystore rmi_keystore.jks -storepass changeit -validity 7 -keysize 2048
+keytool -genkey -keyalg RSA -alias rmi -keystore rmi_keystore.jks -storepass changeit -validity 7 -keysize 2048 "$@"
 
 echo "Copy the generated rmi_keystore.jks to jmeter/bin folder or reference it in property 'server.rmi.ssl.keystore.file'"
