apache/jmeter

commit 01c85e461d5e9700e73b098631820aea28b7efc6

Avoid too much warn messages when a 3rd party cookie is sent by server.
git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1839891 13f79535-47bb-0310-9956-ffa450edef68

Former-commit-id: 4cb268dbd2cdf3ebc61e09264019ae949494b730

src/protocol/http/org/apache/jmeter/protocol/http/control/HC4CookieHandler.java
@@ -132,7 +132,12 @@ public class HC4CookieHandler implements CookieHandler {
             for (org.apache.http.cookie.Cookie cookie : cookies) {
                 try {
                     if (checkCookies) {
-                        cookieSpec.validate(cookie, cookieOrigin);
+                        try {
+                            cookieSpec.validate(cookie, cookieOrigin);
+                        } catch (MalformedCookieException e) { // This means the cookie was wrong for the URL
+                            log.info("Not storing invalid cookie: <{}> for URL {} ({})",
+                                cookieHeader, url, e.getLocalizedMessage());
+                        }
                     }
                     Date expiryDate = cookie.getExpiryDate();
                     long exp = 0;
@@ -156,12 +161,9 @@ public class HC4CookieHandler implements CookieHandler {
                     } else {
                         cookieManager.removeMatchingCookies(newCookie);
                         if (debugEnabled){
-                            log.info("Dropping expired Cookie: {}", newCookie.toString());
+                            log.info("Dropping expired Cookie: {}", newCookie);
                         }
                     }
-                } catch (MalformedCookieException e) { // This means the cookie was wrong for the URL
-                log.warn("Not storing invalid cookie: <{}> for URL {} ({})",
-                        cookieHeader, url, e.getLocalizedMessage());
                 } catch (IllegalArgumentException e) {
                     log.warn(cookieHeader+e.getLocalizedMessage());
                 }
@@ -176,7 +178,7 @@ public class HC4CookieHandler implements CookieHandler {
         
         boolean debugEnabled = log.isDebugEnabled();
         if (debugEnabled){
-            log.debug("Found {} cookies for {}", c.size(), url.toExternalForm());
+            log.debug("Found {} cookies for {}", c.size(), url);
         }
         if (c.isEmpty()) {
             return null;

commit 2020aca3baa203bc6f7eb9af3c40c2985bf66c59

Fix regression introduced by commit r1839891
git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1839892 13f79535-47bb-0310-9956-ffa450edef68

Former-commit-id: 6e9339fc0c09e150ecaedc0e770124a169e031eb

src/protocol/http/org/apache/jmeter/protocol/http/control/HC4CookieHandler.java
@@ -137,6 +137,7 @@ public class HC4CookieHandler implements CookieHandler {
                         } catch (MalformedCookieException e) { // This means the cookie was wrong for the URL
                             log.info("Not storing invalid cookie: <{}> for URL {} ({})",
                                 cookieHeader, url, e.getLocalizedMessage());
+                            continue;
                         }
                     }
                     Date expiryDate = cookie.getExpiryDate();