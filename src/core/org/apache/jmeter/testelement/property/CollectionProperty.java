32d125eabfc5f926a4d364cd7641b60899902305
Felix Schumacher <felix.schumacher@internetallee.de>
2018-10-06 15:11:26 +0200

    Use isEmpty instead of comparing size of a collection to zero

--- src/core/org/apache/jmeter/testelement/property/CollectionProperty.java
+++ src/core/org/apache/jmeter/testelement/property/CollectionProperty.java
@@ -186,7 +186,7 @@ public class CollectionProperty extends MultiProperty {
      */
     @Override
     protected Class<? extends JMeterProperty> getPropertyType() {
-        if (value != null && value.size() > 0) {
+        if (value != null && !value.isEmpty()) {
             return value.iterator().next().getClass();
         }
         return NullProperty.class;
