4c34925237e7c33daf03e753f9b717908f005748
Felix Schumacher <felix.schumacher@internetallee.de>
2018-11-27 21:45:17 +0100

    Get rid of deprecated newInstance call

--- src/core/org/apache/jmeter/engine/util/CompoundVariable.java
+++ src/core/org/apache/jmeter/engine/util/CompoundVariable.java
@@ -72,7 +72,7 @@ public class CompoundVariable implements Function {
             List<String> classes = ClassFinder.findClassesThatExtend(JMeterUtils.getSearchPaths(),
                     new Class[] { Function.class }, true, contain, notContain);
             for (String clazzName : classes) {
-                Function tempFunc = (Function) Class.forName(clazzName).newInstance();
+                Function tempFunc = (Function) Class.forName(clazzName).getDeclaredConstructor().newInstance();
                 String referenceKey = tempFunc.getReferenceKey();
                 if (referenceKey.length() > 0) { // ignore self
                     functions.put(referenceKey, tempFunc.getClass());
@@ -195,7 +195,7 @@ public class CompoundVariable implements Function {
     static Object getNamedFunction(String functionName) throws InvalidVariableException {
         if (functions.containsKey(functionName)) {
             try {
-                return functions.get(functionName).newInstance();
+                return functions.get(functionName).getDeclaredConstructor().newInstance();
             } catch (Exception e) {
                 log.error("Exception occurred while instantiating a function: {}", functionName, e); // $NON-NLS-1$
                 throw new InvalidVariableException(e);
