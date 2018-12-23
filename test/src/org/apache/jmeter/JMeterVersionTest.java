49b87b646909de67b766adb536d415d8731c7d16
Philippe Mouawad <pmouawad@apache.org>
2018-12-23 19:53:51 +0000

    Updated to Saxon-HE 9.8.0-14 (from 9.8.0-12)
    Revert from 9.9.0-2 as a behavioural change has been introduced, a newline appears on elements extracted by XPath selector.
    Needs further investigations.
    
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1849660 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 9fad81e63b8b92ba5113e836ecf50abd57353379

--- test/src/org/apache/jmeter/JMeterVersionTest.java
+++ test/src/org/apache/jmeter/JMeterVersionTest.java
@@ -161,9 +161,9 @@ public class JMeterVersionTest extends JMeterTestCase {
                 } else if (jar.equals("spock-core-1.0-groovy")) { // special handling
                     jar = "spock-core";
                     version = "1.0-groovy-2.4";
-                } else if (jar.equals("Saxon-HE-9.9.0")) { // special handling
+                } else if (jar.equals("Saxon-HE-9.8.0")) { // special handling
                     jar = "Saxon-HE";
-                    version = "9.9.0-2";
+                    version = "9.8.0-14";
                 }else {
                     String tmp = JAR_TO_BUILD_PROP.get(jar);
                     if (tmp != null) {
