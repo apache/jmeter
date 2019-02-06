5956c059c122cb3c44363586f67a07e673528fee
Philippe Mouawad <pmouawad@apache.org>
2019-02-06 10:23:03 +0000

    Bug 62959 Ability to create an HTTP Request from a Curl command
    Improvements:
    
    - Allow updating Test Plan instead of creating a whole one
    - Add a comment on HTTP Request saying it was created from cURL on date
    Fixes:
    
    - If !request.isCompressed, don't add gzip,deflate
    Bugzilla Id: 62959
    
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1853058 13f79535-47bb-0310-9956-ffa450edef68

--- src/protocol/http/org/apache/jmeter/protocol/http/gui/action/ParseCurlCommandAction.java
+++ src/protocol/http/org/apache/jmeter/protocol/http/gui/action/ParseCurlCommandAction.java
@@ -28,6 +28,8 @@ import java.awt.event.KeyEvent;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.text.MessageFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
@@ -42,7 +44,9 @@ import javax.swing.SwingUtilities;
 import javax.swing.tree.TreePath;
 
 import org.apache.jmeter.config.Arguments;
+import org.apache.jmeter.control.Controller;
 import org.apache.jmeter.control.LoopController;
+import org.apache.jmeter.control.ReplaceableController;
 import org.apache.jmeter.control.gui.TestPlanGui;
 import org.apache.jmeter.exceptions.IllegalUserActionException;
 import org.apache.jmeter.gui.GuiPackage;
@@ -50,6 +54,7 @@ import org.apache.jmeter.gui.action.AbstractAction;
 import org.apache.jmeter.gui.action.ActionNames;
 import org.apache.jmeter.gui.action.ActionRouter;
 import org.apache.jmeter.gui.plugin.MenuCreator;
+import org.apache.jmeter.gui.tree.JMeterTreeModel;
 import org.apache.jmeter.gui.tree.JMeterTreeNode;
 import org.apache.jmeter.gui.util.EscapeDialog;
 import org.apache.jmeter.gui.util.JSyntaxTextArea;
@@ -66,6 +71,7 @@ import org.apache.jmeter.reporters.ResultCollector;
 import org.apache.jmeter.services.FileServer;
 import org.apache.jmeter.testelement.TestElement;
 import org.apache.jmeter.testelement.TestPlan;
+import org.apache.jmeter.threads.AbstractThreadGroup;
 import org.apache.jmeter.threads.ThreadGroup;
 import org.apache.jmeter.threads.gui.ThreadGroupGui;
 import org.apache.jmeter.util.JMeterUtils;
@@ -103,7 +109,6 @@ public class ParseCurlCommandAction extends AbstractAction implements MenuCreato
 
     @Override
     public void doAction(ActionEvent e) {
-        ActionRouter.getInstance().doActionNow(new ActionEvent(e.getSource(), e.getID(), ActionNames.CLOSE));
         showInputDialog();
     }
     
@@ -133,8 +138,24 @@ public class ParseCurlCommandAction extends AbstractAction implements MenuCreato
         ComponentUtil.centerComponentInComponent(GuiPackage.getInstance().getMainFrame(), messageDialog);
         SwingUtilities.invokeLater(() -> messageDialog.setVisible(true));
     }
+    
+    /**
+     * Finds the first enabled node of a given type in the tree.
+     *
+     * @param type      class of the node to be found
+     * @return the first node of the given type in the test component tree, or
+     * <code>null</code> if none was found.
+     */
+    private JMeterTreeNode findFirstNodeOfType(Class<?> type) {
+        JMeterTreeModel treeModel = GuiPackage.getInstance().getTreeModel();
+        return treeModel.getNodesOfType(type).stream()
+                .filter(JMeterTreeNode::isEnabled)
+                .findFirst()
+                .orElse(null);
+    }
 
     private void createTestPlan(ActionEvent e, Request request) throws MalformedURLException, IllegalUserActionException {
+        ActionRouter.getInstance().doActionNow(new ActionEvent(e.getSource(), e.getID(), ActionNames.CLOSE));
         GuiPackage guiPackage = GuiPackage.getInstance();
 
         guiPackage.clearTestPlan();
@@ -183,20 +204,49 @@ public class ParseCurlCommandAction extends AbstractAction implements MenuCreato
     }
     
     private HTTPSamplerProxy createHttpRequest(Request request, HashTree parentHT) throws MalformedURLException {
+        HTTPSamplerProxy httpSampler = createSampler(request);
+
+        HashTree samplerHT = parentHT.add(httpSampler);
+        samplerHT.add(httpSampler.getHeaderManager());
+        return httpSampler;
+    }
+
+    /**
+     * @param request {@link Request}
+     * @return {@link HTTPSamplerProxy}
+     * @throws MalformedURLException
+     */
+    private HTTPSamplerProxy createSampler(Request request) throws MalformedURLException {
         HTTPSamplerProxy httpSampler = (HTTPSamplerProxy) HTTPSamplerFactory.newInstance(HTTPSamplerFactory.DEFAULT_CLASSNAME);
         httpSampler.setProperty(TestElement.GUI_CLASS, HttpTestSampleGui.class.getName());
         httpSampler.setProperty(TestElement.NAME, "HTTP Request");
+        httpSampler.setProperty(TestElement.COMMENTS, "Created from cURL on "+LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME));
         httpSampler.setProtocol(new URL(request.getUrl()).getProtocol());
         httpSampler.setPath(request.getUrl());
         httpSampler.setUseKeepAlive(true);
         httpSampler.setFollowRedirects(true);
         httpSampler.setMethod(request.getMethod());
+        if (!"GET".equals(request.getMethod())) {
+            Arguments arguments = new Arguments();
+            httpSampler.setArguments(arguments);
+            httpSampler.addNonEncodedArgument("", request.getPostData(), "");
+        }
         
-        HashTree samplerHT = parentHT.add(httpSampler);
-        
+        HeaderManager headerManager = createHeaderManager(request);
+        httpSampler.addTestElement(headerManager);
+
+        return httpSampler;
+    }
+
+    /**
+     * @param request {@link Request}
+     * @return {@link HeaderManager} element
+     */
+    private HeaderManager createHeaderManager(Request request) {
         HeaderManager headerManager = new HeaderManager();
         headerManager.setProperty(TestElement.GUI_CLASS, HeaderPanel.class.getName());
         headerManager.setProperty(TestElement.NAME, "HTTP HeaderManager");
+        headerManager.setProperty(TestElement.COMMENTS, "Created from cURL on "+LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME));
         Map<String, String> map = request.getHeaders();
         
         boolean hasAcceptEncoding = false;
@@ -205,17 +255,10 @@ public class ParseCurlCommandAction extends AbstractAction implements MenuCreato
             hasAcceptEncoding = hasAcceptEncoding || key.equalsIgnoreCase(ACCEPT_ENCODING);
             headerManager.getHeaders().addItem(new Header(key, header.getValue()));
         }
-        if(!hasAcceptEncoding) {
+        if(!hasAcceptEncoding && request.isCompressed()) {
             headerManager.getHeaders().addItem(new Header(ACCEPT_ENCODING, "gzip, deflate"));
         }
-        if (!"GET".equals(request.getMethod())) {
-            Arguments arguments = new Arguments();
-            httpSampler.setArguments(arguments);
-            httpSampler.addNonEncodedArgument("", request.getPostData(), "");
-        }
-        httpSampler.addTestElement(headerManager);
-        samplerHT.add(headerManager);
-        return httpSampler;
+        return headerManager;
     }
 
     @Override
@@ -265,7 +308,22 @@ public class ParseCurlCommandAction extends AbstractAction implements MenuCreato
                 LOGGER.info("Parsed CURL command {} into {}", curlCommand, request);
                 GuiPackage guiPackage = GuiPackage.getInstance();
                 guiPackage.updateCurrentNode();
-                createTestPlan(e, request);
+                JMeterTreeNode treeNode = findFirstNodeOfType(AbstractThreadGroup.class);
+                if(treeNode == null) {
+                    LOGGER.info("No AbstractThreadGroup found, potentially empty plan, creating a new plan");
+                    createTestPlan(e, request);
+                } else {
+                    JMeterTreeNode currentNode = guiPackage.getCurrentNode();
+                    Object userObject = currentNode.getUserObject();
+                    if (userObject instanceof Controller &&
+                            ! (userObject instanceof ReplaceableController)) {
+                        LOGGER.info("Newly created element will be placed under current selected node {}", currentNode.getName());
+                        addToTestPlan(currentNode, request);
+                    } else {
+                        LOGGER.info("Newly created element will be placed under first AbstractThreadGroup node {}", treeNode.getName());
+                        addToTestPlan(treeNode, request);
+                    }
+                }
                 statusText.setText(JMeterUtils.getResString("curl_create_success"));
             } catch (Exception ex) {
                 LOGGER.error("Error creating test plan from cURL command:{}, error:{}", curlCommand, ex.getMessage(), ex);
@@ -273,5 +331,23 @@ public class ParseCurlCommandAction extends AbstractAction implements MenuCreato
                 statusText.setForeground(Color.RED);
             }
         }
+    }
+
+    private void addToTestPlan(final JMeterTreeNode currentNode, Request request) 
+            throws MalformedURLException {
+        final HTTPSamplerProxy sampler = createSampler(request);
+        JMeterTreeModel treeModel = GuiPackage.getInstance().getTreeModel();
+        JMeterUtils.runSafe(true, () -> {
+            try {
+                // We get the HeaderManager before adding component otherwise addComponent would remove it 
+                HeaderManager headerManager = sampler.getHeaderManager();
+                // 
+                final JMeterTreeNode newNode = treeModel.addComponent(sampler, currentNode);
+                treeModel.addComponent(headerManager, newNode);
+            } catch (IllegalUserActionException ex) {
+                LOGGER.error("Error placing sampler", ex);
+                JMeterUtils.reportErrorToUser(ex.getMessage());
+            }
+        });
     }
 }
