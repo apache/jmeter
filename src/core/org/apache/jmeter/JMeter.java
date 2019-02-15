07d5a73d9261fd68ae817020456f046f6a33e099
Philippe Mouawad <pmouawad@apache.org>
2019-02-15 07:12:38 +0000

    Bug 63177 - Rename NON GUI mode into CLI Mode in documentation
    Change log
    Bugzilla Id: 63177
    
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1853612 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 7dbdb44c7336f5710ac1ad71780d1db6ae9c6633

--- src/core/org/apache/jmeter/JMeter.java
+++ src/core/org/apache/jmeter/JMeter.java
@@ -365,7 +365,7 @@ public class JMeter implements JMeterPlugin {
     private void startGui(String testFile) {
         System.out.println("================================================================================");//NOSONAR
         System.out.println("Don't use GUI mode for load testing !, only for Test creation and Test debugging.");//NOSONAR
-        System.out.println("For load testing, use NON GUI Mode:");//NOSONAR
+        System.out.println("For load testing, use CLI Mode (was NON GUI):");//NOSONAR
         System.out.println("   jmeter -n -t [jmx file] -l [results file] -e -o [Path to web report folder]");//NOSONAR
         System.out.println("& increase Java Heap to meet your test requirements:");//NOSONAR
         System.out.println("   Modify current env variable HEAP=\"-Xms1g -Xmx1g -XX:MaxMetaspaceSize=256m\" in the jmeter batch file");//NOSONAR
