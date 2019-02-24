da7d7cbf0eb3e30286c4b137dd1530e27797fbc9
Philippe Mouawad <pmouawad@apache.org>
2019-02-24 20:12:09 +0000

    Fix javadoc errors
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1854280 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 9e4861c12416d242579c1d066a9ee09b40fe19a8

--- src/jorphan/org/apache/jorphan/documentation/VisibleForTesting.java
+++ src/jorphan/org/apache/jorphan/documentation/VisibleForTesting.java
@@ -26,7 +26,6 @@ import java.lang.annotation.RetentionPolicy;
  * Denotes that the class, method or field has its visibility relaxed so
  * that unit tests can access it.
  * This means that this API is not-public.
- * <p/>
  * The <code>visibility</code> argument can be used to specific what the original
  * visibility should have been if it had not been made public or package-private for testing.
  * The default is to consider the element private.
@@ -51,8 +50,8 @@ public @interface VisibleForTesting {
     }
 
     /**
-     * Intended visibility if the element had not been made public or package-private for testing.
-     * If not specified, one should assume the element originally intended to be private.
+     * If no visibility specified, one should assume the element originally intended to be private.
+     * @return {@link Visibility} Intended visibility if the element had not been made public or package-private for testing.
      */
     Visibility visibility() default Visibility.PRIVATE;
 }
