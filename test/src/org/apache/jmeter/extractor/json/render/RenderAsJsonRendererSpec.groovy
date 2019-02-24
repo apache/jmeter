d6c77924b0b1ed6645fae8d7ff9620383c7aac3e
Philippe Mouawad <pmouawad@apache.org>
2019-02-24 14:44:00 +0000

    Improve methods/variables naming Make instance variable local Increase coverage
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1854264 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 22cc83a95ca83ff89774046586a5a5e36a6c5cbe

---
+++ test/src/org/apache/jmeter/extractor/json/render/RenderAsJsonRendererSpec.groovy
@@ -0,0 +1,109 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License") you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.jmeter.extractor.json.render
+
+import org.apache.jmeter.samplers.SampleResult
+import org.apache.jmeter.extractor.json.render.RenderAsJsonRenderer
+import org.apache.jmeter.junit.spock.JMeterSpec
+import org.apache.jmeter.util.JMeterUtils
+import javax.swing.JTabbedPane
+
+class RenderAsJsonRendererSpec extends JMeterSpec {
+
+    def sut = new RenderAsJsonRenderer()
+
+    def "init of component doesn't fail"() {
+        when:
+            sut.init()
+        then:
+            noExceptionThrown()
+            sut.jsonWithJSonPathPanel != null;
+    }
+    
+    def "render image"() {
+        given:
+            sut.init()
+            def sampleResult = new SampleResult();
+        when:
+            sut.renderImage(sampleResult)
+        then:
+            sut.jsonDataField.getText() == JMeterUtils.getResString("jsonpath_render_no_text")
+    }
+
+    def "render null Response"() {
+        given:
+            sut.init()
+            def sampleResult = new SampleResult();
+        when:
+            sut.renderResult(sampleResult)
+        then:
+            sut.jsonDataField.getText() == ""
+    }
+    
+    def "render JSON Response"() {
+        given:
+            sut.init();
+            def sampleResult = new SampleResult();
+            sampleResult.setResponseData("{name:\"Ludwig\",age: 23,city: \"Bonn\"}");
+        when:
+            sut.renderResult(sampleResult)
+        then:
+            sut.jsonDataField.getText() == '''{
+    "city": "Bonn",
+    "name": "Ludwig",
+    "age": 23
+}'''
+    }
+    
+    def "clearData clears expected fields"() {
+        given:
+            sut.init()
+            sut.jsonDataField.setText("blabla")
+            sut.jsonPathResultField.setText("blabla")
+        when:
+            sut.clearData()
+        then:
+            sut.jsonDataField.getText() == ""
+            sut.jsonPathResultField.getText() == ""
+    }
+
+    def "setupTabPane adds the tab to rightSide"() {
+        given:
+            sut.init()
+            def rightSideTabbedPane = new JTabbedPane();
+            sut.setRightSide(rightSideTabbedPane)
+        when:
+            sut.setupTabPane()
+        then:
+            sut.rightSide.getTabCount() == 1
+            // Investigate why it's failing
+            // sut.rightSide.getTabComponentAt(0) == sut.jsonWithJSonPathPanel
+    }
+    
+    def "setupTabPane called twice does not add twice the tab"() {
+        given:
+            sut.init()
+            def rightSideTabbedPane = new JTabbedPane();
+            sut.setRightSide(rightSideTabbedPane)
+            sut.setupTabPane()
+        when:
+            sut.setupTabPane()
+        then:
+            sut.rightSide.getTabCount() == 1
+    }
+}
