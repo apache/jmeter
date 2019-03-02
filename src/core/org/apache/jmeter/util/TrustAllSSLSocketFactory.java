6ee4162cc18d57cb79388644d41a7df658fa07de
Felix Schumacher <fschumacher@apache.org>
2019-03-02 14:22:32 +0000

    Take or own advice and name parameters more reader friendly.
    git-svn-id: https://svn.apache.org/repos/asf/jmeter/trunk@1854646 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 87e53a032843f98be12ed859b216243056379162

--- src/core/org/apache/jmeter/util/TrustAllSSLSocketFactory.java
+++ src/core/org/apache/jmeter/util/TrustAllSSLSocketFactory.java
@@ -32,7 +32,7 @@ import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509ExtendedTrustManager;
 
 /**
- * This class can be used as a SocketFactory with SSL-connections.
+ * This class can be used as a SocketFactory with SSL-connections.<p>
  * Its purpose is to ensure that all certificates - no matter from which CA - are accepted to secure the SSL-connection.
  */
 public class TrustAllSSLSocketFactory extends SSLSocketFactory  {
@@ -56,30 +56,30 @@ public class TrustAllSSLSocketFactory extends SSLSocketFactory  {
                             return EMPTY_X509Certificate;
                         }
                         @Override
-                        public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                        public void checkClientTrusted(X509Certificate[] chain, String authType) {
                             // NOOP
                         }
                         @Override
-                        public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                        public void checkServerTrusted(X509Certificate[] chain, String authType) {
                             // NOOP
                         }
                         @Override
-                        public void checkClientTrusted(X509Certificate[] arg0, String arg1, Socket arg2)
+                        public void checkClientTrusted(X509Certificate[] chain, String authType, Socket socket)
                             throws CertificateException {
                             // NOOP
                         }
                         @Override
-                        public void checkClientTrusted(X509Certificate[] arg0, String arg1, SSLEngine arg2)
+                        public void checkClientTrusted(X509Certificate[] chain, String authType, SSLEngine engine)
                             throws CertificateException {
                             // NOOP
                         }
                         @Override
-                        public void checkServerTrusted(X509Certificate[] arg0, String arg1, Socket arg2)
+                        public void checkServerTrusted(X509Certificate[] chain, String authType, Socket socket)
                             throws CertificateException {
                             // NOOP
                         }
                         @Override
-                        public void checkServerTrusted(X509Certificate[] arg0, String arg1, SSLEngine arg2)
+                        public void checkServerTrusted(X509Certificate[] chain, String authType, SSLEngine engine)
                             throws CertificateException {
                             // NOOP
                         }
@@ -104,45 +104,44 @@ public class TrustAllSSLSocketFactory extends SSLSocketFactory  {
      * {@inheritDoc}
      */
     @Override
-    public Socket createSocket( Socket socket, String s, int i, boolean
-            flag)
+    public Socket createSocket(Socket socket, String host, int port, boolean autoClose)
             throws IOException {
-        return factory.createSocket( socket, s, i, flag);
+        return factory.createSocket(socket, host, port, autoClose);
     }
 
     /**
      * {@inheritDoc}
      */
     @Override
-    public Socket createSocket( InetAddress inaddr, int i,
-            InetAddress inaddr1, int j) throws IOException {
-        return factory.createSocket( inaddr, i, inaddr1, j);
+    public Socket createSocket(InetAddress address, int port,
+            InetAddress localAddress, int localPort) throws IOException {
+        return factory.createSocket(address, port, localAddress, localPort);
     }
 
     /**
      * {@inheritDoc}
      */
     @Override
-    public Socket createSocket( InetAddress inaddr, int i) throws
+    public Socket createSocket(InetAddress address, int port) throws
             IOException {
-        return factory.createSocket( inaddr, i);
+        return factory.createSocket(address, port);
     }
 
     /**
      * {@inheritDoc}
      */
     @Override
-    public Socket createSocket( String s, int i, InetAddress inaddr, int j)
+    public Socket createSocket(String host, int port, InetAddress localHost, int localPort)
     throws IOException {
-        return factory.createSocket( s, i, inaddr, j);
+        return factory.createSocket(host, port, localHost, localPort);
     }
 
     /**
      * {@inheritDoc}
      */
     @Override
-    public Socket createSocket( String s, int i) throws IOException {
-        return factory.createSocket( s, i);
+    public Socket createSocket(String host, int port) throws IOException {
+        return factory.createSocket(host, port);
     }
 
     /**
