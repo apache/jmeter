b75c5345f111b5cf38c876e87cf1360a342e4318
shaqianqian <sqq940814@gmail.com>
2019-04-01 23:14:16 +0200

    BUG_62959 add junit test for BasicCurlParser

--- src/protocol/http/org/apache/jmeter/protocol/http/curl/BasicCurlParser.java
+++ src/protocol/http/org/apache/jmeter/protocol/http/curl/BasicCurlParser.java
@@ -15,9 +15,10 @@
  * limitations under the License.
  *
  */
-
 package org.apache.jmeter.protocol.http.curl;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -27,36 +28,56 @@ import java.util.StringTokenizer;
 import org.apache.commons.cli.avalon.CLArgsParser;
 import org.apache.commons.cli.avalon.CLOption;
 import org.apache.commons.cli.avalon.CLOptionDescriptor;
+import org.apache.jmeter.protocol.http.control.Cookie;
 
 /**
- * Basic cURL command parser that handles:
- * -X
- * -H
- * --compressed
- * --data POST with Body data
+ * Basic cURL command parser that handles: -X -H --compressed --data POST with
+ * Body data
  * 
  * @since 5.1
  */
 public class BasicCurlParser {
     private static final int METHOD_OPT = 'X';
-    private static final int COMPRESSED_OPT      = 'c';// $NON-NLS-1$
-    private static final int HEADER_OPT      = 'H';// $NON-NLS-1$
-    private static final int DATA_OPT      = 'd';// $NON-NLS-1$
+    private static final int COMPRESSED_OPT = 'c';// $NON-NLS-1$
+    private static final int HEADER_OPT = 'H';// $NON-NLS-1$
+    private static final int DATA_OPT = 'd';// $NON-NLS-1$
     private static final int USER_AGENT_OPT = 'A';// $NON-NLS-1$
     private static final int CONNECT_TIMEOUT_OPT = "connect_timeout".hashCode();// $NON-NLS-1$
     private static final int COOKIE_OPT = 'b';// $NON-NLS-1$
 
-    
     public static final class Request {
         private boolean compressed;
         private String url;
         private Map<String, String> headers = new LinkedHashMap<>();
         private String method = "GET";
         private String postData;
+        private String connectTimeout = "";
+        private List<Cookie> cookies = new ArrayList<>();
+
         /**
          */
         public Request() {
             super();
+        }
+
+        public String getConnectTimeout() {
+            return connectTimeout;
+        }
+
+        public void setConnectTimeout(String connectTimeout) {
+            this.connectTimeout = connectTimeout;
+        }
+
+        public List<Cookie> getCookies() {
+            return cookies;
+        }
+
+        public void setCookies(List<Cookie> cookies) {
+            this.cookies = cookies;
+        }
+
+        public void setHeaders(Map<String, String> headers) {
+            this.headers = headers;
         }
 
         /**
@@ -65,35 +86,64 @@ public class BasicCurlParser {
         public boolean isCompressed() {
             return compressed;
         }
+
         /**
          * @param compressed the compressed to set
          */
         public void setCompressed(boolean compressed) {
             this.compressed = compressed;
         }
-        
+
         public void addHeader(String name, String value) {
             headers.put(name, value);
         }
+
         /**
          * @return the url
          */
         public String getUrl() {
             return url;
         }
+
         /**
          * @param url the url to set
          */
         public void setUrl(String url) {
             this.url = url;
         }
+
         /**
          * @return the headers
          */
         public Map<String, String> getHeaders() {
             return headers;
         }
-        /* (non-Javadoc)
+
+        public String getMethod() {
+            return method;
+        }
+
+        /**
+         * @param method the method to set
+         */
+        public void setMethod(String method) {
+            this.method = method;
+        }
+
+        public void setPostData(String value) {
+            this.postData = value;
+        }
+
+        /**
+         * @return the postData
+         */
+        public String getPostData() {
+            return postData;
+        }
+
+        /*
+         * (non-Javadoc)
+         * 
          * @see java.lang.Object#toString()
          */
         @Override
@@ -110,61 +160,43 @@ public class BasicCurlParser {
             builder.append("]");
             return builder.toString();
         }
-        public String getMethod() {
-            return method;
-        }
-        /**
-         * @param method the method to set
-         */
-        public void setMethod(String method) {
-            this.method = method;
-        }
-        public void setPostData(String value) {
-            this.postData = value;
-        }
-        /**
-         * @return the postData
-         */
-        public String getPostData() {
-            return postData;
-        }
     }
-    private static final CLOptionDescriptor D_COMPRESSED_OPT =
-            new CLOptionDescriptor("compressed", CLOptionDescriptor.ARGUMENT_DISALLOWED, COMPRESSED_OPT,
-                    "Request compressed response (using deflate or gzip)");
-    private static final CLOptionDescriptor D_HEADER_OPT =
-            new CLOptionDescriptor("header", CLOptionDescriptor.ARGUMENT_REQUIRED | CLOptionDescriptor.DUPLICATES_ALLOWED, HEADER_OPT,
-                    "Pass custom header LINE to server");
-    private static final CLOptionDescriptor D_METHOD_OPT =
-            new CLOptionDescriptor("command", CLOptionDescriptor.ARGUMENT_REQUIRED, METHOD_OPT,
-                    "Pass custom header LINE to server");
-    private static final CLOptionDescriptor D_DATA_OPT =
-            new CLOptionDescriptor("data", CLOptionDescriptor.ARGUMENT_REQUIRED, DATA_OPT,
-                    "HTTP POST data");
 
+    private static final CLOptionDescriptor D_COMPRESSED_OPT = new CLOptionDescriptor("compressed",
+            CLOptionDescriptor.ARGUMENT_DISALLOWED, COMPRESSED_OPT,
+            "Request compressed response (using deflate or gzip)");
+    private static final CLOptionDescriptor D_HEADER_OPT = new CLOptionDescriptor("header",
+            CLOptionDescriptor.ARGUMENT_REQUIRED | CLOptionDescriptor.DUPLICATES_ALLOWED, HEADER_OPT,
+            "Pass custom header LINE to server");
+    private static final CLOptionDescriptor D_METHOD_OPT = new CLOptionDescriptor("command",
+            CLOptionDescriptor.ARGUMENT_REQUIRED, METHOD_OPT, "Pass custom header LINE to server");
+    private static final CLOptionDescriptor D_DATA_OPT = new CLOptionDescriptor("data",
+            CLOptionDescriptor.ARGUMENT_REQUIRED, DATA_OPT, "HTTP POST data");
+    private static final CLOptionDescriptor D_USER_AGENT_OPT = new CLOptionDescriptor("user-agent",
+            CLOptionDescriptor.ARGUMENT_REQUIRED, USER_AGENT_OPT, "The User-Agent string");
+    private static final CLOptionDescriptor D_CONNECT_TIMEOUT_OPT = new CLOptionDescriptor("connect-timeout",
+            CLOptionDescriptor.ARGUMENT_REQUIRED, CONNECT_TIMEOUT_OPT,
+            "Maximum time in seconds that the connection to the server");
+    private static final CLOptionDescriptor D_COOKIE_OPT = new CLOptionDescriptor("cookie",
+            CLOptionDescriptor.ARGUMENT_REQUIRED, COOKIE_OPT, "Pass the data to the HTTP server as a cookie");
+    private static final CLOptionDescriptor[] OPTIONS = new CLOptionDescriptor[] { D_COMPRESSED_OPT, D_HEADER_OPT,
+            D_METHOD_OPT, D_DATA_OPT, D_USER_AGENT_OPT, D_CONNECT_TIMEOUT_OPT, D_COOKIE_OPT };
 
-    private static final CLOptionDescriptor[] OPTIONS = new CLOptionDescriptor[] {
-            D_COMPRESSED_OPT,
-            D_HEADER_OPT,
-            D_METHOD_OPT,
-            D_DATA_OPT
-    };
-    
     public BasicCurlParser() {
         super();
     }
-    
+
     public Request parse(String commandLine) {
         String[] args = translateCommandline(commandLine);
         CLArgsParser parser = new CLArgsParser(args, OPTIONS);
         String error = parser.getErrorString();
-        if(error == null) {
+        if (error == null) {
             List<CLOption> clOptions = parser.getArguments();
             Request request = new Request();
             for (CLOption option : clOptions) {
                 if (option.getDescriptor().getId() == CLOption.TEXT_ARGUMENT) {
                     // Curl or URL
-                    if(!"CURL".equalsIgnoreCase(option.getArgument())) {
+                    if (!"CURL".equalsIgnoreCase(option.getArgument())) {
                         request.setUrl(option.getArgument());
                         continue;
                     }
@@ -174,7 +206,7 @@ public class BasicCurlParser {
                     String nameAndValue = option.getArgument(0);
                     int indexOfSemicolon = nameAndValue.indexOf(':');
                     String name = nameAndValue.substring(0, indexOfSemicolon).trim();
-                    String value = nameAndValue.substring(indexOfSemicolon+1).trim();
+                    String value = nameAndValue.substring(indexOfSemicolon + 1).trim();
                     request.addHeader(name, value);
                 } else if (option.getDescriptor().getId() == METHOD_OPT) {
                     String value = option.getArgument(0);
@@ -183,27 +215,38 @@ public class BasicCurlParser {
                     String value = option.getArgument(0);
                     request.setMethod("POST");
                     request.setPostData(value);
+                } else if (option.getDescriptor().getId() == USER_AGENT_OPT) {
+                    String name = "User-Agent";
+                    String value = option.getArgument(0);
+                    request.addHeader(name, value);
+                } else if (option.getDescriptor().getId() == CONNECT_TIMEOUT_OPT) {
+                    String value = option.getArgument(0);
+                    request.setConnectTimeout(value);
+                } else if (option.getDescriptor().getId() == COOKIE_OPT) {
+                    String value = option.getArgument(0);
+                    request.setCookies(this.stringToCookie(value, request.getUrl()));
                 }
             }
             return request;
         } else {
-            throw new IllegalArgumentException("Unexpected format for command line:"+commandLine+", error:"+error);
+            throw new IllegalArgumentException(
+                    "Unexpected format for command line:" + commandLine + ", error:" + error);
         }
     }
-    
+
     /**
      * Crack a command line.
+     * 
      * @param toProcess the command line to process.
-     * @return the command line broken into strings.
-     * An empty or null toProcess parameter results in a zero sized array.
+     * @return the command line broken into strings. An empty or null toProcess
+     *         parameter results in a zero sized array.
      */
     public static String[] translateCommandline(String toProcess) {
         if (toProcess == null || toProcess.isEmpty()) {
-            //no command? no string
+            // no command? no string
             return new String[0];
         }
         // parse with a simple finite state machine
-
         final int normal = 0;
         final int inQuote = 1;
         final int inDoubleQuote = 2;
@@ -212,7 +255,6 @@ public class BasicCurlParser {
         final ArrayList<String> result = new ArrayList<>();
         final StringBuilder current = new StringBuilder();
         boolean lastTokenHasBeenQuoted = false;
-
         while (tok.hasMoreTokens()) {
             String nextTok = tok.nextToken();
             switch (state) {
@@ -256,5 +298,29 @@ public class BasicCurlParser {
             throw new IllegalArgumentException("unbalanced quotes in " + toProcess);
         }
         return result.toArray(new String[result.size()]);
+    }
+
+    public List<Cookie> stringToCookie(String cookieStr, String url) {
+        List<Cookie> cookies = new ArrayList<>();
+        final StringTokenizer tok = new StringTokenizer(cookieStr, "; ", true);
+        while (tok.hasMoreTokens()) {
+            String nextCookie = tok.nextToken();
+            if (nextCookie.contains("=")) {
+                String[] cookieParameters = nextCookie.split("=");
+                Cookie newCookie = new Cookie();
+                newCookie.setName(cookieParameters[0]);
+                newCookie.setValue(cookieParameters[1]);
+                URL newUrl;
+                try {
+                    newUrl = new URL(url);
+                    newCookie.setDomain(newUrl.getHost());
+                    newCookie.setPath(newUrl.getPath());
+                    cookies.add(newCookie);
+                } catch (MalformedURLException e) {
+                    throw new IllegalArgumentException("unqualified url");
+                }
+            }
+        }
+        return cookies;
     }
 }
--- test/src/org/apache/jmeter/curl/BasicCurlParserTest.java
+++ test/src/org/apache/jmeter/curl/BasicCurlParserTest.java
@@ -18,6 +18,10 @@
 
 package org.apache.jmeter.curl;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.jmeter.protocol.http.control.Cookie;
 import org.apache.jmeter.protocol.http.curl.BasicCurlParser;
 import org.junit.Assert;
 import org.junit.Test;
@@ -46,6 +50,12 @@ public class BasicCurlParserTest {
         Assert.assertEquals(6, request.getHeaders().size());
         Assert.assertTrue(request.isCompressed());
         Assert.assertEquals("GET", request.getMethod());
+        String resParser = "Request [compressed=true, url=http://jmeter.apache.org/, method=GET, "
+                + "headers={User-Agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:63.0) Gecko/20100101 "
+                + "Firefox/63.0, Accept=text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8, "
+                + "Accept-Language=en-US,en;q=0.5, DNT=1, Connection=keep-alive, Upgrade-Insecure-Requests=1}]";
+        Assert.assertEquals( "The method 'toString' should get all parameters correctly",resParser, request.toString());
+        
     }   
     
     @Test
@@ -62,6 +72,16 @@ public class BasicCurlParserTest {
         Assert.assertEquals(7, request.getHeaders().size());
         Assert.assertTrue(request.isCompressed());
         Assert.assertEquals("GET", request.getMethod());
+    }
+    
+    @Test
+    public void testDoubleQuote() {
+        String cmdLine = "curl \"http://jmeter.apache.org/\"";
+        BasicCurlParser basicCurlParser = new BasicCurlParser();
+        BasicCurlParser.Request request = basicCurlParser.parse(cmdLine);
+        Assert.assertEquals(
+                "The method 'translateCommandline' should get the url correctly When using double quotes, ",
+                "http://jmeter.apache.org/", request.getUrl());
     }
     
     @Test
@@ -93,6 +113,23 @@ public class BasicCurlParserTest {
     }
     
     @Test
+    public void testNullCommand() {
+        String cmdLine = "";
+        BasicCurlParser basicCurlParser = new BasicCurlParser();
+        BasicCurlParser.Request request = basicCurlParser.parse(cmdLine);
+        Assert.assertEquals("The method 'translateCommandline' should return 'null' when command is empty, ",
+                "Request [compressed=false, url=null, method=GET, headers={}]", request.toString());
+    }
+    
+    @Test(expected=IllegalArgumentException.class)
+    public void testUnbalancedQuotes() {
+        String cmdLine = "curl \"https://jmeter.apache.org/'";
+        BasicCurlParser basicCurlParser = new BasicCurlParser();
+        basicCurlParser.parse(cmdLine);
+        Assert.fail("The method 'translateCommandline shouldn't run when the quotes are not balanced,");
+    }
+    
+    @Test
     public void testPost() {
         String cmdLine = "curl 'https://jmeter.apache.org/test' "
                 + "-H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:63.0) Gecko/20100101 Firefox/63.0' -H 'Accept: */*' "
@@ -108,6 +145,18 @@ public class BasicCurlParserTest {
         Assert.assertEquals("POST", request.getMethod());
     }
     
+    @Test
+    public void testMethodPut() {
+        String cmdLine = "curl -X 'PUT' 'https://jmeter.apache.org/test' "
+                + "-H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:63.0) Gecko/20100101 Firefox/63.0' -H 'Accept: */*' "
+                + "-H 'Accept-Language: en-US,en;q=0.5' --compressed -H 'Referer: https://www.example.com/' "
+                + "-H 'content-type: application/json;charset=UTF-8' -H 'Origin: https://www.example.com' "
+                + "-H 'DNT: 1' -H 'Connection: keep-alive' -H 'TE: Trailers'";
+        BasicCurlParser basicCurlParser = new BasicCurlParser();
+        BasicCurlParser.Request request = basicCurlParser.parse(cmdLine);
+        Assert.assertEquals("PUT", request.getMethod());
+    }
+    
     @Test(expected=IllegalArgumentException.class)
     public void testError() {
         String cmdLine = "curl 'https://jmeter.apache.org/' -u -H 'Proxy-Connection: keep-alive' "
@@ -120,5 +169,69 @@ public class BasicCurlParserTest {
                     + "-H 'Accept-Language: en-US,en;q=0.9,fr;q=0.8'";
         BasicCurlParser basicCurlParser = new BasicCurlParser();
         basicCurlParser.parse(cmdLine);
+    }
+    @Test
+    public void testUserAuth() {
+        String cmdLine = "curl 'http://jmeter.apache.org/' -H 'User-Agent: Mozilla/5.0 (Macintosh;"
+                + " Intel Mac OS X 10.11; rv:63.0) Gecko/20100101 Firefox/63.0' "
+                + "-H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' "
+                + "-H 'Accept-Language: en-US,en;q=0.5' --compressed -H 'DNT: 1' "
+                + "-H 'Connection: keep-alive' -H 'Upgrade-Insecure-Requests: 1' -A 'Mozilla/5.0'";
+        BasicCurlParser basicCurlParser = new BasicCurlParser();
+        BasicCurlParser.Request request = basicCurlParser.parse(cmdLine);
+        Assert.assertEquals("With method 'parser', the quantity of Headers should be 6' ",6, request.getHeaders().size());
+        Assert.assertEquals("With method 'parser', Headers need to add 'user-agent' with value 'Mozilla/5.0' ","Mozilla/5.0", request.getHeaders().get("User-Agent"));
+    }
+    @Test
+    public void testConnectMax() {
+        String cmdLine = "curl 'http://jmeter.apache.org/' -H 'User-Agent: Mozilla/5.0 "
+                + "(Macintosh; Intel Mac OS X 10.11; rv:63.0) Gecko/20100101 Firefox/63.0' "
+                + "-H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' "
+                + "-H 'Accept-Language: en-US,en;q=0.5' --compressed -H 'DNT: 1' "
+                + "-H 'Connection: keep-alive' -H 'Upgrade-Insecure-Requests: 1' --connect-timeout '2000'";
+        BasicCurlParser basicCurlParser = new BasicCurlParser();
+        BasicCurlParser.Request request = basicCurlParser.parse(cmdLine);
+        Assert.assertEquals("With method 'parser', "
+                + "request should contain 'connect_timeout=2000'",
+                "2000", request.getConnectTimeout());
+    }
+    @Test
+    public void testCookie() {
+        String cmdLine = "curl 'http://jmeter.apache.org/' -H 'User-Agent: Mozilla/5.0"
+                + " (Macintosh; Intel Mac OS X 10.11; rv:63.0) Gecko/20100101 Firefox/63.0' "
+                + "-H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' "
+                + "-H 'Accept-Language: en-US,en;q=0.5' --compressed -H 'DNT: 1' "
+                + "-H 'Connection: keep-alive' -H 'Upgrade-Insecure-Requests: 1' -b 'name=Tom;password=123456'";
+        BasicCurlParser basicCurlParser = new BasicCurlParser();
+        BasicCurlParser.Request request = basicCurlParser.parse(cmdLine);
+        Assert.assertEquals("http://jmeter.apache.org/", request.getUrl());
+        Assert.assertTrue(request.isCompressed());
+        Assert.assertEquals("GET", request.getMethod());
+        List<Cookie>cookies=new ArrayList<>();
+        Cookie cookie1=new Cookie();
+        cookie1.setName("name");
+        cookie1.setValue("Tom");
+        cookie1.setDomain("jmeter.apache.org");
+        cookie1.setPath("/") ;
+        cookies.add(cookie1);
+        Cookie cookie2=new Cookie();
+        cookie2.setName("password");
+        cookie2.setValue("123456");
+        cookie1.setDomain("jmeter.apache.org");
+        cookie1.setPath("/") ;
+        cookies.add(cookie2);
+        Assert.assertTrue("With method 'parser',request should contain the parameters of the cookie",request.getCookies().contains(cookie1));
+        Assert.assertEquals("With method 'parser',request should contain the parameters of the cookie",2, request.getCookies().size());
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testUrlErrorForCookie() {
+
+        String cmdLine = "curl 'jmeter.apache.org' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:63.0) Gecko/20100101 Firefox/63.0' "
+                + "-H 'Connection: keep-alive' -H 'Upgrade-Insecure-Requests: 1' -b 'name=Tom;password=123456'";
+        BasicCurlParser basicCurlParser = new BasicCurlParser();
+        basicCurlParser.parse(cmdLine);
+        Assert.fail("The method 'parser() can't transfer cookies Unqualified URLs,");
+
     }
 }
