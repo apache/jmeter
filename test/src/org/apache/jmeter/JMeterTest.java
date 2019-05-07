b1f374428ab7849fc146a192f745eb1d29e212ab
shaqianqian <sqq940814@gmail.com>
2019-05-07 09:02:17 +0200

    add junit test for JMeter.java

--- test/src/org/apache/jmeter/JMeterTest.java
+++ test/src/org/apache/jmeter/JMeterTest.java
@@ -1,6 +1,25 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package org.apache.jmeter;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import java.io.BufferedWriter;
 import java.io.File;
@@ -12,10 +31,10 @@ import java.security.Permission;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map.Entry;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
-import java.util.Map.Entry;
 import java.util.stream.Collectors;
 
 import org.apache.jmeter.junit.JMeterTestCase;
@@ -120,9 +139,9 @@ public class JMeterTest extends JMeterTestCase {
         try {
             jmeter.start(args);
         } catch (AccessControlException ex) {
-            assertEquals("The jmx file does not exist, the system should exit with system.exit(1)",
-                    ex.getMessage(), "exitVM.1");
-        } 
+            assertEquals("The jmx file does not exist, the system should exit with system.exit(1)", ex.getMessage(),
+                    "exitVM.1");
+        }
     }
 
     @Test
@@ -135,7 +154,8 @@ public class JMeterTest extends JMeterTestCase {
                 + "      <boolProp name=\"TestPlan.functional_mode\">false</boolProp>\n"
                 + "      <boolProp name=\"TestPlan.tearDown_on_shutdown\">true</boolProp>\n"
                 + "      <boolProp name=\"TestPlan.serialize_threadgroups\">false</boolProp>\n"
-                + "      <elementProp name=\"TestPlan.user_defined_variables\" elementType=\"Arguments\" guiclass=\"ArgumentsPanel\" testclass=\"Arguments\" testname=\"User Defined Variables\" enabled=\"true\">\n"
+                + "      <elementProp name=\"TestPlan.user_defined_variables\" elementType=\"Arguments\" guiclass=\"ArgumentsPanel\" "
+                + "testclass=\"Arguments\" testname=\"User Defined Variables\" enabled=\"true\">\n"
                 + "        <collectionProp name=\"Arguments.arguments\"/>\n" + "      </elementProp>\n"
                 + "      <stringProp name=\"TestPlan.user_define_classpath\"></stringProp>\n" + "    </TestPlan>\n"
                 + "    <hashTree/>\n" + "  </hashTree>\n" + "</jmeterTestPlan>\n" + "";
@@ -159,11 +179,13 @@ public class JMeterTest extends JMeterTestCase {
                 + "      <boolProp name=\"TestPlan.functional_mode\">false</boolProp>\n"
                 + "      <boolProp name=\"TestPlan.tearDown_on_shutdown\">true</boolProp>\n"
                 + "      <boolProp name=\"TestPlan.serialize_threadgroups\">false</boolProp>\n"
-                + "      <elementProp name=\"TestPlan.user_defined_variables\" elementType=\"Arguments\" guiclass=\"ArgumentsPanel\" testclass=\"Arguments\" testname=\"User Defined Variables\" enabled=\"true\">\n"
+                + "      <elementProp name=\"TestPlan.user_defined_variables\" elementType=\"Arguments\" "
+                + "guiclass=\"ArgumentsPanel\" testclass=\"Arguments\" testname=\"User Defined Variables\" enabled=\"true\">\n"
                 + "        <collectionProp name=\"Arguments.arguments\"/>\n" + "      </elementProp>\n"
                 + "      <stringProp name=\"TestPlan.user_define_classpath\"></stringProp>\n" + "    </TestPlan>\n"
                 + "    <hashTree>\n" + "      <hashTree>\n"
-                + "        <kg.apc.jmeter.samplers.DummySampler guiclass=\"kg.apc.jmeter.samplers.DummySamplerGui\" testclass=\"kg.apc.jmeter.samplers.DummySampler\" testname=\"jp@gc - Dummy Sampler\" enabled=\"true\">\n"
+                + "        <kg.apc.jmeter.samplers.DummySampler guiclass=\"kg.apc.jmeter.samplers.DummySamplerGui\" "
+                + "testclass=\"kg.apc.jmeter.samplers.DummySampler\" testname=\"jp@gc - Dummy Sampler\" enabled=\"true\">\n"
                 + "          <boolProp name=\"WAITING\">true</boolProp>\n"
                 + "          <boolProp name=\"SUCCESFULL\">true</boolProp>\n"
                 + "          <stringProp name=\"RESPONSE_CODE\">200</stringProp>\n"
@@ -185,10 +207,9 @@ public class JMeterTest extends JMeterTestCase {
         try {
             jmeter.start(args);
         } catch (AccessControlException ex) {
-            assertEquals("The plugin is used for jmx file which doesn't exist, the system should exit with system.exit(1)",
+            assertEquals(
+                    "The plugin is used for jmx file which doesn't exist, the system should exit with system.exit(1)",
                     ex.getMessage(), "exitVM.1");
         }
     }
-      
-    
 }
