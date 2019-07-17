a8d12a5d96414fdf94e35291cee5433257fb2df2
Felix Schumacher <felix.schumacher@internetallee.de>
2019-07-17 21:01:08 +0200

    Remove unused import

--- test/src/org/apache/jmeter/functions/TestStringtoFile.java
+++ test/src/org/apache/jmeter/functions/TestStringtoFile.java
@@ -29,7 +29,6 @@ import org.apache.commons.io.FileUtils;
 import org.apache.jmeter.engine.util.CompoundVariable;
 import org.apache.jmeter.junit.JMeterTestCase;
 import org.apache.jmeter.samplers.SampleResult;
-import org.apache.jmeter.services.FileServer;
 import org.apache.jmeter.threads.JMeterContext;
 import org.apache.jmeter.threads.JMeterContextService;
 import org.apache.jmeter.threads.JMeterVariables;
