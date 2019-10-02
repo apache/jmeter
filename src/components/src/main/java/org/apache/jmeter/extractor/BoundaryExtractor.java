446b21772b56b19247e94ea60bbe731a87d6532d
jmetertea <33323555+jmetertea@users.noreply.github.com>
2019-10-02 15:26:40 +0300

    Update BoundaryExtractor.java

--- src/components/src/main/java/org/apache/jmeter/extractor/BoundaryExtractor.java
+++ src/components/src/main/java/org/apache/jmeter/extractor/BoundaryExtractor.java
@@ -40,6 +40,7 @@ import org.slf4j.LoggerFactory;
 
 /**
  * Extracts Strings from a text response between a start and end boundary.
+ * @see org.apache.jmeter.extractor.TestBoundaryExtractor for unit tests
  */
 public class BoundaryExtractor extends AbstractScopedTestElement implements PostProcessor, Serializable {
 
