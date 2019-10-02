c50b48a601ed48398f2eaa4cd3a12844b7677bc8
jmetertea <33323555+jmetertea@users.noreply.github.com>
2019-10-02 11:23:12 +0300

    Update BoundaryExtractor.java

--- src/components/src/main/java/org/apache/jmeter/extractor/BoundaryExtractor.java
+++ src/components/src/main/java/org/apache/jmeter/extractor/BoundaryExtractor.java
@@ -22,7 +22,6 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
