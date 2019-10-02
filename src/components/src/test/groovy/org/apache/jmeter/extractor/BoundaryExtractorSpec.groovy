222e5a4b5f44be438204daa6a9f93974eb7a8fc6
jmetertea <33323555+jmetertea@users.noreply.github.com>
2019-10-02 11:56:32 +0300

    Update BoundaryExtractorSpec.groovy

--- src/components/src/test/groovy/org/apache/jmeter/extractor/BoundaryExtractorSpec.groovy
+++ src/components/src/test/groovy/org/apache/jmeter/extractor/BoundaryExtractorSpec.groovy
@@ -117,7 +117,7 @@ class BoundaryExtractorSpec extends Specification {
             matchNumber << [-1, 0, 1, 2, 100]
     }
 
-    def "IllegalArgumentException when one of left (#lb), right (#rb), name (#name) are null"() {
+    def "IllegalArgumentException when name (#name) is null"() {
         given:
             sut.setLeftBoundary(lb)
             sut.setRightBoundary(rb)
@@ -128,8 +128,6 @@ class BoundaryExtractorSpec extends Specification {
             thrown(IllegalArgumentException)
         where:
             lb   | rb   | name
-            null | "r"  | "name"
-            "l"  | null | "name"
             "l"  | "r"  | null
     }
 
