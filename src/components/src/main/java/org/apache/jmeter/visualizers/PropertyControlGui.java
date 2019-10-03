b2d497f36cb8eb057939138502dd2b7fe3c3e6ff
Graham Russell <graham@ham1.co.uk>
2019-10-03 12:04:46 +0100

    Anonymous type can be replaced with lambda + formatting along the way

--- src/components/src/main/java/org/apache/jmeter/visualizers/PropertyControlGui.java
+++ src/components/src/main/java/org/apache/jmeter/visualizers/PropertyControlGui.java
@@ -26,8 +26,6 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
@@ -133,18 +131,15 @@ public class PropertyControlGui extends AbstractConfigGui implements
         }
         Set<Map.Entry<Object, Object>> s = p.entrySet();
         List<Map.Entry<Object, Object>> al = new ArrayList<>(s);
-        Collections.sort(al, new Comparator<Map.Entry<Object, Object>>(){
-            @Override
-            public int compare(Map.Entry<Object, Object> o1, Map.Entry<Object, Object> o2) {
-                String m1 = (String) o1.getKey();
-                String m2 = (String) o2.getKey();
-                return m1.compareTo(m2);
-            }
+        al.sort((o1, o2) -> {
+            String m1 = (String) o1.getKey();
+            String m2 = (String) o2.getKey();
+            return m1.compareTo(m2);
         });
+
         for (Map.Entry<Object, Object> row : al) {
             tableModel.addRow(row);
         }
-
     }
 
     @Override
