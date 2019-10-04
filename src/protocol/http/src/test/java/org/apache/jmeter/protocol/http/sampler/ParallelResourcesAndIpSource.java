69a0aeecbf673cad64444619da85f90036472f38
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2019-10-04 22:55:43 +0300

    Rework ParallelResourcesAndIpSource to try all combinations of source and target hosts
    In theory, sourceIp=::1, targetHost=localhost should work, however, in practice in does not.
    
    So the test is reworked to avoid "localhost", and it uses ip addresses explicitly

--- src/protocol/http/src/test/java/org/apache/jmeter/protocol/http/sampler/ParallelResourcesAndIpSource.java
+++ src/protocol/http/src/test/java/org/apache/jmeter/protocol/http/sampler/ParallelResourcesAndIpSource.java
@@ -18,11 +18,19 @@
 
 package org.apache.jmeter.protocol.http.sampler;
 
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+import java.net.Inet4Address;
+import java.net.Inet6Address;
+import java.net.InetAddress;
 import java.net.InterfaceAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
+import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Optional;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.apache.jmeter.engine.util.CompoundVariable;
 import org.apache.jmeter.protocol.http.parser.LagartoBasedHtmlParser;
@@ -34,8 +42,11 @@ import org.apache.jmeter.threads.JMeterVariables;
 import org.apache.jmeter.util.JMeterContextExtension;
 import org.apache.jmeter.wiremock.WireMockExtension;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assumptions;
+import org.junit.jupiter.api.TestInfo;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
@@ -59,31 +70,74 @@ public class ParallelResourcesAndIpSource {
         }
     }
 
-    @ParameterizedTest
-    @MethodSource("org.apache.jmeter.protocol.http.sampler.HTTPSamplerFactory#getImplementations")
-    public void test(String httpImplementation, WireMockServer server, JMeterVariables vars) throws SocketException {
+    public static Iterable<Arguments> implementationsAndIps() throws SocketException {
+        List<Arguments> res = new ArrayList<>();
+
+        // Caveat: 127.0.0.1
+        InetAddress local4 = null;
+        InetAddress local6 = null;
+
+        List<InetAddress> localIps = Collections.list(NetworkInterface.getNetworkInterfaces())
+                .stream()
+                .filter(networkInterface -> {
+                    try {
+                        return networkInterface.isLoopback();
+                    } catch (SocketException e) {
+                        return false;
+                    }
+                })
+                .flatMap(ni -> ni.getInterfaceAddresses().stream())
+                .map(InterfaceAddress::getAddress)
+                .collect(Collectors.toList());
+
+        // We don't want to have accidental O(N^2) in case there are lots of source IPs
+        // So we use just two for the source IP
+        // So we shuffle them to use different ones every time
+        Collections.shuffle(localIps);
+        for (InetAddress localIp : localIps) {
+            if (local4 == null && localIp instanceof Inet4Address) {
+                local4 = localIp;
+            } else if (local6 == null && localIp instanceof Inet6Address) {
+                local6 = localIp;
+            }
+        }
+        // This is to allow the variables for use in lambda below (the vars should be effectively final)
+        InetAddress finalLocal4 = local4;
+        InetAddress finalLocal6 = local6;
+
+        // This is to make test names pretty
+        localIps.sort(Comparator.comparing(InetAddress::toString));
+
+        for (String impl : HTTPSamplerFactory.getImplementations()) {
+            for (String targetHost : new String[]{"127.0.0.1", "[::1]", "localhost"}) {
+                if ("localhost".equals(targetHost)) {
+                    // Neither implementation supports resolving localhost
+                    // with the given source IP
+                    continue;
+                }
+
+                localIps.forEach(localIp -> {
+                    if (finalLocal4 != null) {
+                        res.add(arguments(impl, targetHost, finalLocal4));
+                    }
+                    if (finalLocal6 != null) {
+                        res.add(arguments(impl, targetHost, finalLocal6));
+                    }
+                });
+            }
+        }
+        return res;
+    }
+
+    @ParameterizedTest(name = "{0}, targetHost={1}, sourceIp={2}")
+    @MethodSource("implementationsAndIps")
+    public void test(String httpImplementation, String targetHost, InetAddress sourceIp,
+                     WireMockServer server, JMeterVariables vars, TestInfo testInfo) {
         configureStubs(server);
 
         HTTPSamplerBase http = HTTPSamplerFactory.newInstance(httpImplementation);
-        Optional<InterfaceAddress> localIp =
-                Collections.list(NetworkInterface.getNetworkInterfaces())
-                        .stream()
-                        .filter(networkInterface -> {
-                            try {
-                                return networkInterface.isLoopback();
-                            } catch (SocketException e) {
-                                return false;
-                            }
-                        })
-                        .flatMap(ni -> ni.getInterfaceAddresses().stream())
-                        .findFirst();
 
-        if (!localIp.isPresent()) {
-            // It should not happen
-            Assertions.fail("Unable to find local IP to use as a source IP");
-        }
-
-        vars.put("IP_ADDR", localIp.get().getAddress().getHostAddress());
+        vars.put("IP_ADDR", sourceIp.getHostAddress());
 
         // Typically it is registered with jmeter.properties, however we don't run full JMeter in the test
         HTTPSamplerBase.registerParser("text/html", LagartoBasedHtmlParser.class.getName());
@@ -91,7 +145,10 @@ public class ParallelResourcesAndIpSource {
         http.setConnectTimeout("2000");
         http.setResponseTimeout("4000");
         http.setMethod(HTTPConstants.GET);
-        http.setDomain("localhost");
+        // Theoretically it should work, however it practice it fails
+        // in case HTTP Client (implementation) resolves localhost as 120.0.0.1 even in case
+        // localIp is IPv6
+        http.setDomain(targetHost);
         http.setPort(server.port());
         http.setPath("/index.html");
         http.setFollowRedirects(true);
@@ -105,6 +162,30 @@ public class ParallelResourcesAndIpSource {
         http.setRunningVersion(true);
 
         SampleResult result = http.sample();
+
+        if (sourceIp instanceof Inet4Address && targetHost.startsWith("[") ||
+                sourceIp instanceof Inet6Address && !targetHost.startsWith("[")) {
+            // Connection from IPv4 to IPv6 must fail
+            // Connection from IPv6 to IPv4 must fail
+            if (HTTPSamplerFactory.IMPL_JAVA.equals(httpImplementation)) {
+                // Java implementation is known to ignore source IP, so it should connect anyway
+                // pass to "successful" assertion below
+                if (result.getResponseDataAsString().contains("SocketException: Protocol family unavailable")) {
+                    //noinspection ConstantConditions
+                    Assumptions.assumeTrue(false,
+                            "Java implementation might throw 'SocketException: Protocol family unavailable'" +
+                                    " in case it connects from the wrong source IP");
+                }
+            } else if (result.isSuccessful() || result.isResponseCodeOK() ||
+                    !(result.getResponseDataAsString().contains("ConnectException") ||
+                            result.getResponseDataAsString().contains("SocketException"))) {
+                Assertions.fail("IPv4 <-> IPv6 connectivity must fail." +
+                        " sourceIp = " + sourceIp + ", targetHost = " + targetHost +
+                        " result = " + ResultAsString.toString(result));
+            } else {
+                return;
+            }
+        }
 
         String expected =
                 "url: http://wiremock/index.html\n" +
@@ -127,9 +208,11 @@ public class ParallelResourcesAndIpSource {
                         "  response: OK\n" +
                         "  data.size: 85\n" +
                         "  data: <html><body><img src='image1.png'><img src='image2.png'><img src='image3.png'></body>\n";
+        expected = expected.replace("http://wiremock", "http://" + targetHost + ":" + server.port());
         Assertions.assertEquals(
                 expected.replaceAll("\n", System.lineSeparator()),
-                ResultAsString.toString(result)
-                    .replaceAll(server.baseUrl(), "http://wiremock"));
+                ResultAsString.toString(result),
+                testInfo.getDisplayName() // https://github.com/junit-team/junit5/issues/2041
+        );
     }
 }
