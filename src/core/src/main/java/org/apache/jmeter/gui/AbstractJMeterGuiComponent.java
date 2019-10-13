71e996c286253141fb91986d15fee07ce68a9e1b
pmouawad <p.mouawad@ubik-ingenierie.com>
2019-10-13 15:20:54 +0200

    AbstractJMeterGuiComponent: Fix format violation

--- src/core/src/main/java/org/apache/jmeter/gui/AbstractJMeterGuiComponent.java
+++ src/core/src/main/java/org/apache/jmeter/gui/AbstractJMeterGuiComponent.java
@@ -139,7 +139,7 @@ public abstract class AbstractJMeterGuiComponent extends JPanel implements JMete
      */
     @Override
     public String getName() {
-        if (getNamePanel() != null) { // Check is mandatory because some LAFs (Nimbus) call getName() from 
+        if (getNamePanel() != null) { // Check is mandatory because some LAFs (Nimbus) call getName() from
                                       // super constructor (so can happen before namePanel field is initialized
             return getNamePanel().getName();
         }
