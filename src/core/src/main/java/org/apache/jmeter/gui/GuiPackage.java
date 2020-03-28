apache/jmeter

commit 0bdbe5bd17f6e2385a5fe58216e259dd85a04054

Overhaul the approach for using custom styles in JMeter components
This makes on-the-fly look and feel updates much more robust.
This makes zoom in/out more consistent as well.

src/core/src/main/java/org/apache/jmeter/gui/GuiPackage.java
@@ -26,9 +26,11 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.prefs.Preferences;
 
 import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JComponent;
 import javax.swing.JOptionPane;
 import javax.swing.JPopupMenu;
 import javax.swing.JToolBar;
@@ -58,6 +60,8 @@ import org.apache.jmeter.util.JMeterUtils;
 import org.apache.jmeter.util.LocaleChangeEvent;
 import org.apache.jmeter.util.LocaleChangeListener;
 import org.apache.jorphan.collections.HashTree;
+import org.apache.jorphan.gui.JFactory;
+import org.apiguardian.api.API;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -74,7 +78,9 @@ public final class GuiPackage implements LocaleChangeListener, HistoryListener {
 
     private static final Logger log = LoggerFactory.getLogger(GuiPackage.class);
 
-    private static final String SBR_PREFS_KEY = "save_before_run";
+    private static final String LAF_EPOCH = "JMeter.laf_epoch"; // $NON-NLS-1$
+
+    private static final String SBR_PREFS_KEY = "save_before_run"; // $NON-NLS-1$
 
     private static final String SAVE_BEFORE_RUN_PROPERTY = "save_automatically_before_run"; // $NON-NLS-1$
 
@@ -92,12 +98,6 @@ public final class GuiPackage implements LocaleChangeListener, HistoryListener {
     private boolean dirty = false;
 
     /**
-     * Map from TestElement to JMeterGUIComponent, mapping the nodes in the tree
-     * to their corresponding GUI components.
-     */
-    private Map<TestElement, JMeterGUIComponent> nodesToGui = new HashMap<>();
-
-    /**
      * Map from Class to JMeterGUIComponent, mapping the Class of a GUI
      * component to an instance of that component.
      */
@@ -108,6 +108,12 @@ public final class GuiPackage implements LocaleChangeListener, HistoryListener {
      * instance of TestBeanGUI to be used to edit such components.
      */
     private Map<Class<?>, JMeterGUIComponent> testBeanGUIs = new HashMap<>();
+
+    /**
+     * Tracks the number of times Look and Feel was changed.
+     * It enables partial invalidation of the cached UIs.
+     */
+    private final AtomicInteger lafEpoch = new AtomicInteger();
 
     /** The currently selected node in the tree. */
     private JMeterTreeNode currentNode = null;
@@ -248,11 +254,7 @@ public final class GuiPackage implements LocaleChangeListener, HistoryListener {
      */
     public JMeterGUIComponent getGui(TestElement node, Class<?> guiClass, Class<?> testClass) {
         try {
-            JMeterGUIComponent comp = nodesToGui.get(node);
-            if (comp == null) {
-                comp = getGuiFromCache(guiClass, testClass);
-                nodesToGui.put(node, comp);
-            }
+            JMeterGUIComponent comp = getGuiFromCache(guiClass, testClass);
             log.debug("Gui retrieved = {}", comp);
             return comp;
         } catch (Exception e) {
@@ -268,8 +270,8 @@ public final class GuiPackage implements LocaleChangeListener, HistoryListener {
      * @param node
      *            the test element being removed
      */
+    @API(since = "5.3", status = API.Status.DEPRECATED)
     public void removeNode(TestElement node) {
-        nodesToGui.remove(node);
     }
 
     /**
@@ -323,9 +325,7 @@ public final class GuiPackage implements LocaleChangeListener, HistoryListener {
         try {
             JMeterGUIComponent comp = getGuiFromCache(guiClass, testClass);
             comp.clearGui();
-            TestElement node = comp.createTestElement();
-            nodesToGui.put(node, comp);
-            return node;
+            return comp.createTestElement();
         } catch (Exception e) {
             log.error("Problem retrieving gui", e);
             return null;
@@ -354,9 +354,7 @@ public final class GuiPackage implements LocaleChangeListener, HistoryListener {
                 comp = getGuiFromCache(c, null);
             }
             comp.clearGui();
-            TestElement node = comp.createTestElement();
-            nodesToGui.put(node, comp);
-            return node;
+            return comp.createTestElement();
         } catch (NoClassDefFoundError e) {
             log.error("Problem retrieving gui for " + objClass, e);
             String msg="Cannot find class: "+e.getMessage();
@@ -409,6 +407,15 @@ public final class GuiPackage implements LocaleChangeListener, HistoryListener {
                     guis.put(guiClass, comp);
                 }
             }
+        }
+        if (comp instanceof JComponent) {
+            JComponent jc = (JComponent) comp;
+            Object epoch = jc.getClientProperty(LAF_EPOCH);
+            int currentLafEpoch = lafEpoch.get();
+            if (epoch instanceof Integer && ((Integer) epoch) < currentLafEpoch) {
+                JFactory.updateUi(jc);
+            }
+            jc.putClientProperty(LAF_EPOCH, currentLafEpoch);
         }
         return comp;
     }
@@ -662,13 +669,22 @@ public final class GuiPackage implements LocaleChangeListener, HistoryListener {
 
         // Invalidate UIs
         guis.clear();
-        nodesToGui.clear();
         testBeanGUIs.clear();
 
         // Call "start edit for the current tree node" action to show the UI
         ActionRouter.getInstance().actionPerformed(
                 new ActionEvent(this, 3334, ActionNames.EDIT)
         );
+    }
+
+    /**
+     * Tells hidden GUI components to update UIs when they are shown.
+     * <p>When Look and Feel (or zoom scaling) changes, only visible components are updated.
+     * The hidden ones are updated as they are shown.</p>
+     */
+    @API(since = "5.3", status = API.Status.EXPERIMENTAL)
+    public void updateUIForHiddenComponents() {
+        lafEpoch.getAndIncrement();
     }
 
     private String testPlanFile;
@@ -710,7 +726,6 @@ public final class GuiPackage implements LocaleChangeListener, HistoryListener {
     public void clearTestPlan() {
         testPlanListeners.stream().forEach(TestPlanListener::beforeTestPlanCleared);
         getTreeModel().clearTestPlan();
-        nodesToGui.clear();
         setTestPlanFile(null);
         testPlanListeners.stream().forEach(TestPlanListener::afterTestPlanCleared);
         undoHistory.clear();

gradle.properties
@@ -66,7 +66,7 @@ commons-math3.version=3.6.1
 commons-net.version=3.6
 commons-pool2.version=2.8.0
 commons-text.version=1.8
-darklaf.version=1.4.2.0
+darklaf.version=1.4.3.1
 dec.version=0.1.2
 dnsjava.version=2.1.9
 equalsverifier.version=3.1.12

src/components/src/main/java/org/apache/jmeter/control/gui/ModuleControllerGui.java
@@ -17,11 +17,9 @@
 
 package org.apache.jmeter.control.gui;
 
-import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Collection;
@@ -56,6 +54,7 @@ import org.apache.jmeter.testelement.TestElement;
 import org.apache.jmeter.testelement.TestPlan;
 import org.apache.jmeter.threads.AbstractThreadGroup;
 import org.apache.jmeter.util.JMeterUtils;
+import org.apache.jorphan.gui.JFactory;
 import org.apache.jorphan.gui.layout.VerticalLayout;
 
 /**
@@ -165,9 +164,7 @@ public class ModuleControllerGui extends AbstractControllerGui implements Action
 
         ImageIcon image = JMeterUtils.getImage("warning.png");
         warningLabel = new JLabel("", image, SwingConstants.LEFT); // $NON-NLS-1$
-        warningLabel.setForeground(Color.RED);
-        Font font = warningLabel.getFont();
-        warningLabel.setFont(new Font(font.getFontName(), Font.BOLD, (int)(font.getSize()*1.1)));
+        JFactory.error(warningLabel);
         warningLabel.setVisible(false);
 
         init();

src/components/src/main/java/org/apache/jmeter/visualizers/RespTimeGraphVisualizer.java
@@ -46,7 +46,6 @@ import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
-import javax.swing.UIManager;
 import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
 import javax.swing.event.ChangeListener;
@@ -64,6 +63,7 @@ import org.apache.jmeter.util.JMeterUtils;
 import org.apache.jmeter.visualizers.gui.AbstractVisualizer;
 import org.apache.jmeter.visualizers.utils.Colors;
 import org.apache.jorphan.gui.GuiUtils;
+import org.apache.jorphan.gui.JFactory;
 import org.apache.jorphan.gui.JLabeledTextField;
 import org.apache.jorphan.math.StatCalculatorLong;
 import org.slf4j.Logger;
@@ -74,10 +74,6 @@ public class RespTimeGraphVisualizer extends AbstractVisualizer implements Actio
     private static final long serialVersionUID = 281L;
 
     private static final Logger log = LoggerFactory.getLogger(RespTimeGraphVisualizer.class);
-
-    private static final Font FONT_DEFAULT = UIManager.getDefaults().getFont("TextField.font"); //$NON-NLS-1$
-
-    private static final Font FONT_SMALL = new Font("SansSerif", Font.PLAIN, (int) Math.round(FONT_DEFAULT.getSize() * 0.8)); //$NON-NLS-1$
 
     //+ JMX property names; do not change
 
@@ -756,7 +752,7 @@ public class RespTimeGraphVisualizer extends AbstractVisualizer implements Actio
         intervalPane.add(intervalField);
 
         // Button
-        intervalButton.setFont(FONT_SMALL);
+        JFactory.small(intervalButton);
         intervalButton.addActionListener(this);
         intervalPane.add(intervalButton);
 
@@ -783,14 +779,14 @@ public class RespTimeGraphVisualizer extends AbstractVisualizer implements Actio
         searchPanel.add(Box.createRigidArea(new Dimension(5,0)));
 
         // Button
-        applyFilterBtn.setFont(FONT_SMALL);
+        JFactory.small(applyFilterBtn);
         applyFilterBtn.addActionListener(this);
         searchPanel.add(applyFilterBtn);
 
         // checkboxes
-        caseChkBox.setFont(FONT_SMALL);
+        JFactory.small(caseChkBox);
         searchPanel.add(caseChkBox);
-        regexpChkBox.setFont(FONT_SMALL);
+        JFactory.small(regexpChkBox);
         searchPanel.add(regexpChkBox);
 
         return searchPanel;
@@ -798,7 +794,7 @@ public class RespTimeGraphVisualizer extends AbstractVisualizer implements Actio
 
     private JPanel createGraphTitlePane() {
         JPanel titleNamePane = new JPanel(new BorderLayout());
-        syncWithName.setFont(FONT_SMALL);
+        JFactory.small(syncWithName);
         titleNamePane.add(graphTitle, BorderLayout.CENTER);
         titleNamePane.add(syncWithName, BorderLayout.EAST);
 

src/components/src/main/java/org/apache/jmeter/visualizers/SearchTextExtension.java
@@ -18,7 +18,6 @@
 package org.apache.jmeter.visualizers;
 
 import java.awt.Color;
-import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.regex.Matcher;
@@ -35,7 +34,6 @@ import javax.swing.JEditorPane;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.JToolBar;
-import javax.swing.UIManager;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import javax.swing.text.BadLocationException;
@@ -46,16 +44,13 @@ import javax.swing.text.Highlighter;
 import org.apache.jmeter.gui.action.KeyStrokes;
 import org.apache.jmeter.util.JMeterUtils;
 import org.apache.jmeter.visualizers.utils.Colors;
+import org.apache.jorphan.gui.JFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class SearchTextExtension implements ActionListener, DocumentListener {
 
     private static final Logger log = LoggerFactory.getLogger(SearchTextExtension.class);
-
-    private static final Font FONT_DEFAULT = UIManager.getDefaults().getFont("TextField.font");
-
-    private static final Font FONT_SMALL = new Font("SansSerif", Font.PLAIN, (int) Math.round(FONT_DEFAULT.getSize() * 0.8));
 
     private static final String SEARCH_TEXT_COMMAND = "search_text"; // $NON-NLS-1$
 
@@ -133,7 +128,7 @@ public class SearchTextExtension implements ActionListener, DocumentListener {
         textToFindField = new JTextField(30); // $NON-NLS-1$
         this.toolBar = new JToolBar();
         toolBar.setFloatable(false);
-        toolBar.setFont(FONT_SMALL);
+        JFactory.small(textToFindField);
         toolBar.add(textToFindField);
 
         // add listener to intercept texttofind changes and reset search
@@ -142,7 +137,7 @@ public class SearchTextExtension implements ActionListener, DocumentListener {
         // Buttons
         findButton = new JButton(JMeterUtils
                 .getResString("search_text_button_find")); // $NON-NLS-1$
-        findButton.setFont(FONT_SMALL);
+        JFactory.small(findButton);
         findButton.setActionCommand(SEARCH_TEXT_COMMAND);
         findButton.addActionListener(this);
         toolBar.add(findButton);
@@ -150,11 +145,11 @@ public class SearchTextExtension implements ActionListener, DocumentListener {
         // checkboxes
         caseChkBox = new JCheckBox(JMeterUtils
                 .getResString("search_text_chkbox_case"), false); // $NON-NLS-1$
-        caseChkBox.setFont(FONT_SMALL);
+        JFactory.small(caseChkBox);
         toolBar.add(caseChkBox);
         regexpChkBox = new JCheckBox(JMeterUtils
                 .getResString("search_text_chkbox_regexp"), false); // $NON-NLS-1$
-        regexpChkBox.setFont(FONT_SMALL);
+        JFactory.small(regexpChkBox);
         toolBar.add(regexpChkBox);
 
         // when Enter is pressed, search start

src/components/src/main/java/org/apache/jmeter/visualizers/SearchTreePanel.java
@@ -20,7 +20,6 @@ package org.apache.jmeter.visualizers;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.FlowLayout;
-import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -33,7 +32,6 @@ import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import javax.swing.UIManager;
 import javax.swing.tree.DefaultMutableTreeNode;
 
 import org.apache.commons.lang3.StringUtils;
@@ -44,6 +42,7 @@ import org.apache.jmeter.gui.action.RegexpSearcher;
 import org.apache.jmeter.gui.action.Searcher;
 import org.apache.jmeter.util.JMeterUtils;
 import org.apache.jmeter.visualizers.utils.Colors;
+import org.apache.jorphan.gui.JFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -58,10 +57,6 @@ public class SearchTreePanel extends JPanel implements ActionListener {
     private static final Logger log = LoggerFactory.getLogger(SearchTreePanel.class);
 
     private static final String SEARCH_TEXT_COMMAND = "search_text"; // $NON-NLS-1$
-
-    private static final Font FONT_DEFAULT = UIManager.getDefaults().getFont("TextField.font");
-
-    private static final Font FONT_SMALL = new Font("SansSerif", Font.PLAIN, (int) Math.round(FONT_DEFAULT.getSize() * 0.8));
 
     private JButton searchButton;
 
@@ -108,6 +103,8 @@ public class SearchTreePanel extends JPanel implements ActionListener {
         setLayout(new BorderLayout(10,10));
 
         searchTF = new JTextField(20); //$NON-NLS-1$
+        JFactory.small(searchTF);
+
         InputMap im = searchTF
                 .getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
         im.put(KeyStrokes.ENTER, SEARCH_TEXT_COMMAND);
@@ -117,8 +114,8 @@ public class SearchTreePanel extends JPanel implements ActionListener {
         isRegexpCB = new JCheckBox(JMeterUtils.getResString("search_text_chkbox_regexp"), false); //$NON-NLS-1$
         isCaseSensitiveCB = new JCheckBox(JMeterUtils.getResString("search_text_chkbox_case"), false); //$NON-NLS-1$
 
-        isRegexpCB.setFont(FONT_SMALL);
-        isCaseSensitiveCB.setFont(FONT_SMALL);
+        JFactory.small(isRegexpCB);
+        JFactory.small(isCaseSensitiveCB);
 
         searchButton = new JButton(JMeterUtils.getResString("search")); //$NON-NLS-1$
         searchButton.addActionListener(this);
@@ -127,7 +124,9 @@ public class SearchTreePanel extends JPanel implements ActionListener {
 
         JPanel searchPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
 
-        searchPanel.add(new JLabel(JMeterUtils.getResString("search_text_field")));
+        JLabel searchLabel = new JLabel(JMeterUtils.getResString("search_text_field"));
+        JFactory.small(searchLabel);
+        searchPanel.add(searchLabel);
         searchPanel.add(searchTF);
         searchPanel.add(isCaseSensitiveCB);
         searchPanel.add(isRegexpCB);

src/components/src/main/java/org/apache/jmeter/visualizers/StatGraphVisualizer.java
@@ -60,7 +60,6 @@ import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.Timer;
-import javax.swing.UIManager;
 import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
 import javax.swing.table.TableCellRenderer;
@@ -79,6 +78,7 @@ import org.apache.jmeter.save.CSVSaveService;
 import org.apache.jmeter.util.JMeterUtils;
 import org.apache.jmeter.visualizers.gui.AbstractVisualizer;
 import org.apache.jorphan.gui.GuiUtils;
+import org.apache.jorphan.gui.JFactory;
 import org.apache.jorphan.gui.JLabeledTextField;
 import org.apache.jorphan.gui.NumberRenderer;
 import org.apache.jorphan.gui.ObjectTableModel;
@@ -136,10 +136,6 @@ public class StatGraphVisualizer extends AbstractVisualizer implements Clearable
 
     private static final String TOTAL_ROW_LABEL =
         JMeterUtils.getResString("aggregate_report_total_label");       //$NON-NLS-1$
-
-    private static final Font FONT_DEFAULT = UIManager.getDefaults().getFont("TextField.font"); //$NON-NLS-1$
-
-    private static final Font FONT_SMALL = new Font("SansSerif", Font.PLAIN, (int) Math.round(FONT_DEFAULT.getSize() * 0.8)); //$NON-NLS-1$
 
     private static final int REFRESH_PERIOD = JMeterUtils.getPropDefault("jmeter.gui.refresh_period", 500);
 
@@ -860,7 +856,7 @@ public class StatGraphVisualizer extends AbstractVisualizer implements Clearable
             colPanel.add(createColorBarButton(bar, eltList.indexOf(bar)));
         }
         colPanel.add(Box.createRigidArea(new Dimension(5,0)));
-        chooseForeColor.setFont(FONT_SMALL);
+        JFactory.small(chooseForeColor);
         colPanel.add(chooseForeColor);
         chooseForeColor.addActionListener(this);
 
@@ -890,7 +886,7 @@ public class StatGraphVisualizer extends AbstractVisualizer implements Clearable
         // Button
         JButton colorBtn = new JButton();
         colorBtn.setName(String.valueOf(index));
-        colorBtn.setFont(FONT_SMALL);
+        JFactory.small(colorBtn);
         colorBtn.addActionListener(this);
         colorBtn.setBackground(barGraph.getBackColor());
         return colorBtn;
@@ -913,14 +909,14 @@ public class StatGraphVisualizer extends AbstractVisualizer implements Clearable
         searchPanel.add(Box.createRigidArea(new Dimension(5,0)));
 
         // Button
-        applyFilterBtn.setFont(FONT_SMALL);
+        JFactory.small(applyFilterBtn);
         applyFilterBtn.addActionListener(this);
         searchPanel.add(applyFilterBtn);
 
         // checkboxes
-        caseChkBox.setFont(FONT_SMALL);
+        JFactory.small(caseChkBox);
         searchPanel.add(caseChkBox);
-        regexpChkBox.setFont(FONT_SMALL);
+        JFactory.small(regexpChkBox);
         searchPanel.add(regexpChkBox);
 
         return searchPanel;

src/components/src/main/java/org/apache/jmeter/visualizers/TreeNodeRenderer.java
@@ -17,17 +17,18 @@
 
 package org.apache.jmeter.visualizers;
 
-import java.awt.Color;
 import java.awt.Component;
 
 import javax.swing.ImageIcon;
 import javax.swing.JTree;
+import javax.swing.UIManager;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeCellRenderer;
 
 import org.apache.jmeter.JMeter;
 import org.apache.jmeter.samplers.SampleResult;
 import org.apache.jmeter.util.JMeterUtils;
+import org.apache.jorphan.gui.JMeterUIDefaults;
 
 /**
  * Tree cell renderer used by ComparisonVisualizer.
@@ -59,7 +60,7 @@ public class TreeNodeRenderer extends DefaultTreeCellRenderer {
         if(obj instanceof SampleResult)
         {
             if (!((SampleResult) obj).isSuccessful()) {
-                this.setForeground(Color.red);
+                this.setForeground(UIManager.getColor(JMeterUIDefaults.LABEL_ERROR_FOREGROUND));
                 this.setIcon(imageFailure);
             } else {
                 this.setIcon(imageSuccess);

commit 6b693295d4bf734512cad1d99ddf5cd88f3f2941

Restore GuiPackage#nodesToGui as it is required for UnsharedComponent handling
UI for UnsharedComponent are created once, so per-testelement cache is required.

regression since 0bdbe5bd17f6e2385a5fe58216e259dd85a04054

src/core/src/main/java/org/apache/jmeter/gui/GuiPackage.java
@@ -98,6 +98,13 @@ public final class GuiPackage implements LocaleChangeListener, HistoryListener {
     private boolean dirty = false;
 
     /**
+     * Map from TestElement to JMeterGUIComponent, mapping the nodes in the tree
+     * to their corresponding GUI components.
+     * <p>This enables to associate {@link UnsharedComponent} UIs with their {@link TestElement}.</p>
+     */
+    private Map<TestElement, JMeterGUIComponent> nodesToGui = new HashMap<>();
+
+    /**
      * Map from Class to JMeterGUIComponent, mapping the Class of a GUI
      * component to an instance of that component.
      */
@@ -254,7 +261,13 @@ public final class GuiPackage implements LocaleChangeListener, HistoryListener {
      */
     public JMeterGUIComponent getGui(TestElement node, Class<?> guiClass, Class<?> testClass) {
         try {
-            JMeterGUIComponent comp = getGuiFromCache(guiClass, testClass);
+            JMeterGUIComponent comp = nodesToGui.get(node);
+            if (comp == null) {
+                comp = getGuiFromCache(guiClass, testClass);
+                nodesToGui.put(node, comp);
+            } else {
+                updateUi(comp);
+            }
             log.debug("Gui retrieved = {}", comp);
             return comp;
         } catch (Exception e) {
@@ -270,8 +283,9 @@ public final class GuiPackage implements LocaleChangeListener, HistoryListener {
      * @param node
      *            the test element being removed
      */
-    @API(since = "5.3", status = API.Status.DEPRECATED)
+    @API(since = "5.3", status = API.Status.MAINTAINED)
     public void removeNode(TestElement node) {
+        nodesToGui.remove(node);
     }
 
     /**
@@ -325,7 +339,9 @@ public final class GuiPackage implements LocaleChangeListener, HistoryListener {
         try {
             JMeterGUIComponent comp = getGuiFromCache(guiClass, testClass);
             comp.clearGui();
-            return comp.createTestElement();
+            TestElement node = comp.createTestElement();
+            nodesToGui.put(node, comp);
+            return node;
         } catch (Exception e) {
             log.error("Problem retrieving gui", e);
             return null;
@@ -354,7 +370,9 @@ public final class GuiPackage implements LocaleChangeListener, HistoryListener {
                 comp = getGuiFromCache(c, null);
             }
             comp.clearGui();
-            return comp.createTestElement();
+            TestElement node = comp.createTestElement();
+            nodesToGui.put(node, comp);
+            return node;
         } catch (NoClassDefFoundError e) {
             log.error("Problem retrieving gui for " + objClass, e);
             String msg="Cannot find class: "+e.getMessage();
@@ -408,16 +426,21 @@ public final class GuiPackage implements LocaleChangeListener, HistoryListener {
                 }
             }
         }
-        if (comp instanceof JComponent) {
-            JComponent jc = (JComponent) comp;
-            Object epoch = jc.getClientProperty(LAF_EPOCH);
-            int currentLafEpoch = lafEpoch.get();
-            if (epoch instanceof Integer && ((Integer) epoch) < currentLafEpoch) {
-                JFactory.updateUi(jc);
-            }
-            jc.putClientProperty(LAF_EPOCH, currentLafEpoch);
-        }
+        updateUi(comp);
         return comp;
+    }
+
+    private void updateUi(JMeterGUIComponent comp) {
+        if (!(comp instanceof JComponent)) {
+            return;
+        }
+        JComponent jc = (JComponent) comp;
+        Object epoch = jc.getClientProperty(LAF_EPOCH);
+        int currentLafEpoch = lafEpoch.get();
+        if (epoch instanceof Integer && ((Integer) epoch) < currentLafEpoch) {
+            JFactory.updateUi(jc);
+        }
+        jc.putClientProperty(LAF_EPOCH, currentLafEpoch);
     }
 
     /**
@@ -669,6 +692,7 @@ public final class GuiPackage implements LocaleChangeListener, HistoryListener {
 
         // Invalidate UIs
         guis.clear();
+        nodesToGui.clear();
         testBeanGUIs.clear();
 
         // Call "start edit for the current tree node" action to show the UI
@@ -726,6 +750,7 @@ public final class GuiPackage implements LocaleChangeListener, HistoryListener {
     public void clearTestPlan() {
         testPlanListeners.stream().forEach(TestPlanListener::beforeTestPlanCleared);
         getTreeModel().clearTestPlan();
+        nodesToGui.clear();
         setTestPlanFile(null);
         testPlanListeners.stream().forEach(TestPlanListener::afterTestPlanCleared);
         undoHistory.clear();