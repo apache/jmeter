2c62c994dd3158e7955ac5a38366889e236ca700
pmouawad <p.mouawad@ubik-ingenierie.com>
2020-03-29 15:57:07 +0200

    Fix Violation: Remove unused imports

--- src/core/src/main/java/org/apache/jmeter/control/gui/IfControllerPanel.java
+++ src/core/src/main/java/org/apache/jmeter/control/gui/IfControllerPanel.java
@@ -21,7 +21,6 @@ import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
