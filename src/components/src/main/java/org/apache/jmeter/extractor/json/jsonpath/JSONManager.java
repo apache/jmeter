ea177801bdf5b050b1d144afb58977ebc6c123cc
Felix Schumacher <felix.schumacher@internetallee.de>
2020-08-09 14:12:03 +0200

    Use Map#computeIfAbsent to simplify code

--- src/components/src/main/java/org/apache/jmeter/extractor/json/jsonpath/JSONManager.java
+++ src/components/src/main/java/org/apache/jmeter/extractor/json/jsonpath/JSONManager.java
@@ -53,13 +53,7 @@ public class JSONManager {
     private final Map<String, JsonPath> expressionToJsonPath = new HashMap<>(2);
 
     private JsonPath getJsonPath(String jsonPathExpression) {
-        JsonPath jsonPath = expressionToJsonPath.get(jsonPathExpression);
-        if (jsonPath == null) {
-            jsonPath = JsonPath.compile(jsonPathExpression);
-            expressionToJsonPath.put(jsonPathExpression, jsonPath);
-        }
-
-        return jsonPath;
+        return expressionToJsonPath.computeIfAbsent(jsonPathExpression, JsonPath::compile);
     }
 
     public void reset() {
