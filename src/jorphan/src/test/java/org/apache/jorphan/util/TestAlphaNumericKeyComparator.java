47daf54b7ee33b169e9c276f0e3e185f4f8557e1
Felix Schumacher <felix.schumacher@internetallee.de>
2021-01-26 19:41:03 +0100

    spacepolice

--- src/jorphan/src/test/java/org/apache/jorphan/util/TestAlphaNumericKeyComparator.java
+++ src/jorphan/src/test/java/org/apache/jorphan/util/TestAlphaNumericKeyComparator.java
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.apache.jorphan.util;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
