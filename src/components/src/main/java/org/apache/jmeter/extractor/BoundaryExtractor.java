c4d31453c29704ff42813ba99b281cb03a51bb5e
Felix Schumacher <felix.schumacher@internetallee.de>
2021-06-03 18:21:22 +0200

    Only skip saving the list, if the mode is not ALL
    Follow-up to c054d3a4d329c02e305fdfb0818f682834130d8f
    
    The tests checked for the existance of <VAR_NAME>_matchNr, even
    when no matches were found.
    
    Bugzilla Id: 65352

--- src/components/src/main/java/org/apache/jmeter/extractor/BoundaryExtractor.java
+++ src/components/src/main/java/org/apache/jmeter/extractor/BoundaryExtractor.java
@@ -163,7 +163,7 @@ public class BoundaryExtractor extends AbstractScopedTestElement implements Post
      * @return 0 if there is only one match, else the number of matches, this is used to remove
      */
     private int saveMatches(JMeterVariables vars, String refName, int matchNumber, List<String> matches) {
-        if (matches.isEmpty()) {
+        if (matchNumber >=0 && matches.isEmpty()) {
             return 0;
         }
         int matchCount = 0;
