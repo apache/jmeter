d9b29c44aa7af64f862d5fe981e787e001dcbc2a
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2021-11-13 18:20:38 +0300

    Improve output of JOrphanUtils#displayThreads

--- src/jorphan/src/main/java/org/apache/jorphan/util/JOrphanUtils.java
+++ src/jorphan/src/main/java/org/apache/jorphan/util/JOrphanUtils.java
@@ -493,20 +493,21 @@ public final class JOrphanUtils {
     public static void displayThreads(boolean includeDaemons) {
         Map<Thread, StackTraceElement[]> m = Thread.getAllStackTraces();
         String lineSeparator = System.getProperty("line.separator");
+        StringBuilder builder = new StringBuilder();
         for (Map.Entry<Thread, StackTraceElement[]> e : m.entrySet()) {
             boolean daemon = e.getKey().isDaemon();
             if (includeDaemons || !daemon) {
-                StringBuilder builder = new StringBuilder();
+                builder.setLength(0);
+                builder.append(e.getKey());
+                if (daemon) {
+                    builder.append(" (daemon)");
+                }
+                builder.append(lineSeparator);
                 StackTraceElement[] ste = e.getValue();
                 for (StackTraceElement stackTraceElement : ste) {
-                    int lineNumber = stackTraceElement.getLineNumber();
-                    builder.append(stackTraceElement.getClassName())
-                            .append("#")
-                            .append(stackTraceElement.getMethodName())
-                            .append(lineNumber >= 0 ? " at line:" + stackTraceElement.getLineNumber() : "")
-                            .append(lineSeparator);
+                    builder.append("  at ").append(stackTraceElement).append(lineSeparator);
                 }
-                System.out.println(e.getKey().toString() + (daemon ? " (daemon)" : "") + ", stackTrace:" + builder.toString());
+                System.out.println(builder);
             }
         }
     }
