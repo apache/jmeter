980e5b8975948bb0dab45b735d88901c185b00a4
Felix Schumacher <felix.schumacher@internetallee.de>
2022-01-19 19:53:12 +0100

    Apply autostyle to imports changed with last commit
    Bugzilla Id: 65794

--- src/components/src/test/java/org/apache/jmeter/assertions/TestJSONPathAssertion.java
+++ src/components/src/test/java/org/apache/jmeter/assertions/TestJSONPathAssertion.java
@@ -22,7 +22,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.nio.charset.Charset;
 import java.util.Locale;
 
 import org.apache.jmeter.samplers.SampleResult;
