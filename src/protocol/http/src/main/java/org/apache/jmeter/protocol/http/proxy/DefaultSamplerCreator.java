ae8c1ec2ba18c74f7900a474989877d26d369915
Felix Schumacher <felix.schumacher@internetallee.de>
2023-04-29 13:08:15 +0200

    Add more variables to template to compute sampler names
    The new variables are
     * method - HTTP method of the request
     * host   - host of the URL recorded (called domain in the sampler)
     * scheme - scheme of the URL recorded (called protocol in the sampler)
     * port   - port of the URL recorded
     * url    - URL as recorded
    
    Issue #5820
    
    Co-authored-by: Alex Schwartz <alexschwartz01@gmail.com>
    
    Guard message formats that might have been given by user
    
    As Vladimir pointed out, we might have users, that construct
    a format string with a part, that might be interpreted by
    MessageFormat. So let us guard those.
    
    asdf

--- src/protocol/http/src/main/java/org/apache/jmeter/protocol/http/proxy/DefaultSamplerCreator.java
+++ src/protocol/http/src/main/java/org/apache/jmeter/protocol/http/proxy/DefaultSamplerCreator.java
@@ -23,6 +23,8 @@ import java.io.StringReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.text.MessageFormat;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 import javax.xml.XMLConstants;
@@ -361,18 +363,44 @@ public class DefaultSamplerCreator extends AbstractSamplerCreator {
         String prefix = StringUtils.defaultString(request.getPrefix(), "");
         int httpSampleNameMode = request.getHttpSampleNameMode();
         String format = getFormat(httpSampleNameMode, request.getHttpSampleNameFormat());
-        if (!HTTPConstants.CONNECT.equals(request.getMethod()) && isNumberRequests()) {
-            sampler.setName(MessageFormat.format(format, prefix, sampler.getPath(), incrementRequestNumberAndGet()));
-        } else {
-            sampler.setName(MessageFormat.format(format, prefix, sampler.getPath()));
+        String url;
+        try {
+            url = sampler.getUrl().toString();
+        } catch (MalformedURLException e) {
+            // If path could not be converted to URL, retain path component
+            url = sampler.getPath();
+            log.warn("Could not get URL to name sample", e);
         }
+        List<Object> values = Arrays.asList(
+                prefix,
+                sampler.getPath(),
+                sampler.getMethod(),
+                sampler.getDomain(),
+                sampler.getProtocol(),
+                sampler.getPort(),
+                url
+        );
+        Object[] valuesArray;
+        if (!HTTPConstants.CONNECT.equals(request.getMethod()) && isNumberRequests()) {
+            valuesArray = values.toArray(new Object[values.size() + 1]);
+            valuesArray[values.size()] = incrementRequestNumberAndGet();
+        } else {
+            valuesArray = values.toArray();
+        }
+        sampler.setName(MessageFormat.format(format,valuesArray));
     }
 
     private static String getFormat(int httpSampleNameMode, String format) {
         if (httpSampleNameMode == SAMPLER_NAME_NAMING_MODE_FORMATTER) {
-            return format.replaceAll("#\\{name([,}])", "{0$1")
+            return format.replaceAll("\\{(\\d+(,[^}]*)?)\\}", "'{'$1'}'")
+                    .replaceAll("#\\{name([,}])", "{0$1")
                     .replaceAll("#\\{path([,}])", "{1$1")
-                    .replaceAll("#\\{counter([,}])", "{2$1");
+                    .replaceAll("#\\{method([,}])", "{2$1")
+                    .replaceAll("#\\{host([,}])", "{3$1")
+                    .replaceAll("#\\{scheme([,}])", "{4$1")
+                    .replaceAll("#\\{port([,}])", "{5$1")
+                    .replaceAll("#\\{url([,}])", "{6$1")
+                    .replaceAll("#\\{counter([,}])", "{7$1");
         }
         if (isNumberRequests()) {
             return getNumberedFormat(httpSampleNameMode);
@@ -391,13 +419,13 @@ public class DefaultSamplerCreator extends AbstractSamplerCreator {
 
     private static String getNumberedFormat(int httpSampleNameMode) {
         if (httpSampleNameMode == SAMPLER_NAME_NAMING_MODE_PREFIX) {
-            return "{0}{1}-{2}";
+            return "{0}{1}-{7}";
         }
         if (httpSampleNameMode == SAMPLER_NAME_NAMING_MODE_COMPLETE) {
-            return "{0}-{2}";
+            return "{0}-{7}";
         }
         if (httpSampleNameMode == SAMPLER_NAME_NAMING_MODE_SUFFIX) {
-            return "{0}-{2} {1}";
+            return "{0}-{7} {1}";
         }
         return "{1}";
     }
