cf5b41f1e17fad19600826d2cff36bc4feaf4cf9
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2023-06-29 10:56:05 +0300

    doc: reduce severity from error to info when logging "JavaFX is missing"

--- src/jorphan/src/main/java/org/apache/jorphan/reflect/LogAndIgnoreServiceLoadExceptionHandler.java
+++ src/jorphan/src/main/java/org/apache/jorphan/reflect/LogAndIgnoreServiceLoadExceptionHandler.java
@@ -33,11 +33,19 @@ public class LogAndIgnoreServiceLoadExceptionHandler implements ServiceLoadExcep
     public void handle(Class<?> service, String className, Throwable throwable) {
         if (throwable instanceof NoClassDefFoundError) {
             if (throwable.getMessage().contains("javafx")) {
-                log.error(
-                        "Exception registering implementation: [{}] of interface: [{}], a dependency used by the plugin class is missing. " +
-                                "Add JavaFX to your Java installation if you want to use renderer: {}",
-                        className, service, className, throwable
-                );
+                if (log.isDebugEnabled()) {
+                    log.debug(
+                            "Unable to load class {} for interface {}. The class depends on JavaFX which is missing in the current JVM. " +
+                                    "Use JVM distribution with JavaFX, or add it as a library for the class to work",
+                            className, service, throwable
+                    );
+                } else {
+                    log.info(
+                            "Class {} depends on JavaFX which is missing in the current JVM. " +
+                                    "Use JVM distribution with JavaFX, or add it as a library for the class to work",
+                            className
+                    );
+                }
             } else {
                 log.error(
                         "Exception registering implementation: [{}] of interface: [{}], a dependency used by the plugin class is missing",
