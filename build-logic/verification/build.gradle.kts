d21c15bfc1b962c0de047fce754cc8645ba0c11a
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2024-01-09 10:14:54 +0300

    chore: bump com.github.autostyle to 4.0 to support Java 17
    "removeUnusedImports" in google-java-format is not supported with Java 17+ (it requires add-opens),
    so move the fix to OpenRewrite

--- build-logic/verification/build.gradle.kts
+++ build-logic/verification/build.gradle.kts
@@ -22,7 +22,7 @@ plugins {
 dependencies {
     api(projects.basics)
     api(projects.buildParameters)
-    api("com.github.autostyle:com.github.autostyle.gradle.plugin:3.2")
+    api("com.github.autostyle:com.github.autostyle.gradle.plugin:4.0")
     api("com.github.spotbugs:com.github.spotbugs.gradle.plugin:6.0.4")
     api("com.github.vlsi.ide:com.github.vlsi.ide.gradle.plugin:1.90")
     api("com.github.vlsi.gradle-extensions:com.github.vlsi.gradle-extensions.gradle.plugin:1.90")
