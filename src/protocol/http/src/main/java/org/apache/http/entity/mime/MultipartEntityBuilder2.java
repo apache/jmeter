3ade0a348f3405c52ebe16acca98faff2f7abd0b
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2025-11-03 23:18:44 +0300

    style: fix style for MultipartEntityBuilder2

--- src/protocol/http/src/main/java/org/apache/http/entity/mime/MultipartEntityBuilder2.java
+++ src/protocol/http/src/main/java/org/apache/http/entity/mime/MultipartEntityBuilder2.java
@@ -1,28 +1,18 @@
 /*
- * ====================================================================
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- * ====================================================================
- *
- * This software consists of voluntary contributions made by many
- * individuals on behalf of the Apache Software Foundation.  For more
- * information on the Apache Software Foundation, please see
- * <http://www.apache.org/>.
- *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.apache.http.entity.mime;
@@ -51,7 +41,8 @@ import org.apiguardian.api.API;
  * Builder for multipart {@link HttpEntity}s.
  *
  * Note: the code was taken from
- * <a href="https://github.com/apache/httpcomponents-client/blob/54900db4653d7f207477e6ee40135b88e9bcf832/httpmime/src/main/java/org/apache/http/entity/mime/MultipartEntityBuilder.java">MultipartEntityBuilder 4.5.14</a>
+ * <a href="https://github.com/apache/httpcomponents-client/blob/54900db4653d7f207477e6ee40135b88e9bcf832/httpmime/src/
+ *   main/java/org/apache/http/entity/mime/MultipartEntityBuilder.java">MultipartEntityBuilder 4.5.14</a>
  *
  */
 @API(status = API.Status.INTERNAL, since = "5.6.4")
