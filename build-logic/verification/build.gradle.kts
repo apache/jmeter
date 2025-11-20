1fab84442f8cbebc0d4d92300cfa94085e15aa68
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2025-11-20 17:02:40 +0300

    chore: bump org.sonarqube Gradle plugin to 7.1.0.6387

--- build-logic/verification/build.gradle.kts
+++ build-logic/verification/build.gradle.kts
@@ -28,5 +28,5 @@ dependencies {
     api("de.thetaphi.forbiddenapis:de.thetaphi.forbiddenapis.gradle.plugin:3.10")
     api("net.ltgt.errorprone:net.ltgt.errorprone.gradle.plugin:4.3.0")
     api("org.jetbrains.gradle.plugin.idea-ext:org.jetbrains.gradle.plugin.idea-ext.gradle.plugin:1.3")
-    api("org.sonarqube:org.sonarqube.gradle.plugin:4.4.1.3373")
+    api("org.sonarqube:org.sonarqube.gradle.plugin:7.1.0.6387")
 }
