f44edd7a0fe2110ec9c3f196c95d935d193ebe8d
Rajat Jain <rajatjain.ix@gmail.com>
2026-07-21 16:32:09 +0530

    Added API Annotation.

--- src/core/src/main/java/org/apache/jmeter/threads/TestCompiler.java
+++ src/core/src/main/java/org/apache/jmeter/threads/TestCompiler.java
@@ -25,6 +25,7 @@ import java.util.List;
 import java.util.ListIterator;
 import java.util.Set;
 
+import org.apiguardian.api.API;
 import org.apache.jmeter.assertions.Assertion;
 import org.apache.jmeter.config.ConfigTestElement;
 import org.apache.jmeter.control.Controller;
@@ -122,6 +123,7 @@ public class TestCompiler implements HashTreeTraverser {
      * @return parent controllers from nearest to root, or an empty list
      *         if the sampler has not been compiled
      */
+    @API(status = API.Status.EXPERIMENTAL, since = "6.0.0")
     public List<Controller> getControllersForSampler(Sampler sampler) {
         SamplePackage pack = samplerConfigMap.get(sampler);
         return pack != null ? pack.getControllers() : List.of();
